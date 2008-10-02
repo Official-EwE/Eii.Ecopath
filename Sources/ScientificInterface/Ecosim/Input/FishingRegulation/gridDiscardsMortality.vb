7ae381b796ac59b904d9bb8ea9a8251c8b3f9f46
JeroenS <jeroen.steenbeek@gmail.com>
2008-10-02 18:48:49 +0000

    Initial version
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@159 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/ScientificInterface/Ecosim/Input/FishingRegulation/gridDiscardsMortality.vb
@@ -0,0 +1,89 @@
+﻿'==============================================================================
+'
+' $Log: gridDiscardsMortality.vb,v $
+' Revision 1.1  2008/10/02 18:48:49  jeroens
+' Initial version
+'
+'==============================================================================
+
+#Region " Imports directive "
+
+Option Strict On
+Imports EwECore
+Imports EwEUtils.Core
+Imports SourceGrid2.Cells
+
+#End Region ' Imports directive
+
+Namespace Ecosim
+
+    ''' ===========================================================================
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' ===========================================================================
+    <CLSCompliant(False)> _
+    Public Class gridDiscardsMortality
+        Inherits EwEGrid
+
+        Public Sub New()
+            MyBase.new()
+        End Sub
+
+        Protected Overrides Sub InitStyle()
+            MyBase.InitStyle()
+
+            Dim core As cCore = cCore.GetInstance()
+            Dim src As cCoreInputOutputBase = Nothing
+
+            Me.Redim(1, 2 + core.nFleets)
+
+            Me(0, 0) = New EwEColumnHeaderCell("")
+            Me(0, 1) = New EwEColumnHeaderCell(My.Resources.HEADER_GROUPNAME)
+
+            For iFleet As Integer = 1 To core.nFleets
+                src = core.FleetInputs(iFleet)
+                Me(0, 1 + iFleet) = New PropertyColumnHeaderCell(src, eVarNameFlags.Name)
+            Next
+
+            Me.FixedColumns = 2
+            Me.FixedColumnWidths = True
+        End Sub
+
+        Protected Overrides Sub FillData()
+
+            Dim core As cCore = cCore.GetInstance()
+            Dim fleet As cCoreInputOutputBase = Nothing
+            Dim group As cCoreInputOutputBase = Nothing
+            Dim cell As ICell = Nothing
+
+            ' For each group
+            For iRow As Integer = 1 To core.nGroups
+
+                Me.AddRow()
+
+                'Get the group info
+                group = core.EcoPathGroupInputs(iRow)
+
+                ' Fleet name As row header
+                Me(iRow, 0) = New EwERowHeaderCell(iRow)
+                Me(iRow, 1) = New PropertyRowHeaderCell(group, eVarNameFlags.Name)
+
+                ' Fleet cells
+                For iFleet As Integer = 1 To core.nFleets
+                    'assigned it to destined cell
+                    Me(iRow, 1 + iFleet) = New SourceGrid2.Cells.Real.Cell(0.3!, GetType(Single))
+                Next
+            Next
+
+        End Sub
+
+        Public Overrides ReadOnly Property MessageSource() As EwECore.eMessageSource
+            Get
+                Return eMessageSource.EcoSim
+            End Get
+        End Property
+
+    End Class
+
+End Namespace ' Ecosim
---
+++ Sources/ScientificInterface/Ecosim/Input/FishingRegulation/gridQuotaOptions.vb
@@ -0,0 +1,105 @@
+﻿'==============================================================================
+'
+' $Log: gridQuotaOptions.vb,v $
+' Revision 1.1  2008/10/02 18:48:49  jeroens
+' Initial version
+'
+'==============================================================================
+
+#Region " Imports directive "
+
+Option Strict On
+Imports EwECore
+Imports EwEUtils.Core
+Imports SourceGrid2.Cells
+
+#End Region ' Imports directive
+
+Namespace Ecosim
+
+    ''' ===========================================================================
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' ===========================================================================
+    <CLSCompliant(False)> _
+    Public Class gridQuotaOptions
+        Inherits EwEGrid
+
+        Private Enum eColumnTypes As Integer
+            Index = 0
+            Name
+            MaxEffort
+            OptionNone
+            OptionWeakestStock
+            OptionStrongsetStockPlusDiscards
+            OptionAsumeSelectiveFishing
+        End Enum
+
+        Public Sub New()
+            MyBase.new()
+        End Sub
+
+        Protected Overrides Sub InitStyle()
+            MyBase.InitStyle()
+
+            Dim core As cCore = cCore.GetInstance()
+            Dim iNumCols As Integer = [Enum].GetValues(GetType(eColumnTypes)).Length
+            Dim src As cCoreInputOutputBase = Nothing
+
+            Me.Redim(1, iNumCols)
+
+            Me(0, eColumnTypes.Index) = New EwEColumnHeaderCell("")
+            Me(0, eColumnTypes.Name) = New EwEColumnHeaderCell(My.Resources.HEADER_FLEETNAME)
+            Me(0, eColumnTypes.MaxEffort) = New EwEColumnHeaderCell("Max Effort")
+            Me(0, eColumnTypes.OptionNone) = New EwEColumnHeaderCell("None")
+            Me(0, eColumnTypes.OptionWeakestStock) = New EwEColumnHeaderCell("Weakest Stock")
+            Me(0, eColumnTypes.OptionStrongsetStockPlusDiscards) = New EwEColumnHeaderCell("Strongest Stock + Discards")
+            Me(0, eColumnTypes.OptionAsumeSelectiveFishing) = New EwEColumnHeaderCell("Assume Selective Fishing")
+
+            Me.FixedColumns = 2
+            Me.FixedColumnWidths = True
+        End Sub
+
+        Protected Overrides Sub FillData()
+
+            Dim core As cCore = cCore.GetInstance()
+            Dim fleet As cCoreInputOutputBase = Nothing
+            Dim group As cCoreInputOutputBase = Nothing
+            Dim cell As ICell = Nothing
+
+            ' For each fleet
+            For rowIndex As Integer = 1 To core.nFleets
+
+                Me.AddRow()
+
+                'Get the fleet info
+                fleet = core.FleetInputs(rowIndex)
+
+                ' Fleet name As row header
+                Me(rowIndex, eColumnTypes.Index) = New EwERowHeaderCell(rowIndex)
+                Me(rowIndex, eColumnTypes.Name) = New PropertyRowHeaderCell(fleet, eVarNameFlags.Name)
+
+                ' Options cells
+                Me(rowIndex, eColumnTypes.OptionNone) = New SourceGrid2.Cells.Real.CheckBox(True)
+                Me(rowIndex, eColumnTypes.OptionWeakestStock) = New SourceGrid2.Cells.Real.CheckBox(True)
+                Me(rowIndex, eColumnTypes.OptionStrongsetStockPlusDiscards) = New SourceGrid2.Cells.Real.CheckBox(True)
+                Me(rowIndex, eColumnTypes.OptionAsumeSelectiveFishing) = New SourceGrid2.Cells.Real.CheckBox(True)
+
+            Next
+
+        End Sub
+
+        Protected Overrides Sub FinishStyle()
+            MyBase.FinishStyle()
+        End Sub
+
+        Public Overrides ReadOnly Property MessageSource() As EwECore.eMessageSource
+            Get
+                Return eMessageSource.EcoSim
+            End Get
+        End Property
+
+    End Class
+
+End Namespace ' Ecosim
