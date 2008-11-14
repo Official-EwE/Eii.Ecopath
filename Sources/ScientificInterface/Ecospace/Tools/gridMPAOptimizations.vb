4bfa2814b90fb20bb73fe4520e61bc70dbd63d5d
JeroenS <jeroen.steenbeek@gmail.com>
2008-11-14 00:29:11 +0000

    Added more indicators
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@455 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterface/Ecospace/Tools/gridMPAOptimizations.vb
+++ Sources/ScientificInterface/Ecospace/Tools/gridMPAOptimizations.vb
@@ -1,49 +1,14 @@
 '==============================================================================
 '
 ' $Log: gridMPAOptimizations.vb,v $
+' Revision 1.3  2008/11/14 00:29:11  jeroens
+' Added more indicators
+'
 ' Revision 1.2  2008/11/12 21:39:05  jeroens
 ' Revamping
 '
 ' Revision 1.1  2008/09/26 07:32:03  sherman
 ' --== DELETED HISTORY ==--
-'
-' Revision 1.10  2008/08/26 18:51:42  jeroens
-' Removed Mean column
-'
-' Revision 1.9  2008/08/18 15:50:07  jeroens
-' Mean-aware
-'
-' Revision 1.8  2008/08/15 21:05:01  jeroens
-' Grid interface more pronounced
-' Added more states to better reflect what is happening
-'
-' Revision 1.7  2008/08/15 12:43:04  jeroens
-' Ecoseed progress logged correctly
-'
-' Revision 1.6  2008/08/14 21:30:08  jeroens
-' Enabled setting values
-'
-' Revision 1.5  2008/08/14 17:51:09  jeroens
-' Undock! UNDOCK, thou fiend!
-'
-' Revision 1.4  2008/08/14 15:39:41  jeroens
-' Working
-'
-' Revision 1.3  2008/08/02 03:04:19  jeroens
-' Renamed resources
-'
-' Revision 1.2  2008/07/29 13:06:46  jeroens
-' Propery renamed 'IsStatic' method
-'
-' Revision 1.1  2008/06/04 15:32:43  jeroens
-' Renamed
-' Implemented series of changes in response to Email VC 03Jun08
-'
-' Revision 1.2  2008/06/02 00:01:32  jeroens
-' Added ScientificInterfaceShared
-'
-' Revision 1.1  2008/04/07 03:38:16  jeroens
-' Gettting there, slowly...
 '
 '==============================================================================
 
@@ -75,12 +40,12 @@ Public Class gridMPAOptimizations
 
         Me.FixedColumnWidths = False
 
-        Me.Redim(8, [Enum].GetValues(GetType(eColumnTypes)).Length)
+        Me.Redim(9, [Enum].GetValues(GetType(eColumnTypes)).Length)
 
-        Me(0, eColumnTypes.Variable) = New EwEColumnHeaderCell("")
+        Me(0, eColumnTypes.Variable) = New EwEColumnHeaderCell(My.Resources.HEADER_INDICATOR)
         Me(0, eColumnTypes.Value) = New EwEColumnHeaderCell(My.Resources.HEADER_VALUE)
 
-        Me(1, eColumnTypes.Variable) = New EwERowHeaderCell(My.Resources.HEADER_NETECONOMICVALUE)
+        Me(1, eColumnTypes.Variable) = New EwERowHeaderCell(My.Resources.SEARCH_LABEL_NETECONOMICVALUE)
         c = New EwECell(0.0!, GetType(Single))
         c.Style = StyleGuide.eStyleFlags.OK Or StyleGuide.eStyleFlags.NotEditable
         Me(1, eColumnTypes.Value) = c
@@ -100,26 +65,26 @@ Public Class gridMPAOptimizations
         c.Style = StyleGuide.eStyleFlags.OK Or StyleGuide.eStyleFlags.NotEditable
         Me(4, eColumnTypes.Value) = c
 
-        Me(4, eColumnTypes.Variable) = New EwERowHeaderCell(My.Resources.SEARCH_LABEL_ECOSYSTEM_STRUCTURE)
-        c = New EwECell(0.0!, GetType(Single))
-        c.Style = StyleGuide.eStyleFlags.OK Or StyleGuide.eStyleFlags.NotEditable
-        Me(4, eColumnTypes.Value) = c
-
         Me(5, eColumnTypes.Variable) = New EwERowHeaderCell(My.Resources.SEARCH_LABEL_BIOMASSDIVERSITY)
         c = New EwECell(0.0!, GetType(Single))
         c.Style = StyleGuide.eStyleFlags.OK Or StyleGuide.eStyleFlags.NotEditable
         Me(5, eColumnTypes.Value) = c
 
-        Me(6, eColumnTypes.Variable) = New EwERowHeaderCell(My.Resources.HEADER_TOTAL)
+        Me(6, eColumnTypes.Variable) = New EwERowHeaderCell(My.Resources.SEARCH_LABEL_BOUNDARYWEIGHT)
         c = New EwECell(0.0!, GetType(Single))
         c.Style = StyleGuide.eStyleFlags.OK Or StyleGuide.eStyleFlags.NotEditable
         Me(6, eColumnTypes.Value) = c
 
-        ' ToDo: globalize this
-        Me(7, eColumnTypes.Variable) = New EwERowHeaderCell("Area closed (%)")
+        Me(7, eColumnTypes.Variable) = New EwERowHeaderCell(My.Resources.HEADER_TOTAL)
         c = New EwECell(0.0!, GetType(Single))
         c.Style = StyleGuide.eStyleFlags.OK Or StyleGuide.eStyleFlags.NotEditable
         Me(7, eColumnTypes.Value) = c
+
+        ' ToDo: globalize this
+        Me(8, eColumnTypes.Variable) = New EwERowHeaderCell(My.Resources.SEARCH_LABEL_AREA_CLOSED)
+        c = New EwECell(0.0!, GetType(Single))
+        c.Style = StyleGuide.eStyleFlags.OK Or StyleGuide.eStyleFlags.NotEditable
+        Me(8, eColumnTypes.Value) = c
 
     End Sub
 
@@ -133,7 +98,7 @@ Public Class gridMPAOptimizations
 
     Public Sub LogResult(ByVal sEconomicValue As Single, ByVal sSocialValue As Single, _
         ByVal sMandatedValue As Single, ByVal sEcologicalValue As Single, _
-        ByVal sBiomassDiversityValue As Single, _
+        ByVal sBiomassDiversityValue As Single, ByVal sBoundaryWeightValue As Single, _
         ByVal sTotalWeighted As Single, ByVal sPercClosed As Single)
 
         Me(1, eColumnTypes.Value).Value = sEconomicValue
@@ -141,8 +106,9 @@ Public Class gridMPAOptimizations
         Me(3, eColumnTypes.Value).Value = sMandatedValue
         Me(4, eColumnTypes.Value).Value = sEcologicalValue
         Me(5, eColumnTypes.Value).Value = sBiomassDiversityValue
-        Me(6, eColumnTypes.Value).Value = sTotalWeighted
-        Me(7, eColumnTypes.Value).Value = sPercClosed
+        Me(6, eColumnTypes.Value).Value = sBoundaryWeightValue
+        Me(7, eColumnTypes.Value).Value = sTotalWeighted
+        Me(8, eColumnTypes.Value).Value = sPercClosed
 
         Me.InvalidateCells()
 
