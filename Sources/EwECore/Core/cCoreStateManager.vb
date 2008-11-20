e429b97b6fa56b1f29aba2bf044b85fbf6167a49
joeb <joeb@ecopath.org>
2008-11-20 17:45:00 +0000

    Added updateDietComp()
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@528 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECore/Core/cCoreStateManager.vb
+++ Sources/EwECore/Core/cCoreStateManager.vb
@@ -5,13 +5,28 @@
 ''' <remarks></remarks>
 Public Class cCoreStateManager
 
+#Region "Private data"
+
     Private m_core As cCore
+
+#End Region
+
+#Region "Construction"
 
     Friend Sub New(ByRef theCore As cCore)
         m_core = theCore
     End Sub
 
+#End Region
 
+#Region "Public methods"
+
+    ''' <summary>
+    ''' Bring the core state up to the requested execution state
+    ''' </summary>
+    ''' <param name="ExecutionState">State to bring the core up to</param>
+    ''' <returns>True if successful. False otherwise.</returns>
+    ''' <remarks></remarks>
     Public Function LoadState(ByVal ExecutionState As EwEUtils.Core.eCoreExecutionState) As Boolean
         Try
 
@@ -36,5 +51,39 @@ Public Class cCoreStateManager
         End Try
 
     End Function
+
+#End Region
+
+#Region "Friend methods: used by the core"
+
+
+    ''' <summary>
+    ''' Copy the Ecopath dietcomp matrix (DC(ngroup,ngroups)) into the Ecosim dietcomp matrix (SimDC(ngroups,ngroups))
+    ''' </summary>
+    ''' <returns></returns>
+    ''' <remarks>This is not really an Execution State thing... but hey it has to go somewhere...</remarks>
+    Friend Function updateDietComp() As Boolean
+
+        Try
+
+            'Only load the dietcomp into ecosim if it is loaded
+            If Not m_core.StateMonitor.HasEcosimLoaded Then
+                Return False
+            End If
+
+            'this will copy diet comp into Ecosim SimDC()
+            m_core.m_EcoSim.RemoveImportFromEcosim()
+
+        Catch ex As Exception
+            cLog.Write(ex)
+            Debug.Assert(False, Me.ToString & ".DietComp() Failed to copy DietComp into Ecosim." & ex.Message)
+            Return False
+        End Try
+
+        Return True
+
+    End Function
+
+#End Region
 
 End Class
