d6d7eabccdd7fe98350928dd44017716a1102242
joeh <joeh@ecopath.org>
2008-12-05 19:45:20 +0000

    Add 'Legend' to be plotted on top of labels
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@650 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterfaceShared/Controls/Graph renderers/ArrayGraph.vb
+++ Sources/ScientificInterfaceShared/Controls/Graph renderers/ArrayGraph.vb
@@ -1,6 +1,9 @@
 ﻿'==============================================================================
 '
 ' $Log: ArrayGraph.vb,v $
+' Revision 1.3  2008/12/05 19:45:20  joeh
+' Add 'Legend' to be plotted on top of labels
+'
 ' Revision 1.2  2008/11/28 20:20:44  joeh
 ' Change the label topaxis angle to 0
 '
@@ -40,12 +43,15 @@ Public Class ArrayGraph
     ''' <param name="g">Graphics to write onto.</param>
     ''' <param name="rcRender">Area the rendition is allowed to use.</param>
     ''' <param name="asData">Array (NxN) of single values to render.</param>
+    ''' <param name="strLegendX">Legend to render along the X axis.</param>
     ''' <param name="astrLabelsX">Labels to render along the X axis.</param>
+    ''' <param name="strLegendY">Legend to render along the Y axis.</param>
     ''' <param name="astrLabelsY">Labels to render along the Y axis. If this parameter is omitted,
     ''' the X-axis labels are plotted along the Y axis and the grid is presumed to be square.</param>
     ''' -----------------------------------------------------------------------
     Public Sub Draw(ByVal g As Graphics, ByVal rcRender As Rectangle, _
-                    ByVal asData As Single(,), ByVal astrLabelsX As String(), Optional ByVal astrLabelsY As String() = Nothing)
+                    ByVal asData As Single(,), ByVal strLegendX As String, ByVal astrLabelsX As String(), _
+                    Optional ByVal strLegendY As String = Nothing, Optional ByVal astrLabelsY As String() = Nothing)
 
         ' ToDo: take right-to-left reading order into account
         ' ToDo: allow side label positioning (left or right)
@@ -62,6 +68,8 @@ Public Class ArrayGraph
         ' Measure max label sizes
         Dim szLabelTopMaxSize As Size = Me.CalcLabelMaxSize(g, astrLabelsX)
         Dim szLabelSideMaxSize As Size = Me.CalcLabelMaxSize(g, astrLabelsY)
+        Dim szLegendTop As Size = Me.CalcLegendMaxSize(g, strLegendX)
+        Dim szLegendSide As Size = Me.CalcLegendMaxSize(g, strLegendY)
         Dim sCellSize As Single = 0.0!
 
         ' Graph layout explanation:
@@ -74,19 +82,32 @@ Public Class ArrayGraph
         ' | Grid           | Horz. labels
         ' |                |
         ' +----------------+-------------
+        'Dim rcArea1 As Rectangle = New Rectangle(rcRender.X, rcRender.Y, _
+        '                                          rcRender.Width - szLabelTopMaxSize.Width, szLabelTopMaxSize.Width)
+        'Dim rcArea2 As Rectangle = New Rectangle(rcRender.X, rcRender.Y + szLabelTopMaxSize.Width, _
+        '                                         rcRender.Width - szLabelSideMaxSize.Width, rcRender.Height - szLabelTopMaxSize.Width)
+        'Dim rcArea3 As Rectangle = New Rectangle(rcRender.Width - szLabelSideMaxSize.Width, szLabelTopMaxSize.Width, _
+        '                                         szLabelSideMaxSize.Width, rcRender.Height - szLabelTopMaxSize.Width)
+        Dim intArea1Width As Integer = rcRender.Width - szLabelSideMaxSize.Width - szLegendSide.Height * 2
+        Dim intArea1Height As Integer = szLabelTopMaxSize.Width + szLegendTop.Height * 2
+        Dim intArea3Width As Integer = szLabelSideMaxSize.Width + szLegendSide.Height * 2
+        Dim intArea3Height As Integer = rcRender.Height - intArea1Height
+        Dim intArea2Width As Integer = rcRender.Width - intArea3Width
+        Dim intArea2Height As Integer = rcRender.Height - intArea1Height
+
         Dim rcArea1 As Rectangle = New Rectangle(rcRender.X, rcRender.Y, _
-                                                 rcRender.Width - szLabelTopMaxSize.Width, szLabelTopMaxSize.Width)
-        Dim rcArea2 As Rectangle = New Rectangle(rcRender.X, rcRender.Y + szLabelTopMaxSize.Width, _
-                                                 rcRender.Width - szLabelSideMaxSize.Width, rcRender.Height - szLabelTopMaxSize.Width)
-        Dim rcArea3 As Rectangle = New Rectangle(rcRender.Width - szLabelSideMaxSize.Width, szLabelTopMaxSize.Width, _
-                                                 szLabelSideMaxSize.Width, rcRender.Height - szLabelTopMaxSize.Width)
+                                                 intArea1Width, intArea1Height)
+        Dim rcArea2 As Rectangle = New Rectangle(rcRender.X, rcRender.Y + intArea1Height, _
+                                                 intArea2Width, intArea2Height)
+        Dim rcArea3 As Rectangle = New Rectangle(rcRender.Width - intArea3Width, intArea1Height, _
+                                                 intArea3Width, intArea3Height)
 
         ' Figure out where to draw the graphs
         sCellSize = CalcGridSize(rcArea2, asData.GetUpperBound(0), asData.GetUpperBound(1))
         ' Top text
-        DrawLabelsTop(g, rcArea1, sCellSize, astrLabelsX, szLabelTopMaxSize, -90 + cLABEL_TOPAXIS_ANGLE)
+        DrawLabelsTop(g, rcArea1, sCellSize, strLegendX, astrLabelsX, szLabelTopMaxSize, -90 + cLABEL_TOPAXIS_ANGLE)
         ' Side text
-        DrawLabelsSide(g, rcArea3, sCellSize, astrLabelsY)
+        DrawLabelsSide(g, rcArea3, sCellSize, strLegendY, astrLabelsY)
         ' Graph
         DrawGraph(g, rcArea2, sCellSize, asData)
 
@@ -177,6 +198,18 @@ Public Class ArrayGraph
         Return szMax
     End Function
 
+    Private Function CalcLegendMaxSize(ByVal g As Graphics, ByVal strLegend As String) As Size
+
+        Dim szLegend As New Size(0, 0)
+        Dim szfLegend As SizeF = Nothing
+
+        szfLegend = g.MeasureString(strLegend, Me.Font)
+        szLegend.Width = CInt(Math.Ceiling(szfLegend.Width))
+        szLegend.Height = CInt(Math.Ceiling(szfLegend.Height))
+
+        Return szLegend
+    End Function
+
 #End Region ' Measurement calculations
 
 #Region " Rendering "
@@ -188,13 +221,18 @@ Public Class ArrayGraph
     ''' <param name="g"></param>
     ''' <param name="rect"></param>
     ''' <param name="sCellSize">Size of a square cell</param>
+    ''' <param name="strLegend"></param>
     ''' <param name="astrLabels"></param>
     ''' <param name="szLabelMaxSize">Max dimensions (in pixels) of a label.</param>
     ''' <param name="sAngle">Text rotation angle.</param>
     ''' -----------------------------------------------------------------------
     Private Sub DrawLabelsTop(ByVal g As Graphics, ByVal rect As Rectangle, ByVal sCellSize As Single, _
-                              ByVal astrLabels As String(), ByVal szLabelMaxSize As Size, _
+                              ByVal strLegend As String, ByVal astrLabels As String(), ByVal szLabelMaxSize As Size, _
                               Optional ByVal sAngle As Single = 0.0!)
+        Dim szLegendTop As Size = Me.CalcLegendMaxSize(g, strLegend)
+
+        g.DrawString(strLegend, Me.Font, SystemBrushes.WindowText, _
+             CInt(((rect.Width - szLegendTop.Width) / 2.0) + rect.X), rect.Y, StringFormat.GenericDefault)
 
         ' Why the cosine bit in the label positioning logic? 
         '
@@ -213,9 +251,12 @@ Public Class ArrayGraph
         ' must be moved by {szLabelMaxSize.height} * Math.Cos(sAngle)
 
         For i As Integer = 0 To astrLabels.GetUpperBound(0)
+            'DrawAngledText(g, astrLabels(i), _
+            '            CInt(i * sCellSize + sCellSize * cCELL_PADDING_RATIO), _
+            '            CInt(rect.Height + Math.Cos(sAngle) * szLabelMaxSize.Height), sAngle)
             DrawAngledText(g, astrLabels(i), _
-                        CInt(i * sCellSize + sCellSize * cCELL_PADDING_RATIO), _
-                        CInt(rect.Height + Math.Cos(sAngle) * szLabelMaxSize.Height), sAngle)
+                 CInt(i * sCellSize), _
+                 CInt(rect.Height + Math.Cos(sAngle) * szLabelMaxSize.Height), sAngle)
         Next
 
     End Sub
@@ -227,16 +268,24 @@ Public Class ArrayGraph
     ''' <param name="g"></param>
     ''' <param name="rect"></param>
     ''' <param name="sCellSize">Size of a square cell</param>
+    ''' <param name="strLegend"></param>
     ''' <param name="astrLabels"></param>
     ''' <param name="sAngle">Text rotation angle.</param>
     ''' -----------------------------------------------------------------------
     Private Sub DrawLabelsSide(ByVal g As Graphics, ByVal rect As Rectangle, ByVal sCellSize As Single, _
-                               ByVal astrLabels As String(), _
+                               ByVal strLegend As String, ByVal astrLabels As String(), _
                                Optional ByVal sAngle As Single = 0.0!)
+        Dim szLegendSide As Size = Me.CalcLegendMaxSize(g, strLegend)
+
+        DrawAngledText(g, strLegend, rect.Width - szLegendSide.Height + rect.X, _
+                       CInt(rect.Height - (rect.Height - szLegendSide.Width) / 2) + rect.Y, _
+                       -90)
 
         For i As Integer = 0 To astrLabels.GetUpperBound(0)
+            'g.DrawString(astrLabels(i), Me.Font, SystemBrushes.WindowText, _
+            '             rect.X, CInt(i * sCellSize + sCellSize * cCELL_PADDING_RATIO) + rect.Y, StringFormat.GenericDefault)
             g.DrawString(astrLabels(i), Me.Font, SystemBrushes.WindowText, _
-                         rect.X, CInt(i * sCellSize + sCellSize * cCELL_PADDING_RATIO) + rect.Y, StringFormat.GenericDefault)
+                        rect.X, i * sCellSize + rect.Y, StringFormat.GenericDefault)
         Next
     End Sub
 
