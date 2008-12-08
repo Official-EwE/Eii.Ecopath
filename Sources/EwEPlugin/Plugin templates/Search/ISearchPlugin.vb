29a37e9ea293c8d3293e14c11cd2633b09348e3f
JeroenS <jeroen.steenbeek@gmail.com>
2008-12-08 16:43:20 +0000

    Initial version
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@657 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/EwEPlugin/Plugin templates/Search/ISearchPlugin.vb
@@ -0,0 +1,21 @@
+﻿'==============================================================================
+'
+' $Log: ISearchPlugin.vb,v $
+' Revision 1.1  2008/12/08 16:43:20  jeroens
+' Initial version
+'
+'==============================================================================
+
+Option Strict On
+
+''' ---------------------------------------------------------------------------
+''' <summary>
+''' Interface for implementing plugin points that are invoked from the EwE
+''' searches
+''' </summary>
+''' ---------------------------------------------------------------------------
+Public Interface ISearchPlugin
+
+    Sub SearchInitialized(ByVal SearchDatastructures As Object)
+
+End Interface
