116a12c43c01e8fea1fd4eb782eeabd5714acd71
JeroenS <jeroen.steenbeek@gmail.com>
2008-12-15 21:41:48 +0000

    Initial version
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@691 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/ScientificInterfaceShared/Controls/ApplicationStatus/cApplicationStatusNotifier.vb
@@ -0,0 +1,35 @@
+﻿Public Class cApplicationStatusNotifier
+    Implements IApplicationStatusDispatcher
+
+    Private Shared __inst__ As cApplicationStatusNotifier
+
+    Private m_dispatcher As IApplicationStatusDispatcher = Nothing
+
+    Public Sub New(ByVal dispatcher As IApplicationStatusDispatcher)
+        Debug.Assert(__inst__ Is Nothing)
+        Debug.Assert(dispatcher IsNot Nothing)
+        __inst__ = Me
+        Me.m_dispatcher = dispatcher
+    End Sub
+
+    Public Shared Function GetInstance() As cApplicationStatusNotifier
+        Return cApplicationStatusNotifier.__inst__
+    End Function
+
+    Public Sub SetStatusText(Optional ByVal strText As String = "", _
+                             Optional ByVal tsUseWaitCursor As Microsoft.VisualBasic.TriState = Microsoft.VisualBasic.TriState.UseDefault, _
+                             Optional ByVal sProgress As Single = 0.0) _
+        Implements IApplicationStatusDispatcher.SetStatusText
+
+        Me.m_dispatcher.SetStatusText(strText, tsUseWaitCursor, sProgress)
+    End Sub
+
+End Class
+
+Public Interface IApplicationStatusDispatcher
+
+    Sub SetStatusText(Optional ByVal strText As String = "", _
+        Optional ByVal tsUseWaitCursor As TriState = TriState.UseDefault, _
+        Optional ByVal sProgress As Single = 0.0)
+
+End Interface
