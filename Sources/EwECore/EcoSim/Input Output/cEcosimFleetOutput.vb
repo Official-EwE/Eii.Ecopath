038d92c7b919133f67e90e4a60c9e3c89c0efe7a
joeb <joeb@ecopath.org>
2009-01-14 18:45:03 +0000

    Added timeseries results
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@736 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECore/EcoSim/Input Output/cEcosimFleetOutput.vb
+++ Sources/EwECore/EcoSim/Input Output/cEcosimFleetOutput.vb
@@ -6,10 +6,16 @@ Imports EwEUtils.Core
 Public Class cEcosimFleetOutput
     Inherits cCoreInputOutputBase
 
+    'dictionary of vars and wrappers that directly access the core data
+    Private m_coreData As New Dictionary(Of eVarNameFlags, IResultsWrapper)
+    Private m_simData As cEcosimDatastructures
+
+
     Public Sub New(ByRef TheCore As cCore, ByVal iFleet As Integer)
         MyBase.New(TheCore)
 
         Dim val As cValue
+        m_simData = TheCore.m_EcoSimData
 
         Me.m_DataType = eDataTypes.EcosimFleetOutput
         Me.Index = iFleet
@@ -41,7 +47,6 @@ Public Class cEcosimFleetOutput
         val = New cValue(0, eVarNameFlags.EcosimFleetEffort, eStatusFlags.OK, eValueTypes.Sng)
         m_values.Add(val.varName, val)
 
-
         'Profit
         val = New cValue(0, eVarNameFlags.EcosimFleetProfit, eStatusFlags.OK, eValueTypes.Sng)
         m_values.Add(val.varName, val)
@@ -53,11 +58,37 @@ Public Class cEcosimFleetOutput
 
     End Sub
 
+
+    Public Sub Init()
+
+        'the results arrays of ecosim are redim for each run
+        'this means the reference to the results data is lost on each run 
+        'so reset the reference
+        m_coreData.Clear()
+
+        m_coreData.Add(eVarNameFlags.EcosimFleetValueTime, New c2DResultsWrapper(m_simData.ResultsSumValueByGear, Me.Index))
+        m_coreData.Add(eVarNameFlags.EcosimFleetCatchTime, New c2DResultsWrapper(m_simData.ResultsSumCatchByGear, Me.Index))
+        'ResultsSumCatchByGear
+
+    End Sub
+
+
+
+    Public Overrides Function GetVariable(ByVal VarName As EwEUtils.Core.eVarNameFlags, Optional ByVal iIndex1 As Integer = -9999, Optional ByVal iIndex2 As Integer = -9999) As Object
+
+        If Not m_coreData.ContainsKey(VarName) Then
+            'NOT in list of sim vars so get the value from the base class GetVariable(...)
+            Return MyBase.GetVariable(VarName, iIndex1, iIndex2)
+        Else
+            'Varname is access directly via the core data
+            Return m_coreData.Item(VarName).Value(iIndex1, iIndex2)
+        End If
+
+    End Function
+
 #Region "Variable via dot '.' operator"
 
-
-
-    Public Property Profit() As Single
+    Public Property ProfitSummary() As Single
         Get
             Return CSng(GetVariable(eVarNameFlags.EcosimFleetProfit))
         End Get
@@ -68,7 +99,7 @@ Public Class cEcosimFleetOutput
     End Property
 
 
-    Public Property Jobs() As Single
+    Public Property JobsSummary() As Single
         Get
             Return CSng(GetVariable(eVarNameFlags.EcosimFleetJobs))
         End Get
@@ -77,9 +108,6 @@ Public Class cEcosimFleetOutput
             SetVariable(eVarNameFlags.EcosimFleetJobs, value)
         End Set
     End Property
-
-
-
 
     Public Property CatchStart() As Single
         Get
@@ -152,6 +180,22 @@ Public Class cEcosimFleetOutput
             SetVariable(eVarNameFlags.EcosimFleetEffort, value)
         End Set
     End Property
+
+    Public ReadOnly Property Value(ByVal Time As Integer) As Single
+        Get
+            Return CSng(GetVariable(eVarNameFlags.EcosimFleetValueTime, Time))
+        End Get
+
+    End Property
+
+    Public ReadOnly Property CatchBiomass(ByVal Time As Integer) As Single
+        Get
+            Return CSng(GetVariable(eVarNameFlags.EcosimFleetValueTime, Time))
+        End Get
+
+    End Property
+
+
 
 #End Region
 
