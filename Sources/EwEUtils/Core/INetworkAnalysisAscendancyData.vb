c51c52184c08661b4e8f4bfa4c1ebc01de6a9621
JeroenS <jeroen.steenbeek@gmail.com>
2009-01-21 19:25:00 +0000

    Initial version
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@775 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/EwEUtils/Core/INetworkAnalysisAscendancyData.vb
@@ -0,0 +1,23 @@
+﻿'==============================================================================
+'
+' $Log: INetworkAnalysisAscendancyData.vb,v $
+' Revision 1.1  2009/01/21 19:25:00  jeroens
+' Initial version
+'
+'
+'==============================================================================
+
+Namespace Core
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Template for exchanging Network Analysis data.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Public Interface INetworkAnalysisData
+
+        ReadOnly Property Ascendancy() As Single(,)
+
+    End Interface
+
+End Namespace
