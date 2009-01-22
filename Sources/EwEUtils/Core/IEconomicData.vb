01a68a96344b47a285e7065787f02108e97866c4
JeroenS <jeroen.steenbeek@gmail.com>
2009-01-22 17:38:36 +0000

    Added  2 main economic values
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@778 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwEUtils/Core/IEconomicData.vb
+++ Sources/EwEUtils/Core/IEconomicData.vb
@@ -1,6 +1,9 @@
 ﻿'==============================================================================
 '
 ' $Log: IEconomicData.vb,v $
+' Revision 1.2  2009/01/22 17:38:36  jeroens
+' Added  2 main economic values
+'
 ' Revision 1.1  2009/01/21 19:25:00  jeroens
 ' Initial version
 '
@@ -15,6 +18,20 @@ Namespace Core
     ''' </summary>
     ''' -----------------------------------------------------------------------
     Public Interface IEconomicData
+
+        ''' -------------------------------------------------------------------
+        ''' <summary>
+        ''' Return the total value of ???
+        ''' </summary>
+        ''' -------------------------------------------------------------------
+        ReadOnly Property TotalValue() As Single
+
+        ''' -------------------------------------------------------------------
+        ''' <summary>
+        ''' Return the total value of employment.
+        ''' </summary>
+        ''' -------------------------------------------------------------------
+        ReadOnly Property EmploymentValue() As Single
 
     End Interface
 
