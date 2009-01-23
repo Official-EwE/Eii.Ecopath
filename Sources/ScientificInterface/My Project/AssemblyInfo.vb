1d1a855c2d43ee0a087f95e67627b5b326246734
JeroenS <jeroen.steenbeek@gmail.com>
2009-01-23 02:46:24 +0000

    Updated copyright year
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@786 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterface/My Project/AssemblyInfo.vb
+++ Sources/ScientificInterface/My Project/AssemblyInfo.vb
@@ -1,6 +1,9 @@
 '==============================================================================
 '
 ' $Log: AssemblyInfo.vb,v $
+' Revision 1.9  2009/01/23 02:46:24  jeroens
+' Updated copyright year
+'
 ' Revision 1.8  2009/01/14 04:18:33  sherman
 ' Build 6.0.5.1
 '
@@ -25,64 +28,6 @@
 ' Revision 1.1  2008/09/26 07:32:06  sherman
 ' --== DELETED HISTORY ==--
 '
-' Revision 1.30  2008/09/09 01:35:03  sherman
-' Build 6.0.4.1
-'
-' Revision 1.29  2008/08/21 22:39:51  sherman
-' Build 6.0.4.0
-'
-' Revision 1.28  2008/08/20 23:55:08  sherman
-' 6.0.3.23
-'
-' Revision 1.27  2008/08/14 01:32:23  sherman
-' Internal release 6.0.3.22
-'
-' Revision 1.26  2008/07/29 06:06:44  sherman
-' Build 6.0.3.19
-'
-' Revision 1.25  2008/07/25 21:26:43  sherman
-' Build 6.0.3.18
-'
-' Revision 1.24  2008/07/21 19:10:14  sherman
-' Pre-release internal 6.0.3.16
-'
-' Revision 1.23  2008/07/15 22:02:55  sherman
-' Build 6.0.3.15
-'
-' Revision 1.22  2008/05/20 18:57:27  sherman
-' 6.0.3.14 Beta, not released to the public yet,
-' they will still be linked to 13 until released
-'
-' Revision 1.21  2008/04/29 15:56:39  sherman
-' Build 6.0.3.13
-'
-' Revision 1.20  2008/03/14 17:20:45  sherman
-' Build 6.0.3.12
-'
-' Revision 1.19  2008/02/09 21:58:13  jeroens
-' 6.0.3.11
-'
-' Revision 1.18  2008/02/04 00:46:28  sherman
-' 6.0.3.10
-'
-' Revision 1.17  2008/01/31 01:00:32  sherman
-' 6.0.3.9
-'
-' Revision 1.16  2008/01/27 21:20:59  sherman
-' Release version 6.0.3.8
-'
-' Revision 1.15  2007/12/17 16:15:15  sherman
-' Build 6.0.3.7
-'
-' Revision 1.14  2007/11/22 00:14:19  sherman
-' 6.0.3.6 release
-'
-' Revision 1.13  2007/11/21 23:19:45  sherman
-' Commited Version *** NOTE: Should be back dated to 11/14/2007
-'
-' Revision 1.12  2007/10/23 18:58:07  sherman
-' Start of Version Logs
-'
 '==============================================================================
 
 Imports System
@@ -96,10 +41,10 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("Ecopath With Ecosim Scientific Interface")> 
+<Assembly: AssemblyTitle("Ecopath with Ecosim Scientific Interface")> 
 <Assembly: AssemblyCompany("UBC Fisheries Centre")> 
 <Assembly: AssemblyProduct("EwE6SFGUI")> 
-<Assembly: AssemblyCopyright("Copyright © 1991-2008")> 
+<Assembly: AssemblyCopyright("Copyright © 1991-2009")> 
 
 <Assembly: CLSCompliant(True)> 
 
