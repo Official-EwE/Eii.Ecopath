e4276d7a545c4240239bdc36f6709debae68bf76
JeroenS <jeroen.steenbeek@gmail.com>
2009-01-29 23:38:40 +0000

    Added UnitCell
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@812 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterfaceShared/Controls/EwEGrid/EwEGridCell.vb
+++ Sources/ScientificInterfaceShared/Controls/EwEGrid/EwEGridCell.vb
@@ -1,6 +1,9 @@
 '==============================================================================
 '
 ' $Log: EwEGridCell.vb,v $
+' Revision 1.6  2009/01/29 23:38:40  jeroens
+' Added UnitCell
+'
 ' Revision 1.5  2009/01/23 03:11:47  jeroens
 ' Prft
 '
@@ -1094,6 +1097,104 @@ Namespace Controls.EwEGrid
     End Class
 
 #End Region ' Class PropertyColumnHeaderCell 
+
+#Region " UnitCell "
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' UnitCell implements a cell that shows a dynamic unit string.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Public Class UnitCell
+        : Inherits EwECell
+
+        ''' <summary>One visualizer for all cells</summary>
+        Private Shared g_visualizer As EwECellVisualizerBase
+        Protected m_aUnitTypes() As StyleGuide.eUnitType
+        Protected m_strUnitMask As String = ""
+
+
+#Region " Construction "
+
+        ''' -------------------------------------------------------------------
+        ''' <summary>
+        ''' Constructor
+        ''' </summary>
+        ''' -------------------------------------------------------------------
+        Public Sub New(ByVal unitType As StyleGuide.eUnitType)
+            Me.New("{0}", New StyleGuide.eUnitType() {unitType})
+        End Sub
+
+        ''' -------------------------------------------------------------------
+        ''' <summary>
+        ''' Constructor
+        ''' </summary>
+        ''' -------------------------------------------------------------------
+        Public Sub New(ByVal strUnitMask As String, ByVal aUnitTypes() As StyleGuide.eUnitType)
+            MyBase.New(Nothing, GetType(String))
+
+            Me.m_strUnitMask = strUnitMask
+            Me.m_aUnitTypes = aUnitTypes
+        End Sub
+
+        Public Overrides ReadOnly Property DisplayText() As String
+            Get
+                Dim strDisplayText As String = ""
+
+                If Me.m_aUnitTypes IsNot Nothing Then
+
+                    Select Case m_aUnitTypes.Length
+                        Case 0
+                            ' NOP
+                        Case 1
+                            strDisplayText = String.Format(Me.m_strUnitMask, GetUnitString(m_aUnitTypes(0)))
+                        Case 2
+                            strDisplayText = String.Format(Me.m_strUnitMask, GetUnitString(m_aUnitTypes(0)), GetUnitString(m_aUnitTypes(1)))
+                        Case Else
+                            Debug.Assert(False)
+                    End Select
+
+                End If
+
+                Return strDisplayText
+            End Get
+        End Property
+
+        Private Function GetUnitString(ByVal unitType As StyleGuide.eUnitType) As String
+            Dim sg As StyleGuide = StyleGuide.GetInstance()
+            Dim strUnitString As String = ""
+            Select Case unitType
+                Case StyleGuide.eUnitType.Currency
+                    strUnitString = sg.CurrencyUnitText(sg.CurrencyUnit)
+                Case StyleGuide.eUnitType.Time
+                    strUnitString = sg.TimeUnitText(sg.TimeUnit)
+                Case StyleGuide.eUnitType.Monetary
+                    strUnitString = sg.MonetaryUnitText(sg.MonetaryUnit)
+                Case Else
+                    Debug.Assert(False)
+            End Select
+            Return strUnitString
+        End Function
+
+        ''' -----------------------------------------------------------------------
+        ''' <summary>
+        ''' Overridden to enusre that this cell cannot be edited.
+        ''' </summary>
+        ''' -----------------------------------------------------------------------
+        Public Overrides Property Style() As StyleGuide.eStyleFlags
+            Get
+                Return (MyBase.Style Or StyleGuide.eStyleFlags.NotEditable)
+            End Get
+            Set(ByVal styleNew As StyleGuide.eStyleFlags)
+                MyBase.Style = (styleNew Or StyleGuide.eStyleFlags.NotEditable)
+            End Set
+        End Property
+
+#End Region ' Construction 
+
+    End Class
+
+#End Region ' UnitCell
 
 #End Region ' Dynamic cells
 
