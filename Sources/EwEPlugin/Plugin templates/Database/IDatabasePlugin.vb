cc44c5594c19a030f8edcf368f7c3213b2346f0b
JeroenS <jeroen.steenbeek@gmail.com>
2009-03-01 19:38:00 +0000

    Commented
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@905 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwEPlugin/Plugin templates/Database/IDatabasePlugin.vb
+++ Sources/EwEPlugin/Plugin templates/Database/IDatabasePlugin.vb
@@ -1,36 +1,48 @@
 '==============================================================================
 '
 ' $Log: IDatabasePlugin.vb,v $
+' Revision 1.2  2009/03/01 19:38:00  jeroens
+' Commented
+'
 ' Revision 1.1  2009/02/25 07:15:02  jeroens
 ' Initial version
 '
 '==============================================================================
 
 Option Strict On
-Imports EwEUtils.Database
 
 Namespace Data
 
+    ''' =======================================================================
+    ''' <summary>
+    ''' Plug-in point to allow plug-ins to track database movements of EwE.
+    ''' </summary>
+    ''' =======================================================================
     Public Interface IDatabasePlugin
         Inherits IPlugin
 
         ''' -----------------------------------------------------------------------
         ''' <summary>
-        ''' Open a database connection
+        ''' Execution interface for the EwE open database plug-in point. This 
+        ''' method is invoked whenever the EwE core opens a database connection.
         ''' </summary>
         ''' -----------------------------------------------------------------------
         Function Open(ByVal strName As String) As Boolean
 
         ''' -----------------------------------------------------------------------
         ''' <summary>
-        ''' Close a database connection
+        ''' Execution interface for the EwE close database plug-in point. This 
+        ''' method is invoked whenever the EwE core closes a database connection.
         ''' </summary>
         ''' -----------------------------------------------------------------------
         Sub Close()
 
         ''' -----------------------------------------------------------------------
         ''' <summary>
-        ''' States whether a plug-in has pending data.
+        ''' Execution interface for a plug-in point to report whether is has any
+        ''' pending changes. This method is invoked whenever the EwE core polls
+        ''' for unsaved modifications prior to undertaking actions that may cause 
+        ''' data to be lost.
         ''' </summary>
         ''' -----------------------------------------------------------------------
         Function IsModified() As Boolean
