2c6078198d4f6456fe6389058141f96308269ac9
JeroenS <jeroen.steenbeek@gmail.com>
2009-03-05 07:27:11 +0000

    Implemented
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@938 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwEUtils/Core/IEconomicData.vb
+++ Sources/EwEUtils/Core/IEconomicData.vb
@@ -1,6 +1,9 @@
 ﻿'==============================================================================
 '
 ' $Log: IEconomicData.vb,v $
+' Revision 1.4  2009/03/05 07:27:11  jeroens
+' Implemented
+'
 ' Revision 1.3  2009/01/24 17:46:16  joeb
 ' Added EmploymentValueByFleet and ProfitByFleet
 '
@@ -9,7 +12,6 @@
 '
 ' Revision 1.1  2009/01/21 19:25:00  jeroens
 ' Initial version
-'
 '
 '==============================================================================
 
@@ -22,33 +24,26 @@ Namespace Core
     ''' -----------------------------------------------------------------------
     Public Interface IEconomicData
 
-        ''' -------------------------------------------------------------------
-        ''' <summary>
-        ''' Return the total value of ???
-        ''' </summary>
-        ''' -------------------------------------------------------------------
-        ReadOnly Property TotalValue() As Single
-
-        ''' -------------------------------------------------------------------
-        ''' <summary>
-        ''' Return the total value of employment.
-        ''' </summary>
-        ''' -------------------------------------------------------------------
-        ReadOnly Property EmploymentValue() As Single
-
-        ''' -------------------------------------------------------------------
-        ''' <summary>
-        ''' Employment value by fleet
-        ''' </summary>
-        ''' -------------------------------------------------------------------
-        ReadOnly Property EmploymentValueByFleet(ByVal FleetIndex As Integer) As Single
-
-        ''' -------------------------------------------------------------------
-        ''' <summary>
-        ''' Proifit by fleet
-        ''' </summary>
-        ''' -------------------------------------------------------------------
-        ReadOnly Property ProfitByFleet(ByVal FleetIndex As Integer) As Single
+        ReadOnly Property Production() As Single
+        ReadOnly Property ProductionLive() As Single
+        ReadOnly Property RevenueProductsMain() As Single
+        ReadOnly Property RevenueProductsOther() As Single
+        ReadOnly Property RevenueSubsidies() As Single
+        ReadOnly Property RevenueTotal() As Single
+        ReadOnly Property CostSalariesShares() As Single
+        ReadOnly Property CostInput() As Single
+        ReadOnly Property CostTotalInputOther() As Single
+        ReadOnly Property CostTaxes() As Single
+        ReadOnly Property CostLicenseObservers() As Single
+        ReadOnly Property Cost() As Single
+        ReadOnly Property Profit() As Single
+        ReadOnly Property Throughput() As Single
+        ReadOnly Property NumberOfJobsFemaleTotal() As Single
+        ReadOnly Property NumberOfJobsMaleTotal() As Single
+        ReadOnly Property NumberOfJobsTotal() As Single
+        ReadOnly Property NumberOfWorkerDependents() As Single
+        ReadOnly Property NumberOfOwnerDependents() As Single
+        ReadOnly Property NumberOfDependentsTotal() As Single
 
     End Interface
 
