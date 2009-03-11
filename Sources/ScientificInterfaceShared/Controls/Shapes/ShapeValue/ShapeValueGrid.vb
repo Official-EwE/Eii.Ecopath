a805726fd1ff0ea8c42d3af94ca8703e3238f5f0
JeroenS <jeroen.steenbeek@gmail.com>
2009-03-11 00:31:18 +0000

    Able to show year+month/indexed columns
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@954 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterfaceShared/Controls/Shapes/ShapeValue/ShapeValueGrid.vb
+++ Sources/ScientificInterfaceShared/Controls/Shapes/ShapeValue/ShapeValueGrid.vb
@@ -1,6 +1,9 @@
 '==============================================================================
 '
 ' $Log: ShapeValueGrid.vb,v $
+' Revision 1.2  2009/03/11 00:31:18  jeroens
+' Able to show year+month/indexed columns
+'
 ' Revision 1.1  2008/12/15 15:36:39  jeroens
 ' Moved from ScInt
 '
@@ -25,11 +28,26 @@ Public Class ShapeValueGrid
     Private m_iNumValues As Integer = 50
     Private m_bSuppressZeroes As Boolean = False
     Private m_shape As cShapeData = Nothing
+    Private m_displayMode As eDisplayMode = eDisplayMode.Monthly
+
+    Private Enum eDisplayMode As Integer
+        ''' <summary>Display values per year, month</summary>
+        Monthly
+        ''' <summary>Display values per index</summary>
+        Index
+    End Enum
 
     Public Sub SetValues(ByVal iNumValues As Integer, ByVal shape As cShapeData)
         Me.m_iNumValues = iNumValues
         Me.m_bSuppressZeroes = (TypeOf shape Is cTimeSeries)
         Me.m_shape = shape
+
+        ' Determine display mode
+        If TypeOf (shape) Is cMediationFunction Then
+            Me.m_displayMode = eDisplayMode.Index
+        Else
+            Me.m_displayMode = eDisplayMode.Monthly
+        End If
         Me.InitLayout()
     End Sub
 
@@ -64,11 +82,22 @@ Public Class ShapeValueGrid
     Protected Overrides Sub InitStyle()
         MyBase.InitStyle()
 
-        Me.FixedColumns = 1
+        Select Case Me.m_displayMode
 
-        Me.Redim(Me.m_iNumValues + 1, 2)
-        Me(0, 0) = New EwEColumnHeaderCell(My.Resources.HEADER_TIME)
-        Me(0, 1) = New EwEColumnHeaderCell(My.Resources.HEADER_VALUE)
+            Case eDisplayMode.Index
+                Me.Redim(Me.m_iNumValues + 1, 2)
+                Me(0, 0) = New EwEColumnHeaderCell(My.Resources.HEADER_INDEX)
+                Me(0, 1) = New EwEColumnHeaderCell(My.Resources.HEADER_VALUE)
+
+            Case eDisplayMode.Monthly
+                Me.Redim(Me.m_iNumValues + 1, 3)
+                Me(0, 0) = New EwEColumnHeaderCell(My.Resources.HEADER_YEAR)
+                Me(0, 1) = New EwEColumnHeaderCell(My.Resources.HEADER_MONTH)
+                Me(0, 2) = New EwEColumnHeaderCell(My.Resources.HEADER_VALUE)
+
+        End Select
+
+        Me.FixedColumnWidths = False
 
     End Sub
 
@@ -76,42 +105,68 @@ Public Class ShapeValueGrid
 
         Dim core As cCore = cCore.GetInstance()
         Dim cell As EwECell = Nothing
-        Dim sValue As Single = 0.0!
         Dim iStartIndex As Integer = core.EcosimFirstYear
+        Dim sValue As Single = 0.0!
 
         If iStartIndex > 0 Then iStartIndex -= 1
 
-        For iValue As Integer = 1 To Me.m_iNumValues
-
-            cell = New EwECell(iValue + iStartIndex, GetType(Integer))
-            cell.Style = StyleGuide.eStyleFlags.NotEditable
-            cell.SuppressZero = True
-            Me(iValue, 0) = cell
+        For iValue As Integer = 0 To Me.m_iNumValues - 1
 
             sValue = 0.0!
 
             If Me.m_shape IsNot Nothing Then
                 If iValue <= Me.m_shape.XMax Then
-                    sValue = Me.m_shape.ShapeData(iValue)
+                    sValue = Me.m_shape.ShapeData(iValue + 1)
                 End If
             End If
 
-            cell = New EwECell(sValue, GetType(Single))
-            cell.SuppressZero = Me.m_bSuppressZeroes
-            Me(iValue, 1) = cell
+            Select Case Me.m_displayMode
+
+                Case eDisplayMode.Index
+
+                    cell = New EwECell(CStr(iValue + iStartIndex + 1), GetType(String))
+                    cell.Style = StyleGuide.eStyleFlags.NotEditable
+                    Me(iValue + 1, 0) = cell
+
+                    cell = New EwECell(sValue, GetType(Single))
+                    cell.SuppressZero = Me.m_bSuppressZeroes
+                    Me(iValue + 1, 1) = cell
+
+                Case eDisplayMode.Monthly
+
+                    Dim strLabel0 As String = ""
+                    Dim strLabel1 As String = ""
+                    Dim iYear As Integer = iStartIndex + 1 + CInt(Math.Floor(iValue / 12))
+                    Dim iMonth As Integer = 1 + (iValue Mod 12)
+                    Dim d As New Date(iYear, iMonth, 1)
+
+                    If iMonth = 1 Then strLabel0 = CStr(iYear) Else strLabel0 = ""
+                    strLabel1 = d.ToString("MMM")
+
+                    cell = New EwECell(strLabel0, GetType(String))
+                    cell.Style = StyleGuide.eStyleFlags.NotEditable
+                    Me(iValue + 1, 0) = cell
+
+                    cell = New EwECell(strLabel1, GetType(String))
+                    cell.Style = StyleGuide.eStyleFlags.NotEditable
+                    Me(iValue + 1, 1) = cell
+
+                    cell = New EwECell(sValue, GetType(Single))
+                    cell.SuppressZero = Me.m_bSuppressZeroes
+                    Me(iValue + 1, 2) = cell
+
+            End Select
+
         Next
 
     End Sub
 
     Protected Overrides Sub FinishStyle()
         MyBase.FinishStyle()
-
-        Me.Columns(0).Width = 70
-        'Me.Columns(1).Width = 70
     End Sub
 
     Protected Overrides Sub bm_colSelectClick(ByVal sender As Object, ByVal e As SourceGrid2.PositionEventArgs)
-        ' Allow col selectionn
+        ' Allow col selection
         MyBase.bm_colSelectClick(sender, e)
     End Sub
 
