da63a736221a5cde8f52641d54c67f581cf290ac
JeroenS <jeroen.steenbeek@gmail.com>
2009-03-16 16:55:57 +0000

    Initial version
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@987 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/EwECore/Particle size distribution/Input Output/cPSDParameters.vb
@@ -0,0 +1,72 @@
+﻿'==============================================================================
+'
+' $Log: cPSDParameters.vb,v $
+' Revision 1.1  2009/03/16 16:55:57  jeroens
+' Initial version
+'
+'==============================================================================
+
+Option Strict On
+
+Imports EwEUtils.Core
+Imports EwECore.ValueWrapper
+
+''' <summary>
+''' This class wraps the underlying particle size distribution data structures
+''' </summary>
+Public Class cPSDParameters
+    Inherits cCoreInputOutputBase
+
+#Region "Constructor"
+
+    Public Sub New(ByRef m_core As cCore)
+        MyBase.New(m_core)
+
+
+        Try
+            'no data validation at this time
+            Me.AllowValidation = False
+            m_coreComponent = eCoreComponentType.EcoPath
+            m_dataType = eDataTypes.ParticleSizeDistribution
+
+            Dim val As cValue
+            Dim meta As cVariableMetaData
+
+            Me.m_ValidationStatus = New cVariableStatus(Me, eStatusFlags.OK, "", eVarNameFlags.NotSet, m_dataType, m_coreComponent, Index, cCore.NULL_VALUE)
+
+            ' Define variables here
+
+            Me.AllowValidation = True
+
+        Catch ex As Exception
+
+            Debug.Assert(False, ex.Message)
+            cLog.Write(Me.ToString & ".New() Error: " & ex.Message)
+
+        End Try
+
+    End Sub
+
+#End Region
+
+#Region "Variables via dot (.) operator"
+
+    ''' <summary>
+    ''' </summary>
+    ''' <value></value>
+    Public Property MyFristParameterYippee() As Single
+
+        Get
+            'Return CType(GetVariable(eVarNameFlags.NotSet), Single)
+            Return 42.0!
+        End Get
+
+        Set(ByVal value As Single)
+            'SetVariable(eVarNameFlags.NotSet, value)
+        End Set
+
+    End Property
+
+#End Region
+
+End Class
---
+++ Sources/EwECore/Particle size distribution/Model/cPSDDatastructures.vb
@@ -0,0 +1,20 @@
+﻿'==============================================================================
+'
+' $Log: cPSDDatastructures.vb,v $
+' Revision 1.1  2009/03/16 16:55:57  jeroens
+' Initial version
+'
+'==============================================================================
+
+Option Strict On
+
+Imports EwEUtils.Core
+
+''' <summary>
+''' This class wraps the underlying particle size distribution data structures
+''' </summary>
+Public Class cPSDDatastructures
+
+    Public VeryFirstParameterValue As Single = 42.0!
+
+End Class
