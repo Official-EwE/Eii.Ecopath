1260de55a88243c2c33bd472ba108713f072ca0a
JeroenS <jeroen.steenbeek@gmail.com>
2009-03-20 18:00:44 +0000

    Initial version
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@1046 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/ScientificInterfaceShared/Controls/ListView/cSmoothListView.vb
@@ -0,0 +1,27 @@
+﻿'==============================================================================
+'
+' $Log: cSmoothListView.vb,v $
+' Revision 1.1  2009/03/20 18:00:44  jeroens
+' Initial version
+'
+'==============================================================================
+
+#Region " Imports "
+
+Option Strict On
+
+#End Region ' Imports
+
+Namespace Controls
+
+    Public Class cSmoothListView
+        : Inherits ListView
+
+        Public Sub New()
+            MyBase.New()
+            Me.SetStyle(ControlStyles.OptimizedDoubleBuffer, True)
+        End Sub
+
+    End Class
+
+End Namespace ' Controls
