b70fa75c42f2acd24f613193abc9727bea27f61b
JeroenS <jeroen.steenbeek@gmail.com>
2009-03-24 15:56:12 +0000

    Documented
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@1092 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterfaceShared/Controls/ApplicationStatus/cApplicationStatusNotifier.vb
+++ Sources/ScientificInterfaceShared/Controls/ApplicationStatus/cApplicationStatusNotifier.vb
@@ -1,35 +1,107 @@
-﻿Public Class cApplicationStatusNotifier
-    Implements IApplicationStatusDispatcher
+﻿'==============================================================================
+'
+' $Log: cApplicationStatusNotifier.vb,v $
+' Revision 1.2  2009/03/24 15:56:12  jeroens
+' Documented
+'
+'==============================================================================
 
-    Private Shared __inst__ As cApplicationStatusNotifier
+#Region " Imports "
 
-    Private m_dispatcher As IApplicationStatusDispatcher = Nothing
+#End Region ' Imports
 
-    Public Sub New(ByVal dispatcher As IApplicationStatusDispatcher)
-        Debug.Assert(__inst__ Is Nothing)
-        Debug.Assert(dispatcher IsNot Nothing)
-        __inst__ = Me
-        Me.m_dispatcher = dispatcher
-    End Sub
+Namespace Controls
 
-    Public Shared Function GetInstance() As cApplicationStatusNotifier
-        Return cApplicationStatusNotifier.__inst__
-    End Function
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Central class for providing appliation status feedback. This class
+    ''' enables code outside the Scientific Interface application framework
+    ''' such as plug-ins to provide status bar feedack on running operations.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Public Class cApplicationStatusNotifier
+        Implements IApplicationStatusDispatcher
 
-    Public Sub SetStatusText(Optional ByVal strText As String = "", _
-                             Optional ByVal tsUseWaitCursor As Microsoft.VisualBasic.TriState = Microsoft.VisualBasic.TriState.UseDefault, _
-                             Optional ByVal sProgress As Single = 0.0) _
-        Implements IApplicationStatusDispatcher.SetStatusText
+        ''' <summary>The dispatcher that will receives and visualizes status feedback.</summary>
+        Private m_dispatcher As IApplicationStatusDispatcher = Nothing
 
-        Me.m_dispatcher.SetStatusText(strText, tsUseWaitCursor, sProgress)
-    End Sub
+#Region " Singleton "
 
-End Class
+        ''' <summary>Singleton instance of the status notifier.</summary>
+        Private Shared __inst__ As cApplicationStatusNotifier
 
-Public Interface IApplicationStatusDispatcher
+        Public Sub New(ByVal dispatcher As IApplicationStatusDispatcher)
 
-    Sub SetStatusText(Optional ByVal strText As String = "", _
-        Optional ByVal tsUseWaitCursor As TriState = TriState.UseDefault, _
-        Optional ByVal sProgress As Single = 0.0)
+            ' Singleton asserts
+            Debug.Assert(__inst__ Is Nothing)
+            Debug.Assert(dispatcher IsNot Nothing)
 
-End Interface
+            ' Store dispatcher
+            Me.m_dispatcher = dispatcher
+
+            ' Store singleton instance
+            __inst__ = Me
+
+        End Sub
+
+        ''' -------------------------------------------------------------------
+        ''' <summary>
+        ''' Get the single instance of the status notifier.
+        ''' </summary>
+        ''' -------------------------------------------------------------------
+        Public Shared Function GetInstance() As cApplicationStatusNotifier
+            Return cApplicationStatusNotifier.__inst__
+        End Function
+
+#End Region ' Singleton
+
+        ''' -------------------------------------------------------------------
+        ''' <summary>
+        ''' Set status text feedback.
+        ''' </summary>
+        ''' <param name="strText">The text to set.</param>
+        ''' <param name="tsUseWaitCursor">
+        ''' <para>Flag stating whether to show a wait cursor.</para>
+        ''' <para>Flag values are interpreted as follows:
+        ''' <list>
+        ''' <item><term>True</term><description>Shows a wait cursor, and increases the wait cursor count by one.</description></item>
+        ''' <item><term>False</term><description>Decreases the wait cursor count by one, and once the wait cursor count reaches zero a default cursor is restored.</description></item>
+        ''' <item><term>UseDefault</term><description>Use the current application cursor.</description></item>
+        ''' </list>
+        ''' </para>
+        ''' </param>
+        ''' <param name="sProgress">
+        ''' <para>Progress of the process that is running.</para>
+        ''' <para>Values are interpreted as follows:
+        ''' <list>
+        ''' <item><term>0</term><description>The status feedback will not provide progress information.</description></item>
+        ''' <item><term>[0, 1]</term><description>If non-zero, status feedback will show progress information with the given value, where 1 equates to 100% progress.</description></item>
+        ''' <item><term>-1</term><description>Shows continuous progress information that is not related to a given progress value.</description></item>
+        ''' </list>
+        ''' </para>
+        ''' </param>
+        ''' -------------------------------------------------------------------
+        Public Sub SetStatusText(Optional ByVal strText As String = "", _
+                                 Optional ByVal tsUseWaitCursor As Microsoft.VisualBasic.TriState = Microsoft.VisualBasic.TriState.UseDefault, _
+                                 Optional ByVal sProgress As Single = 0.0) _
+            Implements IApplicationStatusDispatcher.SetStatusText
+            ' Pass the word
+            Me.m_dispatcher.SetStatusText(strText, tsUseWaitCursor, sProgress)
+        End Sub
+
+    End Class
+
+    ''' -------------------------------------------------------------------
+    ''' <summary>
+    ''' Interface to implement status feedback.
+    ''' </summary>
+    ''' -------------------------------------------------------------------
+    Public Interface IApplicationStatusDispatcher
+
+        Sub SetStatusText(Optional ByVal strText As String = "", _
+            Optional ByVal tsUseWaitCursor As TriState = TriState.UseDefault, _
+            Optional ByVal sProgress As Single = 0.0)
+
+    End Interface
+
+End Namespace ' Controls
