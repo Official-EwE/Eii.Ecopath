4118f44b91d728ef0d313b03b424d976f28c831d
JeroenS <jeroen.steenbeek@gmail.com>
2009-03-30 17:09:33 +0000

    Split cells
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@1148 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterfaceShared/Controls/EwEGrid/EwEHierarchyGridCell.vb
+++ Sources/ScientificInterfaceShared/Controls/EwEGrid/EwEHierarchyGridCell.vb
@@ -1,23 +1,8 @@
 '==============================================================================
 '
 ' $Log: EwEHierarchyGridCell.vb,v $
-' Revision 1.1  2008/09/26 07:31:16  sherman
-' --== DELETED HISTORY ==--
-'
-' Revision 1.1  2008/06/01 23:45:08  jeroens
-' Separated from Scientific Interface
-'
-' Revision 1.4  2008/05/29 22:23:01  jeroens
-' Moved eVarNameFlags to EwEUtils
-'
-' Revision 1.3  2008/01/11 12:27:08  jeroens
-' Child rows tracked by row index, no longer by simple child row count because child rows may not be placed in consecutive order
-'
-' Revision 1.2  2007/05/31 13:11:23  jeroens
-' * Renamed StyleGuide StyleFlags to eStyleFlags
-'
-' Revision 1.1  2006/10/18 15:51:28  jeroens
-' Initial version
+' Revision 1.2  2009/03/30 17:09:33  jeroens
+' Split cells
 '
 '==============================================================================
 
@@ -98,103 +83,5 @@ Namespace Controls.EwEGrid
         End Property
 
     End Class
-
-#Region " Class PropertyRowHeaderParentCell "
-
-    ''' -----------------------------------------------------------------------
-    ''' <summary>
-    ''' PropertyRowHeaderParentCell implements a PropertyRowHeaderCell rendered as an EwE name field.
-    ''' </summary>
-    ''' -----------------------------------------------------------------------
-    <CLSCompliant(False)> _
-    Public Class PropertyRowHeaderParentCell
-        : Inherits PropertyRowHeaderCell
-
-        ''' <summary>One visualizer for all cells</summary>
-        Private Shared g_visualizer As New cVisualizerEwEParentRowHeader()
-
-#Region " Construction "
-
-        ''' -------------------------------------------------------------------
-        ''' <summary>
-        ''' Constructor
-        ''' </summary>
-        ''' <param name="Source">The <see cref="cCoreInputOutputBase">cCoreInputOutputBase</see> data source</param>
-        ''' <param name="VarName">The <see cref="eVarNameFlags">VarName flag</see> that defines which aspect of the Source to acces</param>
-        ''' <param name="SourceSec">An optional secundary index in the VarName, or <see cref="cCore.NULL_VALUE">cCore.NULL_VALUE</see> when irrelevant</param>
-        ''' -------------------------------------------------------------------
-        Public Sub New(ByVal Source As cCoreInputOutputBase, ByVal VarName As eVarNameFlags, _
-                Optional ByVal SourceSec As cCoreInputOutputBase = Nothing)
-            Me.New(cPropertyManager.GetInstance().GetProperty(Source, VarName, SourceSec))
-        End Sub
-
-        ''' -------------------------------------------------------------------
-        ''' <summary>
-        ''' Constructor
-        ''' </summary>
-        ''' <param name="prop">cProperty to attach to the cell</param>
-        ''' -------------------------------------------------------------------
-        Public Sub New(ByVal prop As cProperty)
-            ' Call baseclass constructor
-            MyBase.New(prop)
-            ' Set shared visualizer
-            Me.VisualModel = g_visualizer
-            Me.DataModel.EditableMode = SourceGrid2.EditableMode.None
-        End Sub
-
-#End Region ' Construction 
-
-    End Class
-
-#End Region ' Class PropertyRowHeaderParentCell 
-
-#Region " Class PropertyRowHeaderChildCell "
-
-    ''' -----------------------------------------------------------------------
-    ''' <summary>
-    ''' PropertyRowHeaderChildCell implements a PropertyRowHeaderCell rendered as an EwE name field.
-    ''' </summary>
-    ''' -----------------------------------------------------------------------
-    <CLSCompliant(False)> _
-    Public Class PropertyRowHeaderChildCell
-        : Inherits PropertyRowHeaderCell
-
-        ''' <summary>One visualizer for all cells</summary>
-        Private Shared g_visualizer As New cVisualizerEwEChildRowHeader()
-
-#Region " Construction "
-
-        ''' -------------------------------------------------------------------
-        ''' <summary>
-        ''' Constructor
-        ''' </summary>
-        ''' <param name="Source">The <see cref="cCoreInputOutputBase">cCoreInputOutputBase</see> data source</param>
-        ''' <param name="VarName">The <see cref="eVarNameFlags">VarName flag</see> that defines which aspect of the Source to acces</param>
-        ''' <param name="SourceSec">An optional secundary index in the VarName, or <see cref="cCore.NULL_VALUE">cCore.NULL_VALUE</see> when irrelevant</param>
-        ''' -------------------------------------------------------------------
-        Public Sub New(ByVal Source As cCoreInputOutputBase, ByVal VarName As eVarNameFlags, _
-                Optional ByVal SourceSec As cCoreInputOutputBase = Nothing)
-            Me.New(cPropertyManager.GetInstance().GetProperty(Source, VarName, SourceSec))
-        End Sub
-
-        ''' -------------------------------------------------------------------
-        ''' <summary>
-        ''' Constructor
-        ''' </summary>
-        ''' <param name="prop">cProperty to attach to the cell</param>
-        ''' -------------------------------------------------------------------
-        Public Sub New(ByVal prop As cProperty)
-            ' Call baseclass constructor
-            MyBase.New(prop)
-            ' Set shared visualizer
-            Me.VisualModel = g_visualizer
-            Me.DataModel.EditableMode = SourceGrid2.EditableMode.None
-        End Sub
-
-#End Region ' Construction 
-
-    End Class
-
-#End Region ' Class PropertyRowHeaderChildCell
 
 End Namespace
