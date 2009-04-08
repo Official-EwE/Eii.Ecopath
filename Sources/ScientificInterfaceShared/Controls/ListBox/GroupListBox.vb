2dbc6637fe595a854e15cfbd3cd6b29d9d9190d9
JeroenS <jeroen.steenbeek@gmail.com>
2009-04-08 16:01:52 +0000

    Made sortable
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@1298 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterfaceShared/Controls/ListBox/GroupListBox.vb
+++ Sources/ScientificInterfaceShared/Controls/ListBox/GroupListBox.vb
@@ -1,6 +1,9 @@
 '==============================================================================
 '
 ' $Log: GroupListBox.vb,v $
+' Revision 1.4  2009/04/08 16:01:52  jeroens
+' Made sortable
+'
 ' Revision 1.3  2009/04/08 15:09:10  jeroens
 ' GroupListBox -> cGroupListBox
 '
@@ -45,9 +48,26 @@ Namespace Controls
     Public Class cGroupListBox
         Inherits ListBox
 
-#Region " Helper classes "
+#Region " Public enums "
 
-#Region " LegendListBox.GroupItem "
+        ''' -------------------------------------------------------------------
+        ''' <summary>
+        ''' Supported sort styles for a cGroupListBox.
+        ''' </summary>
+        ''' -------------------------------------------------------------------
+        Public Enum eSortType As Byte
+            GroupIndexAsc = 0
+            GroupIndexDesc
+            GroupNameAsc
+            GroupNameDesc
+            ValueAsc
+            ValueDesc
+            [Default] = GroupIndexAsc
+        End Enum
+
+#End Region ' Public enums
+
+#Region " cGroupItem "
 
         ''' -----------------------------------------------------------------------
         ''' <summary>
@@ -58,8 +78,8 @@ Namespace Controls
 
             ''' <summary>Group to show.</summary>
             Private m_group As cCoreGroupBase = Nothing
-            ''' <summary>States if group must be displayed as relevant.</summary>
-            Private m_bIsRelevant As Boolean = True
+            ''' <summary>A value to sort by.</summary>
+            Private m_sValue As Single = 0.0
 
             ''' ---------------------------------------------------------------
             ''' <summary>
@@ -69,7 +89,6 @@ Namespace Controls
             ''' ---------------------------------------------------------------
             Public Sub New(ByVal group As cCoreGroupBase)
                 Me.m_group = group
-                Me.m_bIsRelevant = True
             End Sub
 
             ''' ---------------------------------------------------------------
@@ -96,28 +115,28 @@ Namespace Controls
 
             ''' ---------------------------------------------------------------
             ''' <summary>
-            ''' Get/set whether a group should be displayed as relevant.
+            ''' Get/set the sort value for this item.
             ''' </summary>
             ''' ---------------------------------------------------------------
-            Public Property IsRelevant() As Boolean
+            Public Property SortValue() As Single
                 Get
-                    Return Me.m_bIsRelevant
+                    Return Me.m_sValue
                 End Get
-                Set(ByVal bIsRelevant As Boolean)
-                    Me.m_bIsRelevant = bIsRelevant
+                Set(ByVal sSortValue As Single)
+                    Me.m_sValue = sSortValue
                 End Set
             End Property
 
         End Class
 
-#End Region ' LegendListBox.Item
-
-#End Region ' Helper classes
+#End Region ' cGroupItem
 
 #Region " Privates "
 
         Private m_core As cCore = Nothing
         Private m_sg As StyleGuide = Nothing
+        Private m_sortType As eSortType = eSortType.Default
+        Private m_sSortThreshold As Single = cCore.NULL_VALUE
 
 #End Region ' Privates
 
@@ -136,11 +155,19 @@ Namespace Controls
 
             ' This box draws its own items
             Me.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed
+            ' This control always sorts
+            Me.Sorted = True
 
         End Sub
 
-        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            MyBase.Dispose(disposing)
+        ''' ---------------------------------------------------------------
+        ''' <summary>
+        ''' Thrash me.
+        ''' </summary>
+        ''' <param name="bDisposing"></param>
+        ''' ---------------------------------------------------------------
+        Protected Overrides Sub Dispose(ByVal bDisposing As Boolean)
+            MyBase.Dispose(bDisposing)
 
             If (Me.m_core IsNot Nothing) Then
                 RemoveHandler m_sg.StyleGuideChanged, AddressOf OnStyleGuideChanged
@@ -148,6 +175,38 @@ Namespace Controls
                 Me.m_core = Nothing
             End If
         End Sub
+
+        ''' ---------------------------------------------------------------
+        ''' <summary>
+        ''' Get/set how to sort the data in this list box.
+        ''' </summary>
+        ''' ---------------------------------------------------------------
+        Public Property SortType() As eSortType
+            Get
+                Return Me.m_sortType
+            End Get
+            Set(ByVal sortType As eSortType)
+                Me.m_sortType = sortType
+                Me.Sort()
+            End Set
+        End Property
+
+        ''' ---------------------------------------------------------------
+        ''' <summary>
+        ''' Get/set the value that sort values have to exceed.
+        ''' </summary>
+        ''' ---------------------------------------------------------------
+        Public Property SortThreshold() As Single
+            Get
+                Return Me.m_sSortThreshold
+            End Get
+            Set(ByVal sSortThreshold As Single)
+                Me.m_sSortThreshold = sSortThreshold
+                Me.Sort()
+            End Set
+        End Property
+
+#Region " Internals "
 
         ''' -----------------------------------------------------------------------
         ''' <summary>
@@ -174,7 +233,7 @@ Namespace Controls
                     clrLegend = SystemColors.Control
                     clrText = SystemColors.ControlDark
                     ' Allowed to display and colour group?
-                    If Me.m_sg.GroupVisible(gi.Group.Index) And gi.IsRelevant Then
+                    If Me.m_sg.GroupVisible(gi.Group.Index) And gi.SortValue >= Me.SortThreshold Then
                         ' #Yes: display group in full color
                         clrText = e.ForeColor
                         clrLegend = Me.m_sg.GroupColor(Me.m_core, gi.Group.Index)
@@ -209,6 +268,136 @@ Namespace Controls
         Private Sub OnStyleGuideChanged(ByVal ct As StyleGuide.eChangeType)
             Me.Invalidate()
         End Sub
+
+        ''' ---------------------------------------------------------------
+        ''' <summary>
+        ''' Sort me!
+        ''' </summary>
+        ''' ---------------------------------------------------------------
+        Protected Overrides Sub Sort()
+
+            Dim objSwap As Object = Nothing
+            Dim iCounter As Integer = 0
+            Dim bSwapped As Boolean = False
+
+            If (Items.Count > 1) Then
+                ' Bubble away
+                Do
+                    ' Reset bubble loop
+                    iCounter = Items.Count - 1
+                    bSwapped = False
+                    ' Bubble deeper
+                    While ((iCounter - 1) > 0)
+
+                        ' Need to swap items based on current sort order?
+                        If Me.Compare(Items(iCounter - 1), Items(iCounter)) = 1 Then
+                            ' #Yes: swap the items.
+                            objSwap = Items(iCounter)
+                            Items(iCounter) = Items(iCounter - 1)
+                            Items(iCounter - 1) = objSwap
+                            bSwapped = True
+                        End If
+                        ' Decrement the counter.
+                        iCounter -= 1
+                    End While
+                Loop While (bSwapped = True)
+            End If
+
+        End Sub
+
+        ''' -------------------------------------------------------------------
+        ''' <summary>
+        ''' Compare two items in the listbox for sorting.
+        ''' </summary>
+        ''' <param name="i1"></param>
+        ''' <param name="i2"></param>
+        ''' <returns>
+        ''' <list>
+        ''' <item><description>-1 if i1 is less than i2</description></item>
+        ''' <item><description>0 if i1 equals i2</description></item>
+        ''' <item><description>1 if i1 is greater than i2</description></item>
+        ''' </list>
+        ''' </returns>
+        ''' -------------------------------------------------------------------
+        Protected Overridable Function Compare(ByVal i1 As Object, ByVal i2 As Object) As Integer
+            Dim gi1 As cGroupItem = Nothing
+            Dim group1 As cCoreGroupBase = Nothing
+            Dim gi2 As cGroupItem = Nothing
+            Dim group2 As cCoreGroupBase = Nothing
+
+            ' Get sortable items
+            If TypeOf (i1) Is cGroupItem Then gi1 = DirectCast(i1, cGroupItem) : group1 = gi1.Group
+            If TypeOf (i2) Is cGroupItem Then gi2 = DirectCast(i2, cGroupItem) : group2 = gi2.Group
+
+            ' Weed out any incompatible item comparisons
+            If (gi1 Is Nothing) Then
+                If (gi2 Is Nothing) Then
+                    ' Not sortable
+                    Return 0
+                Else
+                    ' Non-group item sorts before group item
+                    Return 1
+                End If
+            Else
+                If (gi2 Is Nothing) Then
+                    ' Non-group item sorts before group item
+                    Return -1
+                End If
+            End If
+
+            ' Ok, two cGroupItems to compare
+            ' Do both have groups attached?
+            If (group1 Is Nothing) Then
+                If (group2 Is Nothing) Then
+                    ' Not sortable
+                    Return 0
+                Else
+                    ' Non-group item sorts before group item
+                    Return 1
+                End If
+            Else
+                If (group2 Is Nothing) Then
+                    ' Non-group item sorts before group item
+                    Return -1
+                End If
+            End If
+
+            ' Ok, we have two valid groups to compare!
+            Select Case Me.m_sortType
+
+                Case eSortType.GroupIndexAsc
+                    If group1.Index < group2.Index Then Return -1
+                    If group1.Index = group2.Index Then Return 0
+                    Return 1
+
+                Case eSortType.GroupIndexDesc
+                    If group1.Index > group2.Index Then Return -1
+                    If group1.Index = group2.Index Then Return 0
+                    Return 1
+
+                Case eSortType.GroupNameAsc
+                    Return String.Compare(group1.Name, group2.Name)
+
+                Case eSortType.GroupNameDesc
+                    Return String.Compare(group2.Name, group1.Name)
+
+                Case eSortType.ValueAsc
+                    If gi1.SortValue < gi2.SortValue Then Return -1
+                    If gi1.SortValue = gi2.SortValue Then Return 0
+                    Return 1
+
+                Case eSortType.ValueDesc
+                    If gi1.SortValue > gi2.SortValue Then Return -1
+                    If gi1.SortValue = gi2.SortValue Then Return 0
+                    Return 1
+
+            End Select
+
+            Return 0
+
+        End Function
+
+#End Region ' Internals
 
     End Class
 
