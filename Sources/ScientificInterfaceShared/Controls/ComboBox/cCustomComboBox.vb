3eb3f88f4ed6f1322ec26cfe74eb03734cf629af
JeroenS <jeroen.steenbeek@gmail.com>
2009-04-24 13:58:42 +0000

    Me
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@1410 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterfaceShared/Controls/ComboBox/cCustomComboBox.vb
+++ Sources/ScientificInterfaceShared/Controls/ComboBox/cCustomComboBox.vb
@@ -1,6 +1,9 @@
 '==============================================================================
 '
 ' $Log: cCustomComboBox.vb,v $
+' Revision 1.2  2009/04/24 13:58:42  jeroens
+' Me
+'
 ' Revision 1.1  2009/02/24 03:47:36  jeroens
 ' Renamed
 '
@@ -31,7 +34,8 @@ Namespace Controls
     ''' ---------------------------------------------------------------------------
     ''' <summary>
     ''' <para>ComboBox-derived class that drops down any custom control.</para>
-    ''' <para>This class was based on the Custom ComboBox by Jaredpar, http://blogs.msdn.com/jaredpar/archive/2006/10/13/custom-combobox.aspx</para>
+    ''' <para>This class was based on the Custom ComboBox by Jaredpar, 
+    ''' http://blogs.msdn.com/jaredpar/archive/2006/10/13/custom-combobox.aspx</para>
     ''' </summary>
     ''' ---------------------------------------------------------------------------
     Public Class CustomComboBox
@@ -49,22 +53,23 @@ Namespace Controls
             InitializeComponent()
 
             ' Setup the form to display the control
-            m_form = New Form()
-            m_form.StartPosition = FormStartPosition.Manual
-            m_form.FormBorderStyle = FormBorderStyle.None
-            m_form.Hide()
-            m_form.ShowInTaskbar = False
+            Me.m_form = New Form()
+            Me.m_form.StartPosition = FormStartPosition.Manual
+            Me.m_form.FormBorderStyle = FormBorderStyle.None
+            Me.m_form.Hide()
+            Me.m_form.ShowInTaskbar = False
 
-            Me.DropdownControl = New Control()   ' Default Control
-            m_dropDownHeight = Me.DropDownHeight
-            Me.DropDownHeight = 1       ' Prevent the DropDown from showing
+            Me.DropdownControl = New Control()
+            Me.m_dropDownHeight = Me.DropDownHeight
+            ' Prevent the DropDown from showing
+            Me.DropDownHeight = 1
         End Sub
 
         Protected Overrides Sub OnDropDown(ByVal e As System.EventArgs)
             MyBase.OnDropDown(e)
 
-            If Not m_form.Visible Then
-                DisplayControl()
+            If Not Me.m_form.Visible Then
+                Me.DisplayControl()
             End If
 
             Me.DroppedDown = False
@@ -74,10 +79,10 @@ Namespace Controls
             Dim loc As Point = Me.PointToScreen(Point.Empty)
             loc.Y += Me.Height
 
-            m_form.Location = loc
-            m_form.Width = Me.Width
-            m_form.Height = Me.m_dropDownHeight
-            m_form.Show()
+            Me.m_form.Location = loc
+            Me.m_form.Width = Me.Width
+            Me.m_form.Height = Me.m_dropDownHeight
+            Me.m_form.Show()
         End Sub
 
         Public Property DropdownControl() As Control
@@ -86,25 +91,25 @@ Namespace Controls
             End Get
             Set(ByVal value As Control)
                 If Not m_control Is Nothing Then
-                    m_form.Controls.Remove(m_control)
+                    Me.m_form.Controls.Remove(m_control)
                     RemoveHandler m_control.LostFocus, AddressOf Me.OnControlLostFocus
                     RemoveHandler m_control.DoubleClick, AddressOf Me.OnControlDoubleClick
                 End If
 
-                m_control = value
-                m_control.Dock = DockStyle.Fill
+                Me.m_control = value
+                Me.m_control.Dock = DockStyle.Fill
                 AddHandler m_control.LostFocus, AddressOf Me.OnControlLostFocus
                 AddHandler m_control.DoubleClick, AddressOf Me.OnControlDoubleClick
-                m_form.Controls.Add(m_control)
+                Me.m_form.Controls.Add(m_control)
             End Set
         End Property
 
         Private Sub OnControlLostFocus(ByVal sender As Object, ByVal e As EventArgs)
-            m_form.Hide()
+            Me.m_form.Hide()
         End Sub
 
         Private Sub OnControlDoubleClick(ByVal sender As Object, ByVal e As EventArgs)
-            m_form.Hide()
+            Me.m_form.Hide()
         End Sub
 
     End Class
