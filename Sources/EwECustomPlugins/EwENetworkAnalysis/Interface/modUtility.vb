d648366d1ec929cced78f5ffab4e97cc9e039d7f
JeroenS <jeroen.steenbeek@gmail.com>
2009-04-28 16:20:55 +0000

    Fixed graph max axis Graph styling done with ZedGraphHelper Uses true roman number converter
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@1428 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECustomPlugins/EwENetworkAnalysis/Interface/modUtility.vb
+++ Sources/EwECustomPlugins/EwENetworkAnalysis/Interface/modUtility.vb
@@ -1,6 +1,11 @@
 '==============================================================================
 '
 ' $Log: modUtility.vb,v $
+' Revision 1.3  2009/04/28 16:20:55  jeroens
+' Fixed graph max axis
+' Graph styling done with ZedGraphHelper
+' Uses true roman number converter
+'
 ' Revision 1.2  2008/11/25 05:47:34  joeh
 ' Copy and paste in cells of data grid view
 '
@@ -44,6 +49,7 @@ Imports ZedGraph
 Imports System.Drawing
 Imports System.IO
 Imports System.Reflection
+Imports System.Text
 
 Module modUtility
     Public Const DEFAULT_COL_WIDTH As Integer = 70
@@ -54,52 +60,123 @@ Module modUtility
     'Public Declare Function FindWindow& Lib "user32" Alias "FindWindowA" (ByVal lpClassName As String, ByVal lpWindowName As String)
     Public Declare Function FindWindow Lib "user32" Alias "FindWindowA" (ByVal lpClassName As String, ByVal lpWindowName As String) As Integer
 
+    Public Function CRoman(ByVal nArabicValue As Integer) As String
 
+        Dim nThousands As Integer
+        Dim nFiveHundreds As Integer
+        Dim nHundreds As Integer
+        Dim nFifties As Integer
+        Dim nTens As Integer
+        Dim nFives As Integer
+        Dim nOnes As Integer
+        Dim sbNumber As New StringBuilder()
 
-    Public Function CRoman(ByVal intArabic As Integer) As String
-        Select Case intArabic
-            Case 1
-                Return "I"
-            Case 2
-                Return "II"
-            Case 3
-                Return "III"
-            Case 4
-                Return "IV"
-            Case 5
-                Return "V"
-            Case 6
-                Return "VI"
-            Case 7
-                Return "VII"
-            Case 8
-                Return "VIII"
-            Case 9
-                Return "IX"
-            Case 10
-                Return "X"
-            Case 11
-                Return "XI"
-            Case 12
-                Return "XII"
-            Case 13
-                Return "XIII"
-            Case 14
-                Return "XIV"
-            Case 15
-                Return "XV"
-            Case 16
-                Return "XVI"
-            Case 17
-                Return "XVII"
-            Case 18
-                Return "XIX"
-            Case 19
-                Return "XX"
-            Case Else
-                Return "??"
-        End Select
+        'take the value passed and split it out
+        'to values representing the number of
+        'ones, tens, hundreds, etc
+        nOnes = nArabicValue
+        nThousands = nOnes \ 1000
+        nOnes = nOnes - nThousands * 1000
+        nFiveHundreds = nOnes \ 500
+        nOnes = nOnes - nFiveHundreds * 500
+        nHundreds = nOnes \ 100
+        nOnes = nOnes - nHundreds * 100
+        nFifties = nOnes \ 50
+        nOnes = nOnes - nFifties * 50
+        nTens = nOnes \ 10
+        nOnes = nOnes - nTens * 10
+        nFives = nOnes \ 5
+        nOnes = nOnes - nFives * 5
+
+        'using VB's String function, create
+        'a series of strings representing
+        'the number of each respective denomination
+        sbNumber.Append(New String("M"c, nThousands))
+
+        'handle those cases where the denominator
+        'value is on either side of a roman numeral
+        If nHundreds = 4 Then
+            If nFiveHundreds = 1 Then
+                sbNumber.Append("CM")
+            Else
+                sbNumber.Append("CD")
+            End If
+        Else
+            'not a 4, so create the string
+            sbNumber.Append(New String("D"c, nFiveHundreds))
+            sbNumber.Append(New String("C"c, nHundreds))
+        End If
+
+        If nTens = 4 Then
+            If nFifties = 1 Then
+                sbNumber.Append("XC")
+            Else
+                sbNumber.Append("XL")
+            End If
+        Else
+            sbNumber.Append(New String("L"c, nFifties))
+            sbNumber.Append(New String("X"c, nTens))
+        End If
+
+        If nOnes = 4 Then
+            If nFives = 1 Then
+                sbNumber.Append("IX")
+            Else
+                sbNumber.Append("IV")
+            End If
+        Else
+            sbNumber.Append(New String("V"c, nFives))
+            sbNumber.Append(New String("I"c, nOnes))
+        End If
+
+        Return sbNumber.ToString()
+
     End Function
+
+    'Public Function CRoman2_ORG(ByVal intArabic As Integer) As String
+    '    Select Case intArabic
+    '        Case 1
+    '            Return "I"
+    '        Case 2
+    '            Return "II"
+    '        Case 3
+    '            Return "III"
+    '        Case 4
+    '            Return "IV"
+    '        Case 5
+    '            Return "V"
+    '        Case 6
+    '            Return "VI"
+    '        Case 7
+    '            Return "VII"
+    '        Case 8
+    '            Return "VIII"
+    '        Case 9
+    '            Return "IX"
+    '        Case 10
+    '            Return "X"
+    '        Case 11
+    '            Return "XI"
+    '        Case 12
+    '            Return "XII"
+    '        Case 13
+    '            Return "XIII"
+    '        Case 14
+    '            Return "XIV"
+    '        Case 15
+    '            Return "XV"
+    '        Case 16
+    '            Return "XVI"
+    '        Case 17
+    '            Return "XVII"
+    '        Case 18
+    '            Return "XIX"
+    '        Case 19
+    '            Return "XX"
+    '        Case Else
+    '            Return "??"
+    '    End Select
+    'End Function
 
     Public Sub SetGridColumnPropertyDefault(ByVal DataGrid As Windows.Forms.DataGridView)
         DataGrid.ColumnHeadersVisible = False
@@ -135,31 +212,18 @@ Module modUtility
         Return CBool(IIf(Found = 0, False, True))
     End Function
 
-    Public Sub InitializePane(ByVal Pane As GraphPane, ByVal XAxisTitle As String, ByVal YAxisTitle As String)
-        Pane.Title.Text = ""
-        Pane.XAxis.Title.Text = XAxisTitle
-        Pane.XAxis.Title.FontSpec.Size = 17
-        Pane.XAxis.MajorTic.IsOutside = False
-        Pane.XAxis.MinorTic.IsOutside = False
-        Pane.YAxis.Title.Text = YAxisTitle
-        Pane.YAxis.Title.FontSpec.Size = 17
-        Pane.YAxis.MajorTic.IsOutside = False
-        Pane.YAxis.MinorTic.IsOutside = False
-        Pane.YAxis.MinorTic.IsAllTics = False
-        Pane.Legend.Position = LegendPos.TopCenter
-        Pane.Legend.FontSpec.Size = 15
-    End Sub
-
-    Public Sub AddCurve(ByVal CurveName As String, ByVal CurveVar() As Single, ByVal Pane As GraphPane, _
+    Public Sub AddCurve(ByVal strName As String, ByVal CurveVar() As Single, ByVal Pane As GraphPane, _
       ByVal MyColor As Color, Optional ByVal Symbol As SymbolType = SymbolType.None)
         Dim List As PointPairList
-        Dim Curve As LineItem
+        Dim iNumPoints As Integer = CurveVar.GetUpperBound(0)
 
         List = New PointPairList()
-        For iTime As Integer = 1 To CurveVar.GetUpperBound(0)
+        For iTime As Integer = 1 To iNumPoints
             List.Add(iTime, CurveVar(iTime))
         Next
-        Curve = Pane.AddCurve(CurveName, List, MyColor, Symbol)
+        Pane.AddCurve(strName, List, MyColor, Symbol)
+
+        Pane.XAxis.Scale.Max = iNumPoints
     End Sub
 
 End Module
