192c1d0cd647e4851a3c7293297d425cac902765
JeroenS <jeroen.steenbeek@gmail.com>
2009-05-01 17:41:42 +0000

    Initial version
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@1448 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/EwECustomPlugins/EwENetworkAnalysis/Controls/ucPlot.vb
@@ -0,0 +1,18 @@
+﻿Imports ScientificInterfaceShared
+
+Public Class ucPlot
+
+    Public Sub New()
+
+        Me.InitializeComponent()
+        'Me.SetStyle(Windows.Forms.ControlStyles.AllPaintingInWmPaint, True)
+        'Me.SetStyle(Windows.Forms.ControlStyles.OptimizedDoubleBuffer, True)
+        'Me.SetStyle(Windows.Forms.ControlStyles.ResizeRedraw, True)
+
+    End Sub
+
+    'Protected Overrides Sub OnPaint(ByVal e As System.Windows.Forms.PaintEventArgs)
+    '    ' MyBase.OnPaint(e)
+    'End Sub
+
+End Class
---
+++ Sources/EwECustomPlugins/EwENetworkAnalysis/Interface/cContentManager.vb
@@ -0,0 +1,228 @@
+﻿'==============================================================================
+'
+' $Log: cContentManager.vb,v $
+' Revision 1.1  2009/05/01 17:41:42  jeroens
+' Initial version
+'
+'==============================================================================
+
+#Region " Imports "
+
+Option Strict On
+Imports System.Windows.Forms
+Imports ZedGraph
+
+#End Region ' Imports
+
+''' ---------------------------------------------------------------------------
+''' <summary>
+''' Class for populating NA view controls.
+''' </summary>
+''' ---------------------------------------------------------------------------
+Public MustInherit Class cContentManager
+
+#Region " Private variables "
+
+    ''' <summary></summary>
+    Private m_manager As cNetworkManager = Nothing
+    ''' <summary></summary>
+    Private m_graph As ZedGraphControl = Nothing
+    ''' <summary></summary>
+    Private m_plot As ucPlot = Nothing
+    ''' <summary></summary>
+    Private m_datagrid As DataGridView = Nothing
+
+#End Region ' Private variables
+
+#Region " Attach / detach "
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="manager"></param>
+    ''' <param name="datagrid"></param>
+    ''' <param name="graph"></param>
+    ''' <param name="plot"></param>
+    ''' <remarks>
+    ''' The default implementation stores all controls and hides them.
+    ''' </remarks>
+    ''' -----------------------------------------------------------------------
+    Public Overridable Sub Attach(ByVal manager As cNetworkManager, _
+                                  ByVal datagrid As DataGridView, _
+                                  ByVal graph As ZedGraphControl, _
+                                  ByVal plot As ucPlot)
+
+        ' Store all references
+        Me.m_manager = manager
+        Me.m_datagrid = datagrid
+        Me.m_graph = graph
+        Me.m_plot = plot
+
+        ' Hide all controls
+        Me.Graph.Visible = False
+        Me.Plot.Visible = False
+        Me.DataGrid.Visible = False
+        Me.Plot.Visible = False
+
+        ' Reset grid
+        Me.DataGrid.Rows.Clear()
+        Me.DataGrid.Columns.Clear()
+        Me.DataGrid.ReadOnly = True
+
+    End Sub
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <remarks>
+    ''' The default implementation hides all controls and then releases them.
+    ''' </remarks>
+    ''' -----------------------------------------------------------------------
+    Public Overridable Sub Detach()
+
+        ' Hide all controls
+        Me.Graph.Visible = False
+        Me.Plot.Visible = False
+        Me.DataGrid.Visible = False
+        Me.Plot.Visible = False
+
+        Me.m_manager = Nothing
+        Me.m_datagrid = Nothing
+        Me.m_graph = Nothing
+        Me.m_plot = Plot
+
+    End Sub
+
+#End Region ' Attach / detach
+
+#Region " Overrides "
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Public MustOverride Sub DisplayData()
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Update a 'view' to a selection.
+    ''' </summary>
+    ''' <param name="iGroup1">Index of the first selected group, if any. This
+    ''' value is one-based.</param>
+    ''' <param name="iGroup2">Index of the second selected group, if any. This
+    ''' value is one-based.</param>
+    ''' -----------------------------------------------------------------------
+    Public Overridable Sub UpdateData(ByVal iGroup1 As Integer, ByVal iGroup2 As Integer)
+
+    End Sub
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Public Overridable Sub ClearData()
+    End Sub
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' States whether a view requires a toolstrip.
+    ''' </summary>
+    ''' <returns>True if a toolstrip is required.</returns>
+    ''' <remarks>
+    ''' The default implementation will not require a toolstrip.
+    ''' </remarks>
+    ''' -----------------------------------------------------------------------
+    Public Overridable Function RequiresToolstrip() As Boolean
+        Return False
+    End Function
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Update toolstrip controls
+    ''' </summary>
+    ''' <param name="ts"></param>
+    ''' <remarks>
+    ''' The default implementation will hide all toolstrip controls.
+    ''' </remarks>
+    ''' -----------------------------------------------------------------------
+    Public Overridable Sub SetupToolstrip(ByVal ts As ToolStrip)
+        For Each tsi As ToolStripItem In ts.Items
+            tsi.Visible = False
+        Next
+    End Sub
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Implement to save the content of a view to a EMF file.
+    ''' </summary>
+    ''' <param name="strFileName"></param>
+    ''' -----------------------------------------------------------------------
+    Public Overridable Sub SaveToEMF(ByVal strFileName As String)
+        ' NOP
+    End Sub
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Implement to save the content of a view to a CSV file.
+    ''' </summary>
+    ''' <param name="strFileName"></param>
+    ''' -----------------------------------------------------------------------
+    Public Overridable Sub SaveToCSV(ByVal strFileName As String)
+
+    End Sub
+
+#End Region ' Overrides
+
+#Region " Properties "
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Return the one and only network manager.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Protected ReadOnly Property NetworkManager() As cNetworkManager
+        Get
+            Return Me.m_manager
+        End Get
+    End Property
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Return the one and only data grid.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Protected ReadOnly Property DataGrid() As DataGridView
+        Get
+            Return Me.m_datagrid
+        End Get
+    End Property
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Return the one and only graph.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Protected ReadOnly Property Graph() As ZedGraphControl
+        Get
+            Return Me.m_graph
+        End Get
+    End Property
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Return the one and only plot.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Protected ReadOnly Property Plot() As ucPlot
+        Get
+            Return Me.m_plot
+        End Get
+    End Property
+
+#End Region ' Properties
+
+End Class
