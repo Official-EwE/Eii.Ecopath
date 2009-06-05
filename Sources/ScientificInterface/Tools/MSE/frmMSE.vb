03690418abb797b18311d6505c877b0bbd832dd3
joeb <joeb@ecopath.org>
2009-06-05 16:47:56 +0000

    Added MSE directories and form
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@1655 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/ScientificInterface/Tools/MSE/frmMSE.vb
@@ -0,0 +1,17 @@
+﻿
+#Region " Imports "
+
+Option Strict On
+Option Explicit On
+
+Imports EwECore
+Imports EwECore.FishingPolicy
+Imports EwECore.SearchObjectives
+Imports ScientificInterface.Controls
+Imports EwEUtils.Core
+
+#End Region
+
+Public Class frmMSE
+
+End Class
