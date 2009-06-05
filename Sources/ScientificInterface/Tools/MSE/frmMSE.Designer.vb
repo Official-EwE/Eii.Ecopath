6255cec9657e976196e6c52356a1ffffba6dd374
joeb <joeb@ecopath.org>
2009-06-05 20:20:31 +0000

    MSE
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@1658 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterface/Tools/MSE/frmMSE.Designer.vb
+++ Sources/ScientificInterface/Tools/MSE/frmMSE.Designer.vb
@@ -25,8 +25,152 @@ Partial Class frmMSE
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.components = New System.ComponentModel.Container
+        Me.btRun = New System.Windows.Forms.Button
+        Me.prgProgress = New System.Windows.Forms.ProgressBar
+        Me.txNTrials = New System.Windows.Forms.TextBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.lbParams = New System.Windows.Forms.Label
+        Me.tbOutput = New System.Windows.Forms.TabControl
+        Me.pgGraphs = New System.Windows.Forms.TabPage
+        Me.pgRisk = New System.Windows.Forms.TabPage
+        Me.pgPerformance = New System.Windows.Forms.TabPage
+        Me.zdGraph = New ZedGraph.ZedGraphControl
+        Me.tbOutput.SuspendLayout()
+        Me.pgGraphs.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btRun
+        '
+        Me.btRun.Location = New System.Drawing.Point(12, 88)
+        Me.btRun.Name = "btRun"
+        Me.btRun.Size = New System.Drawing.Size(93, 20)
+        Me.btRun.TabIndex = 0
+        Me.btRun.Text = "Run"
+        Me.btRun.UseVisualStyleBackColor = True
+        '
+        'prgProgress
+        '
+        Me.prgProgress.Location = New System.Drawing.Point(12, 125)
+        Me.prgProgress.Name = "prgProgress"
+        Me.prgProgress.Size = New System.Drawing.Size(195, 18)
+        Me.prgProgress.TabIndex = 1
+        '
+        'txNTrials
+        '
+        Me.txNTrials.Location = New System.Drawing.Point(111, 42)
+        Me.txNTrials.Name = "txNTrials"
+        Me.txNTrials.Size = New System.Drawing.Size(63, 20)
+        Me.txNTrials.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(80, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Number of trials"
+        '
+        'lbParams
+        '
+        Me.lbParams.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.lbParams.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbParams.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.lbParams.Location = New System.Drawing.Point(1, 0)
+        Me.lbParams.Name = "lbParams"
+        Me.lbParams.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.lbParams.Size = New System.Drawing.Size(243, 21)
+        Me.lbParams.TabIndex = 4
+        Me.lbParams.Text = "Parameters"
+        Me.lbParams.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'tbOutput
+        '
+        Me.tbOutput.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.tbOutput.Controls.Add(Me.pgGraphs)
+        Me.tbOutput.Controls.Add(Me.pgRisk)
+        Me.tbOutput.Controls.Add(Me.pgPerformance)
+        Me.tbOutput.Location = New System.Drawing.Point(259, 0)
+        Me.tbOutput.Name = "tbOutput"
+        Me.tbOutput.SelectedIndex = 0
+        Me.tbOutput.Size = New System.Drawing.Size(769, 597)
+        Me.tbOutput.TabIndex = 5
+        '
+        'pgGraphs
+        '
+        Me.pgGraphs.Controls.Add(Me.zdGraph)
+        Me.pgGraphs.Location = New System.Drawing.Point(4, 22)
+        Me.pgGraphs.Name = "pgGraphs"
+        Me.pgGraphs.Padding = New System.Windows.Forms.Padding(3)
+        Me.pgGraphs.Size = New System.Drawing.Size(761, 571)
+        Me.pgGraphs.TabIndex = 0
+        Me.pgGraphs.Text = "Graphs"
+        Me.pgGraphs.UseVisualStyleBackColor = True
+        '
+        'pgRisk
+        '
+        Me.pgRisk.Location = New System.Drawing.Point(4, 22)
+        Me.pgRisk.Name = "pgRisk"
+        Me.pgRisk.Padding = New System.Windows.Forms.Padding(3)
+        Me.pgRisk.Size = New System.Drawing.Size(761, 571)
+        Me.pgRisk.TabIndex = 1
+        Me.pgRisk.Text = "Risk"
+        Me.pgRisk.UseVisualStyleBackColor = True
+        '
+        'pgPerformance
+        '
+        Me.pgPerformance.Location = New System.Drawing.Point(4, 22)
+        Me.pgPerformance.Name = "pgPerformance"
+        Me.pgPerformance.Size = New System.Drawing.Size(761, 571)
+        Me.pgPerformance.TabIndex = 2
+        Me.pgPerformance.Text = "Performance"
+        Me.pgPerformance.UseVisualStyleBackColor = True
+        '
+        'zdGraph
+        '
+        Me.zdGraph.Location = New System.Drawing.Point(-4, 0)
+        Me.zdGraph.Name = "zdGraph"
+        Me.zdGraph.ScrollGrace = 0
+        Me.zdGraph.ScrollMaxX = 0
+        Me.zdGraph.ScrollMaxY = 0
+        Me.zdGraph.ScrollMaxY2 = 0
+        Me.zdGraph.ScrollMinX = 0
+        Me.zdGraph.ScrollMinY = 0
+        Me.zdGraph.ScrollMinY2 = 0
+        Me.zdGraph.Size = New System.Drawing.Size(764, 570)
+        Me.zdGraph.TabIndex = 0
+        '
+        'frmMSE
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1028, 595)
+        Me.Controls.Add(Me.tbOutput)
+        Me.Controls.Add(Me.lbParams)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txNTrials)
+        Me.Controls.Add(Me.prgProgress)
+        Me.Controls.Add(Me.btRun)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "frmMSE"
         Me.Text = "frmMSE"
+        Me.tbOutput.ResumeLayout(False)
+        Me.pgGraphs.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents btRun As System.Windows.Forms.Button
+    Friend WithEvents prgProgress As System.Windows.Forms.ProgressBar
+    Friend WithEvents txNTrials As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lbParams As System.Windows.Forms.Label
+    Friend WithEvents tbOutput As System.Windows.Forms.TabControl
+    Friend WithEvents pgGraphs As System.Windows.Forms.TabPage
+    Friend WithEvents pgRisk As System.Windows.Forms.TabPage
+    Friend WithEvents pgPerformance As System.Windows.Forms.TabPage
+    Friend WithEvents zdGraph As ZedGraph.ZedGraphControl
 End Class
--- Sources/ScientificInterface/Tools/MSE/frmMSE.vb
+++ Sources/ScientificInterface/Tools/MSE/frmMSE.vb
@@ -2,6 +2,9 @@
 '==============================================================================
 '
 ' $Log: frmMSE.vb,v $
+' Revision 1.3  2009/06/05 20:20:31  joeb
+' MSE
+'
 ' Revision 1.2  2009/06/05 19:01:50  joeb
 ' Added MSE node to navigation tree
 '
@@ -13,15 +16,36 @@ Option Strict On
 Option Explicit On
 
 Imports EwECore
-Imports EwECore.FishingPolicy
+Imports EwECore.MSE
 Imports EwECore.SearchObjectives
 Imports ScientificInterface.Controls
 Imports EwEUtils.Core
+
+Imports ZedGraph
 
 #End Region
 
 Public Class frmMSE
 
+    Private Enum eMSEStates
+        InActive
+        Running
+        Completed
+    End Enum
+
+#Region "Private variables"
+
+    Dim m_core As cCore
+    Dim m_MSE As cMSEManager
+
+    Private m_fpNTrials As cPropertyFormatProvider
+
+    Private m_paneMaster As MasterPane = Nothing
+    Private m_sg As cStyleGuide = Nothing
+    Private m_zgh As cZedGraphHelper = Nothing
+
+
+#End Region
 
 #Region "Construction Initialization and Distruction"
 
@@ -31,6 +55,11 @@ Public Class frmMSE
         InitializeComponent()
         ' Add any initialization after the InitializeComponent() call.
 
+        Me.m_core = cCore.GetInstance
+        Me.m_MSE = Me.m_core.MSEManager
+
+        Me.m_fpNTrials = New cPropertyFormatProvider(Me.txNTrials, Me.m_MSE.ModelParameters, eVarNameFlags.MSENTrials)
+
         Me.CoreComponents = New eCoreComponentType() {eCoreComponentType.MSE, eCoreComponentType.SearchObjective}
 
     End Sub
@@ -39,19 +68,25 @@ Public Class frmMSE
         MyBase.OnFormClosing(e)
 
         Me.CoreComponents = Nothing
+        Me.m_MSE.Disconnect()
 
     End Sub
 
     Private Sub OnfrmMSELoad(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
+        Me.m_paneMaster = Me.zdGraph.MasterPane
+        Me.m_zgh = New cZedGraphHelper()
+        Me.m_zgh.Attach(Me.m_core, Me.zdGraph, Me.m_core.nGroups)
+
+        Me.LoadGraphPanes()
+
+        Me.UpdateControls(eMSEStates.InActive)
+
     End Sub
 
 #End Region
 
-
 #Region "Core interactions"
-
-
 
     Public Overrides Sub OnCoreMessage(ByVal msg As cMessage)
 
@@ -60,5 +95,125 @@ Public Class frmMSE
 
 #End Region
 
+#Region "MSE interactions"
+
+    Private Sub runMSE()
+
+        Me.m_MSE.Connect(AddressOf Me.onMSECallBack)
+
+        Me.prgProgress.Maximum = Me.m_MSE.ModelParameters.NTrials
+        Me.prgProgress.Value = 0
+
+        Me.m_MSE.Run()
+
+    End Sub
+
+
+    Private Sub onMSECompleted()
+
+        Me.prgProgress.Value = 0
+        Me.m_MSE.Disconnect()
+
+    End Sub
+
+    Private Sub onMSECallBack(ByVal CallBackType As MSE.eCallBackTypes)
+
+        Dim state As eMSEStates
+        Select Case CallBackType
+
+            Case eCallBackTypes.Started
+                state = eMSEStates.Running
+
+            Case eCallBackTypes.IterationCompleted
+                Me.onMSEProgress()
+
+            Case eCallBackTypes.RunCompleted
+                Me.onMSECompleted()
+                state = eMSEStates.Completed
+
+        End Select
+
+        UpdateControls(state)
+
+    End Sub
+
+    Private Sub onMSEProgress()
+
+
+        Try
+            Me.prgProgress.Value = Me.m_MSE.Output.TrialNumber
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+
+#End Region
+
+#Region "Interface events"
+
+    Private Sub btRun_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btRun.Click
+
+        Try
+            Me.runMSE()
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+#End Region
+
+#Region "Interface"
+
+
+#Region "Graphs"
+
+    Private Sub LoadGraphPanes()
+
+        For igrp As Integer = 1 To Me.m_core.nGroups
+            Dim grp As cEcoPathGroupInput = Me.m_core.EcoPathGroupInputs(igrp)
+            Me.ConfigurePane(igrp, grp.Name)
+        Next
+
+    End Sub
+
+#End Region
+
+
+    ''' -------------------------------------------------------------------
+    ''' <summary>
+    ''' Configure a plot on the main graph
+    ''' </summary>
+    ''' -------------------------------------------------------------------
+    Private Sub ConfigurePane(ByVal iPane As Integer, ByVal strTitle As String)
+
+        Me.m_zgh.ConfigurePane(strTitle, _
+                   "", CDbl(Me.m_core.EcosimFirstYear), CDbl(Me.m_core.EcosimFirstYear + (m_core.nEcosimTimeSteps / cCore.N_MONTHS)), _
+                   "", 0, 0, _
+                   False, LegendPos.Top, iPane)
+
+    End Sub
+
+
+    Private Sub UpdateControls(ByVal State As eMSEStates)
+
+        Select Case State
+
+            Case eMSEStates.InActive
+                Me.btRun.Enabled = True
+
+            Case eMSEStates.Running
+                Me.btRun.Enabled = False
+
+            Case eMSEStates.Completed
+                Me.btRun.Enabled = True
+
+
+        End Select
+
+    End Sub
+
+#End Region
 
 End Class
