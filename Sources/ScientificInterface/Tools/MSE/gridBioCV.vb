fb2e443d160fd3c006c73fe5bbca1e07d6a11135
joeb <joeb@ecopath.org>
2009-07-03 23:49:33 +0000

    Added Bio and Fishing grids
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@1788 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/ScientificInterface/Tools/MSE/gridBioCV.vb
@@ -0,0 +1,51 @@
+﻿
+#Region " Imports "
+
+Option Strict On
+Option Explicit On
+
+Imports EwECore
+Imports SourceGrid2.Cells.Real
+Imports EwEUtils.Core
+Imports EwECore.MSE
+
+#End Region
+
+
+<CLSCompliant(False)> _
+Public Class gridBioCV
+    : Inherits EwEGrid
+
+    Private m_core As cCore
+
+    Public Sub New()
+
+        Me.m_core = cCore.GetInstance
+
+    End Sub
+
+    Protected Overrides Sub InitStyle()
+
+        MyBase.InitStyle()
+        Me.Redim(1, 2)
+        Me(0, 0) = New EwEColumnHeaderCell(My.Resources.HEADER_GROUP)
+        Me(0, 1) = New EwEColumnHeaderCell("C.V.")
+
+    End Sub
+
+    Protected Overrides Sub FillData()
+        Dim mse As cMSEManager = Me.m_core.MSEManager
+        If mse Is Nothing Then Exit Sub
+
+        For igrp As Integer = 1 To m_core.nLivingGroups
+
+            Me.Rows.Insert(igrp)
+
+            Me(igrp, 0) = New PropertyRowHeaderCell(mse.GroupInputs(igrp), eVarNameFlags.Name)
+            Me(igrp, 1) = New PropertyCell(mse.GroupInputs(igrp), eVarNameFlags.MSEBioCV)
+
+        Next
+
+    End Sub
+
+End Class
---
+++ Sources/ScientificInterface/Tools/MSE/gridFishingWeights.vb
@@ -0,0 +1,82 @@
+﻿
+#Region " Imports "
+
+Option Strict On
+Option Explicit On
+
+Imports EwECore
+Imports SourceGrid2.Cells.Real
+Imports EwEUtils.Core
+Imports EwECore.MSE
+
+#End Region
+
+
+<CLSCompliant(False)> _
+Public Class gridFishingWeights
+    : Inherits EwEGrid
+
+    Private m_core As cCore
+
+
+    Public Sub New()
+
+        Me.m_core = cCore.GetInstance
+
+    End Sub
+
+    Protected Overrides Sub InitStyle()
+        MyBase.InitStyle()
+
+        Dim core As cCore = cCore.GetInstance()
+        Dim src As cCoreInputOutputBase = Nothing
+
+        Me.Redim(1, 2 + core.nFleets)
+
+        Me(0, 0) = New EwEColumnHeaderCell("")
+        Me(0, 1) = New EwEColumnHeaderCell(My.Resources.HEADER_GROUPNAME)
+
+        For iFleet As Integer = 1 To core.nFleets
+            src = core.FleetInputs(iFleet)
+            Me(0, 1 + iFleet) = New PropertyColumnHeaderCell(src, _
+                eVarNameFlags.Name, Nothing, _
+                "{0} ({1})", cStyleGuide.eUnitType.Currency)
+        Next
+
+        Me.FixedColumns = 2
+        Me.FixedColumnWidths = True
+
+    End Sub
+
+    Protected Overrides Sub FillData()
+
+        Dim core As cCore = cCore.GetInstance()
+        Dim mse As cMSEManager = Me.m_core.MSEManager
+        If mse Is Nothing Then Exit Sub
+
+        Dim group As cCoreInputOutputBase = Nothing
+        Dim fleet As cMSEFleetInput = Nothing
+        ' Dim cell As ICell = Nothing
+
+        ' For each group
+        For iGroup As Integer = 1 To core.nGroups
+
+            Me.AddRow()
+
+            'Get the group info
+            group = core.EcoPathGroupInputs(iGroup)
+
+            ' Fleet name As row header
+            Me(iGroup, 0) = New EwERowHeaderCell(iGroup)
+            Me(iGroup, 1) = New PropertyRowHeaderCell(group, eVarNameFlags.Name)
+
+            ' Fleet cells
+            For iFleet As Integer = 1 To core.nFleets
+                fleet = MSE.FleetInputs(iFleet)
+                Me(iGroup, 1 + iFleet) = New PropertyCell(fleet, eVarNameFlags.MSEFleetWeight, group)
+            Next
+        Next
+
+    End Sub
+
+End Class
