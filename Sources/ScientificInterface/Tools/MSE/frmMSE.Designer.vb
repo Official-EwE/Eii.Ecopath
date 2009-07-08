05ce2045fa8f660b4187bd0cef8121c4459086f4
joeb <joeb@ecopath.org>
2009-07-08 19:53:41 +0000

    Changes to Bio and Fishing weights grids
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@1811 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterface/Tools/MSE/frmMSE.Designer.vb
+++ Sources/ScientificInterface/Tools/MSE/frmMSE.Designer.vb
@@ -35,14 +35,12 @@ Partial Class frmMSE
         Me.tbObjectives = New System.Windows.Forms.TabControl
         Me.pgObjective = New System.Windows.Forms.TabPage
         Me.pgEcoObjectives = New System.Windows.Forms.TabPage
-        Me.pgCV = New System.Windows.Forms.TabPage
-        Me.Label4 = New System.Windows.Forms.Label
-        Me.panelCV = New ScientificInterface.gridBioCV
         Me.pgFleetWeight = New System.Windows.Forms.TabPage
-        Me.Label3 = New System.Windows.Forms.Label
         Me.PanelFleetWeight = New ScientificInterface.gridFishingWeights
         Me.pgCatchabiltiy = New System.Windows.Forms.TabPage
         Me.pgRiskBounds = New System.Windows.Forms.TabPage
+        Me.pgCV = New System.Windows.Forms.TabPage
+        Me.panelCV = New ScientificInterface.gridBioCV
         Me.tbOutput = New System.Windows.Forms.TabControl
         Me.pgGraphs = New System.Windows.Forms.TabPage
         Me.zdGraph = New ZedGraph.ZedGraphControl
@@ -53,8 +51,8 @@ Partial Class frmMSE
         Me.spInputOutput.Panel2.SuspendLayout()
         Me.spInputOutput.SuspendLayout()
         Me.tbObjectives.SuspendLayout()
-        Me.pgCV.SuspendLayout()
         Me.pgFleetWeight.SuspendLayout()
+        Me.pgCV.SuspendLayout()
         Me.tbOutput.SuspendLayout()
         Me.pgGraphs.SuspendLayout()
         Me.SuspendLayout()
@@ -131,10 +129,10 @@ Partial Class frmMSE
         '
         Me.tbObjectives.Controls.Add(Me.pgObjective)
         Me.tbObjectives.Controls.Add(Me.pgEcoObjectives)
-        Me.tbObjectives.Controls.Add(Me.pgCV)
         Me.tbObjectives.Controls.Add(Me.pgFleetWeight)
         Me.tbObjectives.Controls.Add(Me.pgCatchabiltiy)
         Me.tbObjectives.Controls.Add(Me.pgRiskBounds)
+        Me.tbObjectives.Controls.Add(Me.pgCV)
         Me.tbObjectives.Dock = System.Windows.Forms.DockStyle.Fill
         Me.tbObjectives.Location = New System.Drawing.Point(0, 0)
         Me.tbObjectives.MinimumSize = New System.Drawing.Size(10, 0)
@@ -163,65 +161,8 @@ Partial Class frmMSE
         Me.pgEcoObjectives.Text = "Eco Objectives"
         Me.pgEcoObjectives.UseVisualStyleBackColor = True
         '
-        'pgCV
-        '
-        Me.pgCV.Controls.Add(Me.Label4)
-        Me.pgCV.Controls.Add(Me.panelCV)
-        Me.pgCV.Location = New System.Drawing.Point(4, 22)
-        Me.pgCV.Name = "pgCV"
-        Me.pgCV.Size = New System.Drawing.Size(521, 452)
-        Me.pgCV.TabIndex = 2
-        Me.pgCV.Text = "C.V. Fishing Rate"
-        Me.pgCV.UseVisualStyleBackColor = True
-        '
-        'Label4
-        '
-        Me.Label4.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
-                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Label4.BackColor = System.Drawing.SystemColors.ButtonShadow
-        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
-        Me.Label4.Location = New System.Drawing.Point(3, 3)
-        Me.Label4.Name = "Label4"
-        Me.Label4.RightToLeft = System.Windows.Forms.RightToLeft.No
-        Me.Label4.Size = New System.Drawing.Size(515, 21)
-        Me.Label4.TabIndex = 6
-        Me.Label4.Text = "C.V in annual direct estimate of fishing rate"
-        Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
-        '
-        'panelCV
-        '
-        Me.panelCV.AutoSizeMinHeight = 10
-        Me.panelCV.AutoSizeMinWidth = 10
-        Me.panelCV.AutoStretchColumnsToFitWidth = False
-        Me.panelCV.AutoStretchRowsToFitHeight = False
-        Me.panelCV.BackColor = System.Drawing.Color.White
-        Me.panelCV.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
-        Me.panelCV.ContextMenuStyle = CType((((SourceGrid2.ContextMenuStyle.ColumnResize Or SourceGrid2.ContextMenuStyle.AutoSize) _
-                    Or SourceGrid2.ContextMenuStyle.CopyPasteSelection) _
-                    Or SourceGrid2.ContextMenuStyle.CellContextMenu), SourceGrid2.ContextMenuStyle)
-        Me.panelCV.CustomSort = False
-        Me.panelCV.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.panelCV.FixedColumnWidths = True
-        Me.panelCV.FocusStyle = SourceGrid2.FocusStyle.None
-        Me.panelCV.GridToolTipActive = True
-        Me.panelCV.Location = New System.Drawing.Point(0, 0)
-        Me.panelCV.Name = "panelCV"
-        Me.panelCV.Size = New System.Drawing.Size(521, 452)
-        Me.panelCV.SpecialKeys = CType((((((((((SourceGrid2.GridSpecialKeys.Ctrl_C Or SourceGrid2.GridSpecialKeys.Ctrl_V) _
-                    Or SourceGrid2.GridSpecialKeys.Ctrl_X) _
-                    Or SourceGrid2.GridSpecialKeys.Delete) _
-                    Or SourceGrid2.GridSpecialKeys.Arrows) _
-                    Or SourceGrid2.GridSpecialKeys.Tab) _
-                    Or SourceGrid2.GridSpecialKeys.PageDownUp) _
-                    Or SourceGrid2.GridSpecialKeys.Enter) _
-                    Or SourceGrid2.GridSpecialKeys.Escape) _
-                    Or SourceGrid2.GridSpecialKeys.Backspace), SourceGrid2.GridSpecialKeys)
-        Me.panelCV.TabIndex = 0
-        '
         'pgFleetWeight
         '
-        Me.pgFleetWeight.Controls.Add(Me.Label3)
         Me.pgFleetWeight.Controls.Add(Me.PanelFleetWeight)
         Me.pgFleetWeight.Location = New System.Drawing.Point(4, 22)
         Me.pgFleetWeight.Name = "pgFleetWeight"
@@ -229,21 +170,6 @@ Partial Class frmMSE
         Me.pgFleetWeight.TabIndex = 3
         Me.pgFleetWeight.Text = "Fleet Weight"
         Me.pgFleetWeight.UseVisualStyleBackColor = True
-        '
-        'Label3
-        '
-        Me.Label3.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
-                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.Label3.BackColor = System.Drawing.SystemColors.ButtonShadow
-        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
-        Me.Label3.Location = New System.Drawing.Point(-1, 0)
-        Me.Label3.Name = "Label3"
-        Me.Label3.RightToLeft = System.Windows.Forms.RightToLeft.No
-        Me.Label3.Size = New System.Drawing.Size(522, 21)
-        Me.Label3.TabIndex = 5
-        Me.Label3.Text = "Importance weight of fishing on group"
-        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
         '
         'PanelFleetWeight
         '
@@ -257,12 +183,13 @@ Partial Class frmMSE
                     Or SourceGrid2.ContextMenuStyle.CopyPasteSelection) _
                     Or SourceGrid2.ContextMenuStyle.CellContextMenu), SourceGrid2.ContextMenuStyle)
         Me.PanelFleetWeight.CustomSort = False
+        Me.PanelFleetWeight.Dock = System.Windows.Forms.DockStyle.Fill
         Me.PanelFleetWeight.FixedColumnWidths = True
         Me.PanelFleetWeight.FocusStyle = SourceGrid2.FocusStyle.None
         Me.PanelFleetWeight.GridToolTipActive = True
-        Me.PanelFleetWeight.Location = New System.Drawing.Point(0, 24)
+        Me.PanelFleetWeight.Location = New System.Drawing.Point(0, 0)
         Me.PanelFleetWeight.Name = "PanelFleetWeight"
-        Me.PanelFleetWeight.Size = New System.Drawing.Size(521, 428)
+        Me.PanelFleetWeight.Size = New System.Drawing.Size(521, 452)
         Me.PanelFleetWeight.SpecialKeys = CType((((((((((SourceGrid2.GridSpecialKeys.Ctrl_C Or SourceGrid2.GridSpecialKeys.Ctrl_V) _
                     Or SourceGrid2.GridSpecialKeys.Ctrl_X) _
                     Or SourceGrid2.GridSpecialKeys.Delete) _
@@ -291,6 +218,45 @@ Partial Class frmMSE
         Me.pgRiskBounds.TabIndex = 5
         Me.pgRiskBounds.Text = "Risk Bounds"
         Me.pgRiskBounds.UseVisualStyleBackColor = True
+        '
+        'pgCV
+        '
+        Me.pgCV.Controls.Add(Me.panelCV)
+        Me.pgCV.Location = New System.Drawing.Point(4, 22)
+        Me.pgCV.Name = "pgCV"
+        Me.pgCV.Size = New System.Drawing.Size(521, 452)
+        Me.pgCV.TabIndex = 2
+        Me.pgCV.Text = "C.V. Fishing Rate"
+        Me.pgCV.UseVisualStyleBackColor = True
+        '
+        'panelCV
+        '
+        Me.panelCV.AutoSizeMinHeight = 10
+        Me.panelCV.AutoSizeMinWidth = 10
+        Me.panelCV.AutoStretchColumnsToFitWidth = False
+        Me.panelCV.AutoStretchRowsToFitHeight = False
+        Me.panelCV.BackColor = System.Drawing.Color.White
+        Me.panelCV.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.panelCV.ContextMenuStyle = CType((((SourceGrid2.ContextMenuStyle.ColumnResize Or SourceGrid2.ContextMenuStyle.AutoSize) _
+                    Or SourceGrid2.ContextMenuStyle.CopyPasteSelection) _
+                    Or SourceGrid2.ContextMenuStyle.CellContextMenu), SourceGrid2.ContextMenuStyle)
+        Me.panelCV.CustomSort = False
+        Me.panelCV.FixedColumnWidths = True
+        Me.panelCV.FocusStyle = SourceGrid2.FocusStyle.None
+        Me.panelCV.GridToolTipActive = True
+        Me.panelCV.Location = New System.Drawing.Point(3, 38)
+        Me.panelCV.Name = "panelCV"
+        Me.panelCV.Size = New System.Drawing.Size(515, 402)
+        Me.panelCV.SpecialKeys = CType((((((((((SourceGrid2.GridSpecialKeys.Ctrl_C Or SourceGrid2.GridSpecialKeys.Ctrl_V) _
+                    Or SourceGrid2.GridSpecialKeys.Ctrl_X) _
+                    Or SourceGrid2.GridSpecialKeys.Delete) _
+                    Or SourceGrid2.GridSpecialKeys.Arrows) _
+                    Or SourceGrid2.GridSpecialKeys.Tab) _
+                    Or SourceGrid2.GridSpecialKeys.PageDownUp) _
+                    Or SourceGrid2.GridSpecialKeys.Enter) _
+                    Or SourceGrid2.GridSpecialKeys.Escape) _
+                    Or SourceGrid2.GridSpecialKeys.Backspace), SourceGrid2.GridSpecialKeys)
+        Me.panelCV.TabIndex = 0
         '
         'tbOutput
         '
@@ -384,8 +350,8 @@ Partial Class frmMSE
         Me.spInputOutput.Panel2.ResumeLayout(False)
         Me.spInputOutput.ResumeLayout(False)
         Me.tbObjectives.ResumeLayout(False)
-        Me.pgCV.ResumeLayout(False)
         Me.pgFleetWeight.ResumeLayout(False)
+        Me.pgCV.ResumeLayout(False)
         Me.tbOutput.ResumeLayout(False)
         Me.pgGraphs.ResumeLayout(False)
         Me.ResumeLayout(False)
@@ -413,6 +379,4 @@ Partial Class frmMSE
     Friend WithEvents Label2 As System.Windows.Forms.Label
     Friend WithEvents panelCV As gridBioCV
     Friend WithEvents PanelFleetWeight As gridFishingWeights
-    Friend WithEvents Label3 As System.Windows.Forms.Label
-    Friend WithEvents Label4 As System.Windows.Forms.Label
 End Class
