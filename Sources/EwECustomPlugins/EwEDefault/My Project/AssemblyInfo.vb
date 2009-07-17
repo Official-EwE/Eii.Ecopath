63bcccb0f6c28bedb56009d2fc8754b801a68973
shermanl <shermanl@ecopath.org>
2009-07-17 06:28:47 +0000

    testing svn
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@1848 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECustomPlugins/EwEDefault/My Project/AssemblyInfo.vb
+++ Sources/EwECustomPlugins/EwEDefault/My Project/AssemblyInfo.vb
@@ -15,6 +15,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyCopyright("Copyright © 2006 - 2007")> 
 <Assembly: AssemblyTrademark("")> 
 
+
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
