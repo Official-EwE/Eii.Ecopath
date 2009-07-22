68612924db2caa85eb3218ed36ee3f5b7b7ee256
joeb <joeb@ecopath.org>
2009-07-22 15:13:22 +0000

    Minor interface tweaks
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@1860 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterface/Tools/MSE/frmMSE.vb
+++ Sources/ScientificInterface/Tools/MSE/frmMSE.vb
@@ -37,11 +37,23 @@ Imports ZedGraph
 
 Public Class frmMSE
 
+    'ToDo_jb frmMSE Add group selecting to the graphs to hide and display graphs
+    'ToDo_jb frmMSE Improve the MSE state change code to Enable and Disable controlls based on the MSE state
+    'ToDo_jb frmMSE output performance grid needs debugging the values look wrong
+
+#Region "Private Enum definitions"
+
+    ''' <summary>
+    ''' MSE state enumerators use by the interface to set control states
+    ''' </summary>
+    ''' <remarks></remarks>
     Private Enum eMSEStates
         InActive
         Running
         Completed
     End Enum
+
+#End Region
 
 #Region "Private variables"
 
@@ -50,6 +62,11 @@ Public Class frmMSE
 
     Private m_fpNTrials As cPropertyFormatProvider
     Private m_fpUsePlugin As cPropertyFormatProvider
+
+    Private m_fpKalman As cPropertyFormatProvider
+    Private m_fpForecast As cPropertyFormatProvider
+    Private m_fpSBPower As cPropertyFormatProvider
+
 
     Private m_paneMaster As MasterPane = Nothing
     Private m_sg As cStyleGuide = Nothing
@@ -76,6 +93,9 @@ Public Class frmMSE
         Me.m_fpNTrials = New cPropertyFormatProvider(Me.txNTrials, Me.m_MSE.ModelParameters, eVarNameFlags.MSENTrials)
         Me.m_fpUsePlugin = New cPropertyFormatProvider(Me.ckPlugin, Me.m_MSE.ModelParameters, eVarNameFlags.MSEUseEconomicPlugin)
 
+        Me.m_fpKalman = New cPropertyFormatProvider(Me.txKalman, Me.m_MSE.ModelParameters, eVarNameFlags.MSEKalmanGain)
+        Me.m_fpForecast = New cPropertyFormatProvider(Me.txForecast, Me.m_MSE.ModelParameters, eVarNameFlags.MSEForcastGain)
+        Me.m_fpSBPower = New cPropertyFormatProvider(Me.txSBPower, Me.m_MSE.ModelParameters, eVarNameFlags.MSEAssessPower)
 
         Me.m_gridObjectiveWeights = New gridSearchObjectivesWeight(Me.m_core.FishingPolicyManager)
         Me.m_gridGroupObjectives = New gridSearchObjectivesGroup(Me.m_core.FishingPolicyManager)
@@ -94,9 +114,14 @@ Public Class frmMSE
 
     Private Sub OnfrmMSELoad(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
+        'Assessment methods Catch Estimated Biomass and Direct Exploitation are stored in the tag property of the radio buttons
+        'see the Changed event of the radio buttons for setting the parameters
+        Me.rbCatchEstBio.Tag = eAssessmentMethods.CatchEstmBio
+        Me.rbDirectExp.Tag = eAssessmentMethods.DirectExploitation
+
         Me.m_paneMaster = Me.zdGraph.MasterPane
         Me.m_zgh = New cZedGraphHelper()
-        Me.m_zgh.Attach(Me.m_core, Me.zdGraph, Me.m_core.nGroups)
+        Me.m_zgh.Attach(Me.m_core, Me.zdGraph, Me.m_core.nLivingGroups)
 
         Me.tbObjectives.TabPages("pgObjective").Controls.Add(Me.m_gridObjectiveWeights)
         Me.m_gridObjectiveWeights.Dock = DockStyle.Fill
@@ -125,12 +150,20 @@ Public Class frmMSE
 
     Private Sub runMSE()
 
-        Me.m_MSE.Connect(AddressOf Me.onMSECallBack)
+        Try
 
-        Me.prgProgress.Maximum = Me.m_MSE.ModelParameters.NTrials
-        Me.prgProgress.Value = 0
+            Me.m_MSE.Connect(AddressOf Me.onMSECallBack)
 
-        Me.m_MSE.Run()
+            Me.prgProgress.Maximum = Me.m_MSE.ModelParameters.NTrials
+            Me.prgProgress.Value = 0
+            Me.ClearGraphs()
+
+            Me.m_MSE.Run()
+
+        Catch ex As Exception
+
+        End Try
+
 
     End Sub
 
@@ -156,6 +189,7 @@ Public Class frmMSE
 
             Case eCallBackTypes.IterationCompleted
                 Me.onMSEProgress()
+                Me.AddLineToGraph()
                 state = eMSEStates.Running
 
             Case eCallBackTypes.RunCompleted
@@ -183,10 +217,32 @@ Public Class frmMSE
 
 #Region "Interface events"
 
-    Private Sub btRun_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btRun.Click
+    Private Sub onRunClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles btRun.Click
 
         Try
             Me.runMSE()
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+
+    ''' <summary>
+    ''' Change the biomass assessment method based on the selected radio button
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
+    Private Sub onAssessmentMethodChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles rbCatchEstBio.CheckedChanged, rbDirectExp.CheckedChanged
+        Try
+            Debug.Assert(TypeOf sender Is RadioButton)
+            Dim rb As RadioButton = DirectCast(sender, RadioButton)
+            'This event handler is call for both radio buttons Changed events Checked and UnChecked
+            'Use the tag of the Checked radio button to set the MSE.AssessmentMethod
+            If rb.Checked = True Then
+                Me.m_MSE.ModelParameters.AssessmentMethod = DirectCast(rb.Tag, eAssessmentMethods)
+            End If
         Catch ex As Exception
 
         End Try
@@ -197,16 +253,26 @@ Public Class frmMSE
 
 #Region "Interface"
 
-
 #Region "Graphs"
+
+    'xxxxxxxxxxxxxxxxxxxxxxxx
+    'Graph plotting code was copied from EcosimPlots
+    'xxxxxxxxxxxxxxxxxxxxxx
 
     Private Sub LoadGraphPanes()
 
-        For igrp As Integer = 1 To Me.m_core.nGroups
+        For igrp As Integer = 1 To Me.m_core.nLivingGroups
             Dim grp As cEcoPathGroupInput = Me.m_core.EcoPathGroupInputs(igrp)
             Me.ConfigurePane(igrp, grp.Name)
         Next
 
+    End Sub
+
+
+    Private Sub ClearGraphs()
+        For Each Pane As GraphPane In zdGraph.MasterPane.PaneList
+            Pane.CurveList.Clear()
+        Next
     End Sub
 
     ''' -------------------------------------------------------------------
@@ -216,15 +282,59 @@ Public Class frmMSE
     ''' -------------------------------------------------------------------
     Private Sub ConfigurePane(ByVal iPane As Integer, ByVal strTitle As String)
 
-        Me.m_zgh.ConfigurePane(strTitle, _
-                   "", CDbl(Me.m_core.EcosimFirstYear), CDbl(Me.m_core.EcosimFirstYear + (m_core.nEcosimTimeSteps / cCore.N_MONTHS)), _
-                   "", 0, 0, _
-                   False, LegendPos.Top, iPane)
+        Me.m_zgh.ConfigurePane(strTitle, "", CDbl(Me.m_core.EcosimFirstYear), CDbl(Me.m_core.EcosimFirstYear + (m_core.nEcosimTimeSteps / cCore.N_MONTHS)), _
+                   "", 0, 0, False, LegendPos.Top, iPane)
 
     End Sub
 
 
+    Private Sub AddLineToGraph()
+
+        Try
+
+            Dim dx As Double
+            Dim igrp As Integer
+            For Each grp As cMSEGroupOutput In Me.m_MSE.GroupOutputs
+                igrp += 1
+                If igrp > Me.m_core.nLivingGroups Then Exit For
+
+                Dim ppl As New PointPairList
+                For iTime As Integer = 1 To m_core.nEcosimTimeSteps
+                    dx = Me.m_core.EcosimFirstYear + (iTime / cCore.N_MONTHS)
+                    ppl.Add(dx, grp.Biomass(iTime))
+                Next
+
+                Me.AddCurveToGraphPane(igrp, Me.m_zgh.CreateLineItem(cZedGraphHelper.eCurveTypes.EcosimOutput, igrp, ppl), False)
+
+            Next
+
+        Catch ex As Exception
+            System.Console.WriteLine(Me.ToString & ".PopulateGraphs() Error: " & ex.Message)
+        End Try
+
+    End Sub
+
+
+
+    ''' -------------------------------------------------------------------
+    ''' <summary>
+    ''' Add one curve into the graph pane
+    ''' </summary>
+    Private Sub AddCurveToGraphPane(ByVal PaneIndex As Integer, ByVal li As LineItem, _
+                                    Optional ByVal bClearExistingCurves As Boolean = True)
+
+        Dim lli As New List(Of ZedGraph.LineItem)
+        lli.Add(li)
+
+        Me.m_zgh.PlotLines(lli, PaneIndex, True, bClearExistingCurves)
+
+    End Sub
+
+
+
 #End Region
+
+#Region "Controls"
 
     Private Sub UpdateControls(ByVal State As eMSEStates)
 
@@ -239,10 +349,11 @@ Public Class frmMSE
             Case eMSEStates.Completed
                 Me.btRun.Enabled = True
 
-
         End Select
 
     End Sub
+
+#End Region
 
 #End Region
 
