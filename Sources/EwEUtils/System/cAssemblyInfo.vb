a0b9215ce5ccf4010fe512eeb284b74557541e76
JeroenS <jeroen.steenbeek@gmail.com>
2010-01-19 14:57:38 +0000

    ..but don't forget the namespace
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@2616 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwEUtils/System/cAssemblyInfo.vb
+++ Sources/EwEUtils/System/cAssemblyInfo.vb
@@ -5,180 +5,185 @@ Imports System.Reflection
 
 #End Region ' Imports
 
-''' ===========================================================================
-''' <summary>
-''' Interface for obtaining assembly information.
-''' </summary>
-''' <remarks>
-''' Code adapted 20 Jan 2010 from "Reading Assembly attributes in VB.NET" by Mihir Patak,
-''' http://www.vbdotnetheaven.com/UploadFile/mpathak/ReadingAssembly04112005053044AM/ReadingAssembly.aspx
-''' </remarks>
-''' ===========================================================================
-Public Interface IAssemblyInfo
+Public Class SystemUtilities
 
-    ''' ------------------------------------------------------------------------
+
+    ''' ===========================================================================
     ''' <summary>
-    ''' Get the value for an assembly 'Title' attribute.
+    ''' Interface for obtaining assembly information.
     ''' </summary>
-    ''' ------------------------------------------------------------------------
-    ReadOnly Property Title() As String
+    ''' <remarks>
+    ''' Code adapted 20 Jan 2010 from "Reading Assembly attributes in VB.NET" by Mihir Patak,
+    ''' http://www.vbdotnetheaven.com/UploadFile/mpathak/ReadingAssembly04112005053044AM/ReadingAssembly.aspx
+    ''' </remarks>
+    ''' ===========================================================================
+    Public Interface IAssemblyInfo
 
-    ''' ------------------------------------------------------------------------
+        ''' ------------------------------------------------------------------------
+        ''' <summary>
+        ''' Get the value for an assembly 'Title' attribute.
+        ''' </summary>
+        ''' ------------------------------------------------------------------------
+        ReadOnly Property Title() As String
+
+        ''' ------------------------------------------------------------------------
+        ''' <summary>
+        ''' Get the value for an assembly 'Description' attribute.
+        ''' </summary>
+        ''' ------------------------------------------------------------------------
+        ReadOnly Property Description() As String
+
+        ''' ------------------------------------------------------------------------
+        ''' <summary>
+        ''' Get the value for an assembly 'Company' attribute.
+        ''' </summary>
+        ''' ------------------------------------------------------------------------
+        ReadOnly Property Company() As String
+
+        ''' ------------------------------------------------------------------------
+        ''' <summary>
+        ''' Get the value for an assembly 'Product' attribute.
+        ''' </summary>
+        ''' ------------------------------------------------------------------------
+        ReadOnly Property Product() As String
+
+        ''' ------------------------------------------------------------------------
+        ''' <summary>
+        ''' Get the value for an assembly 'Copyright' attribute.
+        ''' </summary>
+        ''' ------------------------------------------------------------------------
+        ReadOnly Property Copyright() As String
+
+        ''' ------------------------------------------------------------------------
+        ''' <summary>
+        ''' Get the value for an assembly 'Trademark' attribute.
+        ''' </summary>
+        ''' ------------------------------------------------------------------------
+        ReadOnly Property Trademark() As String
+
+    End Interface
+
+    ''' ===========================================================================
     ''' <summary>
-    ''' Get the value for an assembly 'Description' attribute.
+    ''' Class that provides easy access to assembly information. 
     ''' </summary>
-    ''' ------------------------------------------------------------------------
-    ReadOnly Property Description() As String
-
-    ''' ------------------------------------------------------------------------
-    ''' <summary>
-    ''' Get the value for an assembly 'Company' attribute.
-    ''' </summary>
-    ''' ------------------------------------------------------------------------
-    ReadOnly Property Company() As String
-
-    ''' ------------------------------------------------------------------------
-    ''' <summary>
-    ''' Get the value for an assembly 'Product' attribute.
-    ''' </summary>
-    ''' ------------------------------------------------------------------------
-    ReadOnly Property Product() As String
-
-    ''' ------------------------------------------------------------------------
-    ''' <summary>
-    ''' Get the value for an assembly 'Copyright' attribute.
-    ''' </summary>
-    ''' ------------------------------------------------------------------------
-    ReadOnly Property Copyright() As String
-
-    ''' ------------------------------------------------------------------------
-    ''' <summary>
-    ''' Get the value for an assembly 'Trademark' attribute.
-    ''' </summary>
-    ''' ------------------------------------------------------------------------
-    ReadOnly Property Trademark() As String
-
-End Interface
-
-''' ===========================================================================
-''' <summary>
-''' Class that provides easy access to assembly information. 
-''' </summary>
-''' <remarks>
-''' Code adapted 20 Jan 2010 from "Reading Assembly attributes in VB.NET" by Mihir Patak,
-''' http://www.vbdotnetheaven.com/UploadFile/mpathak/ReadingAssembly04112005053044AM/ReadingAssembly.aspx
-''' </remarks>
-''' ===========================================================================
-Public Class cAssemblyInfo
-    Implements IAssemblyInfo
+    ''' <remarks>
+    ''' Code adapted 20 Jan 2010 from "Reading Assembly attributes in VB.NET" by Mihir Patak,
+    ''' http://www.vbdotnetheaven.com/UploadFile/mpathak/ReadingAssembly04112005053044AM/ReadingAssembly.aspx
+    ''' </remarks>
+    ''' ===========================================================================
+    Public Class cAssemblyInfo
+        Implements IAssemblyInfo
 
 #Region " Private vars "
 
-    ''' <summary>The assembly to explore.</summary>
-    Dim m_ass As System.Reflection.Assembly = Nothing
+        ''' <summary>The assembly to explore.</summary>
+        Dim m_ass As System.Reflection.Assembly = Nothing
 
 #End Region ' Private vars
 
 #Region " Constructor "
 
-    ''' ------------------------------------------------------------------------
-    ''' <summary>
-    ''' Constructor.
-    ''' </summary>
-    ''' <param name="ass">The assembly to explore.</param>
-    ''' ------------------------------------------------------------------------
-    Sub New(ByVal ass As Assembly)
-        Me.m_ass = ass
-    End Sub
+        ''' ------------------------------------------------------------------------
+        ''' <summary>
+        ''' Constructor.
+        ''' </summary>
+        ''' <param name="ass">The assembly to explore.</param>
+        ''' ------------------------------------------------------------------------
+        Sub New(ByVal ass As Assembly)
+            Me.m_ass = ass
+        End Sub
 
 #End Region ' Constructor
 
 #Region " Public properties "
 
-    ''' ------------------------------------------------------------------------
-    ''' <summary>
-    ''' Get the value for an assembly 'Company' attribute.
-    ''' </summary>
-    ''' ------------------------------------------------------------------------
-    Public ReadOnly Property Company() As String _
-        Implements IAssemblyInfo.Company
-        Get
-            Dim ca As AssemblyCompanyAttribute = Nothing
-            ca = DirectCast(m_ass.GetCustomAttributes(GetType(AssemblyCompanyAttribute), False)(0), AssemblyCompanyAttribute)
-            Return ca.Company.ToString
-        End Get
-    End Property
+        ''' ------------------------------------------------------------------------
+        ''' <summary>
+        ''' Get the value for an assembly 'Company' attribute.
+        ''' </summary>
+        ''' ------------------------------------------------------------------------
+        Public ReadOnly Property Company() As String _
+            Implements IAssemblyInfo.Company
+            Get
+                Dim ca As AssemblyCompanyAttribute = Nothing
+                ca = DirectCast(m_ass.GetCustomAttributes(GetType(AssemblyCompanyAttribute), False)(0), AssemblyCompanyAttribute)
+                Return ca.Company.ToString
+            End Get
+        End Property
 
-    ''' ------------------------------------------------------------------------
-    ''' <summary>
-    ''' Get the value for an assembly 'Copyright' attribute.
-    ''' </summary>
-    ''' ------------------------------------------------------------------------
-    Public ReadOnly Property Copyright() As String _
-        Implements IAssemblyInfo.Copyright
-        Get
-            Dim ca As AssemblyCopyrightAttribute = Nothing
-            ca = DirectCast(m_ass.GetCustomAttributes(GetType(AssemblyCopyrightAttribute), False)(0), AssemblyCopyrightAttribute)
-            Return ca.Copyright.ToCharArray
-        End Get
-    End Property
+        ''' ------------------------------------------------------------------------
+        ''' <summary>
+        ''' Get the value for an assembly 'Copyright' attribute.
+        ''' </summary>
+        ''' ------------------------------------------------------------------------
+        Public ReadOnly Property Copyright() As String _
+            Implements IAssemblyInfo.Copyright
+            Get
+                Dim ca As AssemblyCopyrightAttribute = Nothing
+                ca = DirectCast(m_ass.GetCustomAttributes(GetType(AssemblyCopyrightAttribute), False)(0), AssemblyCopyrightAttribute)
+                Return ca.Copyright.ToCharArray
+            End Get
+        End Property
 
-    ''' ------------------------------------------------------------------------
-    ''' <summary>
-    ''' Get the value for an assembly 'Description' attribute.
-    ''' </summary>
-    ''' ------------------------------------------------------------------------
-    Public ReadOnly Property Description() As String _
-         Implements IAssemblyInfo.Description
-        Get
-            Dim da As AssemblyDescriptionAttribute = Nothing
-            da = DirectCast(m_ass.GetCustomAttributes(GetType(AssemblyDescriptionAttribute), False)(0), AssemblyDescriptionAttribute)
-            Return da.Description.ToString
-        End Get
-    End Property
+        ''' ------------------------------------------------------------------------
+        ''' <summary>
+        ''' Get the value for an assembly 'Description' attribute.
+        ''' </summary>
+        ''' ------------------------------------------------------------------------
+        Public ReadOnly Property Description() As String _
+             Implements IAssemblyInfo.Description
+            Get
+                Dim da As AssemblyDescriptionAttribute = Nothing
+                da = DirectCast(m_ass.GetCustomAttributes(GetType(AssemblyDescriptionAttribute), False)(0), AssemblyDescriptionAttribute)
+                Return da.Description.ToString
+            End Get
+        End Property
 
-    ''' ------------------------------------------------------------------------
-    ''' <summary>
-    ''' Get the value for an assembly 'Product' attribute.
-    ''' </summary>
-    ''' ------------------------------------------------------------------------
-    Public ReadOnly Property Product() As String _
-            Implements IAssemblyInfo.Product
-        Get
-            Dim pa As AssemblyProductAttribute = Nothing
-            pa = DirectCast(m_ass.GetCustomAttributes(GetType(AssemblyProductAttribute), False)(0), AssemblyProductAttribute)
-            Return pa.Product.ToString
-        End Get
-    End Property
+        ''' ------------------------------------------------------------------------
+        ''' <summary>
+        ''' Get the value for an assembly 'Product' attribute.
+        ''' </summary>
+        ''' ------------------------------------------------------------------------
+        Public ReadOnly Property Product() As String _
+                Implements IAssemblyInfo.Product
+            Get
+                Dim pa As AssemblyProductAttribute = Nothing
+                pa = DirectCast(m_ass.GetCustomAttributes(GetType(AssemblyProductAttribute), False)(0), AssemblyProductAttribute)
+                Return pa.Product.ToString
+            End Get
+        End Property
 
-    ''' ------------------------------------------------------------------------
-    ''' <summary>
-    ''' Get the value for an assembly 'Title' attribute.
-    ''' </summary>
-    ''' ------------------------------------------------------------------------
-    Public ReadOnly Property Title() As String _
-           Implements IAssemblyInfo.Title
-        Get
-            Dim ta As AssemblyTitleAttribute = Nothing
-            ta = DirectCast(m_ass.GetCustomAttributes(GetType(AssemblyTitleAttribute), False)(0), AssemblyTitleAttribute)
-            Return ta.Title.ToString
-        End Get
-    End Property
+        ''' ------------------------------------------------------------------------
+        ''' <summary>
+        ''' Get the value for an assembly 'Title' attribute.
+        ''' </summary>
+        ''' ------------------------------------------------------------------------
+        Public ReadOnly Property Title() As String _
+               Implements IAssemblyInfo.Title
+            Get
+                Dim ta As AssemblyTitleAttribute = Nothing
+                ta = DirectCast(m_ass.GetCustomAttributes(GetType(AssemblyTitleAttribute), False)(0), AssemblyTitleAttribute)
+                Return ta.Title.ToString
+            End Get
+        End Property
 
-    ''' ------------------------------------------------------------------------
-    ''' <summary>
-    ''' Get the value for an assembly 'Trademark' attribute.
-    ''' </summary>
-    ''' ------------------------------------------------------------------------
-    Public ReadOnly Property Trademark() As String _
-         Implements IAssemblyInfo.Trademark
-        Get
-            Dim ta As AssemblyTrademarkAttribute = Nothing
-            ta = DirectCast(m_ass.GetCustomAttributes(GetType(AssemblyTrademarkAttribute), False)(0), AssemblyTrademarkAttribute)
-            Return ta.Trademark.ToString
-        End Get
-    End Property
+        ''' ------------------------------------------------------------------------
+        ''' <summary>
+        ''' Get the value for an assembly 'Trademark' attribute.
+        ''' </summary>
+        ''' ------------------------------------------------------------------------
+        Public ReadOnly Property Trademark() As String _
+             Implements IAssemblyInfo.Trademark
+            Get
+                Dim ta As AssemblyTrademarkAttribute = Nothing
+                ta = DirectCast(m_ass.GetCustomAttributes(GetType(AssemblyTrademarkAttribute), False)(0), AssemblyTrademarkAttribute)
+                Return ta.Trademark.ToString
+            End Get
+        End Property
 
 #End Region ' Public access
 
-End Class
+    End Class
+
+End Class ' SystemUtilities
