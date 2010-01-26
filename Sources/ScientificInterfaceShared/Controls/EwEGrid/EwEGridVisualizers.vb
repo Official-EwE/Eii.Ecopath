28f0ad30f91f16209a677404aa96b5d42bbcfa04
JeroenS <jeroen.steenbeek@gmail.com>
2010-01-26 15:06:00 +0000

    Moved cColorCellVisualizer here
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@2703 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterfaceShared/Controls/EwEGrid/EwEGridVisualizers.vb
+++ Sources/ScientificInterfaceShared/Controls/EwEGrid/EwEGridVisualizers.vb
@@ -13,7 +13,7 @@ Imports SourceGrid2.VisualModels
 
 Namespace Controls.EwEGrid
 
-#Region " EwE visualizers "
+#Region " EwECellVisualizerBase "
 
     ''' -------------------------------------------------------------------
     ''' <summary>
@@ -227,6 +227,10 @@ Namespace Controls.EwEGrid
 
     End Class
 
+#End Region ' EwECellVisualizerBase
+
+#Region " EwECellVisualizer "
+
     ''' -------------------------------------------------------------------
     ''' <summary>
     ''' EwECellVisualizer implements a EwECellVisualizerBase visualizer
@@ -248,6 +252,10 @@ Namespace Controls.EwEGrid
 
     End Class
 
+#End Region ' EwECellVisualizer
+
+#Region " cVisualizerEwERowHeader "
+
     ''' -------------------------------------------------------------------
     ''' <summary>
     ''' PropertyRowHeaderVisualizer implements a PropertyCellVisualizerBase visualizer
@@ -266,6 +274,10 @@ Namespace Controls.EwEGrid
 
     End Class
 
+#End Region ' cVisualizerEwERowHeader
+
+#Region " EwERowIndexVisualizer "
+
     <CLSCompliant(False)> _
     Public Class EwERowIndexVisualizer
         : Inherits EwECellVisualizerBase
@@ -277,6 +289,10 @@ Namespace Controls.EwEGrid
         End Sub
 
     End Class
+
+#End Region ' EwERowIndexVisualizer
+
+#Region " EwEColumnHeaderVisualizer "
 
     ''' -------------------------------------------------------------------
     ''' <summary>
@@ -319,6 +335,47 @@ Namespace Controls.EwEGrid
 
     End Class
 
-#End Region ' EwE visualizers
+#End Region ' EwEColumnHeaderVisualizer
+
+#Region " cColorCellVisualizer "
+
+    ''' -------------------------------------------------------------------
+    ''' <summary>
+    ''' cColorCellVisualizer is a cell visualizer that provides color feedback.
+    ''' </summary>
+    ''' -------------------------------------------------------------------
+    <CLSCompliant(False)> _
+    Public Class cColorCellVisualizer
+        Inherits SourceGrid2.VisualModels.Common
+
+        ''' -------------------------------------------------------------------
+        ''' <summary>
+        ''' Overidden to render cell value as a color
+        ''' </summary>
+        ''' -------------------------------------------------------------------
+        Protected Overrides Sub DrawCell_ImageAndText( _
+                ByVal p_Cell As SourceGrid2.Cells.ICellVirtual, _
+                ByVal p_CellPosition As SourceGrid2.Position, _
+                ByVal e As System.Windows.Forms.PaintEventArgs, _
+                ByVal p_ClientRectangle As System.Drawing.Rectangle, _
+                ByVal p_Status As SourceGrid2.DrawCellStatus)
+
+            Dim value As Object = p_Cell.GetValue(p_CellPosition)
+
+            If Not (TypeOf value Is Color) Then Return
+
+            Dim clr As Color = DirectCast(value, Color)
+            Dim rcColor As New Rectangle(p_ClientRectangle.X + 2, p_ClientRectangle.Y + 2, p_ClientRectangle.Width - 4, p_ClientRectangle.Height - 4)
+
+            ' Draw the background
+            Using br As New SolidBrush(clr)
+                e.Graphics.FillRectangle(br, rcColor)
+            End Using
+
+        End Sub
+
+    End Class
+
+#End Region ' cColorCellVisualizer
 
 End Namespace
