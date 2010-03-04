151e1affc5c3ff77029e6cc80b9b37ceb2741488
JeroenS <jeroen.steenbeek@gmail.com>
2010-03-04 15:43:03 +0000

    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@3052 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/EwEUtils/Core/ICoreFleetFilter.vb
@@ -0,0 +1,17 @@
+﻿Namespace Core
+
+    ''' <summary>
+    ''' Interface for objects that can be filtered by fleet.
+    ''' </summary>
+    Public Interface ICoreFleetFilter
+
+        ''' -----------------------------------------------------------------------
+        ''' <summary>
+        ''' Get/set the fleet to filter by.
+        ''' </summary>
+        ''' -----------------------------------------------------------------------
+        Property Fleet() As Integer
+
+    End Interface
+
+End Namespace
---
+++ Sources/EwEUtils/Core/ICoreGroupFilter.vb
@@ -0,0 +1,17 @@
+﻿Namespace Core
+
+    ''' <summary>
+    ''' Interface for objects that can be filtered by group.
+    ''' </summary>
+    Public Interface ICoreGroupFilter
+
+        ''' -----------------------------------------------------------------------
+        ''' <summary>
+        ''' Get/set the group to filter by.
+        ''' </summary>
+        ''' -----------------------------------------------------------------------
+        Property Group() As Integer
+
+    End Interface
+
+End Namespace
