7e75b04def8bc5f342b4f777eb2fe318bb5a4df5
JeroenS <jeroen.steenbeek@gmail.com>
2010-03-09 02:23:02 +0000

    Commented
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@3141 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwEPlugin/Plugin templates/Core/ICorePlugin.vb
+++ Sources/EwEPlugin/Plugin templates/Core/ICorePlugin.vb
@@ -1,5 +1,12 @@
 Option Strict On
 
+''' ===========================================================================
+''' <summary>
+''' Interface for a plug-in that is invoked when the EwE Core loads the three main
+''' models Ecopath, Ecosim and Ecospace. Plug-in points in this interface
+''' will allow an implementing plug-in to obtain a reference to the three models.
+''' </summary>
+''' ===========================================================================
 Public Interface ICorePlugin
     Inherits IPlugin
 
@@ -9,8 +16,6 @@ Public Interface ICorePlugin
     ''' <param name="objEcoPath">The Ecopath model</param>
     ''' <param name="objEcoSim">The Ecosim model</param>
     ''' <param name="objEcoSpace">The Ecospace model</param>
-    ''' <remarks></remarks>
     Sub CoreInitialized(ByRef objEcoPath As Object, ByRef objEcoSim As Object, ByRef objEcoSpace As Object)
-
 
 End Interface
