ce4b383a82dea3fe68ddf7859abf61216fda4366
JeroenS <jeroen.steenbeek@gmail.com>
2010-03-09 02:38:36 +0000

    Added XML comments
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@3144 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwEPlugin/Plugin templates/IPlugin.vb
+++ Sources/EwEPlugin/Plugin templates/IPlugin.vb
@@ -1,33 +1,16 @@
-'==============================================================================
-'
-' $Log: IPlugin.vb,v $
-' Revision 1.1  2008/09/26 07:31:09  sherman
-' --== DELETED HISTORY ==--
-'
-' Revision 1.4  2008/06/19 08:50:53  jeroens
-' *sigh*
-'
-' Revision 1.3  2007/10/30 19:20:52  jeroens
-' + Plugins need Author, contact
-'
-' Revision 1.2  2007/03/14 00:45:00  jeroens
-' + Added Description
-'
-' Revision 1.1  2006/08/30 20:52:35  jeroens
-' * Moved and/or created
-'
-' Revision 1.1  2006/08/08 14:11:50  jeroens
-' + Initial version
-'
-'==============================================================================
-
 Option Strict On
 
+''' ===========================================================================
+''' <summary>
+''' Base interface for defining an EwE6 plug-in. Plug-ins are detected by the
+''' presence of this Interface.
+''' </summary>
+''' ===========================================================================
 Public Interface IPlugin
 
     ''' -----------------------------------------------------------------------
     ''' <summary>
-    ''' Initializes the plugin.
+    ''' Initialize the plugin.
     ''' </summary>
     ''' <param name="core">The core this plugin is initialized for.</param>
     ''' -----------------------------------------------------------------------
