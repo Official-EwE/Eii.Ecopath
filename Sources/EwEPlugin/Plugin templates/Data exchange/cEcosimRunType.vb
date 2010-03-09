5aba989fd9416f4bc2fd8266096229b82be149bd
JeroenS <jeroen.steenbeek@gmail.com>
2010-03-09 02:17:06 +0000

    Added XML comments
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@3140 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwEPlugin/Plugin templates/Data exchange/cEcosimRunType.vb
+++ Sources/EwEPlugin/Plugin templates/Data exchange/cEcosimRunType.vb
@@ -5,6 +5,11 @@ Imports EwEUtils.Core
 
 #End Region ' Imports
 
+''' -----------------------------------------------------------------------
+''' <summary>
+''' Class defining Ecosim run/computation dependencies for exchanging data.
+''' </summary>
+''' -----------------------------------------------------------------------
 Public Class cEcosimRunType
     Implements IRunType
 End Class
--- Sources/EwEPlugin/Plugin templates/Data exchange/cEcospaceRunType.vb
+++ Sources/EwEPlugin/Plugin templates/Data exchange/cEcospaceRunType.vb
@@ -5,6 +5,11 @@ Imports EwEUtils.Core
 
 #End Region ' Imports
 
+''' -----------------------------------------------------------------------
+''' <summary>
+''' Class defining Ecospace run/computation dependencies for exchanging data.
+''' </summary>
+''' -----------------------------------------------------------------------
 Public Class cEcospaceRunType
     Implements IRunType
 End Class
