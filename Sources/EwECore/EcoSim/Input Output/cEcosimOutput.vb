fdab2667cc49ceffd7bdd48f7116ee44fbb811bf
JeroenS <jeroen.steenbeek@gmail.com>
2010-03-22 00:09:58 +0000

    Fixed issue 700
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@3283 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECore/EcoSim/Input Output/cEcosimOutput.vb
+++ Sources/EwECore/EcoSim/Input Output/cEcosimOutput.vb
@@ -23,16 +23,15 @@ Public Class cEcosimOutput
         End Get
     End Property
 
+    Public ReadOnly Property FIBStatus(ByVal iTimeStep As Integer) As eStatusFlags
+        Get
+            Return GetStatus(eVarNameFlags.FIB, iTimeStep)
+        End Get
+    End Property
 
     Public ReadOnly Property TLCatch(ByVal iTimeStep As Integer) As Single
         Get
             Return CSng(GetVariable(eVarNameFlags.TLCatch, iTimeStep))
-        End Get
-    End Property
-
-    Public ReadOnly Property FIBStatus(ByVal iTimeStep As Integer) As eStatusFlags
-        Get
-            Return GetStatus(eVarNameFlags.FIB, iTimeStep)
         End Get
     End Property
 
@@ -42,16 +41,53 @@ Public Class cEcosimOutput
         End Get
     End Property
 
+    Public ReadOnly Property TotalCatch(ByVal iTimeStep As Integer) As Single
+        Get
+            Return CSng(GetVariable(eVarNameFlags.TotalCatch, iTimeStep))
+        End Get
+    End Property
+
+    Public ReadOnly Property TotalCatchStatus(ByVal iTimeStep As Integer) As eStatusFlags
+        Get
+            Return GetStatus(eVarNameFlags.TotalCatch, iTimeStep)
+        End Get
+    End Property
+
+    Public ReadOnly Property KemptonsQ(ByVal iTimeStep As Integer) As Single
+        Get
+            Return CSng(GetVariable(eVarNameFlags.KemptonsQ, iTimeStep))
+        End Get
+    End Property
+
+    Public ReadOnly Property KemptonsQStatus(ByVal iTimeStep As Integer) As eStatusFlags
+        Get
+            Return GetStatus(eVarNameFlags.KemptonsQ, iTimeStep)
+        End Get
+    End Property
+
     Public Overrides Function GetVariable(ByVal VarName As EwEUtils.Core.eVarNameFlags, _
                                           Optional ByVal iIndex As Integer = -9999, _
                                           Optional ByVal iIndex2 As Integer = -9999, _
                                           Optional ByVal iIndex3 As Integer = -9999) As Object
-        Select Case VarName
-            Case eVarNameFlags.FIB
-                Return Me.m_core.m_EcoSimData.FIB(iIndex)
-            Case eVarNameFlags.TLCatch
-                Return Me.m_core.m_EcoSimData.TLC(iIndex)
-        End Select
+
+        Try
+
+            Select Case VarName
+                Case eVarNameFlags.FIB
+                    Return Me.m_core.m_EcoSimData.FIB(iIndex)
+                Case eVarNameFlags.TLCatch
+                    Return Me.m_core.m_EcoSimData.TLC(iIndex)
+                Case eVarNameFlags.KemptonsQ
+                    Return Me.m_core.m_EcoSimData.Kemptons(iIndex)
+                Case eVarNameFlags.TotalCatch
+                    Return Me.m_core.m_EcoSimData.CatchSim(iIndex)
+
+            End Select
+
+        Catch ex As Exception
+            Debug.Assert(False, ex.Message)
+        End Try
+
         Return cCore.NULL_VALUE
 
     End Function
