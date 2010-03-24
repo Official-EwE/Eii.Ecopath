821251e09349a0e1cfdbd5fda0ca588ebc1dda73
JeroenS <jeroen.steenbeek@gmail.com>
2010-03-24 15:59:45 +0000

    Scratch one more cCore.GetInstance
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@3319 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterface/Other/WebPages/WebBrowserDC.vb
+++ Sources/ScientificInterface/Other/WebPages/WebBrowserDC.vb
@@ -1,28 +1,4 @@
-'==============================================================================
-'
-' $Log: WebBrowserDC.vb,v $
-' Revision 1.2  2008/11/27 19:45:51  jeroens
-' Renamed ApplicationComponents interfaces to more properly reflect their function
-'
-' Revision 1.1  2008/09/26 07:32:11  sherman
-' --== DELETED HISTORY ==--
-'
-' Revision 1.13  2008/05/22 17:31:49  jeroens
-' Added command-line support for opening a model
-'
-' Revision 1.12  2007/11/06 14:17:36  sherman
-' Jereon fixed disposing of form and objects within it
-'
-' Revision 1.11  2007/10/10 19:47:01  jeroens
-' * URL includes names of loaded plugins
-'
-' Revision 1.10  2007/09/27 18:04:23  jeroens
-' + Uses ApplicationComponents
-'
-' Revision 1.9  2007/09/27 17:02:21  jeroens
-' * Oooh! Pretty header!
-'
-'==============================================================================
+#Region " Imports "
 
 Option Strict On
 
@@ -32,23 +8,27 @@ Imports System.Reflection
 Imports EwEUtils.Utilities
 Imports EwEPlugin
 
+#End Region ' Imports
+
 Public Class WebBrowserDC
 
     Private Shared cBASEURL As String = "http://www.ecopath.org/nonewe/eweexe/index.php"
+    Private m_uic As cUIContext = Nothing
 
-    Public Sub New()
+    Public Sub New(ByVal uic As cUIContext)
 
-        ' This call is required by the Windows Form Designer.
-        InitializeComponent()
-        ' Setting the label
+        Me.InitializeComponent()
+
+        Me.m_uic = uic
+
         Me.Text = My.Resources.GENERIC_LABEL_HOME
         Me.TabText = My.Resources.GENERIC_LABEL_HOME
-        ' Navigate to default URL
         Me.URL = ""
 
     End Sub
 
-    Private Sub OnNavigating(ByVal sender As Object, ByVal e As System.Windows.Forms.WebBrowserNavigatingEventArgs) Handles m_browser.Navigating
+    Private Sub OnNavigating(ByVal sender As Object, ByVal e As System.Windows.Forms.WebBrowserNavigatingEventArgs) _
+        Handles m_browser.Navigating
 
         Dim appl As AppLauncher = AppLauncher.GetInstance()
 
@@ -73,10 +53,10 @@ Public Class WebBrowserDC
     End Property
 
     Private Function EwEBaseURL() As String
-        Dim core As cCore = cCore.GetInstance()
+
         Dim ac As ApplicationComponents = AppLauncher.GetInstance().ApplicationComponents()
         Dim aAssemblyNames As AssemblyName() = ac.RequiredComponents()
-        Dim pm As cPluginManager = core.PluginManager
+        Dim pm As cPluginManager = Me.m_uic.Core.PluginManager
         Dim ub As New EwEUtils.Utilities.UrlBuilder(cBASEURL)
 
         For Each an As AssemblyName In aAssemblyNames
@@ -93,11 +73,13 @@ Public Class WebBrowserDC
         Return ub.ToString()
     End Function
 
-    Private Sub WebBrowserDC_Disposed(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Disposed
-        Me.m_browser = Nothing
+    Protected Overrides Sub OnLoad(ByVal e As System.EventArgs)
+        MyBase.OnLoad(e)
     End Sub
 
-    Private Sub WebBrowserDC_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        '
+    Protected Overrides Sub OnFormClosed(ByVal e As System.Windows.Forms.FormClosedEventArgs)
+        Me.m_browser = Nothing
+        MyBase.OnFormClosed(e)
     End Sub
+
 End Class
