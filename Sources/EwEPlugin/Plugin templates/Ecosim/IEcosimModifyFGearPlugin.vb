0f338974f96038f0835c5a5d5ae7343e5e3f82a6
joeb <joeb@ecopath.org>
2010-04-08 17:19:57 +0000

    Changes the signature of Plugin point to use proper types where it can
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@3444 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwEPlugin/Plugin templates/Ecosim/IEcosimModifyFGearPlugin.vb
+++ Sources/EwEPlugin/Plugin templates/Ecosim/IEcosimModifyFGearPlugin.vb
@@ -8,9 +8,22 @@
 ' Added Plugin point EcosimModifyFGear
 '
 '==============================================================================
+
+''' <summary>
+''' Plugin Point to modify Ecosim Fishing Effort during a MSE or Fishing Policy Search.
+''' </summary>
+''' <remarks>This will not modify effort during a normal Ecosim run.</remarks>
 Public Interface IEcosimModifyFGearPlugin
     Inherits IPlugin
 
-    Sub EcosimModifyFGear(ByVal FGear As Object, ByVal BB As Object, ByVal EcosimDataStructures As Object, ByVal CurrentTime As Object)
+    ''' <summary>
+    ''' Method that gets called when a Fishing Policy or MSE search is modifying Fishing Effort.
+    ''' </summary>
+    ''' <param name="FGear">Array of Relative Fishing Effort dimensioned by fleet for the current timestep.</param>
+    ''' <param name="BB">Array of Biomass by group for the current timestep</param>
+    ''' <param name="EcosimDataStructures">Reference to the current EcosimDataStructures passed as an object.</param>
+    ''' <param name="CurrentTimeStepIndex">Current timestep index.</param>
+    ''' <remarks>At this time this only changes effort during a search there is no easy way to change effort during a normal run. </remarks>
+    Sub EcosimModifyFGear(ByVal FGear() As Single, ByVal BB() As Single, ByVal EcosimDataStructures As Object, ByVal CurrentTimeStepIndex As Integer)
 
 End Interface
