e5538ed5ea6cfdbaa4e3c8ab5bc13b9d574af85e
JeroenS <jeroen.steenbeek@gmail.com>
2010-04-11 16:11:44 +0000

    Added cEwEColorDialog
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@3480 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/ScientificInterfaceShared/Controls/ColorDialog/cEwEColorDialog.vb
@@ -0,0 +1,34 @@
+﻿#Region " Imports "
+
+Option Strict On
+
+#End Region ' Imports
+
+Namespace Controls
+
+    ''' ---------------------------------------------------------------------------
+    ''' <summary>
+    ''' Override for the .NET Color dialog, overridden to manage custom colours.
+    ''' </summary>
+    ''' ---------------------------------------------------------------------------
+    Public Class cEwEColorDialog
+        Inherits ColorDialog
+
+        Private Shared s_lCustomColors As New List(Of Integer)
+
+        Public Sub New()
+            MyBase.New()
+            Me.AllowFullOpen = True
+            Me.FullOpen = True
+            Me.CustomColors = s_lCustomColors.ToArray
+        End Sub
+
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            s_lCustomColors.Clear()
+            s_lCustomColors.AddRange(Me.CustomColors)
+            MyBase.Dispose(disposing)
+        End Sub
+
+    End Class
+
+End Namespace ' Controls
