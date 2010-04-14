5a36bd07917756c60b6745120f24a7fdca15a007
JeroenS <jeroen.steenbeek@gmail.com>
2010-04-14 17:57:16 +0000

    Added cAssemblyNameUtils.GetSummary
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@3516 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwEUtils/Utilities/cAssemblyNameUtils.vb
+++ Sources/EwEUtils/Utilities/cAssemblyNameUtils.vb
@@ -5,6 +5,7 @@ Imports System.Text
 Imports System.Reflection
 Imports Microsoft.VisualBasic
 Imports System
+Imports System.Collections.Generic
 
 #End Region ' Imports
 
@@ -16,6 +17,23 @@ Namespace Utilities
     ''' </summary>
     ''' =======================================================================
     Public Class cAssemblyUtils
+
+#Region " Internal helper classes "
+
+        ''' -----------------------------------------------------------------------
+        ''' <summary>
+        ''' Helper class to sort an assembly name list
+        ''' </summary>
+        ''' -----------------------------------------------------------------------
+        Private Class AssemblyNameComparer
+            Implements IComparer(Of AssemblyName)
+
+            Public Function Compare(ByVal x As System.Reflection.AssemblyName, ByVal y As System.Reflection.AssemblyName) As Integer Implements System.Collections.Generic.IComparer(Of System.Reflection.AssemblyName).Compare
+                Return String.Compare(x.Name, y.Name)
+            End Function
+        End Class
+
+#End Region ' Internal helper classes
 
         ''' -----------------------------------------------------------------------
         ''' <summary>
@@ -69,6 +87,55 @@ Namespace Utilities
             Dim ass As Assembly = Assembly.GetAssembly(tclass)
             If (ass Is Nothing) Then Return Nothing
             Return ass.GetName()
+
+        End Function
+
+        ''' -----------------------------------------------------------------------
+        ''' <summary>
+        ''' Reports all referenced, proprietary <see cref="AssemblyName">assemblies</see> 
+        ''' for a given assembly.
+        ''' </summary>
+        ''' <param name="entry">The entry assembly to find the summary of referenced
+        ''' assemblies for.</param>
+        ''' <remarks>
+        ''' The array of assembly names will be sorted by name.
+        ''' </remarks>
+        ''' -----------------------------------------------------------------------
+        Public Shared Function GetSummary(ByVal entry As Assembly) As AssemblyName()
+
+            ' List to hold collected summary data
+            Dim lAssemblies As New List(Of AssemblyName)
+            ' List of assembly name prefixes NOT to include in the list. These are all .NET framework prefixes.
+            Dim astrFrameworkPrefixes() As String = {"mscorlib", "system", "microsoft"}
+            ' All required assemblies (not actually loaded!)
+            Dim anRequired() As AssemblyName = entry.GetReferencedAssemblies()
+
+            lAssemblies.Add(Assembly.GetEntryAssembly().GetName())
+
+            ' Figure out which assemblies to show
+            For Each an As AssemblyName In anRequired
+                ' Not already accepted?
+                If Not lAssemblies.Contains(an) Then
+                    ' #Yes: this is a new assembly
+                    ' Assume that assembly can be added
+                    Dim bAddAssembly As Boolean = True
+                    ' Check if this a blacklisted assembly
+                    For Each strName As String In astrFrameworkPrefixes
+                        ' Does name begin with a blacklisted string?
+                        If an.FullName.ToLower().IndexOf(strName) = 0 Then
+                            ' #Yes: can not add assembly
+                            bAddAssembly = False
+                        End If
+                    Next
+                    ' So what did the jury decide?
+                    If bAddAssembly Then lAssemblies.Add(an)
+                End If
+            Next
+
+            ' Sort accepted assembly list
+            lAssemblies.Sort(New AssemblyNameComparer())
+
+            Return lAssemblies.ToArray()
 
         End Function
 
