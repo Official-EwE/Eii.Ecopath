3ff1a2417484e562ab0042c0dc2b629531501a7f
joeb <joeb@ecopath.org>
2010-04-14 18:44:06 +0000

    Added frmOptions
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@3520 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/ScientificInterface/Tools/MSE/frmOptions.vb
@@ -0,0 +1,26 @@
+﻿#Region " Imports "
+
+Option Strict On
+Option Explicit On
+
+Imports EwECore
+Imports EwECore.MSE
+Imports EwECore.SearchObjectives
+Imports ScientificInterface.Controls
+Imports EwEUtils.Core
+Imports ScientificInterface.Ecosim
+Imports EwEUtils.Commands
+
+#End Region
+
+
+Public Class frmOptions
+
+    Protected Overrides Sub OnLoad(ByVal e As System.EventArgs)
+        MyBase.OnLoad(e)
+
+        Me.CoreComponents = New eCoreComponentType() {eCoreComponentType.MSE, eCoreComponentType.SearchObjective}
+
+    End Sub
+
+End Class
