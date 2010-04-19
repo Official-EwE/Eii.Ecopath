9e72fa31fe64cdaf22bd4a68c4d8fb4db986e983
joeb <joeb@ecopath.org>
2010-04-19 23:57:40 +0000

    Added IEcosimDataInitializedPlugin
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@3550 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/EwEPlugin/Plugin templates/Ecosim/IEcosimDataInitialized.vb
@@ -0,0 +1,29 @@
+﻿
+
+''' <summary>
+''' Plugin points for the initialization of Ecosim data cEcosimDataStructures 
+''' </summary>
+''' <remarks></remarks>
+Public Interface IEcosimDataInitializedPlugin
+    Inherits IPlugin
+
+    ''' <summary>
+    ''' Ecosim has loaded a scenario and is about to call cEcosimModel.Init() to initialize its data.
+    ''' </summary>
+    ''' <param name="EcosimDatastructures">Ecosim datastructures as an object</param>
+    ''' <remarks>This can be used prior to the initialization of Ecosim data to set variables that are used by Ecosim to set derived variables. </remarks>
+    Sub EcosimPreDataInitialized(ByVal EcosimDatastructures As Object)
+
+    ''' <summary>
+    ''' Ecosim has loaded a scenario and is about to call cEcosimModel.Init() to initialize its data.
+    ''' </summary>
+    ''' <param name="EcosimDatastructures"></param>
+    ''' <remarks></remarks>
+    Sub EcosimPostDataInitialized(ByVal EcosimDatastructures As Object)
+
+
+
+    Sub EcosimPreRunInitialized(ByVal EcosimDatastructures As Object)
+
+
+End Interface
