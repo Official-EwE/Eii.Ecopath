352ef7c1321df07bc427a155e9e224520d3d1f46
joeb <joeb@ecopath.org>
2010-04-19 23:58:16 +0000

    Added IEcosimDataInitializedPlugin
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@3551 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwEPlugin/Plugin templates/Ecosim/IEcosimInitializedPlugin.vb
+++ Sources/EwEPlugin/Plugin templates/Ecosim/IEcosimInitializedPlugin.vb
@@ -11,9 +11,18 @@
 '
 '==============================================================================
 
+''' <summary>
+''' Plugin for initialization of Ecosim Scenarios
+''' </summary>
+''' <remarks>Contains plugin points for initialization of Ecosim Scenarios</remarks>
 Public Interface IEcosimInitializedPlugin
     Inherits IPlugin
 
+    ''' <summary>
+    ''' Plugin Point called when an Ecosim Scenario has loaded
+    ''' </summary>
+    ''' <param name="EcosimDatastructures">cEcosimDataStructures passed as an object.</param>
+    ''' <remarks>Called after an Ecosim scenario has loaded.</remarks>
     Sub EcosimInitialized(ByVal EcosimDatastructures As Object)
 
 End Interface
