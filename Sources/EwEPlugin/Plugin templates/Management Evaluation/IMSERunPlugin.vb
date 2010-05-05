9746160249a025fab06463f2dcc2e9dea8fa9fe2
joeb <joeb@ecopath.org>
2010-05-05 20:38:01 +0000

    Added IMSERunPlugin
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@3726 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/EwEPlugin/Plugin templates/Management Evaluation/IMSERunPlugin.vb
@@ -0,0 +1,24 @@
+﻿
+''' <summary>
+''' Interface for MSE initialization plugin points that are invoked once the MSE model has been loaded
+''' </summary>
+''' <remarks></remarks>
+Public Interface IMSERunPlugin
+    Inherits IPlugin
+
+    Sub MSERunStarted()
+
+    Sub MSERunCompleted()
+
+    Sub MSEIterationStarted()
+
+    Sub MSEIterationCompleted()
+
+    Sub MSEDoAssessment(ByVal Biomass() As Single)
+
+    Sub MSEUpdateQuotas(ByVal Biomass() As Single)
+
+    Sub MSERegulateEffort(ByVal Biomass() As Single, ByVal QMult() As Single, ByVal QYear() As Single, ByVal t As Integer)
+
+
+End Interface
