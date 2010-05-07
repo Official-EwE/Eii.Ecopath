41910705e89d4693b2b9d32e8a2628ae24d0a6eb
JeroenS <jeroen.steenbeek@gmail.com>
2010-05-07 15:22:09 +0000

    Check boxes are back
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@3751 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterface/Ecosim/Input/TimeSeries/frmTimeSeries.vb
+++ Sources/ScientificInterface/Ecosim/Input/TimeSeries/frmTimeSeries.vb
@@ -23,12 +23,12 @@ Namespace Ecosim
     ''' =======================================================================
     Public Class frmTimeSeries
 
-#Region "Private variables"
+#Region " Private variables "
 
         ''' <summary></summary>
-        Private m_sgh As cShapeGUIHandler = Nothing
+        Private m_handler As cTimeSeriesShapeGUIHandler = Nothing
 
-#End Region
+#End Region ' Private variables
 
 #Region " Constructors "
 
@@ -53,8 +53,8 @@ Namespace Ecosim
             ' Hook up message sources
             Me.CoreComponents = New eCoreComponentType() {eCoreComponentType.TimeSeries}
 
-            Me.m_sgh = New cTimeSeriesShapeGUIHandler()
-            Me.m_sgh.Attach(Me.UIContext, Me.m_shapeToolbox, Me.m_shapeToolboxToolbar, Me.m_sketchPad, Me.m_sketchPadToolbar)
+            Me.m_handler = New cTimeSeriesShapeGUIHandler()
+            Me.m_handler.Attach(Me.UIContext, Me.m_shapeToolbox, Me.m_shapeToolboxToolbar, Me.m_sketchPad, Me.m_sketchPadToolbar)
 
             ' Once hooked up, try to get TS if not here yet
             If Not Me.UIContext.Core.HasTimeSeries Then
@@ -68,7 +68,7 @@ Namespace Ecosim
         End Sub
 
         Protected Overrides Sub OnFormClosed(ByVal e As System.Windows.Forms.FormClosedEventArgs)
-            Me.m_sgh.Detach()
+            Me.m_handler.Detach()
             MyBase.OnFormClosed(e)
         End Sub
 
@@ -80,7 +80,7 @@ Namespace Ecosim
             If ((msg.Source = eCoreComponentType.TimeSeries) And _
                 (msg.Type = eMessageType.DataAddedOrRemoved Or msg.Type = eMessageType.DataModified)) Then
                 ' Refresh content
-                Me.m_sgh.Refresh()
+                Me.m_handler.Refresh()
             End If
         End Sub
 
