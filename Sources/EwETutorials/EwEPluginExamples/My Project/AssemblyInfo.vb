9a6d47ed1ff2e55557ced2e8c9fdd062620b00a5
automatedbuild <automatedbuild@ecopath.org>
2010-05-13 09:35:38 +0000

    Build 6.1.0.0513
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@3790 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwETutorials/EwEPluginExamples/My Project/AssemblyInfo.vb
+++ Sources/EwETutorials/EwEPluginExamples/My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("6.1.0.0512")> 
-<Assembly: AssemblyFileVersion("6.1.0.0512")> 
+<Assembly: AssemblyVersion("6.1.0.0513")> 
+<Assembly: AssemblyFileVersion("6.1.0.0513")> 
