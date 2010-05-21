153f8246f927d15cb2144c9d3eb294c4c2f35f7a
JeroenS <jeroen.steenbeek@gmail.com>
2010-05-21 18:59:18 +0000

    Fixed compiler warning
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@3856 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterface/Ecosim/Tools/Fit to time series/frmFitToTimeSeries.Designer.vb
+++ Sources/ScientificInterface/Ecosim/Tools/Fit to time series/frmFitToTimeSeries.Designer.vb
@@ -500,7 +500,6 @@ Namespace Ecosim
             Me.m_sketchPad.ShowXMark = False
             Me.m_sketchPad.Size = New System.Drawing.Size(523, 386)
             Me.m_sketchPad.SketchDrawMode = ScientificInterfaceShared.Definitions.eSketchDrawModeTypes.Fill
-            Me.m_sketchPad.Style = ScientificInterfaceShared.Style.cStyleGuide.eStyleFlags.OK
             Me.m_sketchPad.TabIndex = 0
             Me.m_sketchPad.UIContext = Nothing
             Me.m_sketchPad.XMarkLabel = ""
