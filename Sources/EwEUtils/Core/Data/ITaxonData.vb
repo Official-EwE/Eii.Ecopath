96a0961e6c07a1821bddff851c9bbec0652c9b8c
JeroenS <jeroen.steenbeek@gmail.com>
2010-05-31 16:53:10 +0000

    Added ITaxonData
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@3888 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/EwEUtils/Core/Data/ITaxonData.vb
@@ -0,0 +1,29 @@
+﻿#Region " Imports "
+
+Option Strict On
+
+#End Region ' Imports
+
+Namespace Core
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Template for exchanging Taxonomy data.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Public Interface ITaxonData
+
+        ReadOnly Property CommonName() As String
+        ReadOnly Property [Class]() As String
+        ReadOnly Property Order() As String
+        ReadOnly Property Family() As String
+        ReadOnly Property Genus() As String
+        ReadOnly Property Species() As String
+        ReadOnly Property ISSCAAP() As String
+        ReadOnly Property URL() As String
+        ReadOnly Property Source() As String
+        ReadOnly Property LastUpdated() As Date
+
+    End Interface
+
+End Namespace
