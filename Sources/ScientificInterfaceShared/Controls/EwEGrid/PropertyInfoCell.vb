34b58c3ae9d6ed703eca1a9d9fa2fff6f80788e0
JeroenS <jeroen.steenbeek@gmail.com>
2010-06-14 19:09:18 +0000

    Moved property cells from Value Chain to here
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@3935 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/ScientificInterfaceShared/Controls/EwEGrid/PropertyInfoCell.vb
@@ -0,0 +1,121 @@
+#Region " Imports "
+
+Option Strict On
+Imports System.Drawing
+Imports System.Reflection
+Imports System.ComponentModel
+Imports SourceGrid2
+Imports ScientificInterfaceShared.Controls.EwEGrid
+Imports ScientificInterfaceShared.Style
+
+#End Region ' Imports
+
+Namespace Controls.EwEGrid
+
+    ''' ===========================================================================
+    ''' <summary>
+    ''' Cell that manages a single value in an object via a PropertyInfo instance.
+    ''' </summary>
+    ''' ===========================================================================
+    <CLSCompliant(False)> _
+    Public Class cPropertyInfoCell
+        : Inherits EwECell
+
+#Region " Privates "
+
+        ''' <summary>Object instance to manage the property value for.</summary>
+        Private m_obj As Object = Nothing
+        ''' <summary>PropertyInfo instance to manage the value for.</summary>
+        Private m_pi As PropertyInfo = Nothing
+
+#End Region ' Privates
+
+#Region " Constructor "
+
+        ''' -----------------------------------------------------------------------
+        ''' <summary>
+        ''' Constructor.
+        ''' </summary>
+        ''' <param name="obj">The object instance to manage the property value for.</param>
+        ''' <param name="pi">The PropertyInfo instance to manage the value for.</param>
+        ''' -----------------------------------------------------------------------
+        Public Sub New(ByVal obj As Object, _
+                       ByVal pi As PropertyInfo, _
+                       Optional ByVal style As cStyleGuide.eStyleFlags = cStyleGuide.eStyleFlags.OK)
+
+            ' Set the cell value to the intial property value and type
+            MyBase.New(pi.GetValue(obj, Nothing), pi.PropertyType, style)
+
+            ' Sanity checks
+            Debug.Assert(obj IsNot Nothing)
+            Debug.Assert(pi IsNot Nothing)
+
+            ' Store refs
+            Me.m_obj = obj
+            Me.m_pi = pi
+
+            ' Update cell edit state
+            Me.EditableMode = DirectCast(IIf(pi.CanWrite(), SourceGrid2.EditableMode.Default, SourceGrid2.EditableMode.None), SourceGrid2.EditableMode)
+            Me.EnableEdit = pi.CanWrite()
+            Me.SuppressZero = True
+
+            ' ToDo: respond to property changes by refreshing the cell value
+
+        End Sub
+
+#End Region ' Constructor
+
+#Region " Overrides "
+
+        ''' -----------------------------------------------------------------------
+        ''' <summary>
+        ''' Set the value in the underlying cell and PropertyInfo.
+        ''' </summary>
+        ''' <param name="pos"></param>
+        ''' <param name="objValue"></param>
+        ''' -----------------------------------------------------------------------
+        Public Overloads Sub SetValue(ByVal pos As SourceGrid2.Position, ByVal objValue As Object)
+
+            ' Update the cell value
+            MyBase.SetValue(pos, objValue)
+
+            ' Has attached object and property?
+            If (Me.m_obj IsNot Nothing) And (Me.m_pi IsNot Nothing) Then
+
+                ' #Yes: use the (hopefully adjusted) cell value to update the 
+                '       property in the underlying object.
+
+                Try
+                    ' PropertyInfo cannot perform type casts, we'll have to do this
+                    ' manually.
+
+                    ' Amazingly, Val is still the most robust value converter for
+                    ' converting strings to numerical values in VB.NET
+
+                    If (Me.m_pi.PropertyType Is GetType(Single)) Then
+                        Me.m_pi.SetValue(Me.m_obj, CSng(Val(Me.Value)), Nothing)
+                    ElseIf (Me.m_pi.PropertyType Is GetType(String)) Then
+                        Me.m_pi.SetValue(Me.m_obj, CStr(Me.Value), Nothing)
+                    ElseIf (Me.m_pi.PropertyType Is GetType(Integer)) Then
+                        Me.m_pi.SetValue(Me.m_obj, CInt(Val(Me.Value)), Nothing)
+                    ElseIf (Me.m_pi.PropertyType Is GetType(Double)) Then
+                        Me.m_pi.SetValue(Me.m_obj, CDbl(Val(Me.Value)), Nothing)
+                    ElseIf (Me.m_pi.PropertyType Is GetType(Boolean)) Then
+                        Me.m_pi.SetValue(Me.m_obj, Convert.ToBoolean(Me.Value), Nothing)
+                    Else
+                        Debug.Assert(False, String.Format("Value type '{0}' not supported yet in PICell", Me.m_pi.PropertyType))
+                    End If
+
+                Catch ex As Exception
+                    ' Kaboom
+                    Debug.Assert(False, ex.Message)
+                End Try
+
+            End If
+        End Sub
+
+#End Region ' Overrides
+
+    End Class
+
+End Namespace
