b8070730505f28edd5d45278d5212128f33fc82a
JeroenS <jeroen.steenbeek@gmail.com>
2010-06-22 14:06:11 +0000

    Added CEFAS results extractor plugin
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@3958 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECustomPlugins/EwEResultsExtractorPlugin/My Project/AssemblyInfo.vb
+++ Sources/EwECustomPlugins/EwEResultsExtractorPlugin/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.1.0.0504")> 
-<Assembly: AssemblyFileVersion("6.1.0.0504")> 
+<Assembly: AssemblyVersion("6.1.0.0610")> 
+<Assembly: AssemblyFileVersion("6.1.0.0610")> 
