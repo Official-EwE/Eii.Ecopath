ffcc669502b36e500d88ce5e38d762f286e6a743
JeroenS <jeroen.steenbeek@gmail.com>
2010-07-08 20:34:46 +0000

    Added IConfigurablePersistPlugin
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@3987 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/EwEPlugin/Plugin templates/GUI/IConfigurablePersistPlugin.vb
@@ -0,0 +1,36 @@
+﻿#Region " Imports "
+
+Option Strict On
+Imports System.Windows.Forms
+Imports System.Xml
+
+#End Region ' Imports
+
+''' ===========================================================================
+''' <summary>
+''' Plugin point that allows a configurabe plug-in to store and retrieve its
+''' settings
+''' </summary>
+''' ===========================================================================
+Public Interface IConfigurablePersistPlugin
+    Inherits IConfigurablePlugin
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Enables a configurable plug-in to read its configuration from an XML
+    ''' node. This node will have the same name as the plug-in.
+    ''' </summary>
+    ''' <param name="node">The node to read configuration information from.</param>
+    ''' -----------------------------------------------------------------------
+    Sub Read(ByVal node As XmlNode)
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Enables a configurable plug-in to write its configuration to an XML
+    ''' node. This node will have the same name as the plug-in.
+    ''' </summary>
+    ''' <param name="node">The node to write configuration information to.</param>
+    ''' -----------------------------------------------------------------------
+    Sub Write(ByVal node As XmlNode)
+
+End Interface
