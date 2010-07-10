d1a5cec23a6844a42aa64f7e3a0955053ff6f240
automatedbuild <automatedbuild@ecopath.org>
2010-07-10 18:58:51 +0000

    Build 6.1.0.0710
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@3995 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECustomPlugins/EwEFlowDiagramPlugin/My Project/AssemblyInfo.vb
+++ Sources/EwECustomPlugins/EwEFlowDiagramPlugin/My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("6.1.0.0709")> 
-<Assembly: AssemblyFileVersion("6.1.0.0709")> 
+<Assembly: AssemblyVersion("6.1.0.0710")> 
+<Assembly: AssemblyFileVersion("6.1.0.0710")> 
--- Sources/EwECustomPlugins/EwEValueChain/My Project/AssemblyInfo.vb
+++ Sources/EwECustomPlugins/EwEValueChain/My Project/AssemblyInfo.vb
@@ -32,5 +32,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.1.0.0709")> 
-<Assembly: AssemblyFileVersion("6.1.0.0709")> 
+<Assembly: AssemblyVersion("6.1.0.0710")> 
+<Assembly: AssemblyFileVersion("6.1.0.0710")> 
