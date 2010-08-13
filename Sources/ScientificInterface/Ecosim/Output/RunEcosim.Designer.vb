ead59bba1ed02957d9c1cbfe03fb43c08613dcb3
JeroenS <jeroen.steenbeek@gmail.com>
2010-08-13 13:09:21 +0000

    Removed deprecated bits
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@4099 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterface/Ecosim/Output/RunEcosim.Designer.vb
+++ Sources/ScientificInterface/Ecosim/Output/RunEcosim.Designer.vb
@@ -167,7 +167,6 @@ Namespace Ecosim
             Me.m_sketchPad.ShapeColor = System.Drawing.Color.AliceBlue
             Me.m_sketchPad.ShowXMark = False
             Me.m_sketchPad.SketchDrawMode = ScientificInterfaceShared.Definitions.eSketchDrawModeTypes.Fill
-            Me.m_sketchPad.Style = ScientificInterfaceShared.Style.cStyleGuide.eStyleFlags.OK
             Me.m_sketchPad.UIContext = Nothing
             Me.m_sketchPad.XMarkLabel = ""
             Me.m_sketchPad.XMarkValue = -9999.0!
