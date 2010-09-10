5a0604f97d169d21c971f1358ec1404172581077
JeroenS <jeroen.steenbeek@gmail.com>
2010-09-10 18:53:08 +0000

    Added cDrawingUtils
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@4218 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/EwEUtils/Utilities/cDrawingUtils.vb
@@ -0,0 +1,65 @@
+﻿#Region " Imports "
+
+Option Strict On
+Imports System
+Imports System.Drawing
+
+#End Region ' Imports
+
+Namespace Utilities
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Class providing a collection of <see cref="Drawing">Drawing</see>-related utility methods.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Public Class cDrawingUtils
+
+        ''' -------------------------------------------------------------------
+        ''' <summary>
+        ''' Convert a <see cref="ContentAlignment">ContentAlignment</see> flag
+        ''' into a <see cref="StringFormat">StringFormat</see> flag.
+        ''' </summary>
+        ''' <param name="ca">Content alignment flag to convert.</param>
+        ''' <returns>A StringFormat value.</returns>
+        ''' -------------------------------------------------------------------
+        Public Shared Function ContentAlignmentToStringFormat(ByVal ca As System.Drawing.ContentAlignment) As StringFormat
+            Dim style As New StringFormat()
+
+            Select Case ca
+                Case ContentAlignment.BottomLeft
+                    style.Alignment = StringAlignment.Near
+                    style.LineAlignment = StringAlignment.Far
+                Case ContentAlignment.BottomRight
+                    style.Alignment = StringAlignment.Far
+                    style.LineAlignment = StringAlignment.Far
+                Case ContentAlignment.BottomCenter
+                    style.Alignment = StringAlignment.Center
+                    style.LineAlignment = StringAlignment.Far
+                Case ContentAlignment.MiddleLeft
+                    style.Alignment = StringAlignment.Near
+                    style.LineAlignment = StringAlignment.Center
+                Case ContentAlignment.MiddleRight
+                    style.Alignment = StringAlignment.Far
+                    style.LineAlignment = StringAlignment.Center
+                Case ContentAlignment.MiddleCenter
+                    style.Alignment = StringAlignment.Center
+                    style.LineAlignment = StringAlignment.Center
+                Case ContentAlignment.TopLeft
+                    style.Alignment = StringAlignment.Near
+                    style.LineAlignment = StringAlignment.Near
+                Case ContentAlignment.TopRight
+                    style.Alignment = StringAlignment.Far
+                    style.LineAlignment = StringAlignment.Near
+                Case ContentAlignment.TopCenter
+                    style.Alignment = StringAlignment.Center
+                    style.LineAlignment = StringAlignment.Near
+            End Select
+            Return style
+
+        End Function
+
+    End Class
+
+End Namespace
+
