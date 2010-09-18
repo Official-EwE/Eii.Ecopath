7347c74ef6338764ca8b7eca328ea6153e043568
JeroenS <jeroen.steenbeek@gmail.com>
2010-09-18 18:13:30 +0000

    MLD cannot be 0
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@4288 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECore/EcoSpace/Input Output/EcospaceBasemap/cEcospaceBasemap.vb
+++ Sources/EwECore/EcoSpace/Input Output/EcospaceBasemap/cEcospaceBasemap.vb
@@ -130,7 +130,8 @@ Public Class cEcospaceBasemap
             m_values.Add(val.varName, val)
 
             ' LayerMLD
-            meta = New cVariableMetaData(0, Single.MaxValue, cOperatorManager.getOperator(eOperators.GreaterThan), cOperatorManager.getOperator(eOperators.LessThan))
+            ' # min value is set to 1; users should not be able to enter MLD values of 0.
+            meta = New cVariableMetaData(1, Single.MaxValue, cOperatorManager.getOperator(eOperators.GreaterThan), cOperatorManager.getOperator(eOperators.LessThan))
             val = New cValue(0, eVarNameFlags.LayerMLD, eStatusFlags.Null, eValueTypes.Sng, meta, m_core.m_validators.getValidator(eVarNameFlags.NotSet))
             m_values.Add(val.varName, val)
 
