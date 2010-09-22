8bbd0d689251c3bbd9f42dedfc1a11fcf40999c3
JeroenS <jeroen.steenbeek@gmail.com>
2010-09-22 18:36:58 +0000

    Removed empty overrides
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@4336 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterface/Ecospace/Input/gridEditImportanceLayers.vb
+++ Sources/ScientificInterface/Ecospace/Input/gridEditImportanceLayers.vb
@@ -430,39 +430,6 @@ Namespace Ecospace
 
         ''' -----------------------------------------------------------------------
         ''' <summary>
-        ''' Called Update local admin based on cell value changes.
-        ''' </summary>
-        ''' <returns>
-        ''' True if the value change is allowed, False to block the value change.
-        ''' </returns>
-        ''' <remarks>
-        ''' This method differs from OnCellValueEdited; during a cell value 
-        ''' change notification (at the end of an edit operation) it is unsafe
-        ''' to modify the value of the cell being edited. However, the end edit 
-        ''' event will not be triggered for particular specialized cells which
-        ''' makes this method mandatory. We once again apologize for the confusion; )
-        ''' </remarks>
-        ''' -----------------------------------------------------------------------
-        Protected Overrides Function OnCellValueChanged(ByVal p As Position, ByVal cell As Cells.ICellVirtual) As Boolean
-
-            If Not Me.AllowUpdates Then Return True
-
-            Dim li As LayerInfo = DirectCast(Me.m_alLayers(p.Row - 1), LayerInfo)
-
-            Select Case DirectCast(p.Column, eColumnTypes)
-
-                Case eColumnTypes.LayerName
-                    ' JS: Handled in OnCellEdited()
-                    ' li.Name = CStr(cell.GetValue(p))
-
-            End Select
-
-            Return True
-
-        End Function
-
-        ''' -----------------------------------------------------------------------
-        ''' <summary>
         ''' Called when the user has finished editing a cell. Handled to update 
         ''' local admin based on cell value changes.
         ''' </summary>
