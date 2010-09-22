3f1b5c35e40775eb4976d4a41a3877bbe651360a
JeroenS <jeroen.steenbeek@gmail.com>
2010-09-22 18:37:41 +0000

    Removed empty overrides
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@4339 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterface/Ecopath/gridEditGroupTaxon.vb
+++ Sources/ScientificInterface/Ecopath/gridEditGroupTaxon.vb
@@ -731,27 +731,6 @@ Public Class gridEditGroupTaxon
 
     ''' -----------------------------------------------------------------------
     ''' <summary>
-    ''' Called Update local admin based on cell value changes.
-    ''' </summary>
-    ''' <returns>
-    ''' True if the value change is allowed, False to block the value change.
-    ''' </returns>
-    ''' <remarks>
-    ''' This method differs from OnCellValueEdited; during a cell value 
-    ''' change notification (at the end of an edit operation) it is unsafe
-    ''' to modify the value of the cell being edited. However, the end edit 
-    ''' event will not be triggered for particular specialized cells which
-    ''' makes this method mandatory. We once again apologize for the confusion; )
-    ''' </remarks>
-    ''' -----------------------------------------------------------------------
-    Protected Overrides Function OnCellValueChanged(ByVal p As Position, ByVal cell As Cells.ICellVirtual) As Boolean
-
-        Return True
-
-    End Function
-
-    ''' -----------------------------------------------------------------------
-    ''' <summary>
     ''' Called when the user has finished editing a cell. Handled to update 
     ''' local admin based on cell value changes.
     ''' </summary>
