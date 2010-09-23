8bda68db66591a63c2b8c13b6799f7d7c7d24aaf
JeroenS <jeroen.steenbeek@gmail.com>
2010-09-23 00:29:26 +0000

    Started pedigree interface
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@4342 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/ScientificInterface/Ecopath/Input/frmPedigree.vb
@@ -0,0 +1,170 @@
+﻿#Region " Imports "
+
+Option Strict On
+
+Imports EwEUtils.Core
+Imports EwECore
+
+#End Region ' Imports
+
+Public Class frmPedigree
+
+#Region " Helper classes "
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Item for showing a pedigree level in the pedigree level listbox.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Private Class cPedigreeLevelListboxItem
+
+        Private m_level As cPedigreeLevel = Nothing
+
+        Public Sub New(ByVal level As cPedigreeLevel)
+            Me.m_level = level
+        End Sub
+
+        Public ReadOnly Property Level() As cPedigreeLevel
+            Get
+                Return Me.m_level
+            End Get
+        End Property
+
+        Public Overrides Function ToString() As String
+            Return Me.m_level.Name
+        End Function
+
+    End Class
+
+#End Region ' Helper classes
+
+#Region " Private vars "
+
+    ''' <summary>Varname currently 'selected' in the grid.</summary>
+    Private m_varname As eVarNameFlags = eVarNameFlags.NotSet
+
+#End Region ' Private vars
+
+    Public Sub New()
+        Me.InitializeComponent()
+    End Sub
+
+#Region " Form overloads "
+
+    Protected Overrides Sub OnLoad(ByVal e As System.EventArgs)
+        MyBase.OnLoad(e)
+
+        If (Me.UIContext Is Nothing) Then Return
+
+        AddHandler Me.m_grid.OnSelectionChanged, AddressOf OnGridSelectionChanged
+
+    End Sub
+
+    Protected Overrides Sub OnFormClosed(ByVal e As FormClosedEventArgs)
+
+        RemoveHandler Me.m_grid.OnSelectionChanged, AddressOf OnGridSelectionChanged
+
+        ' Clean up
+        Me.SelectedVariable = eVarNameFlags.NotSet
+
+        ' Done
+        MyBase.OnFormClosed(e)
+
+    End Sub
+
+#End Region ' Form overloads
+
+#Region " Events "
+
+    Private Sub OnGridSelectionChanged(ByVal sel As SourceGrid2.CellVirtualCollection)
+
+        ' ToDo:
+        '   Allow only selections that span 1 column, no headers
+        '   Extract var name from column
+        '   Apply var name
+
+    End Sub
+
+    Private Sub OnViewAsChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) _
+        Handles m_tscmbViewAs.Click
+
+        ' ToDo:
+        '   Store display style somewhere
+        '   Invalidate listbox to reflect this style
+        '   Inform grid of new style
+
+    End Sub
+
+#End Region ' Events
+
+#Region " Internal implementation "
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Get/set the selected <see cref="eVarNameFlags">variable</see> in the grid.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Private Property SelectedVariable() As eVarNameFlags
+        Get
+            Return Me.m_varname
+        End Get
+        Set(ByVal value As eVarNameFlags)
+            ' Sanity check
+            If (Me.UIContext Is Nothing) Then Return
+            ' Optimization
+            If (value = Me.m_varname) Then Return
+            ' Clean up
+            If (Me.m_varname <> eVarNameFlags.NotSet) Then
+                Me.DestroyPedigreeControls()
+            End If
+            ' Remember new
+            Me.m_varname = value
+            ' Build new
+            If (Me.m_varname <> eVarNameFlags.NotSet) Then
+                Me.BuildPedigreeControls()
+            End If
+        End Set
+    End Property
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Get/set the selected <see cref="cPedigreeLevel">pedigree level</see> in
+    ''' the listbox with available levels.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Private Property SelectedLevel() As cPedigreeLevel
+        Get
+            If (Me.m_lbLevels.SelectedItem Is Nothing) Then Return Nothing
+            Return DirectCast(Me.m_lbLevels.SelectedItem, cPedigreeLevelListboxItem).Level
+        End Get
+        Set(ByVal value As cPedigreeLevel)
+
+        End Set
+    End Property
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Lock the current <see cref="SelectedVariable">selected variable</see>.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Private Sub BuildPedigreeControls()
+
+    End Sub
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Release the current <see cref="SelectedVariable">selected variable</see>.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Private Sub DestroyPedigreeControls()
+        Me.m_lbLevels.Items.Clear()
+    End Sub
+
+    Private Sub UpdateControls()
+
+    End Sub
+
+#End Region ' Internal implementation
+
+
+End Class
---
+++ Sources/ScientificInterface/Ecopath/Input/gridPedigree.vb
@@ -0,0 +1,16 @@
+﻿#Region " Imports "
+
+Option Strict On
+
+#End Region 'Imports
+
+<CLSCompliant(False)> _
+Public Class gridPedigree
+    Inherits EwEGrid
+
+
+    Protected Overrides Sub FillData()
+
+    End Sub
+
+End Class
