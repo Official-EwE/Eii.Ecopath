825ec5c1c1c8c170646de553dd3bb59321be2504
JeroenS <jeroen.steenbeek@gmail.com>
2010-09-25 00:27:41 +0000

    Starting to populate
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@4367 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterface/Ecopath/Tools/gridPedigree.vb
+++ Sources/ScientificInterface/Ecopath/Tools/gridPedigree.vb
@@ -3,6 +3,7 @@
 Option Strict On
 
 Imports EwECore
+Imports EwEUtils.Core
 
 #End Region 'Imports
 
@@ -12,56 +13,62 @@ Namespace Ecopath.Tools
     Public Class gridPedigree
         Inherits EwEGrid
 
-        ''' -------------------------------------------------------------------
-        ''' <summary>
-        ''' cColorCellVisualizer is a cell visualizer that provides color feedback.
-        ''' </summary>
-        ''' -------------------------------------------------------------------
-        <CLSCompliant(False)> _
-        Public Class cColorCellVisualizer
-            Inherits SourceGrid2.VisualModels.Common
+        Public Sub New()
+            MyBase.New()
+            Me.FixedColumnWidths = False
+        End Sub
 
-            Private m_viz As frmPedigree.cPedigreeVisualizer = Nothing
+        Protected Overrides Sub InitStyle()
+            MyBase.InitStyle()
 
-            Public Sub New(ByVal viz As frmPedigree.cPedigreeVisualizer)
-                Me.m_viz = viz
-            End Sub
+            ' Test for UI context to prevent core from being accessed
+            If (Me.UIContext Is Nothing) Then Return
 
-            ''' -------------------------------------------------------------------
-            ''' <summary>
-            ''' Overidden to render cell value as a color
-            ''' </summary>
-            ''' -------------------------------------------------------------------
-            Protected Overrides Sub DrawCell_ImageAndText( _
-                    ByVal cell As SourceGrid2.Cells.ICellVirtual, _
-                    ByVal pos As SourceGrid2.Position, _
-                    ByVal e As System.Windows.Forms.PaintEventArgs, _
-                    ByVal rcClient As System.Drawing.Rectangle, _
-                    ByVal status As SourceGrid2.DrawCellStatus)
+            Dim group As cCoreGroupBase = Nothing
 
-                'If Not (TypeOf value Is Color) Then Return
+            Me.Redim(Core.nGroups + 1, cPedigreeManager.SupportVariables.Count + 2)
 
-                'Dim clr As Color = DirectCast(value, Color)
-                'Dim rcColor As New Rectangle(rcClient.X + 2, rcClient.Y + 2, rcClient.Width - 4, rcClient.Height - 4)
+            Me(0, 0) = New EwEColumnHeaderCell("")
+            Me(0, 1) = New EwEColumnHeaderCell(My.Resources.HEADER_GROUPNAME)
 
-                '' Draw the background
-                'Using br As New SolidBrush(clr)
-                '    e.Graphics.FillRectangle(br, rcColor)
-                'End Using
+            For iVariable As Integer = 0 To cPedigreeManager.SupportVariables.Count - 1
+                Me(0, iVariable + 2) = New EwEColumnHeaderCell(cPedigreeManager.SupportVariables(iVariable).ToString)
+            Next iVariable
 
-            End Sub
+            For iGroup As Integer = 1 To Core.nGroups
+                group = Me.Core.EcoPathGroupInputs(iGroup)
+                Me(iGroup, 0) = New PropertyRowHeaderCell(Me.PropertyManager, group, eVarNameFlags.Index)
+                Me(iGroup, 1) = New PropertyRowHeaderCell(Me.PropertyManager, group, eVarNameFlags.Name)
+            Next iGroup
 
-        End Class
+            Me.FixedColumns = 2
 
+        End Sub
 
         Protected Overrides Sub FillData()
 
-            ' Populate grid with cells of different types based on settings:
-            '   - ColorCell
-            '   - Single cell
-            '   - Integer cell
+            Dim group As cCoreGroupBase = Nothing
+            Dim man As cPedigreeManager = Nothing
+            Dim cell As EwECellBase = Nothing
+            Dim style As cStyleGuide.eStyleFlags = (cStyleGuide.eStyleFlags.NotEditable Or cStyleGuide.eStyleFlags.ValueComputed)
 
+            For iGroup As Integer = 1 To Core.nGroups
+                ' Get group
+                group = Me.Core.EcoPathGroupInputs(iGroup)
+                For iVariable As Integer = 0 To cPedigreeManager.SupportVariables.Count - 1
+                    ' Get pedigree
+                    man = Me.Core.GetPedigreeManager(cPedigreeManager.SupportVariables(iVariable))
+                    cell = New PropertyCell(Me.PropertyManager, group, eVarNameFlags.Pedigree, man)
+                    Me(iGroup, 2 + iVariable) = cell
+                Next
+            Next
         End Sub
+
+        Public Overrides ReadOnly Property MessageSource() As eCoreComponentType
+            Get
+                Return eCoreComponentType.EcoPath
+            End Get
+        End Property
 
     End Class
 
