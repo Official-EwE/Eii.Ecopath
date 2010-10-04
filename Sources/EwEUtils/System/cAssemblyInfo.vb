a992f96c117185b0ac338c0cde41964104bb646d
JeroenS <jeroen.steenbeek@gmail.com>
2010-10-04 15:37:43 +0000

    Added version attributes
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@4499 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwEUtils/System/cAssemblyInfo.vb
+++ Sources/EwEUtils/System/cAssemblyInfo.vb
@@ -2,6 +2,7 @@
 
 Option Strict On
 Imports System.Reflection
+Imports System
 
 #End Region ' Imports
 
@@ -60,6 +61,27 @@ Namespace SystemUtilities
         ''' ------------------------------------------------------------------------
         ReadOnly Property Trademark() As String
 
+        ''' ------------------------------------------------------------------------
+        ''' <summary>
+        ''' Get the value for an assembly 'AssemblyVersion' attribute.
+        ''' </summary>
+        ''' ------------------------------------------------------------------------
+        ReadOnly Property AssemblyVersion() As String
+
+        ''' ------------------------------------------------------------------------
+        ''' <summary>
+        ''' Get the value for an assembly 'AssemblyInformationalVersion' attribute.
+        ''' </summary>
+        ''' ------------------------------------------------------------------------
+        ReadOnly Property ProductVersion() As String
+
+        ''' ------------------------------------------------------------------------
+        ''' <summary>
+        ''' Get the value for an assembly 'AssemblyFileVersion' attribute.
+        ''' </summary>
+        ''' ------------------------------------------------------------------------
+        ReadOnly Property FileVersion() As String
+
     End Interface
 
     ''' ===========================================================================
@@ -97,11 +119,7 @@ Namespace SystemUtilities
 
 #Region " Public properties "
 
-        ''' ------------------------------------------------------------------------
-        ''' <summary>
-        ''' Get the value for an assembly 'Company' attribute.
-        ''' </summary>
-        ''' ------------------------------------------------------------------------
+        ''' <inheritdoc cref="IAssemblyInfo.Company"/>
         Public ReadOnly Property Company() As String _
             Implements IAssemblyInfo.Company
             Get
@@ -111,11 +129,7 @@ Namespace SystemUtilities
             End Get
         End Property
 
-        ''' ------------------------------------------------------------------------
-        ''' <summary>
-        ''' Get the value for an assembly 'Copyright' attribute.
-        ''' </summary>
-        ''' ------------------------------------------------------------------------
+        ''' <inheritdoc cref="IAssemblyInfo.Copyright"/>
         Public ReadOnly Property Copyright() As String _
             Implements IAssemblyInfo.Copyright
             Get
@@ -125,13 +139,9 @@ Namespace SystemUtilities
             End Get
         End Property
 
-        ''' ------------------------------------------------------------------------
-        ''' <summary>
-        ''' Get the value for an assembly 'Description' attribute.
-        ''' </summary>
-        ''' ------------------------------------------------------------------------
+        ''' <inheritdoc cref="IAssemblyInfo.Description"/>
         Public ReadOnly Property Description() As String _
-             Implements IAssemblyInfo.Description
+            Implements IAssemblyInfo.Description
             Get
                 Dim da As AssemblyDescriptionAttribute = Nothing
                 da = DirectCast(m_ass.GetCustomAttributes(GetType(AssemblyDescriptionAttribute), False)(0), AssemblyDescriptionAttribute)
@@ -139,13 +149,9 @@ Namespace SystemUtilities
             End Get
         End Property
 
-        ''' ------------------------------------------------------------------------
-        ''' <summary>
-        ''' Get the value for an assembly 'Product' attribute.
-        ''' </summary>
-        ''' ------------------------------------------------------------------------
+        ''' <inheritdoc cref="IAssemblyInfo.Product"/>
         Public ReadOnly Property Product() As String _
-                Implements IAssemblyInfo.Product
+            Implements IAssemblyInfo.Product
             Get
                 Dim pa As AssemblyProductAttribute = Nothing
                 pa = DirectCast(m_ass.GetCustomAttributes(GetType(AssemblyProductAttribute), False)(0), AssemblyProductAttribute)
@@ -153,13 +159,9 @@ Namespace SystemUtilities
             End Get
         End Property
 
-        ''' ------------------------------------------------------------------------
-        ''' <summary>
-        ''' Get the value for an assembly 'Title' attribute.
-        ''' </summary>
-        ''' ------------------------------------------------------------------------
+        ''' <inheritdoc cref="IAssemblyInfo.Title"/>
         Public ReadOnly Property Title() As String _
-               Implements IAssemblyInfo.Title
+            Implements IAssemblyInfo.Title
             Get
                 Dim ta As AssemblyTitleAttribute = Nothing
                 ta = DirectCast(m_ass.GetCustomAttributes(GetType(AssemblyTitleAttribute), False)(0), AssemblyTitleAttribute)
@@ -167,17 +169,44 @@ Namespace SystemUtilities
             End Get
         End Property
 
-        ''' ------------------------------------------------------------------------
-        ''' <summary>
-        ''' Get the value for an assembly 'Trademark' attribute.
-        ''' </summary>
-        ''' ------------------------------------------------------------------------
+        ''' <inheritdoc cref="IAssemblyInfo.Trademark"/>
         Public ReadOnly Property Trademark() As String _
-             Implements IAssemblyInfo.Trademark
+            Implements IAssemblyInfo.Trademark
             Get
                 Dim ta As AssemblyTrademarkAttribute = Nothing
                 ta = DirectCast(m_ass.GetCustomAttributes(GetType(AssemblyTrademarkAttribute), False)(0), AssemblyTrademarkAttribute)
                 Return ta.Trademark.ToString
+            End Get
+        End Property
+
+        ''' <inheritdoc cref="IAssemblyInfo.AssemblyVersion"/>
+        Public ReadOnly Property AssemblyVersion() As String _
+            Implements IAssemblyInfo.AssemblyVersion
+            Get
+                Dim ca As AssemblyVersionAttribute = Nothing
+                ca = DirectCast(m_ass.GetCustomAttributes(GetType(AssemblyVersionAttribute), False)(0), AssemblyVersionAttribute)
+                If ca Is Nothing Then Return Nothing
+                Return ca.Version
+            End Get
+        End Property
+
+        ''' <inheritdoc cref="IAssemblyInfo.FileVersion"/>
+        Public ReadOnly Property FileVersion() As String Implements IAssemblyInfo.FileVersion
+            Get
+                Dim ca As AssemblyFileVersionAttribute = Nothing
+                ca = DirectCast(m_ass.GetCustomAttributes(GetType(AssemblyFileVersionAttribute), False)(0), AssemblyFileVersionAttribute)
+                If ca Is Nothing Then Return Nothing
+                Return ca.Version
+            End Get
+        End Property
+
+        ''' <inheritdoc cref="IAssemblyInfo.ProductVersion"/>
+        Public ReadOnly Property ProductVersion() As String Implements IAssemblyInfo.ProductVersion
+            Get
+                Dim ca As AssemblyInformationalVersionAttribute = Nothing
+                ca = DirectCast(m_ass.GetCustomAttributes(GetType(AssemblyInformationalVersionAttribute), False)(0), AssemblyInformationalVersionAttribute)
+                If ca Is Nothing Then Return Nothing
+                Return ca.ToString
             End Get
         End Property
 
