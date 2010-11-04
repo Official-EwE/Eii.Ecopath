91624fc479bc07bda0693a82d0fbf3218633e344
JeroenS <jeroen.steenbeek@gmail.com>
2010-11-04 17:42:21 +0000

    Made localizable
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@4697 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterface/Ecosim/Output/SuitabilityPlot.Designer.vb
+++ Sources/ScientificInterface/Ecosim/Output/SuitabilityPlot.Designer.vb
@@ -26,26 +26,25 @@ Partial Class SuitabilityPlot
         'Do not modify it using the code editor.
         <System.Diagnostics.DebuggerStepThrough()> _
         Private Sub InitializeComponent()
+            Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(SuitabilityPlot))
             Me.m_plot = New ScientificInterface.ucSuitabilityPlot
             Me.SuspendLayout()
             '
             'm_plot
             '
-            Me.m_plot.Dock = System.Windows.Forms.DockStyle.Fill
-            Me.m_plot.Location = New System.Drawing.Point(0, 0)
+            resources.ApplyResources(Me.m_plot, "m_plot")
             Me.m_plot.Name = "m_plot"
-            Me.m_plot.Size = New System.Drawing.Size(416, 266)
-            Me.m_plot.TabIndex = 0
+            Me.m_plot.UIContext = Nothing
             '
             'SuitabilityPlot
             '
-            Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+            resources.ApplyResources(Me, "$this")
             Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-            Me.ClientSize = New System.Drawing.Size(416, 266)
+            Me.ControlBox = False
             Me.Controls.Add(Me.m_plot)
-            Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
             Me.Name = "SuitabilityPlot"
-            Me.Text = "SuitabilityPlot"
+            Me.ShowIcon = False
+            Me.ShowInTaskbar = False
             Me.ResumeLayout(False)
 
         End Sub
