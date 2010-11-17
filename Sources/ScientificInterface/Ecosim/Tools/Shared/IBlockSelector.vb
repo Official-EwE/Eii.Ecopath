9082869e001586dbaa72b7ca91276dbec0ffd390
joeb <joeb@ecopath.org>
2010-11-17 21:28:11 +0000

    Committed IBlockSelector
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@4809 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/ScientificInterface/Ecosim/Tools/Shared/IBlockSelector.vb
@@ -0,0 +1,56 @@
+﻿#Region " Imports "
+
+Option Explicit On
+Option Strict On
+
+Imports ScientificInterface.Other
+
+#End Region ' Imports
+
+Namespace Ecosim
+
+    Public Interface IBlockSelector
+        Inherits IUIElement
+
+
+        ''' -------------------------------------------------------------------
+        ''' <summary>
+        ''' Event notifying that the number of blocks have changed.
+        ''' </summary>
+        ''' <param name="sender">
+        ''' The <see cref="ucParmBlockCodes">block code parameters control</see>
+        ''' that sent this event.
+        ''' </param>
+        ''' -------------------------------------------------------------------
+        Event OnNumBlocksChanged(ByVal sender As IBlockSelector)
+
+
+        ''' -------------------------------------------------------------------
+        ''' <summary>
+        ''' Event notifying that selected block has changed.
+        ''' </summary>
+        ''' <param name="sender">
+        ''' The <see cref="ucParmBlockCodes">block code parameters control</see>
+        ''' that sent this event.
+        ''' </param>
+        ''' -------------------------------------------------------------------
+        Event OnBlockSelected(ByVal sender As IBlockSelector)
+
+        ''' <summary>
+        ''' Value of a cell (CV) has changed
+        ''' </summary>
+        Event onValueChanged(ByVal newValue As Single, ByVal Index As Integer)
+
+        Property NumBlocks() As Integer
+        Property SelectedBlock() As Integer
+        ReadOnly Property BlockColors() As Color()
+        ReadOnly Property BlockColor(ByVal iBlock As Integer) As Color
+        ReadOnly Property SelectedBlockColor() As Color
+
+        Function ValuetoBlock(ByVal cv As Single) As Integer
+        Function BlocktoValue(ByVal iBlock As Integer) As Single
+
+
+    End Interface
+
+End Namespace
