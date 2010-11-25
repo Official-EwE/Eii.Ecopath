7192b24b313dd4fcc58af82f5dc96b608c3df0a5
JeroenS <jeroen.steenbeek@gmail.com>
2010-11-25 12:47:08 +0000

    Fixed warnings
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@4857 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterface/Ecosim/Output/RunEcosim.Designer.vb
+++ Sources/ScientificInterface/Ecosim/Output/RunEcosim.Designer.vb
@@ -171,7 +171,6 @@ Namespace Ecosim
             Me.m_sketchPad.ShowXMark = False
             Me.m_sketchPad.ShowYMark = False
             Me.m_sketchPad.SketchDrawMode = ScientificInterfaceShared.Definitions.eSketchDrawModeTypes.Fill
-            Me.m_sketchPad.Style = ScientificInterfaceShared.Style.cStyleGuide.eStyleFlags.OK
             Me.m_sketchPad.UIContext = Nothing
             Me.m_sketchPad.XMarkLabel = ""
             Me.m_sketchPad.XMarkValue = -9999.0!
