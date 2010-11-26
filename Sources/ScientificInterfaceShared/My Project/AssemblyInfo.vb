9e26dc6c19c0590910261d9e81821b41e6cfbd14
automatedbuild <automatedbuild@ecopath.org>
2010-11-26 10:36:22 +0000

    Build 6.1.1.1126
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@4864 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterfaceShared/My Project/AssemblyInfo.vb
+++ Sources/ScientificInterfaceShared/My Project/AssemblyInfo.vb
@@ -33,5 +33,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.1.1.1125")> 
-<Assembly: AssemblyFileVersion("6.1.1.1125")> 
+<Assembly: AssemblyVersion("6.1.1.1126")> 
+<Assembly: AssemblyFileVersion("6.1.1.1126")> 
