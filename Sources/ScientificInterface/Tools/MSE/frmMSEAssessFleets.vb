4f21c61f4c08f3160bc5296852e4a28ef5458d30
JeroenS <jeroen.steenbeek@gmail.com>
2010-12-01 22:58:51 +0000

    Fixed cleanup issue
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@4892 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterface/Tools/MSE/frmMSEAssessFleets.vb
+++ Sources/ScientificInterface/Tools/MSE/frmMSEAssessFleets.vb
@@ -58,16 +58,16 @@ Public Class frmMSEAssessFleets
     End Sub
 
     Protected Overrides Sub OnFormClosed(ByVal e As System.Windows.Forms.FormClosedEventArgs)
+
         Try
-            MyBase.OnFormClosed(e)
+            RemoveHandler Me.StyleGuide.StyleGuideChanged, AddressOf OnStyleGuideChanged
+            RemoveHandler Me.m_fpStartYear.PropertyChanged, AddressOf OnLastYearChanged
         Catch ex As Exception
             Debug.Assert(False, Me.ToString & " Exception: " & ex.Message)
         End Try
 
         Try
-
-            RemoveHandler Me.StyleGuide.StyleGuideChanged, AddressOf OnStyleGuideChanged
-            RemoveHandler Me.m_fpStartYear.PropertyChanged, AddressOf OnLastYearChanged
+            MyBase.OnFormClosed(e)
         Catch ex As Exception
             Debug.Assert(False, Me.ToString & " Exception: " & ex.Message)
         End Try
