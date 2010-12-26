48c808dc62430801bc03526a713b215f2971f62d
JeroenS <jeroen.steenbeek@gmail.com>
2010-12-26 20:12:57 +0000

    Fixed comment errors
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@5055 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwETutorials/EwEConsoleAppExample/EwE7.vb
+++ Sources/EwETutorials/EwEConsoleAppExample/EwE7.vb
@@ -6,38 +6,24 @@ Imports EwEUtils.Core
 
 Module EwE7
 
-    ' Main routine is called when the program is run.
+    ' Main Sub routine called when program is run.
     Sub Main()
+        Dim core As New cCore()                     ' (1) Define core and database variables
+        Dim ds As IEwEDataSource = cDataSourceFactory.Create(eDataSourceTypes.ACCDB)
 
-        ' (1) Create a new core
-        Dim core As New cCore()
-        ' (2) Create a datasource to read 'mdb' formatted data
-        Dim ds As IEwEDataSource = cDataSourceFactory.Create(eDataSourceTypes.MDB)
+        ds.Open("baltic.ewemdb", core)              ' (2) Tell the datasource to open a database called baltic.ewemdb 
 
-        ' (3) Tell the datasource to open a database called 'baltic.ewemdb'.  
-        ds.Open("baltic.ewemdb", core)
+        core.InitCore()                             ' (3) Tell the core to initialize 
+        core.LoadModel(ds)                          ' (4) Tell the core to load a model from the datasource
+        core.RunEcoPath()                           ' (4) Tell the core to run Ecopath
 
-        ' (4) Tell the core to initialize itself
-        core.InitCore()
-        ' (5) Tell the core to load a model from the datasource
-        core.LoadModel(ds)
-        ' (6) Tell the core to run Ecopath
-        core.RunEcoPath()
+                                                    ' (5) Write to the console the group name and Ecopath EE for group 1
+        Console.WriteLine("Group '" & core.EcoPathGroupOutputs(1).Name & "'" & _
+                          " EE estimated to " & core.EcoPathGroupOutputs(1).EEOutput)
+        core.CloseModel()                           ' (6) Tell the core to close the Baltic model
 
-        ' (7) Write to the console the group name and Ecopath EE for all groups
-        For iGroup As Integer = 1 To core.nGroups
-            ' (7.1) Get ecopath results group 'iGroup'
-            Dim group As cEcoPathGroupOutput = core.EcoPathGroupOutputs(iGroup)
-            ' (7.2) Write information for this group to the console
-            Console.WriteLine("Group '" & group.Name & "' EE = " & group.EEOutput)
-        Next
-
-        ' (8) Tell the core to close the model
-        core.CloseModel()
-
-        ' Ask the user to hit a key before closing, and wait for the key press
-        Console.WriteLine("Press a key to exit")
-        Console.ReadKey()
+        Console.WriteLine("Press a key to exit")    ' Tell the user to press a key
+        Console.ReadKey()                           ' Wait for the key press
     End Sub
 
 End Module
