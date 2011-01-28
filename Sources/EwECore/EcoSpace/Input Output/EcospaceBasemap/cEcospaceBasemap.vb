390ccb1eed60c64880cafd3b6879f8b28cfb1c8a
JeroenS <jeroen.steenbeek@gmail.com>
2011-01-28 02:41:21 +0000

    Added rudimentary wrap fix to cell to spatial loc translation
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@5231 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECore/EcoSpace/Input Output/EcospaceBasemap/cEcospaceBasemap.vb
+++ Sources/EwECore/EcoSpace/Input Output/EcospaceBasemap/cEcospaceBasemap.vb
@@ -686,6 +686,12 @@ Public Class cEcospaceBasemap
     ''' <returns></returns>
     ''' -----------------------------------------------------------------------
     Public Function LonToCol(ByVal sLon As Single) As Integer
+        ' Coarse wrap check
+        If (sLon < Me.Longitude) Then
+            sLon += 360
+        ElseIf (sLon > (sLon + Me.InCol * Me.CellLength)) Then
+            sLon += 360
+        End If
         Return CInt(Math.Floor((sLon - Me.Longitude) / Me.CellLength)) + 1
     End Function
 
