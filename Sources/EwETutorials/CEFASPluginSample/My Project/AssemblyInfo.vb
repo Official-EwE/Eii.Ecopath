f97f6c8b0286e315d9f077ac160d78133861954b
automatedbuild <automatedbuild@ecopath.org>
2011-02-19 10:43:44 +0000

    Build 6.1.1.0219
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@5309 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwETutorials/CEFASPluginSample/My Project/AssemblyInfo.vb
+++ Sources/EwETutorials/CEFASPluginSample/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.1.1.0218")> 
-<Assembly: AssemblyFileVersion("6.1.1.0218")> 
+<Assembly: AssemblyVersion("6.1.1.0219")> 
+<Assembly: AssemblyFileVersion("6.1.1.0219")> 
