e21f005ad9439f86b4e850ce46130e5b14f3aa18
JeroenS <jeroen.steenbeek@gmail.com>
2011-03-02 21:45:56 +0000

    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@5382 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/ScientificInterface/Ecosim/Tools/Fit to time series/gridFitToTimeSeriesOutput.vb
@@ -0,0 +1,32 @@
+﻿#Region " Imports "
+
+Option Strict On
+Imports ScientificInterfaceShared.Controls.EwEGrid
+Imports SourceGrid2
+
+#End Region ' Imports
+
+Namespace Ecosim
+
+    ''' <summary>
+    ''' Grid for displaying Fit to time series run results
+    ''' </summary>
+    <CLSCompliant(False)> _
+    Public Class gridFitToTimeSeriesOutput
+        Inherits EwEGrid
+
+        Protected Overrides Sub InitStyle()
+            MyBase.InitStyle()
+        End Sub
+
+        Protected Overrides Sub FillData()
+
+        End Sub
+
+        Protected Overrides Sub FinishStyle()
+            MyBase.FinishStyle()
+        End Sub
+
+    End Class
+
+End Namespace
