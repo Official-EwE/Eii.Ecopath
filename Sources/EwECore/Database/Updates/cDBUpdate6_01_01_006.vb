e5a82e5937a83154467b4e8f7754173a9453bf27
JeroenS <jeroen.steenbeek@gmail.com>
2011-03-03 23:31:09 +0000

    New! New! Newewew!
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@5416 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/EwECore/Database/Updates/cDBUpdate6_01_01_006.vb
@@ -0,0 +1,53 @@
+﻿Option Strict On
+Imports EwEPlugin
+Imports EwEUtils.Database
+Imports EwEUtils.Core
+
+''' --------------------------------------------------------------------------
+''' <summary>
+''' <para>Database update 6.1.1.006:</para>
+''' <para>
+''' <list type="bullet">
+''' <item><description>Verified pedigree table</description></item>
+''' </list>
+''' </para>
+''' </summary>
+''' --------------------------------------------------------------------------
+Friend Class cDBUpdate6_01_01_006
+    Inherits cDBUpdate
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' This method provides the update version number that will be entered in
+    ''' the update log of the database. This version number is also used to check
+    ''' whether an update should run.
+    ''' </summary>
+    ''' <remarks>
+    ''' If <see cref="cCore.NULL_VALUE">cCore.NULL_VALUE</see> is provided, the
+    ''' update is ran regardless of version number.
+    ''' </remarks>
+    ''' -----------------------------------------------------------------------
+    Public Overrides ReadOnly Property UpdateVersion() As Single
+        Get
+            Return 6.101006!
+        End Get
+    End Property
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' This method provides the text that will be entered in the update log in
+    ''' the database.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Public Overrides ReadOnly Property UpdateDescription() As String
+        Get
+            Return "Verified pedigree table."
+        End Get
+    End Property
+
+    Public Overrides Function ApplyUpdate(ByRef db As cEwEDatabase) As Boolean
+        db.Execute("ALTER TABLE Pedigree ADD COLUMN LevelColor LONG")
+        Return True
+    End Function
+
+End Class
