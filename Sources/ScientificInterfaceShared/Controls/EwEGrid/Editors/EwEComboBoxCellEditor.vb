50724f48ca827434afc832ae8c4d2e76fd1aedf5
JeroenS <jeroen.steenbeek@gmail.com>
2011-03-08 21:00:39 +0000

    Fixed SourceGrid copy/paste bug when pasting text
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@5493 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterfaceShared/Controls/EwEGrid/Editors/EwEComboBoxCellEditor.vb
+++ Sources/ScientificInterfaceShared/Controls/EwEGrid/Editors/EwEComboBoxCellEditor.vb
@@ -15,7 +15,9 @@ Namespace Controls.EwEGrid
         Public Sub New(ByVal aValues() As t, ByVal formatter As ITypeFormatter(Of t))
 
             MyBase.New(aValues.GetType.GetElementType)
+
             Dim mapping As New SourceLibrary.ComponentModel.Validator.ValueMapping()
+            Dim lValues As New List(Of Integer)
             Dim lRepresentations As New List(Of String)
 
             For i As Integer = 0 To aValues.Length - 1
@@ -24,13 +26,39 @@ Namespace Controls.EwEGrid
 
             Me.StandardValues = aValues
             Me.StandardValuesExclusive = True
-            Me.AllowStringConversion = False
+            Me.AllowStringConversion = True
 
             mapping.ValueList = aValues
             mapping.DisplayStringList = lRepresentations
             mapping.BindValidator(Me)
 
         End Sub
+
+        'Protected Overrides Sub OnConvertingObjectToValue(ByVal e As SourceLibrary.ComponentModel.ConvertingObjectEventArgs)
+        '    ' JS: called internally when done editing the combo box
+        '    MyBase.OnConvertingObjectToValue(e)
+        'End Sub
+
+        Protected Overrides Sub OnConvertingValueToDisplayString(ByVal e As SourceLibrary.ComponentModel.ConvertingObjectEventArgs)
+            Try
+                If Not Me.ValueType.UnderlyingSystemType.IsAssignableFrom(e.Value.GetType) Then
+                    Dim iValue As Integer = 0
+                    If TypeOf (e.Value) Is String Then
+                        iValue = Integer.Parse(CStr(e.Value))
+                    Else
+                        iValue = CInt(e.Value)
+                    End If
+                    e.Value = [Enum].ToObject(Me.ValueType, iValue)
+                End If
+                MyBase.OnConvertingValueToDisplayString(e)
+            Catch ex As Exception
+                ' Should not occur any more
+            End Try
+        End Sub
+
+        'Protected Overrides Sub OnConvertingValueToObject(ByVal e As SourceLibrary.ComponentModel.ConvertingObjectEventArgs)
+        '    MyBase.OnConvertingValueToObject(e)
+        'End Sub
 
     End Class
 
