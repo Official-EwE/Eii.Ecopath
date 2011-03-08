d9e3b99f526ded489f208c59dd80ed43140ac170
JeroenS <jeroen.steenbeek@gmail.com>
2011-03-08 00:52:03 +0000

    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@5485 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/ScientificInterfaceShared/Style/Type formatters/cOccurrenceTypeFormatter.vb
@@ -0,0 +1,30 @@
+﻿#Region " Imports "
+
+Option Strict On
+Imports EwECore
+Imports EwEUtils.Core
+Imports EwEUtils.Utilities
+
+#End Region ' Imports
+
+Namespace Style
+
+    ''' ---------------------------------------------------------------------------
+    ''' <summary>
+    ''' Class for providing a textual description of <see cref="eOccurrenceStatusTypes"/>.
+    ''' </summary>
+    ''' ---------------------------------------------------------------------------
+    Public Class cOccurrenceTypeFormatter
+        Implements ITypeFormatter(Of eOccurrenceStatusTypes)
+
+        Public Function GetDescriptor(ByVal value As eOccurrenceStatusTypes, _
+                                      Optional ByVal descriptor As eDescriptorTypes = eDescriptorTypes.Name) As String _
+                                      Implements ITypeFormatter(Of eOccurrenceStatusTypes).GetDescriptor
+
+            Return value.ToString ' Muahaha
+
+        End Function
+
+    End Class
+
+End Namespace
---
+++ Sources/ScientificInterfaceShared/Style/Type formatters/cOrganismTypeFormatter.vb
@@ -0,0 +1,30 @@
+﻿#Region " Imports "
+
+Option Strict On
+Imports EwECore
+Imports EwEUtils.Core
+Imports EwEUtils.Utilities
+
+#End Region ' Imports
+
+Namespace Style
+
+    ''' ---------------------------------------------------------------------------
+    ''' <summary>
+    ''' Class for providing a textual description of <see cref="eOrganismTypes"/>.
+    ''' </summary>
+    ''' ---------------------------------------------------------------------------
+    Public Class cOrganismTypeFormatter
+        Implements ITypeFormatter(Of eOrganismTypes)
+
+        Public Function GetDescriptor(ByVal value As eOrganismTypes, _
+                                      Optional ByVal descriptor As eDescriptorTypes = eDescriptorTypes.Name) As String _
+                                      Implements ITypeFormatter(Of eOrganismTypes).GetDescriptor
+
+            Return value.ToString ' Muahaha
+
+        End Function
+
+    End Class
+
+End Namespace
