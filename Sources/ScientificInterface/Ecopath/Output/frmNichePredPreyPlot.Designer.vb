304193412f630d1fca4bbaa3fb9dbf9e273e46b5
JeroenS <jeroen.steenbeek@gmail.com>
2011-03-21 01:00:08 +0000

    Documented
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@5570 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterface/Ecopath/Output/frmNichePredPreyPlot.Designer.vb
+++ Sources/ScientificInterface/Ecopath/Output/frmNichePredPreyPlot.Designer.vb
@@ -32,16 +32,16 @@ Namespace Ecopath.Output
             Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmNichePredPreyPlot))
             Me.m_graph = New ZedGraph.ZedGraphControl
             Me.m_scMain = New System.Windows.Forms.SplitContainer
+            Me.m_cbLabels = New System.Windows.Forms.CheckBox
+            Me.m_lblColours = New System.Windows.Forms.Label
+            Me.m_btnShowHideGroups = New System.Windows.Forms.Button
             Me.m_rbNone = New System.Windows.Forms.RadioButton
             Me.m_rbOverlap = New System.Windows.Forms.RadioButton
             Me.m_rbPrey = New System.Windows.Forms.RadioButton
             Me.m_rbPredator = New System.Windows.Forms.RadioButton
             Me.m_nudCutOff = New System.Windows.Forms.NumericUpDown
-            Me.m_hdrOptions = New ScientificInterfaceShared.Controls.cEwEHeaderLabel
-            Me.m_lblCutOff = New System.Windows.Forms.Label
             Me.m_hdrDisplay = New ScientificInterfaceShared.Controls.cEwEHeaderLabel
-            Me.m_btnShowHideGroups = New System.Windows.Forms.Button
-            Me.m_lblColours = New System.Windows.Forms.Label
+            Me.m_lblCutOff = New System.Windows.Forms.Label
             Me.m_scMain.Panel1.SuspendLayout()
             Me.m_scMain.Panel2.SuspendLayout()
             Me.m_scMain.SuspendLayout()
@@ -67,6 +67,7 @@ Namespace Ecopath.Output
             '
             'm_scMain.Panel1
             '
+            Me.m_scMain.Panel1.Controls.Add(Me.m_cbLabels)
             Me.m_scMain.Panel1.Controls.Add(Me.m_lblColours)
             Me.m_scMain.Panel1.Controls.Add(Me.m_btnShowHideGroups)
             Me.m_scMain.Panel1.Controls.Add(Me.m_rbNone)
@@ -75,12 +76,28 @@ Namespace Ecopath.Output
             Me.m_scMain.Panel1.Controls.Add(Me.m_rbPredator)
             Me.m_scMain.Panel1.Controls.Add(Me.m_nudCutOff)
             Me.m_scMain.Panel1.Controls.Add(Me.m_hdrDisplay)
-            Me.m_scMain.Panel1.Controls.Add(Me.m_hdrOptions)
             Me.m_scMain.Panel1.Controls.Add(Me.m_lblCutOff)
             '
             'm_scMain.Panel2
             '
             Me.m_scMain.Panel2.Controls.Add(Me.m_graph)
+            '
+            'm_cbLabels
+            '
+            resources.ApplyResources(Me.m_cbLabels, "m_cbLabels")
+            Me.m_cbLabels.Name = "m_cbLabels"
+            Me.m_cbLabels.UseVisualStyleBackColor = True
+            '
+            'm_lblColours
+            '
+            resources.ApplyResources(Me.m_lblColours, "m_lblColours")
+            Me.m_lblColours.Name = "m_lblColours"
+            '
+            'm_btnShowHideGroups
+            '
+            resources.ApplyResources(Me.m_btnShowHideGroups, "m_btnShowHideGroups")
+            Me.m_btnShowHideGroups.Name = "m_btnShowHideGroups"
+            Me.m_btnShowHideGroups.UseVisualStyleBackColor = True
             '
             'm_rbNone
             '
@@ -115,31 +132,15 @@ Namespace Ecopath.Output
             resources.ApplyResources(Me.m_nudCutOff, "m_nudCutOff")
             Me.m_nudCutOff.Name = "m_nudCutOff"
             '
-            'm_hdrOptions
-            '
-            resources.ApplyResources(Me.m_hdrOptions, "m_hdrOptions")
-            Me.m_hdrOptions.Name = "m_hdrOptions"
-            '
-            'm_lblCutOff
-            '
-            resources.ApplyResources(Me.m_lblCutOff, "m_lblCutOff")
-            Me.m_lblCutOff.Name = "m_lblCutOff"
-            '
             'm_hdrDisplay
             '
             resources.ApplyResources(Me.m_hdrDisplay, "m_hdrDisplay")
             Me.m_hdrDisplay.Name = "m_hdrDisplay"
             '
-            'm_btnShowHideGroups
+            'm_lblCutOff
             '
-            resources.ApplyResources(Me.m_btnShowHideGroups, "m_btnShowHideGroups")
-            Me.m_btnShowHideGroups.Name = "m_btnShowHideGroups"
-            Me.m_btnShowHideGroups.UseVisualStyleBackColor = True
-            '
-            'm_lblColours
-            '
-            resources.ApplyResources(Me.m_lblColours, "m_lblColours")
-            Me.m_lblColours.Name = "m_lblColours"
+            resources.ApplyResources(Me.m_lblCutOff, "m_lblCutOff")
+            Me.m_lblCutOff.Name = "m_lblCutOff"
             '
             'frmNichePredPreyPlot
             '
@@ -157,16 +158,16 @@ Namespace Ecopath.Output
         End Sub
         Private WithEvents m_graph As ZedGraph.ZedGraphControl
         Private WithEvents m_scMain As System.Windows.Forms.SplitContainer
-        Private WithEvents m_hdrOptions As ScientificInterfaceShared.Controls.cEwEHeaderLabel
         Private WithEvents m_nudCutOff As System.Windows.Forms.NumericUpDown
         Private WithEvents m_lblCutOff As System.Windows.Forms.Label
-        Friend WithEvents m_rbOverlap As System.Windows.Forms.RadioButton
-        Friend WithEvents m_rbPrey As System.Windows.Forms.RadioButton
-        Friend WithEvents m_rbPredator As System.Windows.Forms.RadioButton
-        Friend WithEvents m_rbNone As System.Windows.Forms.RadioButton
-        Friend WithEvents m_lblColours As System.Windows.Forms.Label
-        Friend WithEvents m_btnShowHideGroups As System.Windows.Forms.Button
+        Private WithEvents m_rbOverlap As System.Windows.Forms.RadioButton
+        Private WithEvents m_rbPrey As System.Windows.Forms.RadioButton
+        Private WithEvents m_rbPredator As System.Windows.Forms.RadioButton
+        Private WithEvents m_rbNone As System.Windows.Forms.RadioButton
+        Private WithEvents m_lblColours As System.Windows.Forms.Label
+        Private WithEvents m_btnShowHideGroups As System.Windows.Forms.Button
         Private WithEvents m_hdrDisplay As ScientificInterfaceShared.Controls.cEwEHeaderLabel
+        Private WithEvents m_cbLabels As System.Windows.Forms.CheckBox
     End Class
 
 End Namespace
--- Sources/ScientificInterface/Ecopath/Output/frmNichePredPreyPlot.vb
+++ Sources/ScientificInterface/Ecopath/Output/frmNichePredPreyPlot.vb
@@ -16,26 +16,54 @@ Imports ScientificInterfaceShared.Commands
 
 Namespace Ecopath.Output
 
+    ''' =======================================================================
     ''' <summary>
-    ''' Implementation of the EwE5 niche pred/prey plot
+    ''' Implementation of the EwE5 niche pred/prey overlap plot.
     ''' </summary>
-    ''' <remarks></remarks>
+    ''' =======================================================================
     Public Class frmNichePredPreyPlot
 
-        Private m_zgh As cZedGraphHelper = Nothing
-        Private m_sCutOff As Single = 0.1!
-        Private m_fpCutOff As cEwEFormatProvider = Nothing
+#Region " Private vars "
 
+        ''' <summary>
+        ''' Emerated type for controlling how users want to see the graph coloured.
+        ''' </summary>
         Private Enum eColourType As Integer
+            ''' <summary>Do not use colours.</summary>
             None
+            ''' <summary>Colour all graph dots by predator.</summary>
             ByPredator
+            ''' <summary>Colour all graph dots by prey.</summary>
             ByPrey
+            ''' <summary>Colour all graph dots by overlap ratio.</summary>
             ByOverlap
         End Enum
 
+        ''' <summary>Handler for styling and controlling the graph.</summary>
+        Private m_zgh As cZedGraphHelper = Nothing
+
+        ''' <summary>User-provided cut-off value for filtering content [0, 1].</summary>
+        Private m_sCutOff As Single = 0.1!
+        ''' <summary>Format provider for making cut-off UI control look good.</summary>
+        Private m_fpm_sCutOff As cEwEFormatProvider = Nothing
+
+        ''' <summary>User-provided <see cref="eColourType">colour</see> behaviour.</summary>
         Private m_colourType As eColourType = eColourType.ByPredator
+        ''' <summary>Colour ramp for rendering a <see cref="eColourType.ByOverlap"/> graph.</summary>
         Private m_crColor As New ARGBColorRamp(New Color() {Color.White, Color.Gray, Color.Black}, New Double() {0, 0.6, 0.4})
 
+        ''' <summary>Flag stating whether value labels should be shown on the plot.</summary>
+        Private m_bShowLabels As Boolean = True
+
+#End Region ' Private vars
+
+#Region " Form overrides "
+
+        ''' -------------------------------------------------------------------
+        ''' <summary>
+        ''' Form is about to appear; hook up to all dynamic bits, ect.
+        ''' </summary>
+        ''' -------------------------------------------------------------------
         Protected Overrides Sub OnLoad(ByVal e As System.EventArgs)
             MyBase.OnLoad(e)
 
@@ -45,8 +73,8 @@ Namespace Ecopath.Output
             Me.m_zgh.Attach(Me.UIContext, Me.m_graph)
             Me.m_zgh.ShowPointValue = True
 
-            Me.m_fpCutOff = New cEwEFormatProvider(Me.UIContext, Me.m_nudCutOff, GetType(Single))
-            Me.m_fpCutOff.Value = Me.m_sCutOff
+            Me.m_fpm_sCutOff = New cEwEFormatProvider(Me.UIContext, Me.m_nudCutOff, GetType(Single))
+            Me.m_fpm_sCutOff.Value = Me.m_sCutOff
             Me.m_nudCutOff.Maximum = CDec(1)
             Me.m_nudCutOff.Minimum = CDec(0.0)
             Me.m_nudCutOff.Increment = CDec(0.1)
@@ -63,6 +91,11 @@ Namespace Ecopath.Output
 
         End Sub
 
+        ''' -------------------------------------------------------------------
+        ''' <summary>
+        ''' Form is about to close; clean up. Detach from all dynamic bits, etc.
+        ''' </summary>
+        ''' -------------------------------------------------------------------
         Protected Overrides Sub OnFormClosed(ByVal e As System.Windows.Forms.FormClosedEventArgs)
 
             If Me.UIContext Is Nothing Then Return
@@ -72,8 +105,8 @@ Namespace Ecopath.Output
                 cmd.RemoveControl(Me.m_btnShowHideGroups)
             End If
 
-            Me.m_fpCutOff.Release()
-            Me.m_fpCutOff = Nothing
+            Me.m_fpm_sCutOff.Release()
+            Me.m_fpm_sCutOff = Nothing
 
             Me.m_zgh.Detach()
             Me.m_zgh = Nothing
@@ -82,20 +115,41 @@ Namespace Ecopath.Output
 
         End Sub
 
+        ''' -------------------------------------------------------------------
+        ''' <inheritdocs cref="ScientificInterfaceShared.Forms.frmEwE.UpdateControls"/>
+        ''' -------------------------------------------------------------------
         Protected Overrides Sub UpdateControls()
             MyBase.UpdateControls()
             Select Case Me.m_colourType
+                Case eColourType.None : Me.m_rbNone.Checked = True
                 Case eColourType.ByPredator : Me.m_rbPredator.Checked = True
                 Case eColourType.ByPrey : Me.m_rbPrey.Checked = True
                 Case eColourType.ByOverlap : Me.m_rbOverlap.Checked = True
             End Select
+            Me.m_cbLabels.Checked = Me.m_bShowLabels
         End Sub
 
+        ''' -------------------------------------------------------------------
+        ''' <inheritdocs cref="ScientificInterfaceShared.Forms.frmEwE.OnStyleGuideChanged"/>
+        ''' -------------------------------------------------------------------
         Protected Overrides Sub OnStyleGuideChanged(ByVal ct As ScientificInterfaceShared.Style.cStyleGuide.eChangeType)
             MyBase.OnStyleGuideChanged(ct)
+            ' Update graph whenever any style guide aspect has changed. Could have 
+            ' anything really: graph bits, fonts, colours, group visiblity... Yes,
+            ' those eventualities could be tested for.
             Me.UpdatePlot()
         End Sub
 
+#End Region ' Form overrides
+
+#Region " Internals "
+
+#End Region ' Internals
+        ''' -------------------------------------------------------------------
+        ''' <summary>
+        ''' Refresh the content of the plot.
+        ''' </summary>
+        ''' -------------------------------------------------------------------
         Private Sub UpdatePlot()
 
             ' ToDo: globalize this method
@@ -106,34 +160,42 @@ Namespace Ecopath.Output
             Dim pane As GraphPane = Nothing
             Dim li As LineItem = Nothing
             Dim label As ZedGraph.TextObj = Nothing
-            Dim strLabel As String = ""
 
+            ' Configure graph pane
             pane = Me.m_zgh.ConfigurePane("Niche overlap", "Predator overlap index", "Prey overlap index", False)
             pane.XAxis.Scale.Max = 1.1!
             pane.YAxis.Scale.Max = 1.1!
 
-            ' Clear curves
+            ' Clear any existing curves
             pane.CurveList.Clear()
-            ' Clear text objects and other misc objects that may have been added
+            ' Clear any exiting text objects
             pane.GraphObjList.Clear()
 
-            For j As Integer = 1 To Me.Core.nGroups
+            ' For all predators
+            For iPred As Integer = 1 To Me.Core.nGroups
+                ' Get predator
+                pred = Me.Core.EcoPathGroupOutputs(iPred)
+                ' For all prey
+                For iPrey As Integer = 1 To Me.Core.nGroups
+                    ' Get prey
+                    prey = Me.Core.EcoPathGroupOutputs(iPrey)
+                    ' Are both predator and prey visible?
+                    If Me.StyleGuide.GroupVisible(iPrey) And Me.StyleGuide.GroupVisible(iPred) Then
+                        ' #Yes: Is there overlap, and did we not already process this pred/prey combination?
+                        '       (n.b. Hlap and Plap are populated bi-directionally)
+                        ' JS: test set to EXCEED the cut-off (>) rather than include cut-off (>=) to avoid 0 cut-off values.
+                        If (prey.Hlap(iPred) > Me.m_sCutOff) And _
+                           (prey.Plap(iPred) > Me.m_sCutOff) And _
+                           (iPrey > iPred) Then
 
-                pred = Me.Core.EcoPathGroupOutputs(j)
-                For i As Integer = 1 To Me.Core.nGroups
-                    prey = Me.Core.EcoPathGroupOutputs(i)
-
-                    If Me.StyleGuide.GroupVisible(i) And Me.StyleGuide.GroupVisible(j) Then
-
-                        ' Avoid 0
-                        If (prey.Hlap(j) > Me.CutOff) And _
-                           (prey.Plap(j) > Me.CutOff) And _
-                           (i > j) Then
-
-                            ' Create a new line item for each diet
+                            ' #Yes: create a new line item for this pred/prey combo
                             ppl = New PointPairList()
-                            li = New LineItem(strLabel, ppl, Color.Black, SymbolType.Circle)
+                            ppl.Add(prey.Hlap(iPred), prey.Plap(iPred))
+
+                            ' Style line
+                            li = New LineItem(String.Format(SharedResources.GENERIC_LABEL_INDEXED, pred.Name, prey.Name), ppl, Color.Black, SymbolType.Circle)
                             li.Line.Color = Color.Transparent
+                            ' ToDo: make dot size configurable
                             li.Symbol.Size = CSng(10)
 
                             Select Case Me.m_colourType
@@ -144,18 +206,22 @@ Namespace Ecopath.Output
                                 Case eColourType.ByPrey
                                     li.Symbol.Fill = New Fill(Me.StyleGuide.GroupColor(Me.Core, prey.Index))
                                 Case eColourType.ByOverlap
-                                    li.Symbol.Fill = New Fill(Me.m_crColor.GetColor(prey.Hlap(j) + prey.Plap(j) / 2, 1.0))
+                                    li.Symbol.Fill = New Fill(Me.m_crColor.GetColor(prey.Hlap(iPred) + prey.Plap(iPred) / 2, 1.0))
                             End Select
 
-                            strLabel = String.Format(SharedResources.GENERIC_LABEL_INDEXED, pred.Name, prey.Name)
-                            ppl.Add(prey.Hlap(j), prey.Plap(j))
+                            ' Need to show labels?
+                            If (Me.m_bShowLabels) Then
+                                ' #Yes: create text label
+                                label = New TextObj(String.Format("{0}, {1}", pred.Index, prey.Index), _
+                                                    prey.Hlap(iPred), prey.Plap(iPred), CoordType.AxisXYScale, AlignH.Left, AlignV.Top)
+                                ' Style text label
+                                label.FontSpec.Border.IsVisible = False
+                                label.FontSpec.Fill.IsVisible = False
+                                ' Add text label
+                                pane.GraphObjList.Add(label)
+                            End If
 
-                            label = New TextObj(String.Format("{0}, {1}", pred.Index, prey.Index), _
-                                                prey.Hlap(j), prey.Plap(j), CoordType.AxisXYScale, AlignH.Left, AlignV.Top)
-                            label.FontSpec.Border.IsVisible = False
-                            label.FontSpec.Fill.IsVisible = False
-                            pane.GraphObjList.Add(label)
-
+                            ' Add line last
                             pane.CurveList.Add(li)
 
                         End If
@@ -163,14 +229,16 @@ Namespace Ecopath.Output
                 Next
             Next
 
+            ' Done; rescale graph entirely
             Me.m_zgh.RescaleAndRedraw()
 
         End Sub
 
-        Public Overrides Sub OnCoreMessage(ByVal msg As EwECore.cMessage)
-            MyBase.OnCoreMessage(msg)
-        End Sub
-
+        ''' -------------------------------------------------------------------
+        ''' <summary>
+        ''' Event handler to trap user colour behaviour changes.
+        ''' </summary>
+        ''' -------------------------------------------------------------------
         Private Sub OnColourOptionChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) _
             Handles m_rbPredator.CheckedChanged, _
                     m_rbPrey.CheckedChanged, _
@@ -179,6 +247,7 @@ Namespace Ecopath.Output
 
             If (Me.UIContext Is Nothing) Then Return
 
+            ' Determine colour type from UI control settings
             If Me.m_rbNone.Checked Then
                 Me.m_colourType = eColourType.None
             ElseIf Me.m_rbPredator.Checked Then
@@ -188,27 +257,34 @@ Namespace Ecopath.Output
             Else
                 Me.m_colourType = eColourType.ByOverlap
             End If
+            ' Refresh plot
             Me.UpdatePlot()
 
         End Sub
 
+        ''' -------------------------------------------------------------------
+        ''' <summary>
+        ''' Event handler to trap user cut-off value changes.
+        ''' </summary>
+        ''' -------------------------------------------------------------------
         Private Sub OnCutOffValueChanged(ByVal sender As Object, ByVal e As System.EventArgs) _
             Handles m_nudCutOff.ValueChanged
             If (Me.UIContext Is Nothing) Then Return
-            Me.CutOff = CSng(Me.m_nudCutOff.Value)
+            Me.m_sCutOff = CSng(Me.m_nudCutOff.Value)
+            Me.UpdatePlot()
         End Sub
 
-        Protected Property CutOff() As Single
-            Get
-                Return Me.m_sCutOff
-            End Get
-            Set(ByVal value As Single)
-                If (Me.m_sCutOff <> value) Then
-                    Me.m_sCutOff = value
-                    Me.UpdatePlot()
-                End If
-            End Set
-        End Property
+        ''' -------------------------------------------------------------------
+        ''' <summary>
+        ''' Event handler to trap user label display changes.
+        ''' </summary>
+        ''' -------------------------------------------------------------------
+        Private Sub OnShowLabelsChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) _
+           Handles m_cbLabels.CheckedChanged
+            Me.m_bShowLabels = Me.m_cbLabels.Checked
+            Me.UpdatePlot()
+        End Sub
+
     End Class
 
 End Namespace
