e06a7e8edb2f7a6ba9b3d34536b450491eed7325
JeroenS <jeroen.steenbeek@gmail.com>
2011-04-24 21:48:03 +0000

    Double-buffered - *ooooooh*
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@5778 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECustomPlugins/EwENetworkAnalysis/Controls/ucPlot.vb
+++ Sources/EwECustomPlugins/EwENetworkAnalysis/Controls/ucPlot.vb
@@ -5,14 +5,8 @@ Public Class ucPlot
     Public Sub New()
 
         Me.InitializeComponent()
-        'Me.SetStyle(Windows.Forms.ControlStyles.AllPaintingInWmPaint, True)
-        'Me.SetStyle(Windows.Forms.ControlStyles.OptimizedDoubleBuffer, True)
-        'Me.SetStyle(Windows.Forms.ControlStyles.ResizeRedraw, True)
+        Me.SetStyle(Windows.Forms.ControlStyles.AllPaintingInWmPaint Or Windows.Forms.ControlStyles.OptimizedDoubleBuffer Or Windows.Forms.ControlStyles.UserPaint, True)
 
     End Sub
-
-    'Protected Overrides Sub OnPaint(ByVal e As System.Windows.Forms.PaintEventArgs)
-    '    ' MyBase.OnPaint(e)
-    'End Sub
 
 End Class
