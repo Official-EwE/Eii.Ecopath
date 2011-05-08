0af1a3f84d877d6019ea04b37a21dfe7ae87b03f
JeroenS <jeroen.steenbeek@gmail.com>
2011-05-08 23:13:35 +0000

    'Easter' egg
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@5835 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwEUtils/Utilities/cDateUtils.vb
+++ Sources/EwEUtils/Utilities/cDateUtils.vb
@@ -61,6 +61,52 @@ Namespace Utilities
             Return ""
         End Function
 
+        ''' -------------------------------------------------------------------
+        ''' <summary>
+        ''' Calculate the date of Easter for a given year in Gregorian calendars.
+        ''' </summary>
+        ''' <param name="y">The Gregorian year to calculate easter for [1583, 4099]</param>
+        ''' <returns>The date of Easter Sunday.</returns>
+        ''' <remarks>
+        ''' This algorithm is adapted from http://www.gmarts.org/index.php?go=415, 
+        ''' whcih in turn is adapted from a faq document by Claus Tondering
+        ''' URL: http://www.pip.dknet.dk/~pip10160/calendar.faq2.txt
+        ''' E-mail: c-t@pip.dknet.dk.
+        ''' The FAQ algorithm is based in part on the algorithm of Oudin (1940)
+        ''' as quoted in "Explanatory Supplement to the Astronomical Almanac",
+        ''' P. Kenneth Seidelmann, editor.
+        ''' </remarks>
+        ''' -------------------------------------------------------------------
+        Public Shared Function Easter(ByVal y As Integer) As Date
+
+            Debug.Assert(y >= 1583 And y <= 4099, "Gregorian calendar Easters apply for years 1583 to 4099 only")
+
+            Dim d As Integer = 0 ' Day
+            Dim m As Integer = 0 ' Month
+            Dim g As Integer = 0 ' golden year - 1
+            Dim c As Integer = 0 ' century
+            Dim h As Integer = 0 ' = (23 - Epact) mod 30
+            Dim i As Integer = 0 ' no of days from March 21 to Paschal Full Moon
+            Dim j As Integer = 0 ' weekday for PFM (0=Sunday, etc)
+            Dim p As Integer = 0 ' no of days from March 21 to Sunday on or before PFM
+            Dim e As Integer = 0 'extra days to add for method 2 (converting Julian date to Gregorian date)
+
+            g = y Mod 19
+            c = y \ 100
+            h = (c - c \ 4 - (8 * c + 13) \ 25 + 19 * g + 15) Mod 30
+            i = h - (h \ 28) * (1 - (h \ 28) * (29 \ (h + 1)) * ((21 - g) \ 11))
+            j = (y + y \ 4 + i + 2 - c + c \ 4) Mod 7
+            'return day and month
+            p = i - j + e
+            ' p can be from -6 to 56 corresponding to dates 22 March to 23 May
+            ' (later dates apply to method 2, although 23 May never actually occurs)
+            d = 1 + (p + 27 + (p + 6) \ 40) Mod 31
+            m = 3 + (p + 26) \ 30
+
+            Return New Date(y, m, d)
+
+        End Function
+
     End Class
 
 End Namespace
