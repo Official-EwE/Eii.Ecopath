6d5017a384a167582fabdbbcd33368096323a60a
JeroenS <jeroen.steenbeek@gmail.com>
2011-05-11 02:58:46 +0000

    Commented
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@5868 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterface/Settings.vb
+++ Sources/ScientificInterface/Settings.vb
@@ -2,19 +2,21 @@
 Imports System.Configuration
 
 ''' <summary>
-''' <para>This class allows you to handle specific events on the settings class:</para>
-''' <list type="bullet">
-''' <item>The SettingChanging event is raised before a setting's value is changed.</item>
-''' <item>The PropertyChanged event is raised after a setting's value is changed.</item>
-''' <item>The SettingsLoaded event is raised after the setting values are loaded.</item>
-''' <item>The SettingsSaving event is raised before the setting values are saved.</item>
-''' </list>
+''' Settings class that uses a custom <see cref="SettingsProvider"/>.
 ''' </summary>
-''' <remarks></remarks>
+''' <remarks>
+''' For details about the overridden settings behaviour refer to <see cref="cEwESettingsProvider"/>.
+''' </remarks>
 Partial Friend NotInheritable Class Settings
 
+    ''' <summary>Custom <see cref="cEwESettingsProvider">settings provider</see>.</summary>
     Private m_provider As cEwESettingsProvider = Nothing
 
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Constructor.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
     Public Sub New()
 
         MyBase.New()
@@ -32,13 +34,16 @@ Partial Friend NotInheritable Class Settings
 
     End Sub
 
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Get the default value for a given settings property name.
+    ''' </summary>
+    ''' <param name="strName">The name of the property to access. This name is not case-sensitive.</param>
+    ''' <returns>A value, or Nothing if a property by this name does not exist.</returns>
+    ''' -----------------------------------------------------------------------
     Public Function GetDefaultValue(ByVal strName As String) As Object
-        Try
-            Dim prop As SettingsProperty = Me.Properties(strName)
-            Return prop.DefaultValue
-        Catch ex As Exception
-
-        End Try
+        Dim prop As SettingsProperty = Me.Properties(strName)
+        If prop IsNot Nothing Then Return prop.DefaultValue
         Return Nothing
     End Function
 
