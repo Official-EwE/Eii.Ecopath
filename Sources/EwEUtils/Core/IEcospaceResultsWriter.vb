76b5e1b62a6a2f30ba0b0df5848948ec058395d8
joeb <joeb@ecopath.org>
2011-07-06 20:36:39 +0000

    Added IEcospaceResultsWriter to EwEUtils
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@6218 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/EwEUtils/Core/IEcospaceResultsWriter.vb
@@ -0,0 +1,26 @@
+﻿
+Namespace Core
+
+    ''' <summary>
+    ''' Interface for writing Ecospace time step results to file
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Interface IEcospaceResultsWriter
+
+        ''' <summary>
+        ''' Save time step data to file
+        ''' </summary>
+        ''' <param name="SpaceTimeStepResults">cEcospaceTimestep as object containing the data to save.</param>
+        ''' <remarks></remarks>
+        Sub WriteResults(ByVal SpaceTimeStepResults As Object)
+
+        Sub Init(ByVal theCore As Object)
+
+        Sub StartWrite()
+
+        Sub EndWrite()
+
+
+    End Interface
+
+End Namespace
