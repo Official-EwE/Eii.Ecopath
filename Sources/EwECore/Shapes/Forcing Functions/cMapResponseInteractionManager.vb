33584b40d04e51a6f3824fc7988473d9535580a7
joeb <joeb@ecopath.org>
2011-07-26 18:48:09 +0000

    Added MapResponseInteractionManager
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@6316 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/EwECore/Shapes/Forcing Functions/cMapResponseInteractionManager.vb
@@ -0,0 +1,32 @@
+﻿
+
+
+Public Class cMapResponseInteractionManager
+
+    Dim m_maps As List(Of IEnviroInputMap)
+    Dim m_MedData As cMediationDataStructures
+
+    Public Sub Init(ByVal MediationData As cMediationDataStructures, ByVal InputMaps As List(Of IEnviroInputMap))
+        Me.m_maps = InputMaps
+        Me.m_MedData = MediationData
+    End Sub
+
+    Public ReadOnly Property nMaps() As Integer
+        Get
+            Return Me.m_maps.Count
+        End Get
+    End Property
+
+
+    Public ReadOnly Property Maps(ByVal MapIndex As Integer) As IEnviroInputMap
+        Get
+            If MapIndex > 0 And MapIndex <= Me.m_maps.Count Then
+                Return Me.m_maps(MapIndex - 1)
+            End If
+            Return Nothing
+        End Get
+
+    End Property
+
+
+End Class
