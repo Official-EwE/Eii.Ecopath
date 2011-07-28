3753a2ef1c29a32bb199031aa51ea80fbc321b6d
joeb <joeb@ecopath.org>
2011-07-28 19:46:07 +0000

    Added dlgDefineMapResponseAssignments
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@6318 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/ScientificInterfaceShared/Controls/Shapes/ShapeMediation/dlgDefineMapResponseAssignments.Designer.vb
@@ -0,0 +1,159 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class dlgDefineMapResponseAssignments
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel
+        Me.OK_Button = New System.Windows.Forms.Button
+        Me.Cancel_Button = New System.Windows.Forms.Button
+        Me.lvMaps = New System.Windows.Forms.ListView
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.txXMax = New System.Windows.Forms.TextBox
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.ZedGraph = New ZedGraph.ZedGraphControl
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.OK_Button, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Cancel_Button, 1, 0)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(654, 294)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 1
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(146, 29)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'OK_Button
+        '
+        Me.OK_Button.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.OK_Button.Location = New System.Drawing.Point(3, 3)
+        Me.OK_Button.Name = "OK_Button"
+        Me.OK_Button.Size = New System.Drawing.Size(67, 23)
+        Me.OK_Button.TabIndex = 0
+        Me.OK_Button.Text = "OK"
+        '
+        'Cancel_Button
+        '
+        Me.Cancel_Button.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Cancel_Button.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Cancel_Button.Location = New System.Drawing.Point(76, 3)
+        Me.Cancel_Button.Name = "Cancel_Button"
+        Me.Cancel_Button.Size = New System.Drawing.Size(67, 23)
+        Me.Cancel_Button.TabIndex = 1
+        Me.Cancel_Button.Text = "Cancel"
+        '
+        'lvMaps
+        '
+        Me.lvMaps.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.lvMaps.Location = New System.Drawing.Point(12, 29)
+        Me.lvMaps.Name = "lvMaps"
+        Me.lvMaps.Size = New System.Drawing.Size(161, 259)
+        Me.lvMaps.TabIndex = 1
+        Me.lvMaps.UseCompatibleStateImageBehavior = False
+        Me.lvMaps.View = System.Windows.Forms.View.List
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 13)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(117, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Input maps for capacity"
+        '
+        'txXMax
+        '
+        Me.txXMax.Location = New System.Drawing.Point(321, 26)
+        Me.txXMax.Name = "txXMax"
+        Me.txXMax.Size = New System.Drawing.Size(85, 20)
+        Me.txXMax.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(187, 29)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(128, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Response function X max"
+        '
+        'ZedGraph
+        '
+        Me.ZedGraph.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ZedGraph.Location = New System.Drawing.Point(190, 63)
+        Me.ZedGraph.Name = "ZedGraph"
+        Me.ZedGraph.ScrollGrace = 0
+        Me.ZedGraph.ScrollMaxX = 0
+        Me.ZedGraph.ScrollMaxY = 0
+        Me.ZedGraph.ScrollMaxY2 = 0
+        Me.ZedGraph.ScrollMinX = 0
+        Me.ZedGraph.ScrollMinY = 0
+        Me.ZedGraph.ScrollMinY2 = 0
+        Me.ZedGraph.Size = New System.Drawing.Size(610, 225)
+        Me.ZedGraph.TabIndex = 5
+        '
+        'dlgDefineMapResponseAssignments
+        '
+        Me.AcceptButton = Me.OK_Button
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.Cancel_Button
+        Me.ClientSize = New System.Drawing.Size(812, 335)
+        Me.Controls.Add(Me.ZedGraph)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txXMax)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lvMaps)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "dlgDefineMapResponseAssignments"
+        Me.ShowInTaskbar = False
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "dlgDefineMapResponseAssignments"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents OK_Button As System.Windows.Forms.Button
+    Friend WithEvents Cancel_Button As System.Windows.Forms.Button
+    Friend WithEvents lvMaps As System.Windows.Forms.ListView
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txXMax As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents ZedGraph As ZedGraph.ZedGraphControl
+
+End Class
---
+++ Sources/ScientificInterfaceShared/Controls/Shapes/ShapeMediation/dlgDefineMapResponseAssignments.vb
@@ -0,0 +1,180 @@
+﻿#Region " Imports "
+
+Option Explicit On
+Option Strict On
+
+Imports EwECore
+Imports ZedGraph
+Imports System.Windows.Forms
+Imports ScientificInterfaceShared.Controls
+
+#End Region
+
+
+Public Class dlgDefineMapResponseAssignments
+
+    Private m_shape As EwECore.cEnviroResponseFunction
+    Private m_manager As cMapResponseInteractionManager
+    Private m_zgh As cZedGraphHelper
+    Private m_uic As cUIContext
+
+    Public Sub New(ByVal UIC As cUIContext, ByVal ResponseShape As EwECore.cEnviroResponseFunction, ByVal Manager As EwECore.cMapResponseInteractionManager)
+        Me.InitializeComponent()
+
+        Me.m_shape = ResponseShape
+        Me.m_manager = Manager
+
+        Me.m_uic = UIC
+
+        Me.m_zgh = New cZedGraphHelper
+        Me.m_zgh.Attach(Me.m_uic, Me.ZedGraph)
+    End Sub
+
+
+    Private Sub PlotShape()
+
+        Try
+
+            Dim maxX As Single = 10 'Me.m_shape.MapXAxis
+
+            If Me.m_shape.XAxisMax > 0 Then
+                maxX = Me.m_shape.XAxisMax
+            End If
+
+            If Me.m_zgh.GetPane(1).CurveList.Count > 0 Then
+                Me.m_zgh.GetPane(1).CurveList.Clear()
+            End If
+
+            Dim dx As Single = maxX / Me.m_shape.XMax
+            Dim MaxY As Single = Me.m_shape.YMax
+            Dim lstPts As New PointPairList
+            For ipt As Integer = 1 To Me.m_shape.XMax
+                lstPts.Add(dx * (ipt - 1), Me.m_shape.ShapeData(ipt) / MaxY)
+            Next
+
+            Dim il As LineItem = Me.m_zgh.CreateLineItem("Shape", Definitions.eLineType.NotSet, Color.SandyBrown, lstPts)
+
+            Me.m_zgh.GetPane(1).CurveList.Add(il)
+
+            Dim map As IEnviroInputMap = Me.getSelMap()
+            If map IsNot Nothing Then
+                maxX = map.Max
+            End If
+
+            'Me.m_zgh.AutoscalePane = True
+            Me.m_zgh.XScaleMax = maxX
+            Me.m_zgh.YScaleMax = 1.2
+
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+
+    Private Sub loadMaps()
+        Dim map As IEnviroInputMap
+        For imap As Integer = 1 To Me.m_manager.nMaps
+            map = Me.m_manager.Maps(imap)
+
+            Me.lvMaps.Items.Add(map.Name).Tag = map
+
+        Next
+
+    End Sub
+
+
+    Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK_Button.Click
+        Me.DialogResult = System.Windows.Forms.DialogResult.OK
+        Me.Close()
+    End Sub
+
+    Private Sub Cancel_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel_Button.Click
+        Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Close()
+    End Sub
+
+    Private Sub dlgDefineMapResponseAssignments_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        Try
+
+            Me.PlotShape()
+            Me.loadMaps()
+
+            'has to change 
+            'shapes don't contain mapX at this time
+            Me.txXMax.Text = "10.0"
+
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+    Private Function getSelMap() As IEnviroInputMap
+        Try
+
+            Dim ob As Object
+            ob = Me.lvMaps.SelectedItems(0).Tag
+            If ob IsNot Nothing Then
+                Return DirectCast(ob, IEnviroInputMap)
+            End If
+
+        Catch ex As Exception
+
+        End Try
+
+        Return Nothing
+
+    End Function
+
+
+    Private Sub PlotMap()
+        Try
+            Dim map As IEnviroInputMap = Me.getSelMap
+            If map Is Nothing Then
+                Exit Sub
+            End If
+
+            If Me.m_zgh.GetPane(1).CurveList.Count > 1 Then
+                Me.m_zgh.GetPane(1).CurveList.RemoveAt(1)
+            End If
+
+            Dim histPts() As Drawing.PointF = map.Histogram(Me.m_shape.XMax)
+
+            Dim maxX As Single = map.Max
+            Dim lstPts As New PointPairList
+            For ipt As Integer = 0 To 100
+                lstPts.Add(histPts(ipt).X, histPts(ipt).Y)
+            Next
+
+            Dim il As LineItem = Me.m_zgh.CreateLineItem("Map histogram", Definitions.eLineType.NotSet, Color.RoyalBlue, lstPts)
+            Me.m_zgh.GetPane(1).CurveList.Add(il)
+
+            'Me.m_zgh.AutoscalePane = True
+            Me.m_zgh.XScaleMax = maxX
+            Me.m_zgh.YScaleMax = 1.2
+
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+
+    Private Sub lvMaps_SelectedIndexChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles lvMaps.SelectedIndexChanged
+
+        PlotMap()
+    End Sub
+
+
+    Private Sub txXMax_TextChanged(ByVal sender As Object, ByVal e As System.EventArgs) Handles txXMax.TextChanged
+        Dim maxX As Single = Single.Parse(Me.txXMax.Text)
+        Me.m_shape.XAxisMax = maxX
+
+        PlotShape()
+        PlotMap()
+
+    End Sub
+End Class
+
