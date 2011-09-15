a5e56fe4b0a6c8e36f5fe3ba98e9f5b1773de865
JeroenS <jeroen.steenbeek@gmail.com>
2011-09-15 19:49:56 +0000

    Fixed flawed separator hiding logic
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@6484 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterfaceShared/Controls/Toolstrip/cEwEToolstrip.vb
+++ Sources/ScientificInterfaceShared/Controls/Toolstrip/cEwEToolstrip.vb
@@ -65,26 +65,41 @@ Namespace Controls
         Private Sub ShowHideRepeatingSeparators()
 
             Dim tsi As ToolStripItem = Nothing
-            Dim bAllInvisibleControls As Boolean = True
+            Dim iNumVisibleControl As Integer = 0 ' Num vis controls since last separator
+            Dim iLastVisibleSeparator As Integer = -1 ' Position of last visible separator
 
             Me.SuspendLayout()
+
             ' For all toolbar items
             For i As Integer = 0 To Me.Items.Count - 1
                 ' Get item
                 tsi = Me.Items(i)
                 ' Is a separator?
                 If (TypeOf tsi Is ToolStripSeparator) Then
-                    ' #Yes: ok, show it
-                    tsi.Visible = (bAllInvisibleControls = False)
-                    bAllInvisibleControls = True
-                Else
-                    ' #No: regular control. Is this control visible?
-                    If tsi.Visible = True Then
-                        ' #Yes: forget last separator since it separates a visible control
-                        bAllInvisibleControls = False
+                    ' #Yes: show this separator only if it separates visible controls
+                    If (iNumVisibleControl > 0) Then
+                        ' Show separator
+                        tsi.Visible = True
+                        ' Remember this visible separator
+                        iLastVisibleSeparator = i
+                    Else
+                        tsi.Visible = False
                     End If
+
+                    ' Reset visible control count
+                    iNumVisibleControl = 0
+                Else
+                    ' #No: count number of visible regular controls
+                    If (tsi.Visible) Then iNumVisibleControl += 1
                 End If
             Next
+
+            ' Fishished without visible controls since the last visible separator?
+            If (iNumVisibleControl = 0 And iLastVisibleSeparator >= 0) Then
+                ' #Yep: hide the last separator
+                Me.Items(iLastVisibleSeparator).Visible = False
+            End If
+
             Me.ResumeLayout()
 
         End Sub
