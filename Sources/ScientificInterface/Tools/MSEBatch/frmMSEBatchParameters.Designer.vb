8766986d86d6a4dee508d4a5a40985300b85e871
joeb <joeb@ecopath.org>
2011-10-11 21:47:18 +0000

    MSE Batch Parameters form layout
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@6697 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterface/Tools/MSEBatch/frmMSEBatchParameters.Designer.vb
+++ Sources/ScientificInterface/Tools/MSEBatch/frmMSEBatchParameters.Designer.vb
@@ -32,12 +32,13 @@ Partial Class frmMSEBatchParameters
         Me.chkPredMort = New System.Windows.Forms.CheckBox()
         Me.chkQB = New System.Windows.Forms.CheckBox()
         Me.chkFeedingTime = New System.Windows.Forms.CheckBox()
+        Me.eweHdrSave = New ScientificInterfaceShared.Controls.cEwEHeaderLabel()
         Me.SuspendLayout()
         '
         'chkSaveBiomass
         '
         Me.chkSaveBiomass.AutoSize = True
-        Me.chkSaveBiomass.Location = New System.Drawing.Point(27, 31)
+        Me.chkSaveBiomass.Location = New System.Drawing.Point(18, 36)
         Me.chkSaveBiomass.Name = "chkSaveBiomass"
         Me.chkSaveBiomass.Size = New System.Drawing.Size(65, 17)
         Me.chkSaveBiomass.TabIndex = 0
@@ -47,7 +48,7 @@ Partial Class frmMSEBatchParameters
         'chkCatch
         '
         Me.chkCatch.AutoSize = True
-        Me.chkCatch.Location = New System.Drawing.Point(26, 72)
+        Me.chkCatch.Location = New System.Drawing.Point(18, 59)
         Me.chkCatch.Name = "chkCatch"
         Me.chkCatch.Size = New System.Drawing.Size(54, 17)
         Me.chkCatch.TabIndex = 1
@@ -57,7 +58,7 @@ Partial Class frmMSEBatchParameters
         'chkFishingMort
         '
         Me.chkFishingMort.AutoSize = True
-        Me.chkFishingMort.Location = New System.Drawing.Point(25, 106)
+        Me.chkFishingMort.Location = New System.Drawing.Point(18, 82)
         Me.chkFishingMort.Name = "chkFishingMort"
         Me.chkFishingMort.Size = New System.Drawing.Size(85, 17)
         Me.chkFishingMort.TabIndex = 2
@@ -67,7 +68,7 @@ Partial Class frmMSEBatchParameters
         'chkPredMort
         '
         Me.chkPredMort.AutoSize = True
-        Me.chkPredMort.Location = New System.Drawing.Point(28, 145)
+        Me.chkPredMort.Location = New System.Drawing.Point(141, 59)
         Me.chkPredMort.Name = "chkPredMort"
         Me.chkPredMort.Size = New System.Drawing.Size(97, 17)
         Me.chkPredMort.TabIndex = 3
@@ -77,7 +78,7 @@ Partial Class frmMSEBatchParameters
         'chkQB
         '
         Me.chkQB.AutoSize = True
-        Me.chkQB.Location = New System.Drawing.Point(26, 186)
+        Me.chkQB.Location = New System.Drawing.Point(141, 36)
         Me.chkQB.Name = "chkQB"
         Me.chkQB.Size = New System.Drawing.Size(131, 17)
         Me.chkQB.TabIndex = 4
@@ -87,18 +88,33 @@ Partial Class frmMSEBatchParameters
         'chkFeedingTime
         '
         Me.chkFeedingTime.AutoSize = True
-        Me.chkFeedingTime.Location = New System.Drawing.Point(29, 217)
+        Me.chkFeedingTime.Location = New System.Drawing.Point(141, 82)
         Me.chkFeedingTime.Name = "chkFeedingTime"
         Me.chkFeedingTime.Size = New System.Drawing.Size(86, 17)
         Me.chkFeedingTime.TabIndex = 5
         Me.chkFeedingTime.Text = "Feeding time"
         Me.chkFeedingTime.UseVisualStyleBackColor = True
         '
+        'eweHdrSave
+        '
+        Me.eweHdrSave.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.eweHdrSave.CanCollapseParent = False
+        Me.eweHdrSave.CollapsedParentHeight = 0
+        Me.eweHdrSave.IsCollapsed = False
+        Me.eweHdrSave.Location = New System.Drawing.Point(12, 9)
+        Me.eweHdrSave.Name = "eweHdrSave"
+        Me.eweHdrSave.Size = New System.Drawing.Size(446, 24)
+        Me.eweHdrSave.TabIndex = 6
+        Me.eweHdrSave.Text = "Save"
+        Me.eweHdrSave.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
         'frmMSEBatchParameters
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(292, 266)
+        Me.ClientSize = New System.Drawing.Size(470, 416)
+        Me.Controls.Add(Me.eweHdrSave)
         Me.Controls.Add(Me.chkFeedingTime)
         Me.Controls.Add(Me.chkQB)
         Me.Controls.Add(Me.chkPredMort)
@@ -118,4 +134,5 @@ Partial Class frmMSEBatchParameters
     Friend WithEvents chkPredMort As System.Windows.Forms.CheckBox
     Friend WithEvents chkQB As System.Windows.Forms.CheckBox
     Friend WithEvents chkFeedingTime As System.Windows.Forms.CheckBox
+    Friend WithEvents eweHdrSave As ScientificInterfaceShared.Controls.cEwEHeaderLabel
 End Class
