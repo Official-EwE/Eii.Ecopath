e94b2993b78f7a31faa601bfff29fc5d5c8e9c1e
joeb <joeb@ecopath.org>
2011-10-19 00:11:08 +0000

    Fixed references in EwEConsoleAppExample it now runs
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@6741 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Tutorials/EwEConsoleAppExample/EwE7.vb
+++ Tutorials/EwEConsoleAppExample/EwE7.vb
@@ -9,17 +9,22 @@ Module EwE7
     ' Main Sub routine called when program is run.
     Sub Main()
         Dim core As New cCore()                     ' (1) Define core and database variables
-        Dim ds As IEwEDataSource = cDataSourceFactory.Create(eDataSourceTypes.ACCDB)
+        Dim ds As IEwEDataSource = cDataSourceFactory.Create(eDataSourceTypes.Access2007)
 
         ds.Open("baltic.ewemdb", core)              ' (2) Tell the datasource to open a database called baltic.ewemdb 
 
         core.InitCore()                             ' (3) Tell the core to initialize 
         core.LoadModel(ds)                          ' (4) Tell the core to load a model from the datasource
         core.RunEcoPath()                           ' (4) Tell the core to run Ecopath
+        ' (5) Write to the console the group name and Ecopath EE for group 1
 
-                                                    ' (5) Write to the console the group name and Ecopath EE for group 1
-        Console.WriteLine("Group '" & core.EcoPathGroupOutputs(1).Name & "'" & _
-                          " EE estimated to " & core.EcoPathGroupOutputs(1).EEOutput)
+
+        For igrp As Integer = 1 To core.nGroups
+            Console.WriteLine("Group '" & core.EcoPathGroupOutputs(igrp).Name & "'" & _
+                              " EE estimated to " & core.EcoPathGroupOutputs(igrp).EEOutput)
+
+        Next igrp
+
         core.CloseModel()                           ' (6) Tell the core to close the Baltic model
 
         Console.WriteLine("Press a key to exit")    ' Tell the user to press a key
