4d40613fa3a754ff1b9fa1f2d160c3fd71bada58
JeroenS <jeroen.steenbeek@gmail.com>
2011-10-22 15:36:36 +0000

    Stop losing the message handler!!
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@6761 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterfaceShared/Controls/Map/Layers/cLayer.vb
+++ Sources/ScientificInterfaceShared/Controls/Map/Layers/cLayer.vb
@@ -219,6 +219,10 @@ Namespace Controls.Map.Layers
 
             Me.m_uic = uic
 
+            Me.m_mh = New cMessageHandler(AddressOf Me.EcospaceMessageHandler, eCoreComponentType.EcoSpace, eMessageType.DataModified, Me.m_uic.SyncObject)
+#If DEBUG Then
+            Me.m_mh.Name = "Layer " & varName
+#End If
             Me.m_uic.Core.Messages.AddMessageHandler(Me.m_mh)
 
             ' Sanity checks
