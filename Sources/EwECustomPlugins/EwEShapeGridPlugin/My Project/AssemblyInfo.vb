b52e4bec9f58749ffbdd8b5e87d75f4ab12a6dd2
automatedbuild <automatedbuild@ecopath.org>
2011-10-29 09:52:36 +0000

    Build 6.3.1029
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@6790 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECustomPlugins/EwEShapeGridPlugin/My Project/AssemblyInfo.vb
+++ Sources/EwECustomPlugins/EwEShapeGridPlugin/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.3.1028")> 
-<Assembly: AssemblyFileVersion("6.3.1028")> 
+<Assembly: AssemblyVersion("6.3.1029")> 
+<Assembly: AssemblyFileVersion("6.3.1029")> 
