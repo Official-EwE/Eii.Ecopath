d99af8e5eec5b5f4c92ad5ccdf0dffa1349b07f0
JeroenS <jeroen.steenbeek@gmail.com>
2011-12-06 15:10:59 +0000

    Map uses layer sync
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@7122 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/ScientificInterfaceShared/Controls/Map/Layers/cLayerSynchronizer.vb
@@ -0,0 +1,63 @@
+﻿#Region " Imports "
+
+Option Strict On
+Imports EwEUtils.Core
+
+#End Region ' Imports
+
+Namespace Controls.Map.Layers
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Helper class, synchronizes group, fleet and month selections between
+    ''' an array of <see cref="cLayer">UI map layers</see>.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Public Class cLayerSynchronizer
+
+        Private m_bInSync As Boolean = False
+
+        ''' -------------------------------------------------------------------
+        ''' <summary>
+        ''' Apply the same group, fleet and month selection in a given <paramref name="l">layer</paramref>
+        ''' to an collection of layers.
+        ''' </summary>
+        ''' <param name="l">The layer to update group, fleet and month selection from.</param>
+        ''' <param name="layers">The collection of layers to apply the selection to.</param>
+        ''' -------------------------------------------------------------------
+        Public Sub Synchronize(l As cLayer, layers As ICollection(Of cLayer))
+
+            If (Me.m_bInSync) Then Return
+
+            Me.m_bInSync = True
+
+            If TypeOf l.Data Is ICoreFleetFilter Then
+                Dim iFleet As Integer = DirectCast(l.Data, ICoreFleetFilter).Fleet
+                For Each lTest As cLayer In layers
+                    If Not Object.ReferenceEquals(l, lTest) And TypeOf lTest.Data Is ICoreFleetFilter Then
+                        DirectCast(lTest.Data, ICoreFleetFilter).Fleet = iFleet
+                    End If
+                Next
+            ElseIf TypeOf l Is ICoreGroupFilter Then
+                Dim iGroup As Integer = DirectCast(l.Data, ICoreGroupFilter).Group
+                For Each lTest As cLayer In layers
+                    If Not Object.ReferenceEquals(l, lTest) And TypeOf lTest.Data Is ICoreGroupFilter Then
+                        DirectCast(lTest.Data, ICoreGroupFilter).Group = iGroup
+                    End If
+                Next
+            ElseIf TypeOf l Is ICoreMonthFilter Then
+                Dim iMonth As Integer = DirectCast(l.Data, ICoreMonthFilter).Month
+                For Each lTest As cLayer In layers
+                    If Not Object.ReferenceEquals(l, lTest) And TypeOf lTest.Data Is ICoreMonthFilter Then
+                        DirectCast(lTest.Data, ICoreMonthFilter).Month = iMonth
+                    End If
+                Next
+            End If
+
+            Me.m_bInSync = False
+
+        End Sub
+
+    End Class
+
+End Namespace
--- Sources/ScientificInterfaceShared/Controls/Map/ucMap.vb
+++ Sources/ScientificInterfaceShared/Controls/Map/ucMap.vb
@@ -37,6 +37,9 @@ Namespace Controls.Map
         Private m_layerSelected As cLayer = Nothing
         ''' <summary>States whether map must be refreshed</summary>
         Private m_bRefreshMap As Boolean = False
+        ''' <summary>Helper class to keep group, fleet and month selections 
+        ''' synchronized between all layers in the map.</summary>
+        Private m_sync As New cLayerSynchronizer()
 
         Public Sub New()
 
@@ -324,6 +327,8 @@ Namespace Controls.Map
 
             ' Ignore sole descriptive layer changes
             If (cf = cLayer.eChangeFlags.Descriptive) Then Return
+
+            Me.m_sync.Synchronize(l, Me.Layers)
 
             ' Handle selection changes
             If ((cf And cLayer.eChangeFlags.Selected) > 0) Then
