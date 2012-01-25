f6da598f9fb4ace449c6c5c0600f2954dcb4e57c
JeroenS <jeroen.steenbeek@gmail.com>
2012-01-25 19:10:42 +0000

    This MUST have been a bug
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@7380 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECore/Core/cCore.vb
+++ Sources/EwECore/Core/cCore.vb
@@ -8257,7 +8257,7 @@ Public Class cCore
 
             'Save to the current writer always (saveannual = false) or once per year (saveannual=true)
             'Default is to save every time step
-            If iTime Mod 0 = 0 Or m_EcoSpaceData.bSaveAnnual = False Then
+            If iTime Mod 12 = 0 Or m_EcoSpaceData.bSaveAnnual = False Then
                 Me.SaveEcospaceResults(Me.m_spaceresults)
             End If
             'Call the interface delegate
