6fa1ce44539655bab7a65a0f564fb004f1a091e7
automatedbuild <automatedbuild@ecopath.org>
2012-03-04 10:05:16 +0000

    Build 6.3.7522.0
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@7523 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECustomPlugins/EwENetworkAnalysis/My Project/AssemblyInfo.vb
+++ Sources/EwECustomPlugins/EwENetworkAnalysis/My Project/AssemblyInfo.vb
@@ -48,5 +48,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.3.7519.0")> 
+<Assembly: AssemblyVersion("6.3.7522.0")> 
 <Assembly: AssemblyFileVersion("6.3.1125")> 
