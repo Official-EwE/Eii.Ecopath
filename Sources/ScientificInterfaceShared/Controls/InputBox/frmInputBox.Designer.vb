0adc663ef15c1cb35312a4fe37551307207fd746
JeroenS <jeroen.steenbeek@gmail.com>
2012-03-27 17:10:49 +0000

    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@7623 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/ScientificInterfaceShared/Controls/InputBox/frmInputBox.Designer.vb
@@ -0,0 +1,81 @@
+﻿Namespace Controls
+
+    <Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+    Partial Class frmInputBox
+        Inherits System.Windows.Forms.Form
+
+        'Form overrides dispose to clean up the component list.
+        <System.Diagnostics.DebuggerNonUserCode()> _
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            Try
+                If disposing AndAlso components IsNot Nothing Then
+                    components.Dispose()
+                End If
+            Finally
+                MyBase.Dispose(disposing)
+            End Try
+        End Sub
+
+        'Required by the Windows Form Designer
+        Private components As System.ComponentModel.IContainer
+
+        'NOTE: The following procedure is required by the Windows Form Designer
+        'It can be modified using the Windows Form Designer.  
+        'Do not modify it using the code editor.
+        <System.Diagnostics.DebuggerStepThrough()> _
+        Private Sub InitializeComponent()
+            Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmInputBox))
+            Me.m_lblPrompt = New System.Windows.Forms.Label()
+            Me.m_tbxValue = New System.Windows.Forms.TextBox()
+            Me.m_btnOk = New System.Windows.Forms.Button()
+            Me.m_btnCancel = New System.Windows.Forms.Button()
+            Me.SuspendLayout()
+            '
+            'm_lblPrompt
+            '
+            resources.ApplyResources(Me.m_lblPrompt, "m_lblPrompt")
+            Me.m_lblPrompt.Name = "m_lblPrompt"
+            '
+            'm_tbxValue
+            '
+            resources.ApplyResources(Me.m_tbxValue, "m_tbxValue")
+            Me.m_tbxValue.Name = "m_tbxValue"
+            '
+            'm_btnOk
+            '
+            resources.ApplyResources(Me.m_btnOk, "m_btnOk")
+            Me.m_btnOk.Name = "m_btnOk"
+            Me.m_btnOk.UseVisualStyleBackColor = True
+            '
+            'm_btnCancel
+            '
+            resources.ApplyResources(Me.m_btnCancel, "m_btnCancel")
+            Me.m_btnCancel.Name = "m_btnCancel"
+            Me.m_btnCancel.UseVisualStyleBackColor = True
+            '
+            'frmInput
+            '
+            resources.ApplyResources(Me, "$this")
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ControlBox = False
+            Me.Controls.Add(Me.m_btnCancel)
+            Me.Controls.Add(Me.m_btnOk)
+            Me.Controls.Add(Me.m_tbxValue)
+            Me.Controls.Add(Me.m_lblPrompt)
+            Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+            Me.MaximizeBox = False
+            Me.MinimizeBox = False
+            Me.Name = "frmInput"
+            Me.ShowIcon = False
+            Me.ShowInTaskbar = False
+            Me.ResumeLayout(False)
+            Me.PerformLayout()
+
+        End Sub
+        Private WithEvents m_lblPrompt As System.Windows.Forms.Label
+        Private WithEvents m_tbxValue As System.Windows.Forms.TextBox
+        Private WithEvents m_btnOk As System.Windows.Forms.Button
+        Private WithEvents m_btnCancel As System.Windows.Forms.Button
+    End Class
+
+End Namespace
---
+++ Sources/ScientificInterfaceShared/Controls/InputBox/frmInputBox.vb
@@ -0,0 +1,96 @@
+﻿' ===============================================================================
+' This file is part of Ecopath with Ecosim (EwE)
+'
+' EwE is free software: you can redistribute it and/or modify it under the terms
+' of the GNU General Public License version 2 as published by the Free Software 
+' Foundation.
+'
+' EwE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+' without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+' PURPOSE. See the GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License along with EwE.
+' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
+'
+' Copyright 1991-2012 UBC Fisheries Centre, Vancouver BC, Canada.
+' ===============================================================================
+'
+#Region " Imports "
+
+Option Strict On
+Imports EwEUtils.SystemUtilities.cSystemUtils
+
+#End Region ' Imports
+
+Namespace Controls
+
+    ''' =======================================================================
+    ''' <summary>
+    ''' InputBox alternative for Mono compliance.
+    ''' </summary>
+    ''' =======================================================================
+    Public Class frmInputBox
+
+#Region " Private vars "
+
+        ''' <summary>Value maintained in the box.</summary>
+        Private m_strValue As String = ""
+
+#End Region ' Private vars
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+
+        Public Shadows Function Show(owner As IWin32Window, _
+                                     ByVal strPrompt As String, _
+                                     Optional strCaption As String = "", _
+                                     Optional strInitialValue As String = "") As DialogResult
+            Me.Text = strCaption
+            Me.m_lblPrompt.Text = strPrompt
+            Me.m_tbxValue.Text = strInitialValue
+            Return Me.ShowDialog(owner)
+        End Function
+
+        Public Shadows Function Show(ByVal strPrompt As String, _
+                                     Optional strCaption As String = "", _
+                                     Optional strInitialValue As String = "") As DialogResult
+            Return Me.Show(Nothing, strPrompt, strCaption, strInitialValue)
+        End Function
+
+#Region " Events "
+
+        Private Sub OnOK(sender As System.Object, e As System.EventArgs) _
+            Handles m_btnOk.Click
+            Me.m_strValue = Me.m_tbxValue.Text
+            Me.DialogResult = Windows.Forms.DialogResult.OK
+            Me.Close()
+        End Sub
+
+        Private Sub OnCancel(sender As System.Object, e As System.EventArgs) _
+            Handles m_btnCancel.Click
+            Me.m_strValue = ""
+            Me.DialogResult = Windows.Forms.DialogResult.Cancel
+            Me.Close()
+        End Sub
+
+#End Region ' Events
+
+#Region " Public properties "
+
+        ''' -------------------------------------------------------------------
+        ''' <summary>
+        ''' Get the value entered in the input box.
+        ''' </summary>
+        ''' -------------------------------------------------------------------
+        Public ReadOnly Property Value As String
+            Get
+                Return Me.m_strValue
+            End Get
+        End Property
+
+#End Region ' Public properties
+
+    End Class
+
+End Namespace
