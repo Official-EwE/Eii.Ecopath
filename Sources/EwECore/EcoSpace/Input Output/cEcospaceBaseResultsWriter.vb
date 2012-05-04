76cc2215573172d0307a616e35d2b3746cdf1452
joeb <joeb@ecopath.org>
2012-05-04 15:46:38 +0000

    cEcoSpaceBaseResultsWriter added a Console.Write() if CreateOutputDir() fails for running on WestGrid
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@7682 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECore/EcoSpace/Input Output/cEcospaceBaseResultsWriter.vb
+++ Sources/EwECore/EcoSpace/Input Output/cEcospaceBaseResultsWriter.vb
@@ -94,6 +94,7 @@ Public MustInherit Class cEcospaceBaseResultsWriter
 
         If (Not cFileUtils.IsDirectoryAvailable(Me.OutputDirectory, True)) Then
             Debug.Assert(False, Me.ToString & ".CreateTimeStampedDir() cannot create directory")
+            System.Console.WriteLine("Ecospace output writer failed to create directory " & Me.OutputDirectory)
         End If
 
     End Sub
