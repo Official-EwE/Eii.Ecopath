ce4719deed846fd0f9be2be51005760dde81948e
JeroenS <jeroen.steenbeek@gmail.com>
2012-05-23 15:57:27 +0000

    Adheres to spaces coded paths logic
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@7742 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECore/SpatialTemporal/cSpatialOperationLog.vb
+++ Sources/EwECore/SpatialTemporal/cSpatialOperationLog.vb
@@ -171,8 +171,15 @@ Namespace SpatialData
             Dim sb As New StringBuilder()
 
             If (Not Me.m_bLogStarted) Then
-                ' Write header
-                Me.m_strLogFileName = Path.Combine(Me.m_core.OutputPath, Me.m_core.EcospaceOutputFileLocation("SpatialOperations", "", ".txt"))
+
+                ' Allowed to use smart directory>
+                If Me.m_core.m_EcoSpaceData.bUseCoreOuputDir Then
+                    ' #Yes: use core smartness
+                    Me.m_strLogFileName = Path.Combine(Me.m_core.OutputPath, Me.m_core.EcospaceOutputFileLocation("SpatialOperations", "", ".txt"))
+                Else
+                    ' #No: use base output directory
+                    Me.m_strLogFileName = Me.m_core.OutputPath ' 
+                End If
 
                 If Not cFileUtils.IsDirectoryAvailable(Path.GetDirectoryName(Me.m_strLogFileName), True) Then
                     cLog.Write("cSpatialOperationsLog: unable to create output directory " & Me.m_strLogFileName)
