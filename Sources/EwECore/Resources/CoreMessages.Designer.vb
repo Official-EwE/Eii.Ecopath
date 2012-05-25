d42e655339d530a4101ebcc70c0c3057346296a3
JeroenS <jeroen.steenbeek@gmail.com>
2012-05-25 18:21:14 +0000

    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@7757 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECore/Resources/CoreMessages.Designer.vb
+++ Sources/EwECore/Resources/CoreMessages.Designer.vb
@@ -2112,7 +2112,7 @@ Namespace My.Resources
         End Property
         
         '''<summary>
-        '''  Looks up a localized string similar to {0}% indexed, {1}% time steps covered with {2}% partial and {3}% total area overlap.
+        '''  Looks up a localized string similar to {0}% indexed, {1}% temporal overlap with {2}% partial and {3}% total area overlap.
         '''</summary>
         Friend Shared ReadOnly Property STATUS_SPATIALTEMPORAL_COMPATIBILITY() As String
             Get
