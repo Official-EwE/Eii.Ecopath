b9fc487897d2ee0ff11661519d6913f479e68ea7
JeroenS <jeroen.steenbeek@gmail.com>
2012-09-14 16:56:47 +0000

    Renamed DFO plug-in as per Carie's request
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@8031 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/EwECustomPlugins/EwEMultiRunsPlugin/UI/frmMain.Designer.vb
@@ -0,0 +1,258 @@
+﻿' ===============================================================================
+' This file is part of Ecopath with Ecosim (EwE)
+'
+' EwE is free software: you can redistribute it and/or modify it under the terms
+' of the GNU General Public License version 2 as published by the Free Software 
+' Foundation.
+'
+' EwE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+' without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+' PURPOSE. See the GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License along with EwE.
+' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
+'
+' Copyright 1991-2012 UBC Fisheries Centre, Vancouver BC, Canada.
+' ===============================================================================
+'
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits ScientificInterfaceShared.Forms.frmEwE
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.m_csMain = New System.Windows.Forms.SplitContainer()
+        Me.m_lblFilesSrc = New System.Windows.Forms.Label()
+        Me.m_clbFilesSrc = New System.Windows.Forms.CheckedListBox()
+        Me.m_hdrIn = New ScientificInterfaceShared.Controls.cEwEHeaderLabel()
+        Me.m_lblSource = New System.Windows.Forms.Label()
+        Me.m_btnAllSrc = New System.Windows.Forms.Button()
+        Me.m_rbAnnual = New System.Windows.Forms.RadioButton()
+        Me.m_tbxSource = New System.Windows.Forms.TextBox()
+        Me.m_rbMonthly = New System.Windows.Forms.RadioButton()
+        Me.m_btnChooseSrc = New System.Windows.Forms.Button()
+        Me.m_lblReadAs = New System.Windows.Forms.Label()
+        Me.m_hrdOut = New ScientificInterfaceShared.Controls.cEwEHeaderLabel()
+        Me.m_tbxDest = New System.Windows.Forms.TextBox()
+        Me.m_clbValues = New System.Windows.Forms.CheckedListBox()
+        Me.m_lblDest = New System.Windows.Forms.Label()
+        Me.m_btnAllVars = New System.Windows.Forms.Button()
+        Me.m_btnChooseOut = New System.Windows.Forms.Button()
+        Me.m_lblVars = New System.Windows.Forms.Label()
+        Me.m_pbLogo = New System.Windows.Forms.PictureBox()
+        Me.m_btnRun = New System.Windows.Forms.Button()
+        CType(Me.m_csMain, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.m_csMain.Panel1.SuspendLayout()
+        Me.m_csMain.Panel2.SuspendLayout()
+        Me.m_csMain.SuspendLayout()
+        CType(Me.m_pbLogo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'm_csMain
+        '
+        resources.ApplyResources(Me.m_csMain, "m_csMain")
+        Me.m_csMain.Name = "m_csMain"
+        '
+        'm_csMain.Panel1
+        '
+        Me.m_csMain.Panel1.Controls.Add(Me.m_lblFilesSrc)
+        Me.m_csMain.Panel1.Controls.Add(Me.m_clbFilesSrc)
+        Me.m_csMain.Panel1.Controls.Add(Me.m_hdrIn)
+        Me.m_csMain.Panel1.Controls.Add(Me.m_lblSource)
+        Me.m_csMain.Panel1.Controls.Add(Me.m_btnAllSrc)
+        Me.m_csMain.Panel1.Controls.Add(Me.m_rbAnnual)
+        Me.m_csMain.Panel1.Controls.Add(Me.m_tbxSource)
+        Me.m_csMain.Panel1.Controls.Add(Me.m_rbMonthly)
+        Me.m_csMain.Panel1.Controls.Add(Me.m_btnChooseSrc)
+        Me.m_csMain.Panel1.Controls.Add(Me.m_lblReadAs)
+        '
+        'm_csMain.Panel2
+        '
+        Me.m_csMain.Panel2.Controls.Add(Me.m_hrdOut)
+        Me.m_csMain.Panel2.Controls.Add(Me.m_tbxDest)
+        Me.m_csMain.Panel2.Controls.Add(Me.m_clbValues)
+        Me.m_csMain.Panel2.Controls.Add(Me.m_lblDest)
+        Me.m_csMain.Panel2.Controls.Add(Me.m_btnAllVars)
+        Me.m_csMain.Panel2.Controls.Add(Me.m_btnChooseOut)
+        Me.m_csMain.Panel2.Controls.Add(Me.m_lblVars)
+        '
+        'm_lblFilesSrc
+        '
+        resources.ApplyResources(Me.m_lblFilesSrc, "m_lblFilesSrc")
+        Me.m_lblFilesSrc.Name = "m_lblFilesSrc"
+        '
+        'm_clbFilesSrc
+        '
+        resources.ApplyResources(Me.m_clbFilesSrc, "m_clbFilesSrc")
+        Me.m_clbFilesSrc.CheckOnClick = True
+        Me.m_clbFilesSrc.FormattingEnabled = True
+        Me.m_clbFilesSrc.Name = "m_clbFilesSrc"
+        '
+        'm_hdrIn
+        '
+        resources.ApplyResources(Me.m_hdrIn, "m_hdrIn")
+        Me.m_hdrIn.CanCollapseParent = False
+        Me.m_hdrIn.CollapsedParentHeight = 0
+        Me.m_hdrIn.IsCollapsed = False
+        Me.m_hdrIn.Name = "m_hdrIn"
+        '
+        'm_lblSource
+        '
+        resources.ApplyResources(Me.m_lblSource, "m_lblSource")
+        Me.m_lblSource.Name = "m_lblSource"
+        '
+        'm_btnAllSrc
+        '
+        resources.ApplyResources(Me.m_btnAllSrc, "m_btnAllSrc")
+        Me.m_btnAllSrc.Name = "m_btnAllSrc"
+        Me.m_btnAllSrc.UseVisualStyleBackColor = True
+        '
+        'm_rbAnnual
+        '
+        resources.ApplyResources(Me.m_rbAnnual, "m_rbAnnual")
+        Me.m_rbAnnual.Name = "m_rbAnnual"
+        Me.m_rbAnnual.TabStop = True
+        Me.m_rbAnnual.UseVisualStyleBackColor = True
+        '
+        'm_tbxSource
+        '
+        resources.ApplyResources(Me.m_tbxSource, "m_tbxSource")
+        Me.m_tbxSource.Name = "m_tbxSource"
+        Me.m_tbxSource.ReadOnly = True
+        '
+        'm_rbMonthly
+        '
+        resources.ApplyResources(Me.m_rbMonthly, "m_rbMonthly")
+        Me.m_rbMonthly.Name = "m_rbMonthly"
+        Me.m_rbMonthly.TabStop = True
+        Me.m_rbMonthly.UseVisualStyleBackColor = True
+        '
+        'm_btnChooseSrc
+        '
+        resources.ApplyResources(Me.m_btnChooseSrc, "m_btnChooseSrc")
+        Me.m_btnChooseSrc.Name = "m_btnChooseSrc"
+        Me.m_btnChooseSrc.UseVisualStyleBackColor = True
+        '
+        'm_lblReadAs
+        '
+        resources.ApplyResources(Me.m_lblReadAs, "m_lblReadAs")
+        Me.m_lblReadAs.Name = "m_lblReadAs"
+        '
+        'm_hrdOut
+        '
+        resources.ApplyResources(Me.m_hrdOut, "m_hrdOut")
+        Me.m_hrdOut.CanCollapseParent = False
+        Me.m_hrdOut.CollapsedParentHeight = 0
+        Me.m_hrdOut.IsCollapsed = False
+        Me.m_hrdOut.Name = "m_hrdOut"
+        '
+        'm_tbxDest
+        '
+        resources.ApplyResources(Me.m_tbxDest, "m_tbxDest")
+        Me.m_tbxDest.Name = "m_tbxDest"
+        Me.m_tbxDest.ReadOnly = True
+        '
+        'm_clbValues
+        '
+        resources.ApplyResources(Me.m_clbValues, "m_clbValues")
+        Me.m_clbValues.CheckOnClick = True
+        Me.m_clbValues.FormattingEnabled = True
+        Me.m_clbValues.Name = "m_clbValues"
+        '
+        'm_lblDest
+        '
+        resources.ApplyResources(Me.m_lblDest, "m_lblDest")
+        Me.m_lblDest.Name = "m_lblDest"
+        '
+        'm_btnAllVars
+        '
+        resources.ApplyResources(Me.m_btnAllVars, "m_btnAllVars")
+        Me.m_btnAllVars.Name = "m_btnAllVars"
+        Me.m_btnAllVars.UseVisualStyleBackColor = True
+        '
+        'm_btnChooseOut
+        '
+        resources.ApplyResources(Me.m_btnChooseOut, "m_btnChooseOut")
+        Me.m_btnChooseOut.Name = "m_btnChooseOut"
+        Me.m_btnChooseOut.UseVisualStyleBackColor = True
+        '
+        'm_lblVars
+        '
+        resources.ApplyResources(Me.m_lblVars, "m_lblVars")
+        Me.m_lblVars.Name = "m_lblVars"
+        '
+        'm_pbLogo
+        '
+        resources.ApplyResources(Me.m_pbLogo, "m_pbLogo")
+        Me.m_pbLogo.BackColor = System.Drawing.Color.White
+        Me.m_pbLogo.BackgroundImage = Global.EwEMultiRunsPlugin.My.Resources.Resources.logo_canada_dfo
+        Me.m_pbLogo.Name = "m_pbLogo"
+        Me.m_pbLogo.TabStop = False
+        '
+        'm_btnRun
+        '
+        resources.ApplyResources(Me.m_btnRun, "m_btnRun")
+        Me.m_btnRun.Name = "m_btnRun"
+        Me.m_btnRun.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AllowDrop = True
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.m_csMain)
+        Me.Controls.Add(Me.m_pbLogo)
+        Me.Controls.Add(Me.m_btnRun)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "frmMain"
+        Me.ShowInTaskbar = False
+        Me.m_csMain.Panel1.ResumeLayout(False)
+        Me.m_csMain.Panel1.PerformLayout()
+        Me.m_csMain.Panel2.ResumeLayout(False)
+        Me.m_csMain.Panel2.PerformLayout()
+        CType(Me.m_csMain, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.m_csMain.ResumeLayout(False)
+        CType(Me.m_pbLogo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Private WithEvents m_clbFilesSrc As System.Windows.Forms.CheckedListBox
+    Private WithEvents m_tbxDest As System.Windows.Forms.TextBox
+    Private WithEvents m_csMain As System.Windows.Forms.SplitContainer
+    Friend WithEvents m_lblFilesSrc As System.Windows.Forms.Label
+    Private WithEvents m_hdrIn As ScientificInterfaceShared.Controls.cEwEHeaderLabel
+    Private WithEvents m_lblSource As System.Windows.Forms.Label
+    Private WithEvents m_btnAllSrc As System.Windows.Forms.Button
+    Private WithEvents m_rbAnnual As System.Windows.Forms.RadioButton
+    Private WithEvents m_tbxSource As System.Windows.Forms.TextBox
+    Private WithEvents m_rbMonthly As System.Windows.Forms.RadioButton
+    Private WithEvents m_btnChooseSrc As System.Windows.Forms.Button
+    Private WithEvents m_lblReadAs As System.Windows.Forms.Label
+    Private WithEvents m_hrdOut As ScientificInterfaceShared.Controls.cEwEHeaderLabel
+    Private WithEvents m_clbValues As System.Windows.Forms.CheckedListBox
+    Private WithEvents m_lblDest As System.Windows.Forms.Label
+    Private WithEvents m_btnAllVars As System.Windows.Forms.Button
+    Private WithEvents m_btnChooseOut As System.Windows.Forms.Button
+    Private WithEvents m_lblVars As System.Windows.Forms.Label
+    Private WithEvents m_pbLogo As System.Windows.Forms.PictureBox
+    Private WithEvents m_btnRun As System.Windows.Forms.Button
+End Class
---
+++ Sources/EwECustomPlugins/EwEMultiRunsPlugin/cMultiRunsEngine.vb
@@ -0,0 +1,315 @@
+﻿' ===============================================================================
+' This file is part of Ecopath with Ecosim (EwE)
+'
+' EwE is free software: you can redistribute it and/or modify it under the terms
+' of the GNU General Public License version 2 as published by the Free Software 
+' Foundation.
+'
+' EwE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+' without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+' PURPOSE. See the GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License along with EwE.
+' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
+'
+' Copyright 1991-2012 UBC Fisheries Centre, Vancouver BC, Canada.
+' ===============================================================================
+'
+#Region " Imports "
+
+Option Strict On
+Imports System.IO
+Imports EwECore
+Imports EwECore.Ecosim
+Imports ScientificInterfaceShared.Controls
+Imports EwEUtils.Utilities
+Imports EwEUtils.SystemUtilities
+Imports EwEUtils.Core
+Imports SharedResources = ScientificInterfaceShared.My.Resources
+
+#End Region ' Imports
+
+''' ---------------------------------------------------------------------------
+''' <summary>
+''' The perturb engine that does all the work: modifies forcing functions, runs 
+''' Ecosim, and organizes extraction of results.
+''' </summary>
+''' ---------------------------------------------------------------------------
+Friend Class cMultiRunsEngine
+
+#Region " Private helper classes "
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Private Class cFFCache
+
+        Private m_ff As cForcingFunction = Nothing
+        Private m_asData As Single() = Nothing
+
+        Public Sub New(ff As cForcingFunction)
+            Me.m_ff = ff
+            Me.m_asData = ff.ShapeData
+        End Sub
+
+        Public Sub Restore()
+            Me.StartEdit()
+            Me.EndEdit()
+        End Sub
+
+        Public Sub StartEdit()
+            Me.m_ff.LockUpdates()
+            For i As Integer = 0 To Me.m_ff.ShapeData.Length - 1
+                Me.m_ff.ShapeData(i) = Me.m_asData(i)
+            Next
+        End Sub
+
+        Public Sub EndEdit()
+            Me.m_ff.UnlockUpdates(False)
+        End Sub
+
+        Public ReadOnly Property FF As cForcingFunction
+            Get
+                Return Me.m_ff
+            End Get
+        End Property
+
+    End Class
+
+#End Region ' Private helper classes
+
+#Region " Privates "
+
+    Private m_uic As cUIContext = Nothing
+    Private m_man As cForcingFunctionManager = Nothing
+    Private m_astrFiles As String()
+    Private m_strOutFolder As String = ""
+    Private m_bMonthly As Boolean = False
+    Private m_options As cEcosimResultWriter.eResultTypes() = Nothing
+    Private m_FFCache As New Dictionary(Of String, cFFCache)
+
+#End Region ' Privates
+
+#Region " Public bits "
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Create the worker.
+    ''' </summary>
+    ''' <param name="uic">The UI context to operate onto.</param>
+    ''' <param name="astrFiles">The files to read and apply.</param>
+    ''' <param name="strOutFolder">Output folder.</param>
+    ''' <param name="bMonthly">States whether files should be read as monthly (true) or annual (false) values.</param>
+    ''' <param name="options"><see cref="cEcosimResultWriter.eResultTypes">Output options</see>.</param>
+    ''' -----------------------------------------------------------------------
+    Public Sub New(ByVal uic As cUIContext, _
+                   ByVal astrFiles As String(), _
+                   ByVal strOutFolder As String, _
+                   ByVal bMonthly As Boolean, _
+                   ByVal options As cEcosimResultWriter.eResultTypes())
+
+        Me.m_uic = uic
+        Me.m_man = uic.Core.ForcingShapeManager
+        Me.m_bMonthly = bMonthly
+        Me.m_astrFiles = astrFiles
+        Me.m_options = options
+
+        Dim strDate As String = Date.Now.ToString("yy-MM-dd hh-mm")
+        Dim strScope As String = cSystemUtils.IIF(bMonthly, "monthly", "annual")
+        Me.m_strOutFolder = Path.Combine(strOutFolder, cFileUtils.ToValidFileName(String.Format("Run {0} {1}", strDate, strScope), False))
+
+        For Each ff As cForcingFunction In Me.m_man
+            Me.m_FFCache(ff.Name) = New cFFCache(ff)
+        Next
+
+    End Sub
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Run!
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Public Sub Run()
+
+        Dim core As cCore = Me.m_uic.Core
+
+        If Not core.SaveChanges() Then Return
+
+        core.SetBatchLock(cCore.eBatchLockType.Update)
+        cApplicationStatusNotifier.StartProgress(core, My.Resources.STATUS_INITIALIZING, -1)
+        Try
+            Dim iNum As Integer = Me.m_astrFiles.Length
+
+            For i As Integer = 0 To iNum - 1
+
+                Dim strFile As String = Me.m_astrFiles(i)
+                Dim strFileShort As String = Path.GetFileNameWithoutExtension(strFile)
+                Dim strFolder As String = Path.Combine(Me.m_strOutFolder, strFileShort)
+
+                If Not cFileUtils.IsDirectoryAvailable(strFolder, True) Then
+                    'Aargh
+                    Exit For
+                End If
+
+                cApplicationStatusNotifier.UpdateProgress(core, String.Format(My.Resources.STATUS_LOADING, strFileShort), CSng((1 + i * 4) / (iNum * 4)))
+                Me.ReadCSVIntoFF(strFile)
+                cApplicationStatusNotifier.UpdateProgress(core, String.Format(My.Resources.STATUS_RUNNING, strFileShort), CSng((2 + i * 4) / (iNum * 4)))
+                core.RunEcoSim()
+                cApplicationStatusNotifier.UpdateProgress(core, String.Format(My.Resources.STATUS_SAVING, strFileShort), CSng((3 + i * 4) / (iNum * 4)))
+                Me.WriteResults(strFolder, strFile, Me.m_options)
+            Next
+            Me.ReadCSVIntoFF("")
+        Catch ex As Exception
+            Debug.Assert(False, ex.Message)
+        End Try
+
+        cApplicationStatusNotifier.UpdateProgress(core, My.Resources.STATUS_RESTORING, -1)
+        For Each ffc As cFFCache In Me.m_FFCache.Values
+            ffc.Restore()
+        Next
+
+        Me.m_man.Update()
+        core.DiscardChanges()
+        GC.Collect()
+
+        core.ReleaseBatchLock(cCore.eBatchChangeLevelFlags.NotSet)
+        cApplicationStatusNotifier.EndProgress(core)
+
+    End Sub
+
+#End Region ' Public bits
+
+#Region " Internals "
+
+    Private Sub ReadCSVIntoFF(ByVal strFileName As String)
+
+        Dim reader As StreamReader = Nothing
+        Dim values() As String = Nothing
+        Dim strName As String = ""
+        Dim month As Integer = 0
+        Dim value As Single = 0.0!
+        Dim lff As New List(Of cFFCache)
+        Dim ff As cForcingFunction = Nothing
+        Dim msgStatus As cMessage = Nothing
+        Dim iRepetitions As Integer = 0
+
+        If Me.m_bMonthly Then iRepetitions = 1 Else iRepetitions = cCore.N_MONTHS
+
+        If File.Exists(strFileName) Then
+
+            ' Prevent 'our' FFs from updating prematurely while reading CSV file
+            For Each ffc As cFFCache In m_FFCache.Values
+                ffc.StartEdit()
+            Next
+
+            ' (4) Open the CSV file for reading
+            reader = New StreamReader(strFileName) 'read in csv files x1,x2,x3 etc
+
+            ' First line holds FF names
+            values = cStringUtils.SplitQualified(reader.ReadLine(), ","c)
+            ' Map to FF Cache items
+            For i As Integer = 0 To values.Length - 1
+                strName = values(i).Trim
+                If Me.m_FFCache.ContainsKey(strName) Then
+                    lff.Add(Me.m_FFCache(strName))
+                Else
+                    lff.Add(Nothing)
+                    If (msgStatus Is Nothing) Then
+                        msgStatus = New cMessage(String.Format(My.Resources.ERROR_UNKNOWN_FUNCTIONS, Path.GetFileNameWithoutExtension(strFileName)), _
+                                                 eMessageType.DataValidation, _
+                                                 eCoreComponentType.External, _
+                                                 eMessageImportance.Warning)
+                        msgStatus.Hyperlink = strFileName
+                    End If
+                    msgStatus.AddVariable(New cVariableStatus(eStatusFlags.FailedValidation, _
+                                                              String.Format(My.Resources.ERROR_UNKNOWN_FUNCTION, strName), _
+                                                              eVarNameFlags.NotSet, _
+                                                              eDataTypes.External, _
+                                                              eCoreComponentType.External, 0))
+                End If
+            Next
+
+            If (msgStatus IsNot Nothing) Then
+                Me.m_uic.Core.Messages.SendMessage(msgStatus)
+            Else
+
+                ' Check if the end of the file is not reached, the peek would return 0 if at the end of the file
+                'From While to end While is a loop that runs if certain conditions are true (as long as there are characters to read left in the file.
+                While reader.Peek() > 0
+
+                    'split the line into individual values (seperated by commas)
+                    values = cStringUtils.SplitQualified(reader.ReadLine, ","c)
+
+                    For j As Integer = 1 To iRepetitions
+
+                        'month from above +1 for the output of months and also to input for forcing functions
+                        month = month + 1
+
+                        For i As Integer = 0 To Math.Min(values.Length, lff.Count) - 1
+
+                            If lff(i) IsNot Nothing Then
+                                ' Get a FF from the FF manager
+                                ff = lff(i).FF
+                                ' By default, do not force a value
+                                value = 0.0
+                                ' Is a value?
+                                If Not String.IsNullOrWhiteSpace(values(i)) Then
+                                    ' Try to convert this value and set it into the FF
+                                    Try
+                                        ' Convert a value from string to a floating point number
+                                        value = Single.Parse(values(i))
+                                    Catch ex As Exception
+                                        ' Alert that CSV is somehow malformed
+                                        Debug.Assert(False, "Value '" & values(i) & "' unreadable, a number was expected")
+                                    End Try
+                                End If
+
+                                ' Does still fit?
+                                If (month < ff.ShapeData.Length) Then
+                                    ' Set value into FF for a given month
+                                    ff.ShapeData(month) = value
+                                End If
+                            End If
+                        Next
+                    Next
+
+                End While
+
+                ' CSV has been read, now release the update lock on FFs and apply the content of FFs to Ecosim
+                For Each ffc As cFFCache In m_FFCache.Values
+                    ffc.EndEdit()
+                Next
+                Me.m_man.Update()
+
+            End If
+
+            ' Close reader to release the csv file
+            reader.Close()
+
+        End If
+
+    End Sub
+
+    Private Sub WriteResults(ByVal strPath As String, ByVal strFile As String, ByVal outputs As cEcosimResultWriter.eResultTypes())
+
+        Dim resultsWriter As New cEcosimResultWriter(Me.m_uic.Core)
+        resultsWriter.WriteResults(strPath, outputs)
+
+    End Sub
+
+    Private Sub UpdateProgress(ByVal strMessage As String, status As eStatusFlags)
+
+        Try
+            Dim msg As New cMessage(strMessage, eMessageType.Any, eCoreComponentType.External, eMessageImportance.Information)
+            Me.m_uic.Core.Messages.SendMessage(msg)
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+#End Region ' Internals
+
+End Class
