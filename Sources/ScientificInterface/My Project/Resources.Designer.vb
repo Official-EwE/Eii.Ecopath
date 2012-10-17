482ccb8e024f29a6c61ccbbc69e7f4363ab09c29
JeroenS <jeroen.steenbeek@gmail.com>
2012-10-17 19:03:30 +0000

    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@8286 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterface/My Project/Resources.Designer.vb
+++ Sources/ScientificInterface/My Project/Resources.Designer.vb
@@ -1443,7 +1443,7 @@ Namespace My.Resources
         End Property
         
         '''<summary>
-        '''  Looks up a localized string similar to Ecopath with Ecosim CSIRO Beta.
+        '''  Looks up a localized string similar to Ecopath with Ecosim Beta.
         '''</summary>
         Public ReadOnly Property GENERIC_CAPTION() As String
             Get
