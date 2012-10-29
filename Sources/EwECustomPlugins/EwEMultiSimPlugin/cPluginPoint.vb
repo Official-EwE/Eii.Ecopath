1501a8b82896bf5f9ce0b28ec3fb83ee49407a71
JeroenS <jeroen.steenbeek@gmail.com>
2012-10-29 00:43:29 +0000

    No pic
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@8365 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECustomPlugins/EwEMultiSimPlugin/cPluginPoint.vb
+++ Sources/EwECustomPlugins/EwEMultiSimPlugin/cPluginPoint.vb
@@ -96,7 +96,7 @@ Public Class cPluginPoint
     Public ReadOnly Property ControlImage As System.Drawing.Image _
         Implements EwEPlugin.IGUIPlugin.ControlImage
         Get
-            Return My.Resources.logo_canada
+            Return Nothing ' My.Resources.logo_canada
         End Get
     End Property
 
