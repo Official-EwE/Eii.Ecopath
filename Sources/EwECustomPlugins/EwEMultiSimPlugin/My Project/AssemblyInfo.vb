1c21efb8be28dd7bf007d886f743149da053320b
automatedbuild <automatedbuild@ecopath.org>
2012-11-13 10:04:18 +0000

    Build 6.3.8487.0
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@8489 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECustomPlugins/EwEMultiSimPlugin/My Project/AssemblyInfo.vb
+++ Sources/EwECustomPlugins/EwEMultiSimPlugin/My Project/AssemblyInfo.vb
@@ -48,5 +48,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.3.8485.0")> 
+<Assembly: AssemblyVersion("6.3.8487.0")> 
 <Assembly: AssemblyFileVersion("1.0.0.0")> 
--- Sources/ScientificInterfaceShared/My Project/AssemblyInfo.vb
+++ Sources/ScientificInterfaceShared/My Project/AssemblyInfo.vb
@@ -47,5 +47,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.3.8485.0")> 
+<Assembly: AssemblyVersion("6.3.8487.0")> 
 <Assembly: AssemblyFileVersion("6.3.1125")> 
