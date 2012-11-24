55b20df2239e01d665e17c83a8fe5c0fd8f820d0
automatedbuild <automatedbuild@ecopath.org>
2012-11-24 18:57:58 +0000

    Build 6.3.8602.0
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@8603 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECustomPlugins/EwEEcotroph/My Project/AssemblyInfo.vb
+++ Sources/EwECustomPlugins/EwEEcotroph/My Project/AssemblyInfo.vb
@@ -1,4 +1,4 @@
-﻿Imports System
+Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' en utilisant '*', comme indiqué ci-dessous :
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.2.0.0")> 
+<Assembly: AssemblyVersion("6.3.8602.0")> 
 <Assembly: AssemblyFileVersion("1.2.0.0")> 
