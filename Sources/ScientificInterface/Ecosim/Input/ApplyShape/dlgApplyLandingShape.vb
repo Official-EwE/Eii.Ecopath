c2afeee0c7bd93eb1f3a49993c50a3e9e310a0a0
JeroenS <jeroen.steenbeek@gmail.com>
2013-01-08 20:37:29 +0000

    Ditto
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@8772 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterface/Ecosim/Input/ApplyShape/dlgApplyLandingShape.vb
+++ Sources/ScientificInterface/Ecosim/Input/ApplyShape/dlgApplyLandingShape.vb
@@ -421,6 +421,7 @@ Namespace Ecosim
                     itemSrc.ImageIndex = Me.m_lFFs.IndexOf(shapeSelected)
                     itemSrc.SubItems.Add("")
                     itemSrc.Tag = shapeSelected
+                    itemSrc.Selected = True
                     Me.UpdateAppliedShape(itemSrc, Me.m_appl)
 
                     Me.m_lvAppliedShapes.Items.Add(itemSrc)
