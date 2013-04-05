747debed5c436f322480beba4dda2653e8698d90
JeroenS <jeroen.steenbeek@gmail.com>
2013-04-05 20:45:49 +0000

    Made crash-resistant
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@9046 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECore/EcoSpace/Input Output/EcospaceBasemap/cEcospaceLayerImportance.vb
+++ Sources/EwECore/EcoSpace/Input Output/EcospaceBasemap/cEcospaceLayerImportance.vb
@@ -78,10 +78,15 @@ Public Class cEcospaceLayerImportance
 
     Public Overrides Property Cell(ByVal iRow As Integer, ByVal iCol As Integer) As Object
         Get
-            Return DirectCast(Me.Data, Single(,,))(Me.Index, iRow, iCol)
+            If Me.ValidateCellPosition(iRow, iCol) Then
+                Return DirectCast(Me.Data, Single(,,))(Me.Index, iRow, iCol)
+            End If
+            Return cCore.NULL_VALUE
         End Get
         Set(ByVal value As Object)
-            DirectCast(Me.Data, Single(,,))(Me.Index, iRow, iCol) = CSng(value)
+            If Me.ValidateCellPosition(iRow, iCol) Then
+                DirectCast(Me.Data, Single(,,))(Me.Index, iRow, iCol) = CSng(value)
+            End If
         End Set
     End Property
 
