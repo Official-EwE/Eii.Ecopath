db65aa63ff4d09d94e84d26334763b14f5d40584
JeroenS <jeroen.steenbeek@gmail.com>
2013-04-15 17:06:49 +0000

    Deployed
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@9107 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Examples/1. Basics/1. BareBasics/BareBasics.vb
@@ -0,0 +1,36 @@
+﻿Imports EwECore
+
+''' <summary>
+''' Bare basics program that loads an EwE model and checks whether Ecopath balances.
+''' </summary>
+Module BareBasics
+
+    Sub Main()
+
+        ' Create a new core
+        Dim core As New cCore()
+
+        ' Can we load a model into the core?
+        If core.LoadModel("Tampa_Bay.EwEmdb") Then
+            Console.WriteLine("Model loaded")
+
+            ' Does Ecopath balance?
+            If core.RunEcoPath() Then
+                Console.WriteLine("Ecopath balanced")
+            Else
+                Console.WriteLine("Ecopath did not balance")
+            End If
+
+            ' Done
+            core.CloseModel()
+        Else
+            Console.WriteLine("Model did not load")
+        End If
+
+        ' Wait for the user to press a key before closing this program
+        Console.WriteLine("Press a key to exit")
+        Console.ReadKey()
+
+    End Sub
+
+End Module
