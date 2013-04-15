185d1582ea53ba6cc0067eba8921fe5d05e51e27
JeroenS <jeroen.steenbeek@gmail.com>
2013-04-15 18:22:48 +0000

    Deployed
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@9116 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Examples/3. Plugins/2. BaseUserInterfacePlugin/BaseUserInterfacePlugin.vb
@@ -0,0 +1,235 @@
+' ===============================================================================
+' This file is part of Ecopath with Ecosim (EwE)
+'
+' EwE is free software: you can redistribute it and/or modify it under the terms
+' of the GNU General Public License version 2 as published by the Free Software 
+' Foundation.
+'
+' EwE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+' without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+' PURPOSE. See the GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License along with EwE.
+' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
+'
+' Copyright 1991-2012 UBC Fisheries Centre, Vancouver BC, Canada.
+' ===============================================================================
+'
+
+Imports EwECore
+Imports EwEPlugin
+
+''' <summary>
+''' Base code that can be used as a template to create a new plugin that integrates
+''' with the EwE user interface.
+''' </summary>
+''' <remarks>
+''' <para>This plug-in integrates with:</para>
+''' <list type="bullet">
+''' <item><description>the EwE6 navigation tree,</description>></item>
+''' <item><description>the EwE6 main menu.</description>></item>
+''' </list>
+''' <para>In order to run and test this plugin it must be integrated within the EwE6 scientific interface. 
+''' To achieve this, add this project to the EwE6 solution, and reference this project from within the 
+''' ScientificInterface. This ensures that your plug-in will be built with EwE6, and will be loaded by the 
+''' EwE6 plug-in manager when you run EwE6.</para>
+''' </remarks>
+''' 
+Public Class PluginPoint
+    Implements EwEPlugin.IMenuItemPlugin
+    Implements EwEPlugin.INavigationTreeItemPlugin
+
+#Region " Private variables "
+
+    Private m_bInitOK As Boolean = False
+    Private m_core As EwECore.cCore = Nothing
+    Private m_PluginInterface As frmEwEPlugin = Nothing
+
+#End Region ' Private variables
+
+#Region " Plug-in implementation "
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Initialize the plug-in. This is called only once when the EwE6 first 
+    ''' loads the plug-in.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Public Sub Initialize(ByVal core As Object) Implements EwEPlugin.IPlugin.Initialize
+
+        Try
+            ' Store reference to the core. We may need this later
+            m_core = DirectCast(core, EwECore.cCore)
+            ' Plug-in has been initialized and is ready for use
+            m_bInitOK = True
+        Catch ex As Exception
+            ' Plug-in has been initialized and is ready for use
+            m_bInitOK = False
+        End Try
+
+    End Sub
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Provide the internal name of the plug-in to EwE6. This name will not be 
+    ''' shown to the user, but will be used to sort plug-in items in container 
+    ''' user interface structures such as the EwE6 main menu and navigation tree.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Public ReadOnly Property Name() As String Implements EwEPlugin.IPlugin.Name
+        Get
+            Return "any name"
+        End Get
+    End Property
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Provide EwE6 with author information to display for the plug-in.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Public ReadOnly Property Author() As String Implements EwEPlugin.IPlugin.Author
+        Get
+            Return "UBC Fisheries Centre"
+        End Get
+    End Property
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Provide EwE6 with contact information to display for the plug-in.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Public ReadOnly Property Contact() As String Implements EwEPlugin.IPlugin.Contact
+        Get
+            Return "mailto:ewedevteam@gmail.com"
+        End Get
+    End Property
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Tell EwE6 what text to display in controls that provide access to 
+    ''' this plug-in.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Public ReadOnly Property ControlText() As String Implements EwEPlugin.IGUIPlugin.ControlText
+        Get
+            Return "EwE user interface template plug-in"
+        End Get
+    End Property
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Tell EwE6 what image to show for this plug-in.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Public ReadOnly Property ControlImage() As System.Drawing.Image Implements EwEPlugin.IGUIPlugin.ControlImage
+        Get
+            Return My.Resources.MenuItem1
+        End Get
+    End Property
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Tell EwE6 what text to display when the user hovers the mouse cursor
+    ''' over a user interface element for this plug-in.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Public ReadOnly Property ControlTooltipText() As String Implements EwEPlugin.IGUIPlugin.ControlTooltipText
+        Get
+            ' Show the description as a tooltip text
+            Return Me.Description
+        End Get
+    End Property
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Tell EwE6 what text to display for describing the plug-in.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Public ReadOnly Property Description() As String Implements EwEPlugin.IPlugin.Description
+        Get
+            Return "An example of nesting plug-ins in EwE6 interface"
+        End Get
+    End Property
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Provide EwE6 with a method to execute when a user interface control for 
+    ''' this plug-in is clicked by the user.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Public Sub OnControlClick(ByVal sender As Object, ByVal e As System.EventArgs, ByRef f As Windows.Forms.Form) Implements EwEPlugin.IGUIPlugin.OnControlClick
+
+        Dim bHasInterface As Boolean = False
+
+        ' Initialized ok?
+        If m_bInitOK Then
+
+            ' Test if form still exists. This is a two-step test: the interface needs to be defined, and has not been closed previously.
+            If Me.m_PluginInterface IsNot Nothing Then
+                If Not Me.m_PluginInterface.IsDisposed Then
+                    bHasInterface = True
+                End If
+            End If
+
+            ' Create the interface if needed
+            If Not bHasInterface Then
+                Me.m_PluginInterface = New frmEwEPlugin(m_core)
+            End If
+
+            ' Activate the interface
+            Me.m_PluginInterface.Show()
+
+            ' Pass interface reference back to calling app
+            f = Me.m_PluginInterface
+
+            ' Just to show what can be done: test where this function was invoked from
+            If TypeOf sender Is System.Windows.Forms.TreeNode Then
+                ' Plug-in was invoked from the EwE6 navigation panel
+            ElseIf TypeOf sender Is System.Windows.Forms.ToolStripMenuItem Then
+                ' Plug-in was invoked from the EwE6 main menu
+            End If
+        Else
+            Debug.Assert(False, "Plugin was not initialized properly.")
+        End If
+    End Sub
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Tell EwE6 where to place an item in its main menu.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Public ReadOnly Property MenuItemLocation() As String Implements EwEPlugin.IMenuItemPlugin.MenuItemLocation
+        Get
+            ' For example, a plug-in menu item should be placed in the main the 'Tools' menu. 
+            Return "MenuTools"
+        End Get
+    End Property
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Tell EwE6 when during application execution this plug-in should be accessible 
+    ''' to users.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Public ReadOnly Property EnabledState() As EwEUtils.Core.eCoreExecutionState Implements EwEPlugin.IGUIPlugin.EnabledState
+        Get
+            ' As an example, this plug-in is only accessible when Ecosim has loaded.
+            Return EwEUtils.Core.eCoreExecutionState.EcosimLoaded
+        End Get
+    End Property
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Tell EwE6 where to place an item in its navigation tree.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Public ReadOnly Property NavigationTreeItemLocation() As String Implements EwEPlugin.INavigationTreeItemPlugin.NavigationTreeItemLocation
+        Get
+            ' As an example, place a navigation tree item under the main 'tools' node.
+            Return "ndTools"
+        End Get
+    End Property
+
+#End Region ' Plug-in implementation
+
+End Class
---
+++ Examples/3. Plugins/2. BaseUserInterfacePlugin/UserControls/EcopathDataGrid.Designer.vb
@@ -0,0 +1,65 @@
+' ===============================================================================
+' This file is part of Ecopath with Ecosim (EwE)
+'
+' EwE is free software: you can redistribute it and/or modify it under the terms
+' of the GNU General Public License version 2 as published by the Free Software 
+' Foundation.
+'
+' EwE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+' without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+' PURPOSE. See the GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License along with EwE.
+' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
+'
+' Copyright 1991-2012 UBC Fisheries Centre, Vancouver BC, Canada.
+' ===============================================================================
+'
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class EcopathDataGrid
+    Inherits System.Windows.Forms.UserControl
+
+    'UserControl overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+        End If
+        MyBase.Dispose(disposing)
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridView1.Location = New System.Drawing.Point(0, 0)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(1114, 695)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'EcopathDataGrid
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "EcopathDataGrid"
+        Me.Size = New System.Drawing.Size(1114, 695)
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+
+End Class
