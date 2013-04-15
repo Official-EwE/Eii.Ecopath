2d642ba94263776e0c366fd81730978e897f20d4
JeroenS <jeroen.steenbeek@gmail.com>
2013-04-15 17:06:02 +0000

    Deployed
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@9106 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Examples/1. Basics/2. EcopathOutputs/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("EcopathOutputs")> 
+<Assembly: AssemblyDescription("This code provides an example how to load an EwE model, run Ecopath, and extract a few computed results")> 
+<Assembly: AssemblyCompany("UBC Fisheries Centre")> 
+<Assembly: AssemblyProduct("EcopathOutputs")> 
+<Assembly: AssemblyCopyright("Copyright © UBC Fisheries Centre 2013")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("6fbdd00f-0a48-4601-b883-8f21c52c101f")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
