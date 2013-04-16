380a4bb7536793bc8e90ea482c5630f3c3cb2d29
joeb <joeb@ecopath.org>
2013-04-16 19:05:22 +0000

    EcospaceInputs more basic code
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@9147 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Examples/1. Basics/9. EcospaceInputs/EcospaceInputsMainModule.vb
+++ Examples/1. Basics/9. EcospaceInputs/EcospaceInputsMainModule.vb
@@ -25,13 +25,22 @@ Imports EwECore
 
 Module EcospaceInputs
 
+#Region "Private Variables"
+
     Private core As cCore
 
     Private WithEvents statemonitor As cCoreStateMonitor
 
+#End Region
+
+#Region "Main"
+
     Sub Main()
 
+        'Initialize a new instance of the core
         core = New cCore
+
+        'Get the instance of the StateMonitor from the Core
         statemonitor = core.StateMonitor
 
         'Get a file name from the user
@@ -41,20 +50,22 @@ Module EcospaceInputs
         If core.LoadModel(modelfilename) Then
 
             If core.nEcosimScenarios > 0 And core.nEcospaceScenarios > 0 Then
+                'Load the first Ecosim and Ecospace scenarios
                 If core.LoadEcosimScenario(1) Then
                     If core.LoadEcospaceScenario(1) Then
 
                         Console.WriteLine("Loaded first Ecospace scenario")
 
-                        'Change some stuff
-                        'Run the beatch...
-
-                        core.EcospaceModelParameters.NumberOfTimeStepsPerYear = 12
-                        core.EcospaceModelParameters.TotalTime = 2
-
-                        core.RunEcoSpace(AddressOf onEcoSpaceTimeStep, False)
+                        'Set some Model Parameters. i.e. Run length...
+                        setEcoSpaceModelParameters()
 
 
+                        'Run Ecopace on this thread(synchronously)
+                        'core.RunEcoSpace() will block until the run has completed.
+                        'If runAsync = True core.RunEcoSpace() will return before the run has completed(asynchronously)
+                        'and onCoreExecutionStateEvent(cCoreStateMonitor) will be fire when the run has completed.
+                        Dim runAsync As Boolean = False
+                        core.RunEcoSpace(AddressOf onEcoSpaceTimeStep, runAsync)
 
                     Else
                         Console.WriteLine("Failed to load first Ecospace scenario")
@@ -66,7 +77,6 @@ Module EcospaceInputs
                 Console.WriteLine("This model does not contain any Ecosim scenarios")
             End If
 
-            core.CloseModel()
         Else
             Console.WriteLine("Model did not load")
         End If
@@ -74,14 +84,53 @@ Module EcospaceInputs
         Console.WriteLine("Press a key to exit")
         Console.ReadKey()
 
+        core.CloseModel()
+
     End Sub
 
+#End Region
+
+#Region "Set Ecospace Inputs"
+
+    Private Sub setEcoSpaceModelParameters()
+        core.EcospaceModelParameters.NumberOfTimeStepsPerYear = 12
+        core.EcospaceModelParameters.TotalTime = 10
+
+        'Have Ecospace distribute the Fishing Effort as a function of, Catch Value, Area Fished and the Fishing Cost Map
+        'If PredictEffort = False then Ecospace will use the static Ecopath Effort
+        core.EcospaceModelParameters.PredictEffort = True
+
+        'Use the MultiStanza calculations
+        core.EcospaceModelParameters.UseNewMultiStanza = True
+        'Alternativly use the IBM Model for Multistanza species distributions
+        'core.EcospaceModelParameters.UseIBM = True
+
+
+    End Sub
+
+#End Region
+
+#Region "Ecospace Events"
 
     Private Sub onEcoSpaceTimeStep(ByRef EcospaceResults As cEcospaceTimestep)
         System.Console.WriteLine("Ecospace Timestep " + EcospaceResults.iTimeStep.ToString)
 
     End Sub
 
+    Private Sub onCoreExecutionStateEvent(statemonitor As EwECore.cCoreStateMonitor) Handles statemonitor.CoreExecutionStateEvent
+
+        If statemonitor.CoreExecutionState = EwEUtils.Core.eCoreExecutionState.EcospaceCompleted Then
+            'Ecospace has completed a run 
+
+            System.Console.WriteLine("Ecospace run completed")
+
+        End If
+
+    End Sub
+
+#End Region
+
+#Region "Private support methods"
 
     ''' -----------------------------------------------------------------------
     ''' <summary>
@@ -107,12 +156,6 @@ Module EcospaceInputs
 
     End Function
 
-
-    Private Sub onCoreExecutionStateEvent(statemonitor As EwECore.cCoreStateMonitor) Handles statemonitor.CoreExecutionStateEvent
-
-        System.Console.WriteLine("  State change " + statemonitor.CoreExecutionState.ToString)
-
-    End Sub
-
+#End Region
 
 End Module
