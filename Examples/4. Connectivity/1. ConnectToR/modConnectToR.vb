dba993f4b5b649b4b7ab0785cff4b72e853a8f8b
JeroenS <jeroen.steenbeek@gmail.com>
2013-04-27 16:37:21 +0000

    Uses settings to store R path, model path
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@9234 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Examples/4. Connectivity/1. ConnectToR/modConnectToR.vb
+++ Examples/4. Connectivity/1. ConnectToR/modConnectToR.vb
@@ -23,9 +23,6 @@ Imports System.Text
 
 Module modConnectToR
 
-    ' Please change this path to point to R.exe on your local system!!!
-    Dim PathToR As String = "C:\Program Files\R\R-2.15.0\bin\r.exe"
-
     ''' -----------------------------------------------------------------------
     ''' <summary>
     ''' This example demonstrates how to execute an R script through VB.NET
@@ -36,11 +33,17 @@ Module modConnectToR
     ''' -----------------------------------------------------------------------
     Sub Main()
 
-        ' Create a new R connection
-        Dim connection As New cRBridge(PathToR)
+        Dim PathToR As String = PickRLocation()
+        If Not String.IsNullOrWhiteSpace(PathToR) Then
 
-        RunSimpleScript(connection)
-        RunEwEScript(connection)
+            Dim connection As New cRBridge(PathToR)
+
+            RunSimpleScript(connection)
+            RunEwEScript(connection)
+
+        Else
+            Console.WriteLine("R path not selected, aborting")
+        End If
 
         ' Done
         Console.WriteLine("Press any key to exit")
@@ -74,8 +77,7 @@ Module modConnectToR
         Console.WriteLine("Running EwE script")
 
         Dim core As New cCore()
-        Dim model As String = PickModel()
-
+        Dim model As String = PickModelLocation()
         Console.WriteLine(model)
 
         If core.LoadModel(model) Then
@@ -93,7 +95,9 @@ Module modConnectToR
                     script.Append(group.Biomass)
                 Next
                 script.AppendLine(")")
-                ' Add an R script line to return the mean biomass
+                ' Make R report the content of the biomass array
+                script.AppendLine("biomass")
+                ' Do some magical math
                 script.AppendLine("mean(biomass)")
 
                 connection.Execute(script.ToString)
@@ -108,14 +112,15 @@ Module modConnectToR
 
     Private Sub DumpROutputAndErrors(connection As cRBridge)
 
-        Console.WriteLine("Output:")
+        ' Did R run successfully?
         If connection.LastRunSuccess Then
-            ' #Yes: write results to the console window
+            ' #Yes: write R ouptut to the console window
+            Console.WriteLine("Output:")
             For i As Integer = 0 To connection.Output.Length - 1
                 Console.WriteLine(connection.Output(i))
             Next
         Else
-            ' #No: write erros to the console window
+            ' #No: write R errors to the console window
             Console.WriteLine("Errors:")
             For i As Integer = 0 To connection.Errors.Length - 1
                 Console.WriteLine(connection.Errors(i))
@@ -125,19 +130,27 @@ Module modConnectToR
 
     End Sub
 
+#Region " Selecting files "
+
     ''' -----------------------------------------------------------------------
     ''' <summary>
     ''' Presents the user with a standard Windows interface for selecting a file
     ''' </summary>
+    ''' <param name="strCaption">Caption for the file open form.</param>
+    ''' <param name="strFileFilter">File filter to use for the file open form.</param>
+    ''' <param name="strLastPickedFile">The location that the user picked last time.</param>
     ''' <returns>A user-selected file, or an empty string if the user did not select a file.</returns>
     ''' -----------------------------------------------------------------------
-    Private Function PickModel() As String
+    Private Function PickFile(ByVal strCaption As String, _
+                              ByVal strFileFilter As String, _
+                              ByVal strLastPickedFile As String) As String
 
         'Create a new open file dialogue
         Dim openFileDialogue As New OpenFileDialog()
 
-        'Set the file filters
-        openFileDialogue.Filter = "EwE models|*.ewemdb;*.mdb;*.eweaccdb;*.accdb"
+        openFileDialogue.Title = strCaption
+        openFileDialogue.Filter = strFileFilter
+        openFileDialogue.FileName = strLastPickedFile
 
         'Show the dialogue box and get the user-selected filename
         If (openFileDialogue.ShowDialog() = DialogResult.OK) Then
@@ -148,5 +161,50 @@ Module modConnectToR
         Return String.Empty
 
     End Function
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Pick the location of the R application on your system
+    ''' </summary>
+    ''' <returns>The location of the R application, or an empty string if the 
+    ''' user did not complete the selection process.</returns>
+    ''' -----------------------------------------------------------------------
+    Private Function PickRLocation() As String
+
+        ' Retrieve last used path to R from the persistent application settings
+        Dim PathToR As String = PickFile("Select R location", "R application|R.ewe", My.Settings.PathToR)
+
+        If Not String.IsNullOrWhiteSpace(PathToR) Then
+            ' Store path to R in the persistent application settings for the next time you run this application
+            My.Settings.PathToR = PathToR
+            ' Just to be sure
+            My.Settings.Save()
+        End If
+        Return PathToR
+
+    End Function
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Pick the location of the EwE model to use
+    ''' </summary>
+    ''' <returns>The location of a model, or an empty string if the user did not 
+    ''' complete the selection process.</returns>
+    ''' -----------------------------------------------------------------------
+    Private Function PickModelLocation() As String
+
+        ' Retrieve last used model path from the persistent application settings
+        Dim PathToModel As String = PickFile("Select EwE model", "EwE models|*.ewemdb;*.mdb;*.eweaccdb;*.accdb", My.Settings.PathToModel)
+
+        If Not String.IsNullOrWhiteSpace(PathToModel) Then
+            ' Store path to model in the persistent application settings for the next time you run this application
+            My.Settings.PathToModel = PathToModel
+            ' Just to be sure
+            My.Settings.Save()
+        End If
+        Return PathToModel
+
+    End Function
+#End Region ' Selecting files
 
 End Module
