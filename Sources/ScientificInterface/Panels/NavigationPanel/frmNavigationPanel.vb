931147f35b5bc092956173185bb0fba872fb4836
joeb <joeb@ecopath.org>
2013-08-23 17:55:41 +0000

    Removed MSE Batch from Navigation Tree
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@9533 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterface/Panels/NavigationPanel/frmNavigationPanel.vb
+++ Sources/ScientificInterface/Panels/NavigationPanel/frmNavigationPanel.vb
@@ -243,12 +243,13 @@ Public Class frmNavigationPanel
             .Add("ndMSEPlots", eCoreExecutionState.EcosimLoaded, GetType(frmMSEPlots), "") ' ToDo: connect to help
 
             'MSE Batch
-            .Add("ndRunBatch", eCoreExecutionState.EcosimLoaded, GetType(frmMSERunBatch), "") ' ToDo: connect to help
-            .Add("ndMSEBatchTFM", eCoreExecutionState.EcosimLoaded, GetType(frmMSEBatchTFM), "") ' ToDo: connect to help
-            .Add("ndMSEBatchFixedF", eCoreExecutionState.EcosimLoaded, GetType(frmMSEBatchFixedF), "") ' ToDo: connect to help
-            'jb Form not done yet
-            '' .Add("ndMSEBatchTAC", eCoreExecutionState.EcosimLoaded, GetType(frmMSEBatchTAC), "") ' ToDo: connect to help
-            .Add("ndMSEBatchParameters", eCoreExecutionState.EcosimLoaded, GetType(frmMSEBatchParameters), "") ' ToDo: connect to help
+            'Not ready for release yet
+            '.Add("ndRunBatch", eCoreExecutionState.EcosimLoaded, GetType(frmMSERunBatch), "") ' ToDo: connect to help
+            '.Add("ndMSEBatchTFM", eCoreExecutionState.EcosimLoaded, GetType(frmMSEBatchTFM), "") ' ToDo: connect to help
+            '.Add("ndMSEBatchFixedF", eCoreExecutionState.EcosimLoaded, GetType(frmMSEBatchFixedF), "") ' ToDo: connect to help
+            ''jb Form not done yet
+            ' '' .Add("ndMSEBatchTAC", eCoreExecutionState.EcosimLoaded, GetType(frmMSEBatchTAC), "") ' ToDo: connect to help
+            '.Add("ndMSEBatchParameters", eCoreExecutionState.EcosimLoaded, GetType(frmMSEBatchParameters), "") ' ToDo: connect to help
 
             'Ecospace habitat capacity stuff...
             .Add("ndHabCap", eCoreExecutionState.EcospaceLoaded, GetType(frmCapacityFunction), "") ' ToDo: connect to help
