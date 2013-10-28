487b11506e739802d64237423871f4c2ed6e1ad2
JeroenS <jeroen.steenbeek@gmail.com>
2013-10-28 15:22:16 +0000

    Added
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@9906 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources Internal/Plugins/CouplerLibPlugin/PluginExtenderBase/PluginExtenderBase.vb
@@ -0,0 +1,20 @@
+﻿Imports Microsoft.Win32
+Imports Couplerlib
+Public MustInherit Class PluginExtenderBase
+    Inherits System.Object
+
+    Public Overridable Sub Load(ByVal regkey1 As RegistryKey, ByVal iTestDataPath As String, ByVal ixmlfile As String, ByRef cci As CCouplerlib, ByVal itimebase As Double)
+
+    End Sub
+    Public Overridable Sub Edit(ByVal carryoutedit As Boolean, ByVal timerratio As Integer, ByVal spinupdays As Integer, ByRef spec As Xml.XmlDocument, ByRef cpp As CCouplerlib)
+    End Sub
+    Public Overridable Sub simulate(ByVal extfilename As String, ByVal EwEGOTMTimeRatio As Integer, ByVal spinupdays As Integer, ByRef spec As Xml.XmlDocument, ByVal iusespatial As Boolean)
+    End Sub
+    Public Overridable Sub display()
+    End Sub
+    Public Overridable Sub StoreInRegistry()
+
+    End Sub
+    Public Overridable Function UsesNetCDF() As Boolean
+    End Function
+End Class
