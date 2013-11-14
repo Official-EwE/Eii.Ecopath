ed5503c22b88374a4d6df9f039b85ae4ed971eac
joeb <joeb@ecopath.org>
2013-11-14 16:41:24 +0000

    And again to get rid of the change
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@9980 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterfaceShared/Forms/frmEwEDockContent.vb
+++ Sources/ScientificInterfaceShared/Forms/frmEwEDockContent.vb
@@ -39,7 +39,6 @@ Namespace Forms
         Inherits DockContent
 
 #Region " Private variables "
-        'Bogus comment to get this file to update on the SVN server
 
         Private m_icoOrg As Icon = Nothing
         Private m_icoPulse As Icon = Nothing
