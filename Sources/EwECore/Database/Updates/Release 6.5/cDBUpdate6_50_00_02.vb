aae840c06f73c28381444839f98c0f59cc388f06
JeroenS <jeroen.steenbeek@gmail.com>
2013-12-18 16:45:03 +0000

    Made it work for newest models too
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@10132 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/EwECore/Database/Updates/Release 6.5/cDBUpdate6_50_00_02.vb
@@ -0,0 +1,66 @@
+﻿' ===============================================================================
+' This file is part of Ecopath with Ecosim (EwE)
+'
+' EwE is free software: you can redistribute it and/or modify it under the terms
+' of the GNU General Public License version 2 as published by the Free Software 
+' Foundation.
+'
+' EwE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+' without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+' PURPOSE. See the GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License along with EwE.
+' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
+'
+' Copyright 1991-2013 UBC Fisheries Centre, Vancouver BC, Canada.
+' ===============================================================================
+'
+
+Option Strict On
+Imports EwEUtils.Database
+
+''' --------------------------------------------------------------------------
+''' <summary>
+''' <para>Database update 6.50.0.01:</para>
+''' <para>
+''' <list type="bullet">
+''' <item><description>Added model exclusion layer</description></item>
+''' </list>
+''' </para>
+''' </summary>
+''' --------------------------------------------------------------------------
+Friend Class cDBUpdate6_50_00_02
+    Inherits cDBUpdate
+
+    ''' -----------------------------------------------------------------------
+    ''' <inheritdocs cref="cDBUpdate.UpdateVersion"/>
+    ''' -----------------------------------------------------------------------
+    Public Overrides ReadOnly Property UpdateVersion() As Single
+        Get
+            Return 6.500002!
+        End Get
+    End Property
+
+    ''' -----------------------------------------------------------------------
+    ''' <inheritdocs cref="cDBUpdate.UpdateDescription"/>
+    ''' -----------------------------------------------------------------------
+    Public Overrides ReadOnly Property UpdateDescription() As String
+        Get
+            Return "Validated capacity maps"
+        End Get
+    End Property
+
+    Public Overrides Function ApplyUpdate(ByRef db As cEwEDatabase) As Boolean
+
+        Try
+            If db.Execute("ALTER TABLE EcospaceScenarioCapacityDrivers DROP CONSTRAINT " & db.GetPkKeyName("GroupID")) Then
+                db.Execute("ALTER TABLE EcospaceScenarioCapacityDrivers ADD CONSTRAINT fkGroupID FOREIGN KEY (GroupID) REFERENCES EcospaceScenarioGroup (GroupID)")
+            End If
+
+        End Try
+        Me.LogProgress("Validate EcospaceScenarioCapacityDrivers", True)
+        Return True
+
+    End Function
+
+End Class
