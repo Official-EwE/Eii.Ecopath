17212d062ce4b256a04f6a1b16a562a380564d2a
joeb <joeb@ecopath.org>
2014-01-22 21:35:23 +0000

    Depth Change Plugin tweaks
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@10210 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources Internal/Plugins/RobertsBank/DepthChangePlugin/cSpatialDataLoader.vb
+++ Sources Internal/Plugins/RobertsBank/DepthChangePlugin/cSpatialDataLoader.vb
@@ -181,7 +181,7 @@ Public Class cSpatialDataLoader
                 'Ok managed to create the DepthAdapter and the get the Converter from the core
                 'Now hook them up
                 'And add the DepthAdapter to the core spatial data manager          
-                Me.m_DepthAdapter.Converter(0, 0) = Converter
+                'Me.m_DepthAdapter.Converter(1, 1) = Converter
                 Plugin.Core.SpatialDataConnectionManager.AddAdapter(Me.m_DepthAdapter)
                 cSpatialDataLoader.m_isAdapterLoaded = True
             End If
