5e03e792fc61b71986f82ced3b4073480510355b
JeroenS <jeroen.steenbeek@gmail.com>
2014-04-03 19:48:28 +0000

    Updated license headers
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@10573 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwEUtils/Utilities/cFuzzySearch.vb
+++ Sources/EwEUtils/Utilities/cFuzzySearch.vb
@@ -1,20 +1,21 @@
 ﻿' ===============================================================================
-' This file is part of the Nereus Model.
+' This file is part of Ecopath with Ecosim (EwE)
 '
-' The Nereus Model is free software: you can redistribute it and/or modify it 
-' under the terms of the GNU General Public License version 2 as published by the 
-' Free Software Foundation.
+' EwE is free software: you can redistribute it and/or modify it under the terms
+' of the GNU General Public License version 2 as published by the Free Software 
+' Foundation.
 '
-' The software is distributed in the hope that it will be useful, but WITHOUT ANY 
-' WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
-' PARTICULAR PURPOSE. See the GNU General Public License for more details.
+' EwE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+' without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+' PURPOSE. See the GNU General Public License for more details.
 '
-' You should have received a copy of the GNU General Public License along with the
-' software. If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
+' You should have received a copy of the GNU General Public License along with EwE.
+' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
 '
-' Copyright 1991-2013 NF-UBC Nereus Program, Fisheries Centre, Vancouver BC, Canada.
+' Copyright 1991- UBC Fisheries Centre, Vancouver BC, Canada.
 ' ===============================================================================
 '
+
 #Region " Imports "
 
 Option Strict On
