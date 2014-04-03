efa05f94070bcd42093983616ce3e2acf0fe4b5b
JeroenS <jeroen.steenbeek@gmail.com>
2014-04-03 19:48:28 +0000

    Updated license headers
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@10573 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECustomPlugins/EwERemarksPlugin/Utility/cRemarkMonitor.vb
+++ Sources/EwECustomPlugins/EwERemarksPlugin/Utility/cRemarkMonitor.vb
@@ -12,9 +12,10 @@
 ' You should have received a copy of the GNU General Public License along with EwE.
 ' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
 '
-' Copyright 1991-2012 UBC Fisheries Centre, Vancouver BC, Canada.
+' Copyright 1991- UBC Fisheries Centre, Vancouver BC, Canada.
 ' ===============================================================================
 '
+
 #Region " Imports "
 
 Option Strict On
