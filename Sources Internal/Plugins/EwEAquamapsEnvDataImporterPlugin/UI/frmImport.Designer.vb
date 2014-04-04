ecb86e74515bb8abb4314faeb3da71778791d40c
JeroenS <jeroen.steenbeek@gmail.com>
2014-04-04 08:04:55 +0000

    Brought in from mapdb branch
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@10580 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources Internal/Plugins/EwEAquamapsEnvDataImporterPlugin/UI/frmImport.Designer.vb
@@ -0,0 +1,140 @@
+﻿' ===============================================================================
+' This file is part of Ecopath with Ecosim (EwE)
+'
+' EwE is free software: you can redistribute it and/or modify it under the terms
+' of the GNU General Public License version 2 as published by the Free Software 
+' Foundation.
+'
+' EwE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+' without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+' PURPOSE. See the GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License along with EwE.
+' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
+'
+' Copyright 1991- Ecopath International Initiative, Barcelona, Spain and
+'                 Joint Reseach Centre, Ispra, Italy.
+' ===============================================================================
+'
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmImport
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmImport))
+        Me.m_lblDrop = New System.Windows.Forms.Label()
+        Me.m_btnImport = New System.Windows.Forms.Button()
+        Me.m_tlpOptions = New System.Windows.Forms.TableLayoutPanel()
+        Me.m_hdrEnvelopes = New ScientificInterfaceShared.Controls.cEwEHeaderLabel()
+        Me.m_hdrSpecies = New ScientificInterfaceShared.Controls.cEwEHeaderLabel()
+        Me.m_clbxSpecies = New System.Windows.Forms.CheckedListBox()
+        Me.m_clbxEnvelopes = New System.Windows.Forms.CheckedListBox()
+        Me.m_llAquamaps = New System.Windows.Forms.LinkLabel()
+        Me.m_tlpOptions.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'm_lblDrop
+        '
+        Me.m_lblDrop.AllowDrop = True
+        resources.ApplyResources(Me.m_lblDrop, "m_lblDrop")
+        Me.m_lblDrop.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.m_lblDrop.ForeColor = System.Drawing.SystemColors.AppWorkspace
+        Me.m_lblDrop.Name = "m_lblDrop"
+        '
+        'm_btnImport
+        '
+        resources.ApplyResources(Me.m_btnImport, "m_btnImport")
+        Me.m_btnImport.Name = "m_btnImport"
+        Me.m_btnImport.UseVisualStyleBackColor = True
+        '
+        'm_tlpOptions
+        '
+        resources.ApplyResources(Me.m_tlpOptions, "m_tlpOptions")
+        Me.m_tlpOptions.Controls.Add(Me.m_hdrEnvelopes, 1, 0)
+        Me.m_tlpOptions.Controls.Add(Me.m_hdrSpecies, 0, 0)
+        Me.m_tlpOptions.Controls.Add(Me.m_clbxSpecies, 0, 1)
+        Me.m_tlpOptions.Controls.Add(Me.m_clbxEnvelopes, 1, 1)
+        Me.m_tlpOptions.Name = "m_tlpOptions"
+        '
+        'm_hdrEnvelopes
+        '
+        Me.m_hdrEnvelopes.CanCollapseParent = False
+        Me.m_hdrEnvelopes.CollapsedParentHeight = 0
+        resources.ApplyResources(Me.m_hdrEnvelopes, "m_hdrEnvelopes")
+        Me.m_hdrEnvelopes.IsCollapsed = False
+        Me.m_hdrEnvelopes.Name = "m_hdrEnvelopes"
+        '
+        'm_hdrSpecies
+        '
+        Me.m_hdrSpecies.CanCollapseParent = False
+        Me.m_hdrSpecies.CollapsedParentHeight = 0
+        resources.ApplyResources(Me.m_hdrSpecies, "m_hdrSpecies")
+        Me.m_hdrSpecies.IsCollapsed = False
+        Me.m_hdrSpecies.Name = "m_hdrSpecies"
+        '
+        'm_clbxSpecies
+        '
+        Me.m_clbxSpecies.CheckOnClick = True
+        resources.ApplyResources(Me.m_clbxSpecies, "m_clbxSpecies")
+        Me.m_clbxSpecies.FormattingEnabled = True
+        Me.m_clbxSpecies.Name = "m_clbxSpecies"
+        '
+        'm_clbxEnvelopes
+        '
+        Me.m_clbxEnvelopes.CheckOnClick = True
+        resources.ApplyResources(Me.m_clbxEnvelopes, "m_clbxEnvelopes")
+        Me.m_clbxEnvelopes.FormattingEnabled = True
+        Me.m_clbxEnvelopes.Name = "m_clbxEnvelopes"
+        '
+        'm_llAquamaps
+        '
+        resources.ApplyResources(Me.m_llAquamaps, "m_llAquamaps")
+        Me.m_llAquamaps.Name = "m_llAquamaps"
+        Me.m_llAquamaps.TabStop = True
+        '
+        'frmImport
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.m_llAquamaps)
+        Me.Controls.Add(Me.m_tlpOptions)
+        Me.Controls.Add(Me.m_btnImport)
+        Me.Controls.Add(Me.m_lblDrop)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmImport"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.m_tlpOptions.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Private WithEvents m_lblDrop As System.Windows.Forms.Label
+    Private WithEvents m_btnImport As System.Windows.Forms.Button
+    Private WithEvents m_tlpOptions As System.Windows.Forms.TableLayoutPanel
+    Private WithEvents m_hdrEnvelopes As ScientificInterfaceShared.Controls.cEwEHeaderLabel
+    Private WithEvents m_hdrSpecies As ScientificInterfaceShared.Controls.cEwEHeaderLabel
+    Private WithEvents m_clbxSpecies As System.Windows.Forms.CheckedListBox
+    Private WithEvents m_clbxEnvelopes As System.Windows.Forms.CheckedListBox
+    Private WithEvents m_llAquamaps As System.Windows.Forms.LinkLabel
+End Class
---
+++ Sources Internal/Plugins/EwEAquamapsEnvDataImporterPlugin/UI/frmImport.vb
@@ -0,0 +1,198 @@
+﻿' ===============================================================================
+' This file is part of Ecopath with Ecosim (EwE)
+'
+' EwE is free software: you can redistribute it and/or modify it under the terms
+' of the GNU General Public License version 2 as published by the Free Software 
+' Foundation.
+'
+' EwE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+' without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+' PURPOSE. See the GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License along with EwE.
+' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
+'
+' Copyright 1991- Ecopath International Initiative, Barcelona, Spain and
+'                 Joint Reseach Centre, Ispra, Italy.
+' ===============================================================================
+'
+#Region " Imports "
+
+Option Strict On
+Option Explicit On
+
+Imports System.Drawing
+Imports System.Windows.Forms
+Imports EwEUtils.Core
+Imports ScientificInterfaceShared.Commands
+Imports ScientificInterfaceShared.Controls
+
+#End Region ' Imports
+
+''' <summary>
+''' Main UI for the Aquamaps distribution envelope import plug-in
+''' </summary>
+Public Class frmImport
+
+#Region " Private vars "
+
+    Private m_uic As cUIContext = Nothing
+    Private m_data As cImportData = Nothing
+    Private m_bDragOver As Boolean = False
+
+#End Region ' Private vars 
+
+#Region " Construction "
+
+    Public Sub New(uic As cUIContext)
+        MyBase.New()
+        Me.m_uic = uic
+        Me.m_data = New cImportData()
+        Me.InitializeComponent()
+    End Sub
+
+#End Region ' Construction
+
+#Region " Form overrides "
+
+    Protected Overrides Sub OnLoad(e As System.EventArgs)
+        MyBase.OnLoad(e)
+        Me.UpdateControls()
+        Me.CenterToParent()
+    End Sub
+
+    Protected Overrides Sub OnFormClosed(e As System.Windows.Forms.FormClosedEventArgs)
+        MyBase.OnFormClosed(e)
+    End Sub
+
+#End Region ' Form overrides
+
+#Region " Events "
+
+    Private Sub OnDragDropFiles(sender As Object, e As System.Windows.Forms.DragEventArgs) _
+        Handles m_lblDrop.DragDrop
+        Try
+            If Not Me.m_bDragOver Then Return
+            Me.ReadFiles(CType(e.Data.GetData(DataFormats.FileDrop), String()))
+        Catch ex As Exception
+        End Try
+        Me.m_bDragOver = False
+        Me.UpdateControls()
+    End Sub
+
+    Private Sub OnDragEnterFiles(sender As Object, e As System.Windows.Forms.DragEventArgs) _
+        Handles m_lblDrop.DragEnter
+
+        Try
+            If (e.Data.GetDataPresent(DataFormats.FileDrop)) Then
+                e.Effect = DragDropEffects.All
+                Me.m_bDragOver = True
+            End If
+        Catch ex As Exception
+            Me.m_bDragOver = False
+        End Try
+        Me.UpdateControls()
+
+    End Sub
+
+    Private Sub OnDragLeaveFiles(sender As Object, e As System.EventArgs) _
+        Handles m_lblDrop.DragLeave
+
+        Try
+            Me.m_bDragOver = False
+        Catch ex As Exception
+
+        End Try
+        Me.UpdateControls()
+
+    End Sub
+
+    Private Sub OnImport(sender As System.Object, e As System.EventArgs) _
+        Handles m_btnImport.Click
+
+        Try
+            Dim imp As New cImporter(Me.m_data, Me.m_uic)
+
+            Dim lstrSpecies As New List(Of String)
+            For Each strSpecies As String In Me.m_clbxSpecies.CheckedItems
+                lstrSpecies.Add(strSpecies)
+            Next
+
+            Dim lstrEnvelopes As New List(Of String)
+            For Each strEnvelope As String In Me.m_clbxEnvelopes.CheckedItems
+                lstrEnvelopes.Add(strEnvelope)
+            Next
+
+            imp.Import(lstrSpecies.ToArray, lstrEnvelopes.ToArray)
+            Me.Clear()
+
+        Catch ex As Exception
+            ' Whoah!
+        End Try
+
+    End Sub
+
+    Private Sub OnAquamapsLinkClicked(sender As System.Object, e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) _
+        Handles m_llAquamaps.LinkClicked
+
+        Try
+
+            Dim cmd As cBrowserCommand = DirectCast(Me.m_uic.CommandHandler.GetCommand(cBrowserCommand.COMMAND_NAME), cBrowserCommand)
+            If (cmd IsNot Nothing) Then
+                cmd.Invoke("http://aquamaps.org")
+            End If
+
+        Catch ex As Exception
+            cLog.Write(ex, "AquamapsImporter::OnAquamapsLinkClicked")
+        End Try
+
+    End Sub
+
+#End Region ' Events
+
+#Region " Internals "
+
+    Private Sub Clear()
+        Me.m_data.Clear()
+        Me.m_clbxSpecies.Items.Clear()
+        Me.m_clbxEnvelopes.Items.Clear()
+    End Sub
+
+    Private Sub ReadFiles(files As String())
+
+        Dim reader As New cFileReader(Me.m_uic.Core)
+
+        Me.Clear()
+
+        For Each strFile As String In files
+            reader.ReadEnvelopeData(strFile, Me.m_data)
+        Next
+
+        For Each strSpecies As String In Me.m_data.Species
+            Dim i As Integer = Me.m_clbxSpecies.Items.Add(strSpecies)
+            Me.m_clbxSpecies.SetItemChecked(i, True)
+        Next
+
+        For Each strEnv As String In Me.m_data.Envelopes
+            Dim i As Integer = Me.m_clbxEnvelopes.Items.Add(strEnv)
+            Me.m_clbxEnvelopes.SetItemChecked(i, True)
+        Next
+
+        Me.UpdateControls()
+
+    End Sub
+
+    Private Sub UpdateControls()
+
+        Me.m_btnImport.Enabled = (Me.m_data.Files.Length > 0)
+        If Me.m_bDragOver Then
+            Me.m_lblDrop.BackColor = SystemColors.Highlight
+        Else
+            Me.m_lblDrop.BackColor = Drawing.Color.Transparent
+        End If
+
+    End Sub
+
+#End Region ' Internals
+
+End Class
