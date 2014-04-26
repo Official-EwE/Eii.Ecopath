73d492af4eade05667c66d6c3777b598bf18206e
JeroenS <jeroen.steenbeek@gmail.com>
2014-04-26 10:36:29 +0000

    Split out valiators Core handles scenario overwrites, no longer datasource
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@10629 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/EwECore/Validators/cValidatorEnum.vb
@@ -0,0 +1,48 @@
+﻿' ===============================================================================
+' This file is part of Ecopath with Ecosim (EwE)
+'
+' EwE is free software: you can redistribute it and/or modify it under the terms
+' of the GNU General Public License version 2 as published by the Free Software 
+' Foundation.
+'
+' EwE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+' without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+' PURPOSE. See the GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License along with EwE.
+' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
+'
+' Copyright 1991- UBC Fisheries Centre, Vancouver BC, Canada.
+' ===============================================================================
+'
+
+Option Strict On
+
+Imports EwECore.ValueWrapper
+Imports EwEUtils.Core
+
+Public Class cValidatorEnum
+    Inherits cValidatorDefault
+
+    Private m_type As Type = Nothing
+
+    Public Sub New(ByVal t As Type)
+        Debug.Assert(t.IsEnum)
+        Me.m_type = t
+    End Sub
+
+    Public Overrides Function Validate(ByVal ValueObject As ValueWrapper.cValue, ByVal MetaData As cVariableMetaData, Optional ByVal iSecondaryIndex As Integer = -9999) As Boolean
+
+        ' Perform 'normal' validation
+        If Not MyBase.Validate(ValueObject, MetaData, iSecondaryIndex) Then Return False
+        ' Check type
+        If Not [Enum].IsDefined(Me.m_type, ValueObject.Value(iSecondaryIndex)) Then
+            ValueObject.ValidationStatus = eStatusFlags.FailedValidation
+        Else
+            ValueObject.ValidationStatus = eStatusFlags.OK
+        End If
+        Return True
+
+    End Function
+
+End Class
