bf2128dde6da69d9d55f05bda2a72ebee708aec5
JeroenS <jeroen.steenbeek@gmail.com>
2014-04-30 17:29:45 +0000

    Fixed build
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@10658 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources Internal/Plugins/CEFASMSEPlugin/EwEMSEPlugin/frmEditSurvivabilities.Designer.vb
@@ -0,0 +1,65 @@
+﻿
+Imports ScientificInterfaceShared.Forms
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmEditSurvivabilities
+    Inherits frmEwE
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.m_btnOK = New System.Windows.Forms.Button()
+        Me.m_btnCancel = New System.Windows.Forms.Button()
+        Me.m_grid = New EwEMSEPlugin.gridSurviveDistParameters()
+        Me.m_btnOK.Location = New System.Drawing.Point(474, 251)
+        Me.m_btnOK.Name = "m_btnOK"
+        Me.m_btnOK.Size = New System.Drawing.Size(65, 29)
+        Me.m_btnOK.TabIndex = 1
+        Me.m_btnOK.Text = "OK"
+        Me.m_btnOK.UseVisualStyleBackColor = True
+        Me.SuspendLayout()
+        '
+        'm_btnCancel
+        '
+        Me.m_btnCancel.Location = New System.Drawing.Point(474, 207)
+        Me.m_btnCancel.Name = "m_btnCancel"
+        Me.m_btnCancel.Size = New System.Drawing.Size(65, 29)
+        Me.m_btnCancel.TabIndex = 2
+        Me.m_btnCancel.Text = "Cancel"
+        Me.m_btnCancel.UseVisualStyleBackColor = True
+        '
+        'frmEditSurvivabilities
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(558, 334)
+        Me.Controls.Add(Me.m_btnCancel)
+        Me.Controls.Add(Me.m_btnOK)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "frmEditSurvivabilities"
+        Me.Text = "frmEditSurvivabilites"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents m_btnOK As System.Windows.Forms.Button
+    Friend WithEvents m_btnCancel As System.Windows.Forms.Button
+    Friend WithEvents m_grid As EwEMSEPlugin.gridSurviveDistParameters
+End Class
---
+++ Sources Internal/Plugins/CEFASMSEPlugin/EwEMSEPlugin/frmEditSurvivabilities.vb
@@ -0,0 +1,138 @@
+﻿Imports LumenWorks.Framework.IO.Csv
+Imports System.IO
+Imports EwEUtils.Utilities
+Imports ScientificInterfaceShared.Controls
+Imports EwECore
+Imports EwEUtils.Core
+Imports SharedResources = ScientificInterfaceShared.My.Resources
+
+
+Public Class frmEditSurvivabilities
+    Implements IDisposable
+
+    Private m_plugin As cMSEPluginPoint = Nothing
+    Public mSurvivability As cSurvivability
+    Private m_bIsDirty As Boolean
+
+    Public Sub New(MSE As cMSE)
+
+        Me.InitializeComponent()
+
+    End Sub
+
+    Public Sub Init(ByVal uic As cUIContext, Plugin As cMSEPluginPoint)
+        Me.UIContext = uic
+        Me.m_grid.UIContext = uic
+        Me.m_grid.Init(Plugin.MSE, Plugin.MSE.Survivabilities)
+        Me.m_plugin = Plugin
+        Me.mSurvivability = Plugin.MSE.Survivability
+        Me.m_grid.Left = 10
+        Me.m_grid.Top = 10
+        Me.m_grid.Height = 500
+        'Me.m_grid.Width = 1000
+
+
+        Dim bSave As Boolean = Me.m_bIsDirty
+
+        UpdateGrid(mSurvivability.ListofSurvDistParams, My.Resources.HEADER_SURVIVABILITIES)
+
+    End Sub
+
+    Private ReadOnly Property MSE As cMSE
+        Get
+            Return Me.m_plugin.MSE
+        End Get
+    End Property
+
+    Public Overrides Property UIContext As ScientificInterfaceShared.Controls.cUIContext
+        Get
+            Return MyBase.UIContext
+        End Get
+        Set(value As ScientificInterfaceShared.Controls.cUIContext)
+            MyBase.UIContext = value
+        End Set
+    End Property
+
+    Protected Overrides Sub OnLoad(e As System.EventArgs)
+
+        ' JS 30Sep13: globalized this method
+        MyBase.OnLoad(e)
+
+        AddHandler Me.m_grid.onEdited, AddressOf OnGridEdited
+
+        Me.m_bIsDirty = False
+        Me.CenterToParent()
+        Me.UpdateControls()
+
+    End Sub
+
+    Protected Overrides Sub OnFormClosing(e As System.Windows.Forms.FormClosingEventArgs)
+
+        If (Me.m_bIsDirty = True) Then
+            ' JS 02Oct13: globalized this method
+            ' JS 02Oct13: replaced MsgBox with cFeedbackMessage
+            Dim fmsg As New cFeedbackMessage(My.Resources.PROMPT_UNSAVED_CHANGES, _
+                                 eCoreComponentType.External, eMessageType.Any, eMessageImportance.Question, eMessageReplyStyle.YES_NO)
+            fmsg.Reply = eMessageReply.YES
+            Me.Core.Messages.SendMessage(fmsg)
+            e.Cancel = (fmsg.Reply <> eMessageReply.YES)
+        End If
+
+        MyBase.OnFormClosing(e)
+
+    End Sub
+
+    Protected Overrides Sub OnFormClosed(e As System.Windows.Forms.FormClosedEventArgs)
+
+        RemoveHandler Me.m_grid.onEdited, AddressOf OnGridEdited
+        Me.m_grid.UIContext = Nothing
+
+        MyBase.OnFormClosed(e)
+
+    End Sub
+
+    Protected Overrides Sub UpdateControls()
+        MyBase.UpdateControls()
+        ' Me.m_btnOK.Enabled = Me.m_bIsDirty
+    End Sub
+
+    Private Sub UpdateGrid(data As List(Of cSurvivability.cSurvivabilityDistributonParam), strName As String)
+        Me.m_grid.Data = data
+        Me.m_grid.DataName = String.Format(SharedResources.GENERIC_LABEL_DOUBLE, My.Resources.CAPTION, strName)
+    End Sub
+
+    Private Sub m_btnOK_Click(sender As System.Object, e As System.EventArgs) Handles m_btnOK.Click
+        Dim lstrSubMessages As New List(Of String)
+        Dim strFolder As String = cMSEUtils.MSEFolder(Me.MSE.DataPath, cMSEUtils.eMSEPaths.DistrParams)
+
+        If Not Me.MSE.IsInputStructureAvailable(True) Then
+            ' ToDo: report error
+            Return
+        End If
+
+        'Saves all the parameters to csv when user clicks to save
+        If mSurvivability.SaveDistributionParamsToCSV() Then lstrSubMessages.Add(String.Format(My.Resources.STATUS_SAVED_DETAIL, "Survivabilities_dist.csv"))
+
+        Me.m_bIsDirty = False
+
+        Me.m_plugin.InformUser(String.Format(My.Resources.STATUS_SAVED_DISTPARMS, My.Resources.CAPTION, strFolder), _
+                                 eMessageImportance.Information, strFolder, lstrSubMessages.ToArray())
+
+        Me.DialogResult = Windows.Forms.DialogResult.OK
+        Me.Close()
+    End Sub
+
+    Private Sub OnGridEdited()
+        Me.m_bIsDirty = True
+        Me.Invoke(New MethodInvoker(AddressOf UpdateControls))
+    End Sub
+
+    Private Sub OnCancel(ByVal sender As System.Object, ByVal e As System.EventArgs) _
+    Handles m_btnCancel.Click
+
+        Me.DialogResult = Windows.Forms.DialogResult.Cancel
+        Me.Close()
+
+    End Sub
+
+End Class
