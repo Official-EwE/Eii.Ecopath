d0c42ffa49d208c34116365d979f93271670c4a1
joeb <joeb@ecopath.org>
2014-05-28 22:06:30 +0000

    Added EcospaceMonteCarlo
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@10819 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources Internal/Plugins/EcospaceMonteCarlo/cRunManager.vb
@@ -0,0 +1,36 @@
+﻿
+Imports EwECore
+
+
+Public Class cRunManager
+
+    Private m_RunSpace As cRunEcospace
+    Private m_plugin As cEcospaceMonteCarloPluginPoint
+
+    Private core As cCore
+
+
+    Public Sub Init(thePlugin As cEcospaceMonteCarloPluginPoint)
+        Me.m_plugin = thePlugin
+        core = Me.m_plugin.Core
+        Me.m_RunSpace = New cRunEcospace
+
+    End Sub
+
+    Public Sub Run()
+        m_RunSpace.Init(Me.m_plugin.Core, Me.m_plugin.MonteCarlo, Me.m_plugin.EcoSpace)
+        m_RunSpace.SetRunParameters(2000, 2)
+        Me.m_RunSpace.Run()
+        Dim lastTS As Integer = Me.core.nEcospaceYears * 12
+
+        For igrp As Integer = 1 To m_plugin.Core.nGroups
+            System.Console.Write(igrp.ToString + "," + m_plugin.EcoSpaceData.ResultsByGroup(0, igrp, lastTS).ToString + ":")
+        Next
+        System.Console.WriteLine()
+    End Sub
+
+    Public Sub OnEcospaceTimeStep(ByVal iTime As Integer)
+
+    End Sub
+
+End Class
