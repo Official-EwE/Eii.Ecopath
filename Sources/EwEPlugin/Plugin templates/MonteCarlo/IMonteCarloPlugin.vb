58e64e5d9a375124c59de749af741cb495bc864e
joeb <joeb@ecopath.org>
2014-06-03 16:40:40 +0000

    Added comments to IMonteCarloPlugin()
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@10846 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwEPlugin/Plugin templates/MonteCarlo/IMonteCarloPlugin.vb
+++ Sources/EwEPlugin/Plugin templates/MonteCarlo/IMonteCarloPlugin.vb
@@ -22,13 +22,30 @@ Imports System.Threading
 Public Interface IMonteCarloPlugin
     Inherits IPlugin
 
-
+    ''' <summary>
+    ''' The MonteCarlo model has initialized. Passes the MonteCarlo model to a plugin.
+    ''' </summary>
+    ''' <param name="MonteCarloAsObject">The cEcosimMonteCarlo object as an Object </param>
+    ''' <remarks></remarks>
     Sub MontCarloInitialized(ByVal MonteCarloAsObject As Object)
 
-
+    ''' <summary>
+    ''' The MonteCarlo has found a balanced model and it about to run Ecosim.
+    ''' </summary>
+    ''' <param name="WaitLock">A ManualResetEvent object used by the calling routine to Wait for  MonteCarloBalancedEcopathModel(...) to return.</param>
+    ''' <param name="TrialNumber">Number of the current trial.</param>
+    ''' <param name="nIterations">Number of iteration to find a balanced Ecopath model</param>
+    ''' <remarks>The Plugin Manager will marshal the call to MonteCarloBalancedEcopathModel(...) from the MonteCarlo thread onto the main thread. 
+    ''' If the plugin wants to thread its process it can't block the main thread so it has to use WaitLock.Reset() to set the signal state and return immediately. 
+    ''' Then once the thread has completed call WaitLock.Set() to release the signal state and allow the MonteCarlo to continue.
+    ''' If the Plugin ignores the WaitLock the MonteCarlo will continue running when MonteCarloBalancedEcopathModel(...) returns.
+    '''  </remarks>
     Sub MonteCarloBalancedEcopathModel(ByVal WaitLock As ManualResetEvent, ByVal TrialNumber As Integer, ByVal nIterations As Integer)
 
-
+    ''' <summary>
+    ''' The MonteCarlo has completed its Ecosim run and will start another trial.
+    ''' </summary>
+    ''' <remarks></remarks>
     Sub MonteCarloEcosimRunCompleted()
 
 End Interface
