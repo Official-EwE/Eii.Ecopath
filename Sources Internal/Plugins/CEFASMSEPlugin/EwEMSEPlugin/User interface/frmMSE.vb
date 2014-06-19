2872533b56cac32e00ede1e1b35dff35c3c588a9
JeroenS <jeroen.steenbeek@gmail.com>
2014-06-19 09:35:38 +0000

    Hmm, casi casi
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@10902 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources Internal/Plugins/CEFASMSEPlugin/EwEMSEPlugin/User interface/frmMSE.vb
+++ Sources Internal/Plugins/CEFASMSEPlugin/EwEMSEPlugin/User interface/frmMSE.vb
@@ -547,7 +547,7 @@ Public Class frmMSE
             End If
         Else
             ' Check if input structure is compatible
-            If Not Me.MSE.IsRunDataCompatible Then
+            If Not Me.MSE.IsInputDataCompatible() Then
                 If Me.MSE.AskUser("The selected folder is not compatible with the currently loaded model. If you continue, previously saved MSE settings will be lost. Do you wish to continue, delete existing MSE settings, and start anew?", _
                                eMessageReplyStyle.YES_NO, eMessageImportance.Warning, eMessageReply.NO) = eMessageReply.NO Then
                     Return False
