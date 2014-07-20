dce8f3ed8a80c998b2a16820056f77e7c438e30b
automatedbuild <automatedbuild@ecopath.org>
2014-07-20 09:06:11 +0000

    Build 6.5.10989.0
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@10994 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECustomPlugins/EwEMultiSimPlugin/My Project/AssemblyInfo.vb
+++ Sources/EwECustomPlugins/EwEMultiSimPlugin/My Project/AssemblyInfo.vb
@@ -51,7 +51,7 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.5.10987.0")> 
+<Assembly: AssemblyVersion("6.5.10989.0")> 
 <Assembly: AssemblyFileVersion("1.0.0.0")> 
 
 <Assembly: NeutralResourcesLanguageAttribute("en-CA")> 
--- Sources/EwECustomPlugins/FLEMFileReaderPlugin/My Project/AssemblyInfo.vb
+++ Sources/EwECustomPlugins/FLEMFileReaderPlugin/My Project/AssemblyInfo.vb
@@ -48,5 +48,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.5.10987.0")> 
+<Assembly: AssemblyVersion("6.5.10989.0")> 
 <Assembly: AssemblyFileVersion("1.0.0.0")> 
