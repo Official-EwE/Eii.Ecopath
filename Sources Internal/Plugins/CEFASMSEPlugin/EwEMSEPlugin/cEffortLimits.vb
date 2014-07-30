c7f581a4952c0faa5e7a0949a754e9d8ca4d5499
markp <markp@ecopath.org>
2014-07-30 23:19:49 +0000

    Changed default values for the effort limits to 1 (100%).
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@11032 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources Internal/Plugins/CEFASMSEPlugin/EwEMSEPlugin/cEffortLimits.vb
+++ Sources Internal/Plugins/CEFASMSEPlugin/EwEMSEPlugin/cEffortLimits.vb
@@ -63,7 +63,8 @@ Public Class cEffortLimits
     Public Sub Defaults() _
         Implements IMSEData.Defaults
         For i As Integer = 1 To Me.m_core.nFleets
-            Me.Value(i) = cEffortLimits.NoHCR_F
+            'Me.Value(i) = cEffortLimits.NoHCR_F
+            Me.Value(i) = 1
         Next
         Me.m_bChanged = False
     End Sub
