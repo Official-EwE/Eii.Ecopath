8883c31c05a30fe05490b86e900623ad7aec7e7d
joeb <joeb@ecopath.org>
2014-07-30 17:55:43 +0000

    Added cStockAssessmentParameters object
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@11028 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources Internal/Plugins/CEFASMSEPlugin/EwEMSEPlugin/cStockAssessmentParameters.vb
@@ -0,0 +1,96 @@
+﻿' ===============================================================================
+' This file is part of Ecopath with Ecosim (EwE)
+'
+' EwE is free software: you can redistribute it and/or modify it under the terms
+' of the GNU General Public License version 2 as published by the Free Software 
+' Foundation.
+'
+' EwE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+' without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+' PURPOSE. See the GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License along with EwE.
+' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
+'
+' The Cefas MSE plug-in was developed by the Centre for Environment, Fisheries and 
+' Aquaculture Science (Cefas). 
+'
+' EwE copyright: 1991- UBC Fisheries Centre, Vancouver BC, Canada.
+' Cefas MSE plug-in copyright: 2013- Cefas, Lowestoft, UK.
+' ===============================================================================
+
+#Region "Imports"
+
+Option Strict On
+Option Explicit On
+Imports EwECore
+
+#End Region
+
+
+''' <summary>
+''' Wrapper class to hold Stock Assessment parameters
+''' </summary>
+''' <remarks>Used by the interface to access Stock Assessment parameters</remarks>
+Public Class cStockAssessmentParameters
+    Private m_Assessment As cStockAssessmentModel
+    Private m_iGrp As Integer
+    Private m_simdata As cEcosimDatastructures
+    Private m_pathdata As cEcopathDataStructures
+
+    Public Sub New(ByVal iGroup As Integer, ByVal StockAssessmentModel As cStockAssessmentModel, ByVal EcoSimData As cEcosimDatastructures, ByVal EcoPathData As cEcopathDataStructures)
+        Me.m_iGrp = iGroup
+        Me.m_Assessment = StockAssessmentModel
+        Me.m_simdata = EcoSimData
+        Me.m_pathdata = EcoPathData
+    End Sub
+
+    Public Property RHalfB0Ratio As Single
+        Get
+            Return Me.m_Assessment.RHalfB0Ratio(Me.m_iGrp)
+        End Get
+        Set(value As Single)
+            Me.m_Assessment.RHalfB0Ratio(Me.m_iGrp) = value
+        End Set
+    End Property
+
+    Public Property ForcastGain As Single
+        Get
+            Return Me.m_Assessment.RstockRatio(Me.m_iGrp)
+        End Get
+        Set(value As Single)
+            Me.m_Assessment.RstockRatio(Me.m_iGrp) = value
+        End Set
+    End Property
+
+    Public Property HalfRecruitmentBiomass As Single
+        Get
+            Return Me.m_Assessment.RstockRatio(Me.m_iGrp)
+        End Get
+        Set(value As Single)
+            Me.m_Assessment.RstockRatio(Me.m_iGrp) = value
+        End Set
+    End Property
+
+    Public Property cvRec As Single
+        Get
+            Return Me.m_Assessment.cvRec(Me.m_iGrp)
+        End Get
+        Set(value As Single)
+            Me.m_Assessment.cvRec(Me.m_iGrp) = value
+        End Set
+    End Property
+
+    Public ReadOnly Property Name As String
+        Get
+            Return Me.m_pathdata.GroupName(Me.m_iGrp)
+        End Get
+    End Property
+
+    Public ReadOnly Property iGroupIndex As Integer
+        Get
+            Return Me.m_iGrp
+        End Get
+    End Property
+
+End Class
