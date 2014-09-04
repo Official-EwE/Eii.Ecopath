89c84ef8d84b36890adf840d880b89f083268ae4
JeroenS <jeroen.steenbeek@gmail.com>
2014-09-04 09:43:50 +0000

    First version for Maciej
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@11214 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources Internal/Plugins/EwEDASRegionGeneratorPlugin/frmUI.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmUI
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmUI))
+        Me.m_lblFile = New System.Windows.Forms.Label()
+        Me.m_tbxFile = New System.Windows.Forms.TextBox()
+        Me.m_lblLayers = New System.Windows.Forms.Label()
+        Me.m_tbxLayers = New System.Windows.Forms.TextBox()
+        Me.m_btnGenerate = New System.Windows.Forms.Button()
+        Me.m_hdrSponsors = New ScientificInterfaceShared.Controls.cEwEHeaderLabel()
+        Me.m_pbxSponsors = New System.Windows.Forms.PictureBox()
+        CType(Me.m_pbxSponsors, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'm_lblFile
+        '
+        resources.ApplyResources(Me.m_lblFile, "m_lblFile")
+        Me.m_lblFile.Name = "m_lblFile"
+        '
+        'm_tbxFile
+        '
+        resources.ApplyResources(Me.m_tbxFile, "m_tbxFile")
+        Me.m_tbxFile.Name = "m_tbxFile"
+        '
+        'm_lblLayers
+        '
+        resources.ApplyResources(Me.m_lblLayers, "m_lblLayers")
+        Me.m_lblLayers.Name = "m_lblLayers"
+        '
+        'm_tbxLayers
+        '
+        resources.ApplyResources(Me.m_tbxLayers, "m_tbxLayers")
+        Me.m_tbxLayers.Name = "m_tbxLayers"
+        '
+        'm_btnGenerate
+        '
+        resources.ApplyResources(Me.m_btnGenerate, "m_btnGenerate")
+        Me.m_btnGenerate.Name = "m_btnGenerate"
+        Me.m_btnGenerate.UseVisualStyleBackColor = True
+        '
+        'm_hdrSponsors
+        '
+        resources.ApplyResources(Me.m_hdrSponsors, "m_hdrSponsors")
+        Me.m_hdrSponsors.CanCollapseParent = False
+        Me.m_hdrSponsors.CollapsedParentHeight = 0
+        Me.m_hdrSponsors.IsCollapsed = False
+        Me.m_hdrSponsors.Name = "m_hdrSponsors"
+        '
+        'm_pbxSponsors
+        '
+        resources.ApplyResources(Me.m_pbxSponsors, "m_pbxSponsors")
+        Me.m_pbxSponsors.BackColor = System.Drawing.Color.White
+        Me.m_pbxSponsors.Name = "m_pbxSponsors"
+        Me.m_pbxSponsors.TabStop = False
+        '
+        'frmUI
+        '
+        Me.AcceptButton = Me.m_btnGenerate
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.m_pbxSponsors)
+        Me.Controls.Add(Me.m_hdrSponsors)
+        Me.Controls.Add(Me.m_btnGenerate)
+        Me.Controls.Add(Me.m_tbxLayers)
+        Me.Controls.Add(Me.m_lblLayers)
+        Me.Controls.Add(Me.m_tbxFile)
+        Me.Controls.Add(Me.m_lblFile)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmUI"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        CType(Me.m_pbxSponsors, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Private WithEvents m_lblFile As System.Windows.Forms.Label
+    Private WithEvents m_tbxFile As System.Windows.Forms.TextBox
+    Private WithEvents m_lblLayers As System.Windows.Forms.Label
+    Private WithEvents m_tbxLayers As System.Windows.Forms.TextBox
+    Private WithEvents m_btnGenerate As System.Windows.Forms.Button
+    Private WithEvents m_hdrSponsors As ScientificInterfaceShared.Controls.cEwEHeaderLabel
+    Private WithEvents m_pbxSponsors As System.Windows.Forms.PictureBox
+End Class
---
+++ Sources Internal/Plugins/EwEDASRegionGeneratorPlugin/frmUI.vb
@@ -0,0 +1,195 @@
+﻿' Foundation.
+'
+' EwE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+' without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+' PURPOSE. See the GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License along with EwE.
+' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
+'
+' Copyright 1991- UBC Fisheries Centre, Vancouver BC, Canada.
+' ===============================================================================
+'
+
+#Region " Imports "
+
+Option Strict On
+Imports System.IO
+Imports System.Text
+Imports System.Windows.Forms
+Imports EwECore
+Imports EwEUtils.Core
+Imports EwEUtils.Utilities
+Imports ScientificInterfaceShared.Controls
+
+#End Region ' Imports
+
+Public Class frmUI
+
+    Private m_core As cCore = Nothing
+
+    Public Sub New(core As cCore)
+        Me.InitializeComponent()
+        Me.m_core = core
+    End Sub
+
+#Region " Form overrides "
+
+    Protected Overrides Sub OnLoad(e As System.EventArgs)
+        MyBase.OnLoad(e)
+
+        If (Me.m_core Is Nothing) Then Return
+
+        ' Concoct a decent file name
+        Dim scenario As cEwEScenario = Me.m_core.EcospaceScenarios(Me.m_core.ActiveEcospaceScenarioIndex)
+        Dim strFile As String = cFileUtils.ToValidFileName(scenario.Name, False)
+        Me.m_tbxFile.Text = Path.ChangeExtension(strFile, ".rgn")
+
+        ' Restore last used layers
+        Me.m_tbxLayers.Text = My.Settings.MRULayers
+
+        Me.UpdateControls()
+
+    End Sub
+
+    Protected Overrides Sub OnClosed(e As System.EventArgs)
+
+        ' Preserve last used layers
+        My.Settings.MRULayers = Me.m_tbxLayers.Text
+        My.Settings.Save()
+
+        MyBase.OnClosed(e)
+
+    End Sub
+
+#End Region 'Form overrides
+
+#Region " Control events "
+
+    Private Sub OnFileTextChanged(sender As System.Object, e As System.EventArgs) _
+        Handles m_tbxFile.TextChanged, m_tbxLayers.TextChanged
+
+        Me.UpdateControls()
+
+    End Sub
+
+    Private Sub OnLayersValidated(sender As Object, e As System.EventArgs) _
+        Handles m_tbxLayers.Validated
+
+        Me.m_tbxLayers.Text = Me.ToLayers()
+        Me.UpdateControls()
+
+    End Sub
+
+    Private Sub OnGenerateFile(sender As System.Object, e As System.EventArgs) _
+        Handles m_btnGenerate.Click
+
+        Dim strFile As String = cFileUtils.ToValidFileName(Me.m_tbxFile.Text, False)
+        Dim sfd As SaveFileDialog = cEwEFileDialogHelper.SaveFileDialog("Save DAS region file", strFile, "DAS region files|*.rgn")
+        If sfd.ShowDialog = Windows.Forms.DialogResult.OK Then
+            Me.SaveRegionFile(sfd.FileName)
+        End If
+
+    End Sub
+
+#End Region 'Control events
+
+#Region " Internals "
+
+    Private Sub UpdateControls()
+
+        Dim bHasFile As Boolean = Not String.IsNullOrWhiteSpace(Me.m_tbxFile.Text)
+        Dim bHasLayers As Boolean = Not String.IsNullOrWhiteSpace(Me.m_tbxLayers.Text)
+
+        Me.m_btnGenerate.Enabled = bHasFile And bHasLayers
+
+    End Sub
+
+    Private Function SaveRegionFile(strFileName As String) As Boolean
+
+        Dim writer As StreamWriter = Nothing
+        Dim bm As cEcospaceBasemap = Me.m_core.EcospaceBasemap
+        Dim depth As cEcospaceLayerDepth = bm.LayerDepth
+        Dim strLayers = Me.ToLayers
+        Dim sCellLength As Single = bm.CellLength
+        Dim bSuccess As Boolean = True
+
+        Try
+            writer = New StreamWriter(strFileName)
+            If (writer Is Nothing) Then Return False
+
+            ' Using simple DAS region file format:
+            ' <nX> <nY> <nLayers> <cellsize in m> <lat> <lon>
+            ' <layer 1> <layer 2> .. <layer n>
+            ' <depth 1,1> .. <depth 1, nCols>
+            ' .. .. 
+            ' <depth nRows, 1> .. <depth nRows, nCols>
+
+            writer.WriteLine("{0} {1} {2} {3} {4} {5}", _
+                             bm.InCol, bm.InRow, _
+                             strLayers.Split(" "c).Length, _
+                             bm.CellLength, _
+                             bm.PosBottomRight.Y, _
+                             bm.PosBottomRight.X)
+            writer.WriteLine(strLayers)
+            For iRow As Integer = 1 To bm.InRow
+                If (iRow > 1) Then writer.WriteLine()
+                For iCol As Integer = 1 To bm.InCol
+                    If (iCol > 1) Then writer.Write(" "c)
+                    writer.Write(cStringUtils.ToCSVField(depth.Cell(iRow, iCol)))
+                Next
+            Next
+            writer.Flush()
+            writer.Close()
+            Me.SendMessage("DAS region file saved to " & strFileName, Path.GetDirectoryName(strFileName), bSuccess)
+
+        Catch ex As Exception
+            cLog.Write(ex, "DASWriterPlugin.SaveRegionFile")
+            bSuccess = False
+            Me.SendMessage(ex.Message, "", bSuccess)
+
+        End Try
+        Return True
+
+    End Function
+
+    Private Function ToLayers() As String
+
+        Dim sbLayers As New StringBuilder()
+        Dim lLayers As New List(Of Single)
+        Dim sDepth As Single = 0
+
+        For Each strBit As String In Me.m_tbxLayers.Text.Split(" "c)
+            If Not String.IsNullOrWhiteSpace(strBit) Then
+                If Single.TryParse(strBit, sDepth) Then
+                    lLayers.Add(sDepth)
+                End If
+            End If
+        Next
+
+        lLayers.Sort()
+
+        For i As Integer = 0 To lLayers.Count - 1
+            If (i > 0) Then sbLayers.Append(" ")
+            sbLayers.Append(cStringUtils.ToCSVField(lLayers(i)))
+        Next
+
+        Return sbLayers.ToString()
+
+    End Function
+
+    Private Sub SendMessage(strMessage As String, strHyperlink As String, bSucces As Boolean)
+
+        Dim msg As New cMessage(strMessage, eMessageType.DataExport, eCoreComponentType.External, eMessageImportance.Information)
+        If bSucces Then
+            msg.Hyperlink = strHyperlink
+        Else
+            msg.Importance = eMessageImportance.Critical
+        End If
+        Me.m_core.Messages.SendMessage(msg)
+
+    End Sub
+
+#End Region ' Internals
+
+End Class
