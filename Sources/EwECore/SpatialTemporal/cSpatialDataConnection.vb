60de568ea0b1c9bf5401591b21708ef1dbd86c31
JeroenS <jeroen.steenbeek@gmail.com>
2014-09-26 15:58:11 +0000

    Vikset
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@11336 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECore/SpatialTemporal/cSpatialDataConnection.vb
+++ Sources/EwECore/SpatialTemporal/cSpatialDataConnection.vb
@@ -1,28 +1,72 @@
-﻿Imports EwEUtils.SpatialData
+﻿' ===============================================================================
+' This file is part of Ecopath with Ecosim (EwE)
+'
+' EwE is free software: you can redistribute it and/or modify it under the terms
+' of the GNU General Public License version 2 as published by the Free Software 
+' Foundation.
+'
+' EwE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+' without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+' PURPOSE. See the GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License along with EwE.
+' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
+'
+' Copyright 1991- UBC Fisheries Centre, Vancouver BC, Canada.
+' ===============================================================================
+'
+Imports EwECore.SpatialData.cSpatialScalarDataAdapterBase
+Imports EwEUtils.SpatialData
 
-Public Class cSpatialDataConnection
+Namespace SpatialData
 
-    Property Dataset As ISpatialDataSet
-    Property Converter As ISpatialDataConverter
-    'ToDo: add diagnostics? Compatibility?
+    ' TODO: inherit from cCoreInputOutputBase
+    ' Add variables
+    ' Variable statuses: scale may be read-only
 
-    Public Overridable Function IsConfigured() As Boolean
+    Public Class cSpatialDataConnection
 
-        Dim bIsConfigured As Boolean = False
+        Public Property Dataset As ISpatialDataSet = Nothing
+        Public Property Converter As ISpatialDataConverter = Nothing
+        Public Property Scale As Single = 1
+        Public Property ScaleType As eScaleType
 
-        If (Me.Dataset IsNot Nothing) Then
-            If (Me.Dataset.IsConfigured()) Then
-                If Not String.IsNullOrWhiteSpace(Me.Dataset.ConversionFormat) Then
-                    If (Me.Converter IsNot Nothing) Then
-                        bIsConfigured = bIsConfigured Or Me.Converter.IsConfigured()
+        Public Property Adapter As cSpatialDataAdapter = Nothing
+        Public Property iLayer As Integer = 1
+
+#If DEBUG Then
+        Private Shared s_NextID As Integer = 0
+        Private m_ID As Integer = 0
+#End If
+
+        Public Sub New()
+#If DEBUG Then
+            Me.m_ID = s_NextID
+            s_NextID += 1
+#End If
+        End Sub
+
+        Public Overridable Function IsConfigured() As Boolean
+
+            Dim bIsConfigured As Boolean = False
+
+            If (Me.Dataset IsNot Nothing) Then
+                If (Me.Dataset.IsConfigured()) Then
+                    If Not String.IsNullOrWhiteSpace(Me.Dataset.ConversionFormat) Then
+                        If (Me.Converter IsNot Nothing) Then
+                            bIsConfigured = bIsConfigured Or Me.Converter.IsConfigured()
+                        End If
+                    Else
+                        bIsConfigured = True
                     End If
-                Else
-                    bIsConfigured = True
                 End If
             End If
-        End If
-        Return bIsConfigured
+            Return bIsConfigured
 
-    End Function
+        End Function
 
-End Class
+        'ToDo: add diagnostics? Compatibility?
+
+    End Class
+
+End Namespace
