412642b8e9cdff48320fc9407cb60c755a3f2e8f
JeroenS <jeroen.steenbeek@gmail.com>
2014-11-06 16:35:06 +0000

    Resourced
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@11532 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterface/Ecospace/Input/Capacity/Apply/frmCapacityCalcType.Designer.vb
+++ Sources/ScientificInterface/Ecospace/Input/Capacity/Apply/frmCapacityCalcType.Designer.vb
@@ -50,11 +50,12 @@ Namespace Ecospace
         'Do not modify it using the code editor.
         <System.Diagnostics.DebuggerStepThrough()> _
         Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
             Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmCapacityCalcType))
             Me.m_tsMain = New ScientificInterfaceShared.Controls.cEwEToolstrip()
             Me.m_tsbnUseOnlyHabitat = New System.Windows.Forms.ToolStripButton()
             Me.m_tsbnUseOnlyCapacity = New System.Windows.Forms.ToolStripButton()
-            Me.m_grid = New gridCapacityCalcType()
+            Me.m_grid = New ScientificInterface.Ecospace.gridCapacityCalcType(Me.components)
             Me.m_tsMain.SuspendLayout()
             Me.SuspendLayout()
             '
@@ -78,8 +79,34 @@ Namespace Ecospace
             '
             'm_grid
             '
+            Me.m_grid.AllowBlockSelect = True
+            Me.m_grid.AutoSizeMinHeight = 10
+            Me.m_grid.AutoSizeMinWidth = 10
+            Me.m_grid.AutoStretchColumnsToFitWidth = False
+            Me.m_grid.AutoStretchRowsToFitHeight = False
+            Me.m_grid.BackColor = System.Drawing.Color.White
+            Me.m_grid.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+            Me.m_grid.ContextMenuStyle = CType((((SourceGrid2.ContextMenuStyle.ColumnResize Or SourceGrid2.ContextMenuStyle.AutoSize) _
+                Or SourceGrid2.ContextMenuStyle.CopyPasteSelection) _
+                Or SourceGrid2.ContextMenuStyle.CellContextMenu), SourceGrid2.ContextMenuStyle)
+            Me.m_grid.CustomSort = False
+            Me.m_grid.DataName = "grid content"
             resources.ApplyResources(Me.m_grid, "m_grid")
+            Me.m_grid.FixedColumnWidths = False
+            Me.m_grid.FocusStyle = SourceGrid2.FocusStyle.None
+            Me.m_grid.GridToolTipActive = True
+            Me.m_grid.IsLayoutSuspended = False
             Me.m_grid.Name = "m_grid"
+            Me.m_grid.SpecialKeys = CType((((((((((SourceGrid2.GridSpecialKeys.Ctrl_C Or SourceGrid2.GridSpecialKeys.Ctrl_V) _
+                Or SourceGrid2.GridSpecialKeys.Ctrl_X) _
+                Or SourceGrid2.GridSpecialKeys.Delete) _
+                Or SourceGrid2.GridSpecialKeys.Arrows) _
+                Or SourceGrid2.GridSpecialKeys.Tab) _
+                Or SourceGrid2.GridSpecialKeys.PageDownUp) _
+                Or SourceGrid2.GridSpecialKeys.Enter) _
+                Or SourceGrid2.GridSpecialKeys.Escape) _
+                Or SourceGrid2.GridSpecialKeys.Backspace), SourceGrid2.GridSpecialKeys)
+            Me.m_grid.UIContext = Nothing
             '
             'frmCapacityCalcType
             '
