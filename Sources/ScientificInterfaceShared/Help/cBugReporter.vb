db40ed9a7b9663a25784f055eeec4fb056f2e771
JeroenS <jeroen.steenbeek@gmail.com>
2015-01-11 20:27:43 +0000

    Updated resilience documentation Fixed #1372
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@11717 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterfaceShared/Help/cBugReporter.vb
+++ Sources/ScientificInterfaceShared/Help/cBugReporter.vb
@@ -31,23 +31,22 @@ Imports System.Net.Mail
 
 ''' ===========================================================================
 ''' <summary>
-''' Helper class for sending a bug report email. The bug report body includes a
-''' list of loaded assemblies to help diagnose problems.
+''' Helper class for generating a bug report to be sent via the shell.
 ''' </summary>
 ''' ===========================================================================
 Public Class cBugReporter
 
     ''' -----------------------------------------------------------------------
     ''' <summary>
-    ''' Launch an email client with a pre-formatted bug report message.
+    ''' Returns a bug report.
     ''' </summary>
     ''' <param name="strAppName">Name of the application.</param>
     ''' <param name="strMailTo">Target email address.</param>
     ''' <param name="pm">Plug-in manager to extract components from.</param>
     ''' -----------------------------------------------------------------------
-    Public Shared Function InvokeBugReport(ByVal strAppName As String, _
-                                           ByVal strMailTo As String, _
-                                           Optional ByVal pm As cPluginManager = Nothing) As Boolean
+    Public Shared Function BugReport(ByVal strAppName As String, _
+                                     ByVal strMailTo As String, _
+                                     Optional ByVal pm As cPluginManager = Nothing) As String
 
         Dim an As AssemblyName = Nothing
         Dim ub As New UrlBuilder("mailto:" & strMailTo)
@@ -82,71 +81,65 @@ Public Class cBugReporter
 
         ub.QueryString("body") = sbBody.ToString()
 
-        Try
-            System.Diagnostics.Process.Start(ub.ToString())
-        Catch ex As Exception
-            ' Wow, no mail client installed? 
-            Return False
-        End Try
-        Return True
+        Return ub.ToString
 
     End Function
 
-    Private Shared Function SendAttachment(ByVal strAppName As String, _
-                                    ByVal strAddress As String, _
-                                    ByVal pm As cPluginManager) As Boolean
+    'Private Shared Function SendAttachment(ByVal strAppName As String, _
+    '                                ByVal strAddress As String, _
+    '                                ByVal pm As cPluginManager) As Boolean
 
-        Dim an As AssemblyName = Nothing
-        Dim oMsg As New MailMessage()
-        Dim sbBody As New System.Text.StringBuilder
+    '    Dim an As AssemblyName = Nothing
+    '    Dim oMsg As New MailMessage()
+    '    Dim sbBody As New System.Text.StringBuilder
 
-        sbBody.AppendLine("I experienced the following issue with " & strAppName & ":")
-        sbBody.AppendLine("")
-        sbBody.AppendLine("(Please provide a detailed description of the issue, and steps to reproduce the error if possible. If required, please zip up and attach your model)")
+    '    sbBody.AppendLine("I experienced the following issue with " & strAppName & ":")
+    '    sbBody.AppendLine("")
+    '    sbBody.AppendLine("(Please provide a detailed description of the issue, and steps to reproduce the error if possible. If required, please zip up and attach your model)")
 
-        Dim strFile As String = Path.Combine(System.IO.Path.GetTempPath(), "EwE_config.txt")
-        Dim swTemp As New StreamWriter(strFile)
-        swTemp.WriteLine("EwE configuration (do not modify):")
-        swTemp.WriteLine()
-        swTemp.WriteLine(cSysConfig.OSVersion())
-        swTemp.WriteLine(cSysConfig.NETVersion())
-        swTemp.WriteLine()
-        swTemp.WriteLine("EwE modules:")
-        For Each an In cAssemblyUtils.GetSummary(cAssemblyUtils.eSummaryFlags.EwECore)
-            swTemp.WriteLine(String.Format("* {0}={2},{1}", _
-                                            an.Name, cStringUtils.ToHexString(an.GetPublicKeyToken), an.Version))
-        Next
+    '    Dim strFile As String = Path.Combine(System.IO.Path.GetTempPath(), "EwE_config.txt")
+    '    Dim swTemp As New StreamWriter(strFile)
+    '    swTemp.WriteLine("EwE configuration (do not modify):")
+    '    swTemp.WriteLine()
+    '    swTemp.WriteLine(cSysConfig.OSVersion())
+    '    swTemp.WriteLine(cSysConfig.NETVersion())
+    '    swTemp.WriteLine()
+    '    swTemp.WriteLine("EwE modules:")
+    '    For Each an In cAssemblyUtils.GetSummary(cAssemblyUtils.eSummaryFlags.EwECore)
+    '        swTemp.WriteLine(String.Format("* {0}={2},{1}", _
+    '                                        an.Name, cStringUtils.ToHexString(an.GetPublicKeyToken), an.Version))
+    '    Next
 
-        If (pm IsNot Nothing) Then
-            swTemp.WriteLine()
-            swTemp.WriteLine("EwE plug-ins:")
-            For Each pa As cPluginAssembly In pm.PluginAssemblies
-                an = pa.AssemblyName
-                swTemp.WriteLine(String.Format("- {0}={2},{1}", _
-                                                an.Name, cStringUtils.ToHexString(an.GetPublicKeyToken), an.Version))
-            Next
-        End If
-        swTemp.WriteLine("---------------------------------------------------")
-        swTemp.Flush()
-        swTemp.Close()
+    '    If (pm IsNot Nothing) Then
+    '        swTemp.WriteLine()
+    '        swTemp.WriteLine("EwE plug-ins:")
+    '        For Each pa As cPluginAssembly In pm.PluginAssemblies
+    '            an = pa.AssemblyName
+    '            swTemp.WriteLine(String.Format("- {0}={2},{1}", _
+    '                                            an.Name, cStringUtils.ToHexString(an.GetPublicKeyToken), an.Version))
+    '        Next
+    '    End If
+    '    swTemp.WriteLine("---------------------------------------------------")
+    '    swTemp.Flush()
+    '    swTemp.Close()
 
-        'oMsg.From =
-        oMsg.From = New Net.Mail.MailAddress("user@mail.com")
-        oMsg.To.Add(New Net.Mail.MailAddress(strAddress))
-        oMsg.Subject = strAppName & " incident report"
-        oMsg.Body = sbBody.ToString()
+    '    'oMsg.From =
+    '    oMsg.From = New Net.Mail.MailAddress("user@mail.com")
+    '    oMsg.To.Add(New Net.Mail.MailAddress(strAddress))
+    '    oMsg.Subject = strAppName & " incident report"
+    '    oMsg.Body = sbBody.ToString()
 
-        Dim oAttch As New Net.Mail.Attachment(strFile)
-        oMsg.Attachments.Add(oAttch)
+    '    Dim oAttch As New Net.Mail.Attachment(strFile)
+    '    oMsg.Attachments.Add(oAttch)
 
-        Dim cl As New SmtpClient("127.0.0.1")
-        Try
-            cl.Send(oMsg)
-        Catch ex As Exception
-            Return False
-        End Try
+    '    Dim cl As New SmtpClient("127.0.0.1")
+    '    Try
+    '        cl.Send(oMsg)
+    '    Catch ex As Exception
+    '        Return False
+    '    End Try
 
-        Return True
-    End Function
+    '    Return True
+    'End Function
 
 End Class
