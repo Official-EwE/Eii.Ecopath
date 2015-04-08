f0c92497a4f58c2b097ef3853155f8abb5e0b66b
markp <markp@ecopath.org>
2015-04-08 20:11:44 +0000

    missed a file for start to oo results output
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@12148 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources Internal/Plugins/CEFASMSEPlugin/EwEMSEPlugin/cBaseResultsArray.vb
@@ -0,0 +1,9 @@
+﻿Public MustInherit Class cBaseResultsArray
+
+    MustOverride Sub Initialise(MSE As cMSE)
+
+    MustOverride Sub Populate()
+
+    Public MustOverride ReadOnly Property DataName As String
+
+End Class
