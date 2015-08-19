a953498bee2b8b80f08c3479b46721df8268eade
JeroenS <jeroen.steenbeek@gmail.com>
2015-08-19 11:41:58 +0000

    Added Psust SD
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@12645 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECustomPlugins/EwENetworkAnalysis/Interface/cLossinProductionIndex.vb
+++ Sources/EwECustomPlugins/EwENetworkAnalysis/Interface/cLossinProductionIndex.vb
@@ -73,6 +73,8 @@ Public Class cLossinProductionIndex
         astrRowContent(1) = My.Resources.COL_HDR_GRP_NAME
         astrRowContent(2) = My.Resources.COL_HDR_LINDEX
         astrRowContent(3) = My.Resources.COL_HDR_PSUST
+        astrRowContent(4) = My.Resources.COL_HDR_PSUST_SDLOWER
+        astrRowContent(5) = My.Resources.COL_HDR_PSUST_SDUPPER
 
         Grid.Rows(0).SetValues(astrRowContent)
         Grid.Rows(0).Visible = True
@@ -82,6 +84,8 @@ Public Class cLossinProductionIndex
             astrRowContent(1) = NetworkManager.GroupName(i)
             astrRowContent(2) = Me.StyleGuide.FormatNumber(NetworkManager.Lindex(i))
             astrRowContent(3) = Me.StyleGuide.FormatNumber(NetworkManager.Psust(i))
+            astrRowContent(4) = Me.StyleGuide.FormatNumber(NetworkManager.PsustSDlower(i))
+            astrRowContent(5) = Me.StyleGuide.FormatNumber(NetworkManager.PsustSDupper(i))
             LindexTot += NetworkManager.Lindex(i)
             Grid.Rows(i).SetValues(astrRowContent)
             Grid.Rows(i).Visible = True
@@ -108,7 +112,7 @@ Public Class cLossinProductionIndex
     Private Sub SetUpGridColumn()
 
         'DataGrid.RowCount = 1
-        Grid.ColumnCount = 4
+        Grid.ColumnCount = 6
 
         SetGridColumnPropertyDefault(Grid)
 
