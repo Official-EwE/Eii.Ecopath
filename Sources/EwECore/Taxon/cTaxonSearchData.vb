febd40e4209b282cfdce2459d33050cc4475ca07
JeroenS <jeroen.steenbeek@gmail.com>
2015-08-19 09:45:02 +0000

    Simplified
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@12642 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECore/Taxon/cTaxonSearchData.vb
+++ Sources/EwECore/Taxon/cTaxonSearchData.vb
@@ -33,53 +33,10 @@ Public Class cTaxonSearchData
     Implements ITaxonSearchData
     Implements ITaxonDetailsData
 
-#Region " Privates "
-
-    Private m_strSource As String = ""
-    Private m_strSourceKey As String = ""
-    Private m_searchFields As eTaxonLevelType = eTaxonLevelType.Any
-
-    ' -- ID --
-    Private m_lCodeSAUP As Long = 0
-    Private m_lCodeFB As Long = 0
-    Private m_lCodeSLB As Long = 0
-    Private m_strCodeLSID As String = ""
-    Private m_strCodeFAO As String = ""
-
-    ' -- Data --
-    Private m_strCommon As String = ""
-    Private m_strPhylum As String = ""
-    Private m_strClass As String = ""
-    Private m_strOrder As String = ""
-    Private m_strFamily As String = ""
-    Private m_strGenus As String = ""
-    Private m_strSpecies As String = ""
-    Private m_sNorth As Single = cCore.NULL_VALUE
-    Private m_sSouth As Single = cCore.NULL_VALUE
-    Private m_sEast As Single = cCore.NULL_VALUE
-    Private m_sWest As Single = cCore.NULL_VALUE
-    Private m_bExploited As Boolean = False
-    Private m_ecology As eEcologyTypes = eEcologyTypes.NotSet
-    Private m_conservation As eIUCNConservationStatusTypes = eIUCNConservationStatusTypes.NotSet
-    Private m_exploitation As eExploitationTypes = eExploitationTypes.NotSet
-    Private m_occurrence As eOccurrenceStatusTypes = eOccurrenceStatusTypes.NotSet
-    Private m_organism As eOrganismTypes = eOrganismTypes.Fishes
-    Private m_sLastUpdated As Double = cDateUtils.DateToJulian(Date.Now())
-    Private m_sMaxLength As Single = cCore.NULL_VALUE
-    Private m_sMeanLength As Single = cCore.NULL_VALUE
-    Private m_sMeanLifespan As Single = cCore.NULL_VALUE
-    Private m_sMeanWeight As Single = cCore.NULL_VALUE
-    Private m_iVulnerabilityIndex As Integer = cCore.NULL_VALUE
-    ' Growth
-    Private m_vbgkf As Single = cCore.NULL_VALUE
-    Private m_sWinf As Single = cCore.NULL_VALUE
-
-#End Region ' Privates
-
 #Region " Constructor "
 
     Public Sub New(strSource As String)
-        Me.m_strSource = strSource
+        Me.Source = strSource
     End Sub
 
 #End Region ' Constructor
@@ -87,356 +44,100 @@ Public Class cTaxonSearchData
 #Region " Properties "
 
     ''' <inheritdocs cref="ITaxonSearchData.Phylum"/>
-    Public Property Phylum() As String _
-        Implements ITaxonSearchData.Phylum
-        Get
-            Return m_strPhylum
-        End Get
-        Set(ByVal value As String)
-            Me.m_strPhylum = value
-        End Set
-    End Property
+    Public Property Phylum() As String Implements ITaxonSearchData.Phylum
 
     ''' <inheritdocs cref="ITaxonSearchData.[Class]"/>
-    Public Property [Class]() As String _
-        Implements ITaxonSearchData.Class
-        Get
-            Return Me.m_strClass
-        End Get
-        Set(ByVal value As String)
-            Me.m_strClass = value
-        End Set
-    End Property
+    Public Property [Class]() As String Implements ITaxonSearchData.Class
 
     ''' <inheritdocs cref="ITaxonSearchData.Common"/>
-    Public Property Common() As String _
-        Implements ITaxonSearchData.Common
-        Get
-            Return Me.m_strCommon
-        End Get
-        Set(ByVal value As String)
-            Me.m_strCommon = value
-        End Set
-    End Property
+    Public Property Common() As String Implements ITaxonSearchData.Common
 
     ''' <inheritdocs cref="ITaxonSearchData.Family"/>
-    Public Property Family() As String _
-        Implements ITaxonSearchData.Family
-        Get
-            Return Me.m_strFamily
-        End Get
-        Set(ByVal value As String)
-            Me.m_strFamily = value
-        End Set
-    End Property
+    Public Property Family() As String Implements ITaxonSearchData.Family
 
     ''' <inheritdocs cref="ITaxonSearchData.Genus"/>
-    Public Property Genus() As String _
-        Implements ITaxonSearchData.Genus
-        Get
-            Return Me.m_strGenus
-        End Get
-        Set(ByVal value As String)
-            Me.m_strGenus = value
-        End Set
-    End Property
+    Public Property Genus() As String Implements ITaxonSearchData.Genus
 
     ''' <inheritdocs cref="ITaxonSearchData.Order"/>
-    Public Property Order() As String _
-        Implements ITaxonSearchData.Order
-        Get
-            Return Me.m_strOrder
-        End Get
-        Set(ByVal value As String)
-            Me.m_strOrder = value
-        End Set
-    End Property
+    Public Property Order() As String Implements ITaxonSearchData.Order
 
     ''' <inheritdocs cref="ITaxonSearchData.Species"/>
-    Public Property Species() As String _
-        Implements ITaxonSearchData.Species
-        Get
-            Return Me.m_strSpecies
-        End Get
-        Set(ByVal value As String)
-            Me.m_strSpecies = value
-        End Set
-    End Property
+    Public Property Species() As String Implements ITaxonSearchData.Species
 
     ''' <inheritdocs cref="ITaxonSearchData.CodeSAUP"/>
-    Public Property CodeSAUP() As Long _
-        Implements ITaxonSearchData.CodeSAUP
-        Get
-            Return Me.m_lCodeSAUP
-        End Get
-        Set(ByVal value As Long)
-            Me.m_lCodeSAUP = value
-        End Set
-    End Property
+    Public Property CodeSAUP() As Long Implements ITaxonSearchData.CodeSAUP
 
     ''' <inheritdocs cref="ITaxonSearchData.CodeFB"/>
-    Public Property CodeFB As Long _
-        Implements ITaxonSearchData.CodeFB
-        Get
-            Return Me.m_lCodeFB
-        End Get
-        Set(value As Long)
-            Me.m_lCodeFB = value
-        End Set
-    End Property
+    Public Property CodeFB As Long Implements ITaxonSearchData.CodeFB
 
     ''' <inheritdocs cref="ITaxonSearchData.CodeSLB"/>
-    Public Property CodeSLB As Long _
-        Implements ITaxonSearchData.CodeSLB
-        Get
-            Return Me.m_lCodeSLB
-        End Get
-        Set(value As Long)
-            Me.m_lCodeSLB = value
-        End Set
-    End Property
+    Public Property CodeSLB As Long Implements ITaxonSearchData.CodeSLB
 
     ''' <inheritdocs cref="ITaxonSearchData.CodeLSID"/>
-    Public Property CodeLSID() As String _
-        Implements ITaxonSearchData.CodeLSID
-        Get
-            Return Me.m_strCodeLSID
-        End Get
-        Set(ByVal value As String)
-            Me.m_strCodeLSID = value
-        End Set
-    End Property
+    Public Property CodeLSID() As String Implements ITaxonSearchData.CodeLSID
 
     ''' <inheritdocs cref="ITaxonSearchData.CodeFAO"/>
-    Public Property CodeFAO() As String _
-        Implements ITaxonSearchData.CodeFAO
-        Get
-            ' Rerouted to source key
-            Return Me.m_strCodeFAO
-        End Get
-        Set(ByVal value As String)
-            ' Rerouted to source key
-            Me.m_strCodeFAO = value
-        End Set
-    End Property
+    Public Property CodeFAO() As String Implements ITaxonSearchData.CodeFAO
 
     ''' <inheritdocs cref="ITaxonSearchData.Source"/>
-    Public Property Source() As String _
-        Implements ITaxonSearchData.Source
-        Get
-            Return Me.m_strSource
-        End Get
-        Set(ByVal value As String)
-            ' NOP
-        End Set
-    End Property
+    Public Property Source() As String Implements ITaxonSearchData.Source
 
     ''' <inheritdocs cref="ITaxonSearchData.SourceKey"/>
-    Public Property SourceKey() As String _
-        Implements ITaxonSearchData.SourceKey
-        Get
-            Return Me.m_strSourceKey
-        End Get
-        Set(ByVal value As String)
-            Me.m_strSourceKey = value
-        End Set
-    End Property
+    Public Property SourceKey() As String Implements ITaxonSearchData.SourceKey
 
-    Public Property SearchFields As eTaxonLevelType _
-        Implements ITaxonSearchData.SearchFields
-        Get
-            Return Me.m_searchFields
-        End Get
-        Set(value As eTaxonLevelType)
-            Me.m_searchFields = value
-        End Set
-    End Property
+    Public Property SearchFields As eTaxonLevelType Implements ITaxonSearchData.SearchFields
 
     ''' <inheritdocs cref="ITaxonSearchData.North"/>
-    Public Property North() As Single _
-        Implements ITaxonSearchData.North
-        Get
-            Return Me.m_sNorth
-        End Get
-        Set(ByVal value As Single)
-            Me.m_sNorth = value
-        End Set
-    End Property
+    Public Property North() As Single = cCore.NULL_VALUE Implements ITaxonSearchData.North
 
     ''' <inheritdocs cref="ITaxonSearchData.South"/>
-    Public Property South() As Single _
-        Implements ITaxonSearchData.South
-        Get
-            Return Me.m_sSouth
-        End Get
-        Set(ByVal value As Single)
-            Me.m_sSouth = value
-        End Set
-    End Property
-
+    Public Property South() As Single = cCore.NULL_VALUE Implements ITaxonSearchData.South
+    
     ''' <inheritdocs cref="ITaxonSearchData.East"/>
-    Public Property East() As Single _
-        Implements ITaxonSearchData.East
-        Get
-            Return Me.m_sEast
-        End Get
-        Set(ByVal value As Single)
-            Me.m_sEast = value
-        End Set
-    End Property
+    Public Property East() As Single = cCore.NULL_VALUE Implements ITaxonSearchData.East
 
     ''' <inheritdocs cref="ITaxonSearchData.West"/>
-    Public Property West() As Single _
-        Implements ITaxonSearchData.West
-        Get
-            Return Me.m_sWest
-        End Get
-        Set(ByVal value As Single)
-            Me.m_sWest = value
-        End Set
-    End Property
+    Public Property West() As Single = cCore.NULL_VALUE Implements ITaxonSearchData.West
 
     ''' <inheritdocs cref="ITaxonDetailsData.EcologyType"/>
-    Public Property EcologyType() As eEcologyTypes _
-        Implements ITaxonDetailsData.EcologyType
-        Get
-            Return Me.m_ecology
-        End Get
-        Set(ByVal value As eEcologyTypes)
-            Me.m_ecology = value
-        End Set
-    End Property
+    Public Property EcologyType() As eEcologyTypes = eEcologyTypes.NotSet Implements ITaxonDetailsData.EcologyType
 
     ''' <inheritdocs cref="ITaxonDetailsData.IUCNConservationStatus"/>
-    Public Property IUCNConservationStatus() As eIUCNConservationStatusTypes _
-        Implements ITaxonDetailsData.IUCNConservationStatus
-        Get
-            Return Me.m_conservation
-        End Get
-        Set(ByVal value As eIUCNConservationStatusTypes)
-            Me.m_conservation = value
-        End Set
-    End Property
+    Public Property IUCNConservationStatus() As eIUCNConservationStatusTypes = eIUCNConservationStatusTypes.NotSet Implements ITaxonDetailsData.IUCNConservationStatus
 
     ''' <inheritdocs cref="ITaxonDetailsData.ExploitationStatus"/>
-    Public Property ExploitationStatus() As eExploitationTypes _
-        Implements ITaxonDetailsData.ExploitationStatus
-        Get
-            Return Me.m_exploitation
-        End Get
-        Set(ByVal value As eExploitationTypes)
-            Me.m_exploitation = value
-        End Set
-    End Property
+    Public Property ExploitationStatus() As eExploitationTypes = eExploitationTypes.NotSet Implements ITaxonDetailsData.ExploitationStatus
 
     ''' <inheritdocs cref="ITaxonDetailsData.LastUpdated"/>
-    Public Property LastUpdated() As Double _
-        Implements ITaxonDetailsData.LastUpdated
-        Get
-            Return Me.m_sLastUpdated
-        End Get
-        Set(ByVal value As Double)
-            Me.m_sLastUpdated = value
-        End Set
-    End Property
+    Public Property LastUpdated() As Double = cDateUtils.DateToJulian(Date.Now()) Implements ITaxonDetailsData.LastUpdated
 
     ''' <inheritdocs cref="ITaxonDetailsData.MaxLength"/>
-    Public Property MaxLength() As Single _
-        Implements ITaxonDetailsData.MaxLength
-        Get
-            Return Me.m_sMaxLength
-        End Get
-        Set(ByVal value As Single)
-            Me.m_sMaxLength = value
-        End Set
-    End Property
+    Public Property MaxLength() As Single = cCore.NULL_VALUE Implements ITaxonDetailsData.MaxLength
 
     ''' <inheritdocs cref="ITaxonDetailsData.MeanLength"/>
-    Public Property MeanLength() As Single _
-        Implements ITaxonDetailsData.MeanLength
-        Get
-            Return Me.m_sMeanLength
-        End Get
-        Set(ByVal value As Single)
-            Me.m_sMeanLength = value
-        End Set
-    End Property
+    Public Property MeanLength() As Single = cCore.NULL_VALUE Implements ITaxonDetailsData.MeanLength
 
     ''' <inheritdocs cref="ITaxonDetailsData.MeanLifespan"/>
-    Public Property MeanLifespan() As Single _
-        Implements ITaxonDetailsData.MeanLifespan
-        Get
-            Return Me.m_sMeanLifespan
-        End Get
-        Set(ByVal value As Single)
-            Me.m_sMeanLifespan = value
-        End Set
-    End Property
+    Public Property MeanLifespan() As Single = cCore.NULL_VALUE Implements ITaxonDetailsData.MeanLifespan
 
     ''' <inheritdocs cref="ITaxonDetailsData.MeanWeight"/>
-    Public Property MeanWeight() As Single _
-        Implements ITaxonDetailsData.MeanWeight
-        Get
-            Return Me.m_sMeanWeight
-        End Get
-        Set(ByVal value As Single)
-            Me.m_sMeanWeight = value
-        End Set
-    End Property
+    Public Property MeanWeight() As Single = cCore.NULL_VALUE Implements ITaxonDetailsData.MeanWeight
 
     ''' <inheritdocs cref="ITaxonDetailsData.OccurrenceStatus"/>
-    Public Property OccurrenceStatus() As eOccurrenceStatusTypes _
-        Implements ITaxonDetailsData.OccurrenceStatus
-        Get
-            Return Me.m_occurrence
-        End Get
-        Set(ByVal value As eOccurrenceStatusTypes)
-            Me.m_occurrence = value
-        End Set
-    End Property
+    Public Property OccurrenceStatus() As eOccurrenceStatusTypes = eOccurrenceStatusTypes.NotSet Implements ITaxonDetailsData.OccurrenceStatus
 
     ''' <inheritdocs cref="ITaxonDetailsData.OrganismType"/>
-    Public Property OrganismType() As eOrganismTypes _
-        Implements ITaxonDetailsData.OrganismType
-        Get
-            Return Me.m_organism
-        End Get
-        Set(ByVal value As eOrganismTypes)
-            Me.m_organism = value
-        End Set
-    End Property
+    Public Property OrganismType() As eOrganismTypes = eOrganismTypes.NotSet Implements ITaxonDetailsData.OrganismType
 
     ''' <inheritdocs cref="ITaxonDetailsData.VulnerabilityIndex"/>
-    Public Property VulnerabilityIndex() As Integer _
-        Implements ITaxonDetailsData.VulnerabilityIndex
-        Get
-            Return Me.m_iVulnerabilityIndex
-        End Get
-        Set(ByVal value As Integer)
-            Me.m_iVulnerabilityIndex = value
-        End Set
-    End Property
-
-#End Region ' Properties
+    Public Property VulnerabilityIndex() As Integer = cCore.NULL_VALUE Implements ITaxonDetailsData.VulnerabilityIndex
 
     ''' <inheritdocs cref="ITaxonDetailsData.vbgfK"/>
-    Public Property vbgfK As Single Implements EwEUtils.Core.ITaxonDetailsData.vbgfK
-        Get
-            Return Me.m_vbgkf
-        End Get
-        Set(value As Single)
-            Me.m_vbgkf = value
-        End Set
-    End Property
+    Public Property vbgfK As Single = cCore.NULL_VALUE Implements EwEUtils.Core.ITaxonDetailsData.vbgfK
 
     ''' <inheritdocs cref="ITaxonDetailsData.Winf"/>
-    Public Property Winf As Single Implements EwEUtils.Core.ITaxonDetailsData.Winf
-        Get
-            Return Me.m_sWinf
-        End Get
-        Set(value As Single)
-            Me.m_sWinf = value
-        End Set
-    End Property
+    Public Property Winf As Single = cCore.NULL_VALUE Implements EwEUtils.Core.ITaxonDetailsData.Winf
+
+#End Region ' Properties
 
 End Class
