2fe58ac1e56d2440bda54c33876e0ce371e94803
joeb <joeb@ecopath.org>
2015-09-28 16:35:37 +0000

    Tweaks to how Ecosim uses FishMGear to calculate Proportion of catch and discards
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@12791 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECore/EcoSpace/Model/cSpaceSolver.vb
+++ Sources/EwECore/EcoSpace/Model/cSpaceSolver.vb
@@ -1188,9 +1188,10 @@ Public Class cSpaceSolver
 
                 'If m_PathData.NumFleet > 0 Then     'Only if there is fishery
                 For K = 1 To m_PathData.NumFleet
+                    Dim PropDiscMort As Single = m_SimData.Propdiscardtime(K, i) / (m_SimData.PropLandedTime(K, i) + m_SimData.Propdiscardtime(K, i))
                     'jb 07-Jan-2010 Changed to use Propdiscardtime(fleets,groups) (% discarded for this time step) initialized to ecopath PropDiscard() or set in MSE.RegulateEffort() 
                     'discard mort is included in Propdiscardtime() by initialization and MSE 
-                    DetFlowN = m_PathData.DiscardFate(K, j - m_PathData.NumLiving) * Biomass(i) * FishRateGear(K, 0) * m_SimData.FishMGear(K, i) * Me.m_SimData.Propdiscardtime(K, i)
+                    DetFlowN = m_PathData.DiscardFate(K, j - m_PathData.NumLiving) * Biomass(i) * FishRateGear(K, 0) * m_SimData.FishMGear(K, i) * PropDiscMort 'Me.m_SimData.Propdiscardtime(K, i)
                     ToDet = ToDet + DetFlowN
 
                     If m_TracerData.EcoSimConSimOn = True Then
