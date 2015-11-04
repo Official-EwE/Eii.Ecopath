538ff67099d2e57adc25d2cbb75a197311d6bedf
joeb <joeb@ecopath.org>
2015-11-04 21:24:35 +0000

    ICM Writter tweaks
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@12918 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources Internal/Plugins/EcospaceResultsWriterICM/cEcospaceResultsWriterICMPlugin.vb
+++ Sources Internal/Plugins/EcospaceResultsWriterICM/cEcospaceResultsWriterICMPlugin.vb
@@ -214,8 +214,6 @@ Public Class cEcospaceResultsWriterICMPlugin
     Public Sub Init()
 
         m_SpCodes = New String() {"N/A", "JH", "AH", "DO", "BI", "JU", "AU", "JR", "AR", "JB", "AB", "JS", "AS", "JG", "AG", "JT", "AT", "JX", "AX", "JF", "AF", "JC", "AC", "JE", "AE", "JP", "AP", "JO", "AO", "JI", "AI", "JA", "AA", "JM", "AM", "KI", "SI", "JL", "AL", "JN", "AN", "JW", "AW", "GS", "SP", "SE", "SA", "OD", "BC", "MO", "ZB", "ZP", "PH", "BA", "SV", "DE"}
-
-        'm_RegionCodes = New String() {"TOT", "LAV", "LTB", "UBA", "BFD", "UPO", "LPO", "UTA", "MEL", "LBA", "BRE", "CAS", "CHR"}
         m_RegionCodes = New String() {"TOT", "LAV", "LTB", "UBA", "BFD", "UPO", "LPO", "MEL", "LBA", "BRE", "CAS", "CHR"}
         m_FleetCodes = New String() {"N/A", "BS", "WS", "BC", "BD", "OY", "RC", "ME"}
 
