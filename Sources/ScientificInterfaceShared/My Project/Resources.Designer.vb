119c0360b2128f8e530801eed32e5ac1c166206d
JeroenS <jeroen.steenbeek@gmail.com>
2015-11-25 09:30:06 +0000

    Removed database warning icon
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@12987 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterfaceShared/My Project/Resources.Designer.vb
+++ Sources/ScientificInterfaceShared/My Project/Resources.Designer.vb
@@ -889,16 +889,6 @@ Namespace My.Resources
         End Property
         
         '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
-        Public ReadOnly Property database_warning() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("database_warning", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
         '''  Looks up a localized string similar to *.ewemdb|Access 2003 (*.ewemdb)||Microsoft Access 2003 (*.ewemdb).
         '''</summary>
         Public ReadOnly Property DATASOURCE_ACCESS2003() As String
