3dadf06c1b7411371d3d1cf192f90e56b384ff42
JeroenS <jeroen.steenbeek@gmail.com>
2016-02-24 13:58:18 +0000

    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@13336 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterfaceShared/Controls/Map/Layers/Editing/cLayerEditorAdvection.vb
+++ Sources/ScientificInterfaceShared/Controls/Map/Layers/Editing/cLayerEditorAdvection.vb
@@ -48,60 +48,6 @@ Namespace Controls.Map.Layers
             Me.CellValueMax = 1000
         End Sub
 
-        ''' -------------------------------------------------------------------
-        ''' <summary>
-        ''' Smooth layer data across water cells.
-        ''' </summary>
-        ''' -------------------------------------------------------------------
-        Public Overrides Sub Smooth()
-
-            If (Not Me.IsEditable) Then Return
-
-            Dim bm As cEcospaceBasemap = Me.UIContext.Core.EcospaceBasemap
-            Dim layerDepth As cEcospaceLayerDepth = bm.LayerDepth
-            Dim layer As EwECore.cEcospaceLayerVector = CType(Me.Layer.Data, cEcospaceLayerVector)
-            Dim cnewx(,) As Single
-            Dim cnewy(,) As Single
-            Dim i As Integer, j As Integer
-            Dim tx As Single
-            Dim ty As Single
-            Dim n As Integer
-
-            ReDim cnewx(bm.InRow, bm.InCol)
-            ReDim cnewy(bm.InRow, bm.InCol)
-
-            For i = 1 To bm.InRow
-                For j = 1 To bm.InCol
-                    tx = 0
-                    ty = 0
-                    n = 0
-                    For ii As Integer = i - 1 To i + 1
-                        For jj As Integer = j - 1 To j + 1
-                            If Not (ii = 0 Or jj = 0 Or ii = bm.InRow + 1 Or jj = bm.InCol + 1) And layerDepth.IsWaterCell(ii, jj) Then
-                                tx += layer.XVelocity(ii, jj)
-                                ty += layer.YVelocity(ii, jj)
-                                n += 1
-                            End If
-                        Next jj
-                    Next ii
-                    If n > 0 Then
-                        cnewx(i, j) = tx / n
-                        cnewy(i, j) = ty / n
-                    End If
-                Next j
-            Next i
-
-            For i = 1 To bm.InRow
-                For j = 1 To bm.InCol
-                    If layerDepth.IsWaterCell(i, j) Then
-                        layer.XVelocity(i, j) = cnewx(i, j)
-                        layer.YVelocity(i, j) = cnewy(i, j)
-                    End If
-                Next
-            Next
-            Me.Layer.Update(cDisplayLayer.eChangeFlags.Map)
-
-        End Sub
     End Class
 
 End Namespace
