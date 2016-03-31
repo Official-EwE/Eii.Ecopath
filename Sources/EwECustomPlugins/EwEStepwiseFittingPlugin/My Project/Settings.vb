0351562bbffc53850c3be0fae7b14600b922c58e
JeroenS <jeroen.steenbeek@gmail.com>
2016-03-31 19:21:56 +0000

    Updated license headers
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@13488 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECustomPlugins/EwEStepwiseFittingPlugin/My Project/Settings.vb
+++ Sources/EwECustomPlugins/EwEStepwiseFittingPlugin/My Project/Settings.vb
@@ -12,10 +12,17 @@
 ' You should have received a copy of the GNU General Public License along with EwE.
 ' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
 '
-' Copyright 1991- UBC Fisheries Centre, Vancouver BC, Canada.
+' Copyright 1991- 
+'    UBC Fisheries Centre, Vancouver BC, Canada, and 
+'    Ecopath International Initiative, Barcelona, Spain
+'
+' Stepwise Fitting Procedure by Sheila Heymans, Erin Scott, Jeroen Steenbeek
+' Copyright 2015- Scottish Association for Marine Science, Oban, Scotland
+'
+' Erin Scott was funded by the Scottish Informatics and Computer Science
+' Alliance (SICSA) Postgraduate Industry Internship Programme.
 ' ===============================================================================
 '
-
 Imports System.IO
 Imports EwEUtils
 Imports System.Reflection
