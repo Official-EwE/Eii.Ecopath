183cc4f63de8985c1f95b3f59f471a3255ee5ab5
JeroenS <jeroen.steenbeek@gmail.com>
2016-04-04 12:29:48 +0000

    Fixed SWF time series enabling / disabling logic
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@13512 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECustomPlugins/EwEStepwiseFittingPlugin/cSFPGenericIterations.vb
+++ Sources/EwECustomPlugins/EwEStepwiseFittingPlugin/cSFPGenericIterations.vb
@@ -37,10 +37,10 @@ Imports EwEUtils.Core
 Public MustInherit Class cSFPGenericIterations
     Implements ISFPIterations
 
-    Protected core As cCore
-    Protected TimeSeriesIndex As Integer
-    Protected F2TSManager As cF2TSManager
-    Protected PredOrPredPreySSToV As Boolean
+    Protected m_core As cCore
+    Protected m_iTimeSeries As Integer
+    Protected m_F2TSManager As cF2TSManager
+    Protected m_bPredOrPredPreySSToV As Boolean
     Public Property Parameters As cSFPParameters Implements ISFPIterations.Parameters
 
     Public Property k As Integer = 0 Implements ISFPIterations.K
@@ -72,25 +72,25 @@ Public MustInherit Class cSFPGenericIterations
     ''' <param name="SSToVChoice"></param>
     ''' <param name="Params"></param>
     ''' -----------------------------------------------------------------------
-    Protected Sub Initiate(ByVal c As EwECore.cCore, ByVal tsi As Integer, ByVal SSToVChoice As Boolean, ByVal Params As cSFPParameters, ByVal mFrm As Form) _
+    Protected Sub Initiate(ByVal c As EwECore.cCore, ByVal tsi As Integer, ByVal SSToVChoice As Boolean, ByVal Params As cSFPParameters, ByVal frmUI As Form) _
         Implements ISFPIterations.Init
 
         'Get variables needed for SFP iteration
-        core = c
-        TimeSeriesIndex = tsi
-        PredOrPredPreySSToV = SSToVChoice
+        m_core = c
+        m_iTimeSeries = tsi
+        m_bPredOrPredPreySSToV = SSToVChoice
         Parameters = Params
-        F2TSManager = core.EcosimFitToTimeSeries
-        F2TSManager.Connect(mFrm, AddressOf OnRunStarted, AddressOf OnRunStep, AddressOf OnRunStopped, AddressOf OnModelRun)
+        m_F2TSManager = m_core.EcosimFitToTimeSeries
+        m_F2TSManager.Connect(frmUI, AddressOf OnRunStarted, AddressOf OnRunStep, AddressOf OnRunStopped, AddressOf OnModelRun)
 
         ' Allocate memory for anomaly shape
-        ReDim Me.m_anomalyshape(Me.core.nEcosimTimeSteps)
+        ReDim Me.m_anomalyshape(Me.m_core.nEcosimTimeSteps)
 
         ' Allocate memory for vulnerabilities matrix
-        ReDim Me.m_vulnerabilities(Me.core.nGroups, Me.core.nGroups)
+        ReDim Me.m_vulnerabilities(Me.m_core.nGroups, Me.m_core.nGroups)
 
         'Allocate memory for time series SS results
-        ReDim Me.m_timeseriesSS(Me.core.TimeSeriesDataset(TimeSeriesIndex).nTimeSeries)
+        ReDim Me.m_timeseriesSS(Me.m_core.TimeSeriesDataset(m_iTimeSeries).nTimeSeries)
 
     End Sub
 
@@ -115,9 +115,9 @@ Public MustInherit Class cSFPGenericIterations
         Me.m_aicc = Me.GetAICc()
 
         ' Store vulnerabilities
-        For i As Integer = 1 To core.nGroups
-            Dim grp As cEcoSimGroupInput = Me.core.EcoSimGroupInputs(i)
-            For j As Integer = 1 To core.nGroups
+        For i As Integer = 1 To m_core.nGroups
+            Dim grp As cEcoSimGroupInput = Me.m_core.EcoSimGroupInputs(i)
+            For j As Integer = 1 To m_core.nGroups
                 Me.m_vulnerabilities(i, j) = grp.VulMult(j)
             Next
         Next
@@ -125,7 +125,7 @@ Public MustInherit Class cSFPGenericIterations
         ' Store first anomaly shape
         Dim shape As cShapeData = Parameters.AppliedShape
         If (shape IsNot Nothing) Then
-            Me.core.ForcingShapeManager.Load()
+            Me.m_core.ForcingShapeManager.Load()
             Me.m_anomalyshape = shape.ShapeData
         End If
 
@@ -144,7 +144,7 @@ Public MustInherit Class cSFPGenericIterations
 
         If (Not Me.RunState = ISFPIterations.eRunState.Completed) Then Return False
 
-        Me.core.SetBatchLock(cCore.eBatchLockType.Update)
+        Me.m_core.SetBatchLock(cCore.eBatchLockType.Update)
 
         ' ToDo: add error checking!
         Try
@@ -152,9 +152,9 @@ Public MustInherit Class cSFPGenericIterations
             EnableTimeSeries()
 
             ' Restore vulnerabilities
-            For i As Integer = 1 To core.nGroups
-                Dim grp As cEcoSimGroupInput = Me.core.EcoSimGroupInputs(i)
-                For j As Integer = 1 To core.nGroups
+            For i As Integer = 1 To m_core.nGroups
+                Dim grp As cEcoSimGroupInput = Me.m_core.EcoSimGroupInputs(i)
+                For j As Integer = 1 To m_core.nGroups
                     grp.VulMult(j) = Me.m_vulnerabilities(i, j)
                 Next
             Next
@@ -171,7 +171,7 @@ Public MustInherit Class cSFPGenericIterations
             ' ToDo: add error feedback!
         End Try
 
-        Me.core.ReleaseBatchLock(cCore.eBatchChangeLevelFlags.Ecosim)
+        Me.m_core.ReleaseBatchLock(cCore.eBatchChangeLevelFlags.Ecosim)
 
         Return True
 
@@ -184,64 +184,45 @@ Public MustInherit Class cSFPGenericIterations
     ''' -----------------------------------------------------------------------
     Protected Function EnableTimeSeries() As Boolean
 
-        Dim bSuccess As Boolean = False
+        If (m_iTimeSeries < 1) Then Return False
+
+        Dim dataset As cTimeSeriesDataset = m_core.TimeSeriesDataset(m_iTimeSeries)
+
         'Reset fishing effort shapes
-        core.FishingEffortShapeManager.ResetToDefaults()
-        'Reset all time series to be disabled
-        For i As Integer = 1 To core.TimeSeriesDataset(TimeSeriesIndex).nTimeSeries
-            core.TimeSeriesDataset(TimeSeriesIndex).TimeSeries(i).Enabled = False
-        Next
+        m_core.FishingEffortShapeManager.ResetToDefaults()
 
         'If Baseline
         If BaseorFish Then
-
-            Dim tsType As eTimeSeriesType
             'Go through each time series of the time series dataset
-            Dim TimeSeries As cTimeSeriesDataset = core.TimeSeriesDataset(TimeSeriesIndex)
-            For i As Integer = 1 To core.TimeSeriesDataset(TimeSeriesIndex).nTimeSeries
+            For i As Integer = 1 To dataset.nTimeSeries
 
-                'Get the time series type
-                tsType = core.TimeSeriesDataset(TimeSeriesIndex).TimeSeries(i).TimeSeriesType
-
+                Dim ts As cTimeSeries = dataset.TimeSeries(i)
                 'If the time series type is 0, 1, 5, 6, 7 enable it
-                Select Case tsType
+                Select Case ts.TimeSeriesType
                     Case eTimeSeriesType.BiomassRel, _
-                     eTimeSeriesType.TotalMortality, _
-                     eTimeSeriesType.Catches, _
-                     eTimeSeriesType.AverageWeight
-                        core.TimeSeriesDataset(TimeSeriesIndex).TimeSeries(i).Enabled = True
+                         eTimeSeriesType.TotalMortality, _
+                         eTimeSeriesType.Catches, _
+                         eTimeSeriesType.AverageWeight
+                        ts.Enabled = True
                     Case eTimeSeriesType.BiomassAbs
-                        If Parameters.EnableAbsoluteBiomass Then
-                            core.TimeSeriesDataset(TimeSeriesIndex).TimeSeries(i).Enabled = True
-                        End If
+                        ts.Enabled = Parameters.EnableAbsoluteBiomass
+                    Case Else
+                        ts.Enabled = False
                 End Select
-
             Next
-
-            'Apply the enabled time series
-            core.UpdateTimeSeries(False)
-
-            bSuccess = True
-
         Else ' Else Fishing
-
             'Go through each time series of the time series dataset
-            Dim TimeSeries As cTimeSeriesDataset = core.TimeSeriesDataset(TimeSeriesIndex)
-            For i As Integer = 1 To core.TimeSeriesDataset(TimeSeriesIndex).nTimeSeries
-
+            For i As Integer = 1 To dataset.nTimeSeries
                 'Enable Time Series
-                core.TimeSeriesDataset(TimeSeriesIndex).TimeSeries(i).Enabled = True
-
+                Dim ts As cTimeSeries = dataset.TimeSeries(i)
+                ts.Enabled = True
             Next
-
-            'Apply the enabled time series
-            core.UpdateTimeSeries(False)
-
-            bSuccess = True
-
         End If
 
-        Return bSuccess
+        'Apply the enabled time series
+        m_core.UpdateTimeSeries(False)
+
+        Return True
 
     End Function
 
@@ -254,7 +235,7 @@ Public MustInherit Class cSFPGenericIterations
     ''' -----------------------------------------------------------------------
     Protected Function ResetVs() As Boolean
         ' Suppress prompt, just reset the vulnerabilities without asking
-        Return core.CheckResetDefaultVulnerabilities(True)
+        Return m_core.CheckResetDefaultVulnerabilities(True)
     End Function
 
     ''' -----------------------------------------------------------------------
@@ -268,22 +249,22 @@ Public MustInherit Class cSFPGenericIterations
         Dim BSuccess As Boolean = False
 
         'Set the number of blocks selected to Max K
-        F2TSManager.nBlockCodes = Parameters.MaxKValue
+        m_F2TSManager.nBlockCodes = Parameters.MaxKValue
 
         'If PredOrPredPreySSToV = true then run SS2VBy Predator
-        If PredOrPredPreySSToV Then
-            If F2TSManager.RunSensitivitySS2VByPredator(TriState.False) Then
-                Debug.Assert(Not F2TSManager.IsRunning)
+        If m_bPredOrPredPreySSToV Then
+            If m_F2TSManager.RunSensitivitySS2VByPredator(TriState.False) Then
+                Debug.Assert(Not m_F2TSManager.IsRunning)
                 'Set vulnerabiltiy blocks
-                F2TSManager.setNBlocksFromSensitivity(Parameters.MaxKValue)
+                m_F2TSManager.setNBlocksFromSensitivity(Parameters.MaxKValue)
                 BSuccess = True
             End If
             'Else run SS2VBy Pred/Prey
         Else
-            If F2TSManager.RunSensitivitySS2VByPredPrey(TriState.False) Then
-                Debug.Assert(Not F2TSManager.IsRunning)
+            If m_F2TSManager.RunSensitivitySS2VByPredPrey(TriState.False) Then
+                Debug.Assert(Not m_F2TSManager.IsRunning)
                 'Set vulnerabiltiy blocks
-                F2TSManager.setNBlocksFromSensitivity(Parameters.MaxKValue)
+                m_F2TSManager.setNBlocksFromSensitivity(Parameters.MaxKValue)
                 BSuccess = True
             End If
         End If
@@ -299,7 +280,7 @@ Public MustInherit Class cSFPGenericIterations
     ''' <returns>True if a run started successfully.</returns>
     ''' -----------------------------------------------------------------------
     Protected Function RunEcosim() As Boolean
-        Return core.RunEcoSim()
+        Return m_core.RunEcoSim()
     End Function
 
     ''' -----------------------------------------------------------------------
@@ -313,18 +294,18 @@ Public MustInherit Class cSFPGenericIterations
         Dim bSuccess As Boolean = False
 
         'Has a sensitivity search been ran
-        If F2TSManager.HasRunSens Then
+        If m_F2TSManager.HasRunSens Then
 
             'Setup manager to do a vunerability search
-            F2TSManager.VulnerabilitySearch = True
-            F2TSManager.AnomalySearch = False
-            F2TSManager.VulnerabilityVariance = 10.0
+            m_F2TSManager.VulnerabilitySearch = True
+            m_F2TSManager.AnomalySearch = False
+            m_F2TSManager.VulnerabilityVariance = 10.0
             'Set the number of blocks selected (Number of parameters to estimate)
-            F2TSManager.nBlockCodes = EstimatedV
+            m_F2TSManager.nBlockCodes = EstimatedV
 
             ' Run the search silently
-            If F2TSManager.RunSearch(True, TriState.False) Then
-                Debug.Assert(Not F2TSManager.IsRunning)
+            If m_F2TSManager.RunSearch(True, TriState.False) Then
+                Debug.Assert(Not m_F2TSManager.IsRunning)
                 bSuccess = True
             End If
         End If
@@ -383,18 +364,18 @@ Public MustInherit Class cSFPGenericIterations
         If (shape IsNot Nothing) Then
 
             'Setup manager to do a Anomaly search
-            F2TSManager.AnomalySearch = True
-            F2TSManager.VulnerabilitySearch = False
-            F2TSManager.FirstYear = 1
-            F2TSManager.LastYear = core.TimeSeriesDataset(TimeSeriesIndex).NumPoints
-            F2TSManager.PPVariance = 0.1
+            m_F2TSManager.AnomalySearch = True
+            m_F2TSManager.VulnerabilitySearch = False
+            m_F2TSManager.FirstYear = 1
+            m_F2TSManager.LastYear = m_core.TimeSeriesDataset(m_iTimeSeries).NumPoints
+            m_F2TSManager.PPVariance = 0.1
             'Set the number of spline points selected (Number of parameters to estimate)
-            F2TSManager.NumSplinePoints = SplinePoints
-            F2TSManager.AnomalySearchShapeNumber = shape.Index
+            m_F2TSManager.NumSplinePoints = SplinePoints
+            m_F2TSManager.AnomalySearchShapeNumber = shape.Index
 
             ' Run the search silently
-            If F2TSManager.RunSearch(True, TriState.False) Then
-                Debug.Assert(Not Me.F2TSManager.IsRunning)
+            If m_F2TSManager.RunSearch(True, TriState.False) Then
+                Debug.Assert(Not Me.m_F2TSManager.IsRunning)
                 bSuccess = True
             End If
         End If
@@ -415,26 +396,26 @@ Public MustInherit Class cSFPGenericIterations
         Dim bSuccess As Boolean = False
 
         'If there is an applied shape and a sensitivity search has been ran : run the search
-        If (shape IsNot Nothing) And F2TSManager.HasRunSens Then
+        If (shape IsNot Nothing) And m_F2TSManager.HasRunSens Then
 
             'Setup manager to do a Vulnerability and Anomaly search
-            F2TSManager.AnomalySearch = True
-            F2TSManager.FirstYear = 1
-            F2TSManager.LastYear = core.TimeSeriesDataset(TimeSeriesIndex).NumPoints
-            F2TSManager.PPVariance = 0.1
+            m_F2TSManager.AnomalySearch = True
+            m_F2TSManager.FirstYear = 1
+            m_F2TSManager.LastYear = m_core.TimeSeriesDataset(m_iTimeSeries).NumPoints
+            m_F2TSManager.PPVariance = 0.1
             'Set the number of spline points selected (Number of parameters to estimate)
-            F2TSManager.NumSplinePoints = SplinePoints
-            F2TSManager.AnomalySearchShapeNumber = shape.Index
+            m_F2TSManager.NumSplinePoints = SplinePoints
+            m_F2TSManager.AnomalySearchShapeNumber = shape.Index
 
-            F2TSManager.VulnerabilitySearch = True
+            m_F2TSManager.VulnerabilitySearch = True
             'Set the number of blocks selected (Number of parameters to estimate)
-            F2TSManager.nBlockCodes = EstimatedV
-            F2TSManager.VulnerabilityVariance = 10.0
+            m_F2TSManager.nBlockCodes = EstimatedV
+            m_F2TSManager.VulnerabilityVariance = 10.0
 
 
             ' Run the search silently
-            If F2TSManager.RunSearch(True, TriState.False) Then
-                Debug.Assert(Not F2TSManager.IsRunning)
+            If m_F2TSManager.RunSearch(True, TriState.False) Then
+                Debug.Assert(Not m_F2TSManager.IsRunning)
                 bSuccess = True
             End If
         End If
@@ -448,7 +429,7 @@ Public MustInherit Class cSFPGenericIterations
     ''' -----------------------------------------------------------------------
     Public Sub Clear() _
        Implements ISFPIterations.Clear
-        F2TSManager.Disconnect()
+        m_F2TSManager.Disconnect()
     End Sub
 
     ''' -----------------------------------------------------------------------
@@ -655,12 +636,12 @@ Public MustInherit Class cSFPGenericIterations
     Private Function GetSS() As Single
 
         ' Sanity checks
-        Debug.Assert(Me.core IsNot Nothing)
+        Debug.Assert(Me.m_core IsNot Nothing)
 
         If (EstimatedV = 0 And SplinePoints = 0) Then
-            Return core.EcosimStats.SS
+            Return m_core.EcosimStats.SS
         Else
-            Dim res As cSearchResults = DirectCast(F2TSManager.Results, cSearchResults)
+            Dim res As cSearchResults = DirectCast(m_F2TSManager.Results, cSearchResults)
             Return res.IterSS
         End If
     End Function
@@ -672,29 +653,24 @@ Public MustInherit Class cSFPGenericIterations
     ''' -----------------------------------------------------------------------
     Private Function GetAIC() As Single
 
-        Debug.Assert(Me.core IsNot Nothing)
+        Debug.Assert(Me.m_core IsNot Nothing)
         Debug.Assert(Me.Parameters IsNot Nothing)
 
         Dim nData As Integer = Parameters.NumberOfObservationsValue
-        Dim answer As Single = 0
 
         'If simple run
         If (EstimatedV = 0 And SplinePoints = 0) Then
-            answer = F2TSManager.getAIC(0, nData, GetSS)
-            Return answer
+            Return Me.m_F2TSManager.getAIC(0, nData, GetSS)
         End If
         'If Vunerability Search
         If (EstimatedV > 0 And SplinePoints = 0) Then
-            answer = F2TSManager.getAIC(EstimatedV, nData, GetSS)
-            Return answer
+            Return m_F2TSManager.getAIC(EstimatedV, nData, GetSS)
         End If
         'If Anomaly Search
         If (EstimatedV = 0 And SplinePoints > 0) Then
-            answer = F2TSManager.getAIC(SplinePoints, nData, GetSS)
-            Return answer
+            Return m_F2TSManager.getAIC(SplinePoints, nData, GetSS)
         Else 'V and A Search
-            answer = F2TSManager.getAIC(k, nData, GetSS)
-            Return answer
+            Return m_F2TSManager.getAIC(k, nData, GetSS)
         End If
     End Function
 
@@ -705,7 +681,7 @@ Public MustInherit Class cSFPGenericIterations
     ''' -----------------------------------------------------------------------
     Private Function GetAICc() As Single
 
-        Debug.Assert(Me.core IsNot Nothing)
+        Debug.Assert(Me.m_core IsNot Nothing)
         Debug.Assert(Me.Parameters IsNot Nothing)
 
         Dim nData As Integer = Parameters.NumberOfObservationsValue
@@ -734,8 +710,8 @@ Public MustInherit Class cSFPGenericIterations
 
     Private Sub GetTimeSeriesSS()
 
-        For i As Integer = 1 To Me.core.nTimeSeries
-            m_timeseriesSS(i) = core.TimeSeriesDataset(TimeSeriesIndex).TimeSeries(i).DataSS
+        For i As Integer = 1 To Me.m_core.nTimeSeries
+            m_timeseriesSS(i) = m_core.TimeSeriesDataset(m_iTimeSeries).TimeSeries(i).DataSS
         Next
 
     End Sub
