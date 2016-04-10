02961f13bb3889035842e6926f63d353241770f4
JeroenS <jeroen.steenbeek@gmail.com>
2016-04-10 19:30:57 +0000

    Updated license headers Fixed examples to trunk
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@13554 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Examples/3. Plugins/3. BaseUserInterfacePlugin/BaseUserInterfacePlugin.vb
+++ Examples/3. Plugins/3. BaseUserInterfacePlugin/BaseUserInterfacePlugin.vb
@@ -12,7 +12,9 @@
 ' You should have received a copy of the GNU General Public License along with EwE.
 ' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
 '
-' Copyright 1991-2012 UBC Fisheries Centre, Vancouver BC, Canada.
+' Copyright 1991- 
+'    UBC Fisheries Centre, Vancouver BC, Canada, and 
+'    Ecopath International Initiative, Barcelona, Spain
 ' ===============================================================================
 '
 
