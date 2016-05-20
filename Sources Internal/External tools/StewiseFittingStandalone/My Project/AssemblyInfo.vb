90166d160d6e6fe13bc817013ebf6402292bacd9
JeroenS <jeroen.steenbeek@gmail.com>
2016-05-20 13:11:00 +0000

    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@13855 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources Internal/External tools/StewiseFittingStandalone/My Project/AssemblyInfo.vb
@@ -0,0 +1,60 @@
+﻿' ===============================================================================
+' This file is part of Ecopath with Ecosim (EwE)
+'
+' EwE is free software: you can redistribute it and/or modify it under the terms
+' of the GNU General Public License version 2 as published by the Free Software 
+' Foundation.
+'
+' EwE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+' without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+' PURPOSE. See the GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License along with EwE.
+' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
+'
+' Copyright 1991- 
+'    UBC Centre for the Oceans and Fisheries, Vancouver BC, Canada, and 
+'    Ecopath International Initiative, Barcelona, Spain
+'
+' Stepwise Fitting Procedure by Sheila Heymans, Erin Scott, Jeroen Steenbeek
+' Copyright 2015- Scottish Association for Marine Science, Oban, Scotland
+'
+' Erin Scott was funded by the Scottish Informatics and Computer Science
+' Alliance (SICSA) Postgraduate Industry Internship Programme.
+' ===============================================================================
+'
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("StewiseFittingStandalone")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("StewiseFittingStandalone")> 
+<Assembly: AssemblyCopyright("Copyright ©  2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("febb6f7c-526e-4e0f-a25b-c1abd82e4128")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ Sources Internal/External tools/StewiseFittingStandalone/modStart.vb
@@ -0,0 +1,53 @@
+﻿' ===============================================================================
+' This file is part of Ecopath with Ecosim (EwE)
+'
+' EwE is free software: you can redistribute it and/or modify it under the terms
+' of the GNU General Public License version 2 as published by the Free Software 
+' Foundation.
+'
+' EwE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+' without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+' PURPOSE. See the GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License along with EwE.
+' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
+'
+' Copyright 1991- 
+'    UBC Centre for the Oceans and Fisheries, Vancouver BC, Canada, and 
+'    Ecopath International Initiative, Barcelona, Spain
+'
+' Stepwise Fitting Procedure by Sheila Heymans, Erin Scott, Jeroen Steenbeek
+' Copyright 2015- Scottish Association for Marine Science, Oban, Scotland
+'
+' Erin Scott was funded by the Scottish Informatics and Computer Science
+' Alliance (SICSA) Postgraduate Industry Internship Programme.
+' ===============================================================================
+'
+#Region " Imports "
+
+Option Strict On
+Imports EwECore
+Imports EwEStepwiseFittingPlugin
+Imports ScientificInterfaceShared.Controls
+Imports System.Windows.Forms
+
+#End Region ' Imports
+
+Module modStart
+
+    '<STAThread()> _
+    Sub Main()
+
+        Application.EnableVisualStyles()
+
+        Dim engine As New cSFPManager()
+        Dim core As cCore = engine.Core
+        Dim sg As New ScientificInterfaceShared.Style.cStyleGuide(core)
+        Dim uic As New cUIContext(core, sg)
+        Dim frm As New frmRun(uic, engine)
+
+        Application.Run(frm)
+
+    End Sub
+
+End Module
