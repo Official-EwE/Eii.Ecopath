e14ad11dd1782e51a33be01851f1c748fa12e5e9
JeroenS <jeroen.steenbeek@gmail.com>
2016-09-30 22:02:16 +0000

    Spatial layer log now includes adapter name to aid troubleshooting
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@14260 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECore/Resources/CoreMessages.Designer.vb
+++ Sources/EwECore/Resources/CoreMessages.Designer.vb
@@ -2822,7 +2822,7 @@ Namespace My.Resources
         End Property
         
         '''<summary>
-        '''  Looks up a localized string similar to Accessing external data for &apos;{0}&apos; at time {1} ({2:yyyy/MM/dd}).
+        '''  Looks up a localized string similar to {0} loading external data into {1} at step {2} ({3:yyyy/MM}).
         '''</summary>
         Public Shared ReadOnly Property STATUS_SPATIALTEMPORAL_LOADING() As String
             Get
