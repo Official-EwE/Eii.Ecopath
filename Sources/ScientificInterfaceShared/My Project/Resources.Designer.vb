5ecc75279fdaa438edc31cd9a960a675f9c8cfae
JeroenS <jeroen.steenbeek@gmail.com>
2016-10-04 12:42:35 +0000

    More pictures
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@14268 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterfaceShared/My Project/Resources.Designer.vb
+++ Sources/ScientificInterfaceShared/My Project/Resources.Designer.vb
@@ -7107,6 +7107,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Public ReadOnly Property ImportHS() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("ImportHS", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Public ReadOnly Property ImportXMLHS() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("ImportXMLHS", resourceCulture)
