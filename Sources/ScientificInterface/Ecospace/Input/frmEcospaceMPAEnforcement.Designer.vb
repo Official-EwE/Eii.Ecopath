6bff537e45980a42506de2cf747f8e5fd7d5b982
JeroenS <jeroen.steenbeek@gmail.com>
2016-10-31 21:33:08 +0000

    After lengthy discussions about Ecospace, MPA closures and MPA open for fishing, I've provided a concept for a change
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@14364 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/ScientificInterface/Ecospace/Input/frmEcospaceMPAEnforcement.Designer.vb
@@ -0,0 +1,119 @@
+﻿Namespace Ecospace
+
+    Partial Class frmEcospaceMPAEnforcement
+        Inherits frmEwEGrid
+
+        'Form overrides dispose to clean up the component list.
+        <System.Diagnostics.DebuggerNonUserCode()>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            Try
+                If disposing AndAlso components IsNot Nothing Then
+                    components.Dispose()
+                End If
+            Finally
+                MyBase.Dispose(disposing)
+            End Try
+        End Sub
+
+        'Required by the Windows Form Designer
+        Private components As System.ComponentModel.IContainer
+
+        'NOTE: The following procedure is required by the Windows Form Designer
+        'It can be modified using the Windows Form Designer.  
+        'Do not modify it using the code editor.
+        <System.Diagnostics.DebuggerStepThrough()>
+        Private Sub InitializeComponent()
+            Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmEcospaceMPAEnforcement))
+            Me.m_tsMain = New ScientificInterfaceShared.Controls.cEwEToolstrip()
+            Me.m_tsbnDefineMPAs = New System.Windows.Forms.ToolStripButton()
+            Me.m_tlpContent = New System.Windows.Forms.TableLayoutPanel()
+            Me.m_lblInfo = New System.Windows.Forms.Label()
+            Me.m_grid = New ScientificInterface.Ecospace.gridEcospaceMPAEnforcement()
+            Me.m_tsMain.SuspendLayout()
+            Me.m_tlpContent.SuspendLayout()
+            Me.SuspendLayout()
+            '
+            'm_tsMain
+            '
+            resources.ApplyResources(Me.m_tsMain, "m_tsMain")
+            Me.m_tsMain.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden
+            Me.m_tsMain.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.m_tsbnDefineMPAs})
+            Me.m_tsMain.Name = "m_tsMain"
+            Me.m_tsMain.RenderMode = System.Windows.Forms.ToolStripRenderMode.System
+            '
+            'm_tsbnDefineMPAs
+            '
+            resources.ApplyResources(Me.m_tsbnDefineMPAs, "m_tsbnDefineMPAs")
+            Me.m_tsbnDefineMPAs.Name = "m_tsbnDefineMPAs"
+            '
+            'm_tlpContent
+            '
+            resources.ApplyResources(Me.m_tlpContent, "m_tlpContent")
+            Me.m_tlpContent.Controls.Add(Me.m_tsMain, 0, 0)
+            Me.m_tlpContent.Controls.Add(Me.m_grid, 0, 2)
+            Me.m_tlpContent.Controls.Add(Me.m_lblInfo, 0, 1)
+            Me.m_tlpContent.Name = "m_tlpContent"
+            '
+            'm_lblInfo
+            '
+            resources.ApplyResources(Me.m_lblInfo, "m_lblInfo")
+            Me.m_lblInfo.Name = "m_lblInfo"
+            '
+            'm_grid
+            '
+            Me.m_grid.AllowBlockSelect = True
+            Me.m_grid.AutoSizeMinHeight = 10
+            Me.m_grid.AutoSizeMinWidth = 10
+            Me.m_grid.AutoStretchColumnsToFitWidth = False
+            Me.m_grid.AutoStretchRowsToFitHeight = False
+            Me.m_grid.BackColor = System.Drawing.Color.White
+            Me.m_grid.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+            Me.m_grid.ContextMenuStyle = CType((((SourceGrid2.ContextMenuStyle.ColumnResize Or SourceGrid2.ContextMenuStyle.AutoSize) _
+            Or SourceGrid2.ContextMenuStyle.CopyPasteSelection) _
+            Or SourceGrid2.ContextMenuStyle.CellContextMenu), SourceGrid2.ContextMenuStyle)
+            Me.m_grid.CustomSort = False
+            Me.m_grid.DataName = "grid content"
+            resources.ApplyResources(Me.m_grid, "m_grid")
+            Me.m_grid.FixedColumnWidths = False
+            Me.m_grid.FocusStyle = SourceGrid2.FocusStyle.None
+            Me.m_grid.GridToolTipActive = True
+            Me.m_grid.IsLayoutSuspended = False
+            Me.m_grid.IsOutputGrid = False
+            Me.m_grid.Name = "m_grid"
+            Me.m_grid.SpecialKeys = CType((((((((((SourceGrid2.GridSpecialKeys.Ctrl_C Or SourceGrid2.GridSpecialKeys.Ctrl_V) _
+            Or SourceGrid2.GridSpecialKeys.Ctrl_X) _
+            Or SourceGrid2.GridSpecialKeys.Delete) _
+            Or SourceGrid2.GridSpecialKeys.Arrows) _
+            Or SourceGrid2.GridSpecialKeys.Tab) _
+            Or SourceGrid2.GridSpecialKeys.PageDownUp) _
+            Or SourceGrid2.GridSpecialKeys.Enter) _
+            Or SourceGrid2.GridSpecialKeys.Escape) _
+            Or SourceGrid2.GridSpecialKeys.Backspace), SourceGrid2.GridSpecialKeys)
+            Me.m_grid.UIContext = Nothing
+            '
+            'frmEcospaceMPAEnforcement
+            '
+            resources.ApplyResources(Me, "$this")
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+            Me.ControlBox = False
+            Me.Controls.Add(Me.m_tlpContent)
+            Me.Name = "frmEcospaceMPAEnforcement"
+            Me.ShowInTaskbar = False
+            Me.TabText = ""
+            Me.m_tsMain.ResumeLayout(False)
+            Me.m_tsMain.PerformLayout()
+            Me.m_tlpContent.ResumeLayout(False)
+            Me.m_tlpContent.PerformLayout()
+            Me.ResumeLayout(False)
+
+        End Sub
+
+        Private WithEvents m_tsbnDefineMPAs As ToolStripButton
+        Private WithEvents m_tlpContent As TableLayoutPanel
+        Private WithEvents m_tsMain As cEwEToolstrip
+        Private WithEvents m_lblInfo As Label
+        Private WithEvents m_grid As gridEcospaceMPAEnforcement
+
+    End Class
+
+End Namespace
---
+++ Sources/ScientificInterface/Ecospace/Input/frmEcospaceMPAEnforcement.vb
@@ -0,0 +1,59 @@
+﻿' ===============================================================================
+' This file is part of Ecopath with Ecosim (EwE)
+'
+' EwE is free software: you can redistribute it and/or modify it under the terms
+' of the GNU General Public License version 2 as published by the Free Software 
+' Foundation.
+'
+' EwE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+' without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+' PURPOSE. See the GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License along with EwE.
+' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
+'
+' Copyright 1991- 
+'    UBC Fisheries Centre, Vancouver BC, Canada, and 
+'    Ecopath International Initiative, Barcelona, Spain
+' ===============================================================================
+'
+
+#Region " Imports "
+
+Option Strict On
+Imports SharedResources = ScientificInterfaceShared.My.Resources
+
+#End Region ' Imports 
+
+Namespace Ecospace
+
+    Public Class frmEcospaceMPAEnforcement
+
+        Public Sub New()
+            MyBase.New()
+            Me.InitializeComponent()
+            Me.Grid = Me.m_grid
+        End Sub
+
+        Protected Overrides Sub OnLoad(e As EventArgs)
+
+            Dim cmd As cCommand = Nothing
+            MyBase.OnLoad(e)
+
+            If (Me.CommandHandler Is Nothing) Then Return
+
+            cmd = Me.CommandHandler.GetCommand("EditMPAs")
+            If (cmd IsNot Nothing) Then cmd.AddControl(Me.m_tsbnDefineMPAs)
+
+            Me.m_tsbnDefineMPAs.Image = SharedResources.MPA
+            Me.m_lblInfo.Image = SharedResources.Info
+
+        End Sub
+
+        Private Sub m_lblInfo_Click(sender As Object, e As EventArgs) Handles m_lblInfo.Click
+            Me.m_lblInfo.Visible = False
+        End Sub
+
+    End Class
+
+End Namespace
