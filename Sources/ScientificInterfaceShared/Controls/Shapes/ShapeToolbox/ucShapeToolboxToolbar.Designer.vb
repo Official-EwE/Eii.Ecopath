d8a2ce7f79d0647a8fb36c6119213588ce358bef
JeroenS <jeroen.steenbeek@gmail.com>
2016-11-01 19:39:05 +0000

    Cleaned up
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@14376 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterfaceShared/Controls/Shapes/ShapeToolbox/ucShapeToolboxToolbar.Designer.vb
+++ Sources/ScientificInterfaceShared/Controls/Shapes/ShapeToolbox/ucShapeToolboxToolbar.Designer.vb
@@ -73,191 +73,142 @@ Namespace Controls
             '
             Me.m_ts.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden
             Me.m_ts.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.m_tsbImport, Me.m_tsbExport, Me.m_tsbLoad, Me.m_tsbWeight, Me.m_ts1, Me.m_tsbAdd, Me.m_tsbRemove, Me.m_tsbDuplicate, Me.m_ts2, Me.m_tsbSetTo0, Me.m_tsbSetToBaseline, Me.m_tsbSetToValue, Me.m_tsbResetAll, Me.m_tscmbFilter, Me.m_ts3, Me.m_tsbnShowExtraData, Me.m_tsbnDiscardExtraData, Me.m_tsbnFilterCase, Me.m_tstbxFilterName, Me.m_tslFilter})
-            Me.m_ts.Location = New System.Drawing.Point(0, 0)
+            resources.ApplyResources(Me.m_ts, "m_ts")
             Me.m_ts.Name = "m_ts"
             Me.m_ts.RenderMode = System.Windows.Forms.ToolStripRenderMode.System
-            Me.m_ts.Size = New System.Drawing.Size(1271, 25)
-            Me.m_ts.TabIndex = 0
             '
             'm_tsbImport
             '
             Me.m_tsbImport.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
-            Me.m_tsbImport.Image = CType(resources.GetObject("m_tsbImport.Image"), System.Drawing.Image)
-            Me.m_tsbImport.ImageTransparentColor = System.Drawing.Color.Magenta
+            resources.ApplyResources(Me.m_tsbImport, "m_tsbImport")
             Me.m_tsbImport.Name = "m_tsbImport"
-            Me.m_tsbImport.Size = New System.Drawing.Size(56, 22)
-            Me.m_tsbImport.Text = "&Import..."
             '
             'm_tsbExport
             '
             Me.m_tsbExport.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
-            Me.m_tsbExport.Image = CType(resources.GetObject("m_tsbExport.Image"), System.Drawing.Image)
-            Me.m_tsbExport.ImageTransparentColor = System.Drawing.Color.Magenta
+            resources.ApplyResources(Me.m_tsbExport, "m_tsbExport")
             Me.m_tsbExport.Name = "m_tsbExport"
-            Me.m_tsbExport.Size = New System.Drawing.Size(53, 22)
-            Me.m_tsbExport.Text = "E&xport..."
             '
             'm_tsbLoad
             '
             Me.m_tsbLoad.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
-            Me.m_tsbLoad.Image = CType(resources.GetObject("m_tsbLoad.Image"), System.Drawing.Image)
-            Me.m_tsbLoad.ImageTransparentColor = System.Drawing.Color.Magenta
+            resources.ApplyResources(Me.m_tsbLoad, "m_tsbLoad")
             Me.m_tsbLoad.Name = "m_tsbLoad"
-            Me.m_tsbLoad.Size = New System.Drawing.Size(46, 22)
-            Me.m_tsbLoad.Text = "&Load..."
             '
             'm_tsbWeight
             '
             Me.m_tsbWeight.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
-            Me.m_tsbWeight.Image = CType(resources.GetObject("m_tsbWeight.Image"), System.Drawing.Image)
-            Me.m_tsbWeight.ImageTransparentColor = System.Drawing.Color.Magenta
+            resources.ApplyResources(Me.m_tsbWeight, "m_tsbWeight")
             Me.m_tsbWeight.Name = "m_tsbWeight"
-            Me.m_tsbWeight.Size = New System.Drawing.Size(58, 22)
-            Me.m_tsbWeight.Text = "&Weight..."
             '
             'm_ts1
             '
             Me.m_ts1.Name = "m_ts1"
-            Me.m_ts1.Size = New System.Drawing.Size(6, 25)
+            resources.ApplyResources(Me.m_ts1, "m_ts1")
             '
             'm_tsbAdd
             '
             Me.m_tsbAdd.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
-            Me.m_tsbAdd.Image = CType(resources.GetObject("m_tsbAdd.Image"), System.Drawing.Image)
-            Me.m_tsbAdd.ImageTransparentColor = System.Drawing.Color.Magenta
+            resources.ApplyResources(Me.m_tsbAdd, "m_tsbAdd")
             Me.m_tsbAdd.Name = "m_tsbAdd"
-            Me.m_tsbAdd.Size = New System.Drawing.Size(42, 22)
-            Me.m_tsbAdd.Text = "&Add..."
             '
             'm_tsbRemove
             '
             Me.m_tsbRemove.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
-            Me.m_tsbRemove.Image = CType(resources.GetObject("m_tsbRemove.Image"), System.Drawing.Image)
-            Me.m_tsbRemove.ImageTransparentColor = System.Drawing.Color.Magenta
+            resources.ApplyResources(Me.m_tsbRemove, "m_tsbRemove")
             Me.m_tsbRemove.Name = "m_tsbRemove"
-            Me.m_tsbRemove.Size = New System.Drawing.Size(63, 22)
-            Me.m_tsbRemove.Text = "&Remove..."
             '
             'm_tsbDuplicate
             '
             Me.m_tsbDuplicate.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
-            Me.m_tsbDuplicate.Image = CType(resources.GetObject("m_tsbDuplicate.Image"), System.Drawing.Image)
-            Me.m_tsbDuplicate.ImageTransparentColor = System.Drawing.Color.Magenta
+            resources.ApplyResources(Me.m_tsbDuplicate, "m_tsbDuplicate")
             Me.m_tsbDuplicate.Name = "m_tsbDuplicate"
-            Me.m_tsbDuplicate.Size = New System.Drawing.Size(61, 22)
-            Me.m_tsbDuplicate.Text = "&Duplicate"
             '
             'm_ts2
             '
             Me.m_ts2.Name = "m_ts2"
-            Me.m_ts2.Size = New System.Drawing.Size(6, 25)
+            resources.ApplyResources(Me.m_ts2, "m_ts2")
             '
             'm_tsbSetTo0
             '
             Me.m_tsbSetTo0.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
-            Me.m_tsbSetTo0.Image = CType(resources.GetObject("m_tsbSetTo0.Image"), System.Drawing.Image)
-            Me.m_tsbSetTo0.ImageTransparentColor = System.Drawing.Color.Magenta
+            resources.ApplyResources(Me.m_tsbSetTo0, "m_tsbSetTo0")
             Me.m_tsbSetTo0.Name = "m_tsbSetTo0"
-            Me.m_tsbSetTo0.Size = New System.Drawing.Size(50, 22)
-            Me.m_tsbSetTo0.Text = "&Set to 0"
             '
             'm_tsbSetToBaseline
             '
             Me.m_tsbSetToBaseline.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
-            Me.m_tsbSetToBaseline.Image = CType(resources.GetObject("m_tsbSetToBaseline.Image"), System.Drawing.Image)
-            Me.m_tsbSetToBaseline.ImageTransparentColor = System.Drawing.Color.Magenta
+            resources.ApplyResources(Me.m_tsbSetToBaseline, "m_tsbSetToBaseline")
             Me.m_tsbSetToBaseline.Name = "m_tsbSetToBaseline"
-            Me.m_tsbSetToBaseline.Size = New System.Drawing.Size(87, 22)
-            Me.m_tsbSetToBaseline.Text = "Set to &baseline"
             '
             'm_tsbSetToValue
             '
             Me.m_tsbSetToValue.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
-            Me.m_tsbSetToValue.Image = CType(resources.GetObject("m_tsbSetToValue.Image"), System.Drawing.Image)
-            Me.m_tsbSetToValue.ImageTransparentColor = System.Drawing.Color.Magenta
+            resources.ApplyResources(Me.m_tsbSetToValue, "m_tsbSetToValue")
             Me.m_tsbSetToValue.Name = "m_tsbSetToValue"
-            Me.m_tsbSetToValue.Size = New System.Drawing.Size(81, 22)
-            Me.m_tsbSetToValue.Text = "Set to &value..."
             '
             'm_tsbResetAll
             '
             Me.m_tsbResetAll.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
-            Me.m_tsbResetAll.Image = CType(resources.GetObject("m_tsbResetAll.Image"), System.Drawing.Image)
-            Me.m_tsbResetAll.ImageTransparentColor = System.Drawing.Color.Magenta
+            resources.ApplyResources(Me.m_tsbResetAll, "m_tsbResetAll")
             Me.m_tsbResetAll.Name = "m_tsbResetAll"
-            Me.m_tsbResetAll.Size = New System.Drawing.Size(56, 22)
-            Me.m_tsbResetAll.Text = "Reset &All"
             '
             'm_tscmbFilter
             '
             Me.m_tscmbFilter.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
             Me.m_tscmbFilter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
             Me.m_tscmbFilter.DropDownWidth = 200
-            Me.m_tscmbFilter.MaxDropDownItems = 12
+            resources.ApplyResources(Me.m_tscmbFilter, "m_tscmbFilter")
             Me.m_tscmbFilter.Name = "m_tscmbFilter"
-            Me.m_tscmbFilter.Size = New System.Drawing.Size(150, 25)
             Me.m_tscmbFilter.Sorted = True
             '
             'm_ts3
             '
             Me.m_ts3.Name = "m_ts3"
-            Me.m_ts3.Size = New System.Drawing.Size(6, 25)
+            resources.ApplyResources(Me.m_ts3, "m_ts3")
             '
             'm_tsbnShowExtraData
             '
             Me.m_tsbnShowExtraData.CheckOnClick = True
             Me.m_tsbnShowExtraData.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
-            Me.m_tsbnShowExtraData.Image = CType(resources.GetObject("m_tsbnShowExtraData.Image"), System.Drawing.Image)
-            Me.m_tsbnShowExtraData.ImageTransparentColor = System.Drawing.Color.Magenta
+            resources.ApplyResources(Me.m_tsbnShowExtraData, "m_tsbnShowExtraData")
             Me.m_tsbnShowExtraData.Name = "m_tsbnShowExtraData"
-            Me.m_tsbnShowExtraData.Size = New System.Drawing.Size(94, 22)
-            Me.m_tsbnShowExtraData.Text = "Show extra &data"
             '
             'm_tsbnDiscardExtraData
             '
             Me.m_tsbnDiscardExtraData.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
-            Me.m_tsbnDiscardExtraData.Image = CType(resources.GetObject("m_tsbnDiscardExtraData.Image"), System.Drawing.Image)
-            Me.m_tsbnDiscardExtraData.ImageTransparentColor = System.Drawing.Color.Magenta
+            resources.ApplyResources(Me.m_tsbnDiscardExtraData, "m_tsbnDiscardExtraData")
             Me.m_tsbnDiscardExtraData.Name = "m_tsbnDiscardExtraData"
-            Me.m_tsbnDiscardExtraData.Size = New System.Drawing.Size(104, 22)
-            Me.m_tsbnDiscardExtraData.Text = "Discard e&xtra data"
             '
             'm_tsbnFilterCase
             '
             Me.m_tsbnFilterCase.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
             Me.m_tsbnFilterCase.CheckOnClick = True
             Me.m_tsbnFilterCase.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
-            Me.m_tsbnFilterCase.ImageTransparentColor = System.Drawing.Color.Magenta
+            resources.ApplyResources(Me.m_tsbnFilterCase, "m_tsbnFilterCase")
             Me.m_tsbnFilterCase.Name = "m_tsbnFilterCase"
             Me.m_tsbnFilterCase.Overflow = System.Windows.Forms.ToolStripItemOverflow.Never
-            Me.m_tsbnFilterCase.Size = New System.Drawing.Size(25, 22)
-            Me.m_tsbnFilterCase.Text = "Aa"
-            Me.m_tsbnFilterCase.ToolTipText = "Name filter is case-sensitive"
             '
             'm_tstbxFilterName
             '
             Me.m_tstbxFilterName.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
             Me.m_tstbxFilterName.Name = "m_tstbxFilterName"
             Me.m_tstbxFilterName.Overflow = System.Windows.Forms.ToolStripItemOverflow.Never
-            Me.m_tstbxFilterName.Size = New System.Drawing.Size(100, 25)
-            Me.m_tstbxFilterName.ToolTipText = "Filter by name"
+            resources.ApplyResources(Me.m_tstbxFilterName, "m_tstbxFilterName")
             '
             'm_tslFilter
             '
             Me.m_tslFilter.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
             Me.m_tslFilter.Image = Global.ScientificInterfaceShared.My.Resources.Resources.FilterHS
             Me.m_tslFilter.Name = "m_tslFilter"
-            Me.m_tslFilter.Size = New System.Drawing.Size(26, 22)
-            Me.m_tslFilter.Text = ":"
+            resources.ApplyResources(Me.m_tslFilter, "m_tslFilter")
             '
             'ucShapeToolboxToolbar
             '
-            Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
+            resources.ApplyResources(Me, "$this")
             Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
-            Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
             Me.Controls.Add(Me.m_ts)
             Me.Name = "ucShapeToolboxToolbar"
-            Me.Size = New System.Drawing.Size(1271, 25)
             Me.m_ts.ResumeLayout(False)
             Me.m_ts.PerformLayout()
             Me.ResumeLayout(False)
