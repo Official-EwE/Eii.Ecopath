9288e29e437fd4773263e45ea120b43f219a6a18
JeroenS <jeroen.steenbeek@gmail.com>
2017-05-16 11:40:18 +0000

    Added a whack of XML comments
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@14673 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwEPlugin/Plugin templates/Ecospace/IEcospaceInitializedPlugin.vb
+++ Sources/EwEPlugin/Plugin templates/Ecospace/IEcospaceInitializedPlugin.vb
@@ -27,6 +27,14 @@
 Public Interface IEcospaceInitializedPlugin
     Inherits IPlugin
 
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Plug-in point that is called when Ecospace has loaded a new scenario, is
+    ''' initialized, and is ready to be used.
+    ''' </summary>
+    ''' <param name="EcospaceDatastructures">The ecospace datastructures that 
+    ''' just received new scenario data.</param>
+    ''' -----------------------------------------------------------------------
     Sub EcospaceInitialized(ByVal EcospaceDatastructures As Object)
 
 End Interface
