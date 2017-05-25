3d977a9fb272b499f4c022f593dd029ef8b5c9c7
JeroenS <jeroen.steenbeek@gmail.com>
2017-05-25 10:38:17 +0000

    MSE plugin moved to public plug-in location
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@14690 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/EwECustomPlugins/EwEMSEPlugin/HCR_Group.vb
@@ -0,0 +1,196 @@
+﻿' ===============================================================================
+' This file is part of Ecopath with Ecosim (EwE)
+'
+' EwE is free software: you can redistribute it and/or modify it under the terms
+' of the GNU General Public License version 2 as published by the Free Software 
+' Foundation.
+'
+' EwE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+' without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+' PURPOSE. See the GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License along with EwE.
+' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
+'
+' The Cefas MSE plug-in was developed by the Centre for Environment, Fisheries and 
+' Aquaculture Science (Cefas). 
+'
+' EwE copyright 1991- :
+'    UBC Institute for the Oceans and Fisheries, Vancouver BC, Canada, and 
+'    Ecopath International Initiative, Barcelona, Spain
+' Cefas MSE plug-in copyright: 
+'    2013- Cefas, Lowestoft, UK.
+' ===============================================================================
+'
+
+#Region " Imports "
+
+Option Strict On
+Imports System.Text
+Imports EwECore
+Imports EwEUtils.Utilities
+Imports ScientificInterfaceShared.Style
+
+#End Region ' Imports
+
+Public Enum HCRType
+    Target = 0
+    Conservation = 1
+End Enum
+
+Public Class cCostFunctionTypeFormatter
+    Implements ITypeFormatter
+
+    Public Function GetDescribedType() As System.Type Implements ITypeFormatter.GetDescribedType
+        Return GetType(HCRType)
+    End Function
+
+    Public Function GetDescriptor(value As Object, Optional descriptor As eDescriptorTypes = eDescriptorTypes.Name) As String _
+        Implements ITypeFormatter.GetDescriptor
+
+        Select Case DirectCast(value, HCRType)
+            Case HCRType.Target
+                Return My.Resources.COSTFUNCTION_TARGET
+            Case HCRType.Conservation
+                Return My.Resources.COSTFUNCTION_CONSERVATION
+        End Select
+        Return "?"
+    End Function
+
+End Class
+
+''' <summary>
+''' Harvest Control Rules and Strategies all need to be public so they can be accessed in the frmTFMpolicy interface.
+''' </summary>
+Public Class HCR_Group
+
+#Region "Private variables"
+    Private m_core As cCore
+    Private m_MSE As cMSE
+#End Region
+
+#Region "Public variables and Properties"
+
+    ''' <summary>
+    ''' Biomass group.
+    ''' </summary>
+    Public Property GroupB As cEcoPathGroupInput = Nothing
+
+    ''' <summary>
+    ''' Fishing mortality group.
+    ''' </summary>
+    Public Property GroupF As cEcoPathGroupInput = Nothing
+
+    Public Property LowerLimit As Single = cCore.NULL_VALUE
+    Public Property UpperLimit As Single = cCore.NULL_VALUE
+    Public Property MaxF As Single = cCore.NULL_VALUE
+
+    Public Property TypeOfHCR As HCRType = HCRType.Target
+
+    Public Property TimeFrameRule As cTimeFrameRule
+
+    Public Overrides Function ToString() As String
+
+        ' JS 01Oct13: StringBuilder is better at handling newlines on different OS-es
+        Dim sb As New StringBuilder()
+        Dim fmt As New cCoreInterfaceFormatter()
+        Dim fmtC As New cCostFunctionTypeFormatter()
+
+        sb.AppendLine(String.Format(My.Resources.HCR_GROUP_BIOMASS, fmt.GetDescriptor(Me.GroupB)))
+        sb.AppendLine(String.Format(My.Resources.HCR_GROUP_FISHMORT, fmt.GetDescriptor(Me.GroupF)))
+        sb.AppendLine(String.Format(My.Resources.HCR_GROUP_FUNCTION, fmtC.GetDescriptor(Me.TypeOfHCR)))
+
+        Return sb.ToString
+
+    End Function
+
+#End Region
+
+#Region "Construction"
+
+    Public Sub New(theCore As cCore, MSE As cMSE)
+        Me.m_core = theCore
+        Me.m_MSE = MSE
+        TimeFrameRule = New cTimeFrameRule(MSE.EcosimData, Me, MSE)
+    End Sub
+
+#End Region
+
+#Region "Public Methods"
+
+    ''' <summary>
+    ''' Validate the Harvest Control Rule against the core group indexes
+    ''' </summary>
+    ''' <returns>True if this rule is valid. False otherwise.</returns>
+    ''' <remarks></remarks>
+    Public Function isValid(ByRef ValidationString As String) As Boolean
+
+        ' ToDo_JS: Globalize this method
+        Dim sb As New StringBuilder()
+        Dim breturn As Boolean = True
+        Debug.Assert(Me.m_core IsNot Nothing, Me.ToString + ".isValid() cCore has not been set. Validation cannot be run.")
+
+        Try
+            If Not Me.isIndexInBounds(Me.GroupB) Then
+                breturn = False
+                sb.AppendLine("Biomass group number is not valid.")
+            End If
+
+            If Not Me.isIndexInBounds(Me.GroupF) Then
+                breturn = False
+                sb.AppendLine("Fishing Mortality group number is not valid.")
+            End If
+
+        Catch ex As Exception
+            breturn = False
+            Debug.Assert(False, Me.ToString + ".isValid() Exception: " + ex.Message)
+        End Try
+
+        ValidationString = sb.ToString()
+
+        Return breturn
+
+    End Function
+
+    Public Function CalcF(ByRef Biomass As Single(), ByRef iYearProjecting As Integer, ByVal iTimeStep As Integer) As Single
+
+        Dim HCR_F As Single = CalcFfromHCR(Biomass, iYearProjecting)
+
+        If TimeFrameRule.CheckValidRule(iYearProjecting, HCR_F, iTimeStep) And Me.TypeOfHCR = HCRType.Target Then 'Use a time frame rule
+
+#If DEBUG Then
+            Console.WriteLine("(HCR_Group.CalcFfromHCR) Model = " & m_MSE.CurrentModelID & "   Strategy = " & m_MSE.currentStrategy.Name & "   Group = " & Me.GroupF.Name)
+#End If
+
+            Return TimeFrameRule.F(iTimeStep, iYearProjecting, HCR_F)
+        Else
+            Return HCR_F
+        End If
+
+
+    End Function
+
+    Public Function CalcFfromHCR(ByRef Biomass As Single(), ByRef iYearProjecting As Integer) As Single
+
+        If Biomass(Me.GroupB.Index) > UpperLimit Then 'otherwise use the standard HCR
+            Return MaxF
+        ElseIf Biomass(Me.GroupB.Index) < LowerLimit Then
+            Return 0
+        Else
+            Return ((Biomass(Me.GroupB.Index) - LowerLimit) / (UpperLimit - LowerLimit)) * MaxF
+        End If
+
+    End Function
+
+#End Region
+
+#Region "Private Methods"
+
+    Private Function isIndexInBounds(group As cEcoPathGroupInput) As Boolean
+        If (group Is Nothing) Then Return False
+        Return group.IsFished
+    End Function
+
+#End Region
+
+End Class
---
+++ Sources/EwECustomPlugins/EwEMSEPlugin/User interface/frmBiomassLimits.vb
@@ -0,0 +1,234 @@
+﻿Option Strict On
+Imports EwECore
+Imports EwEUtils.Core
+Imports ScientificInterfaceShared.Controls
+Imports SharedResources = ScientificInterfaceShared.My.Resources
+
+Public Class frmBiomassLimits
+
+    Private m_mse As cMSE = Nothing
+    Private m_bIsDirty As Boolean
+    Private m_data As cBiomassLimits = Nothing
+
+    Public Sub New(MSE As cMSE)
+
+        MyBase.New()
+        Me.m_mse = MSE
+        Me.InitializeComponent()
+        Me.Grid = Me.m_grid
+
+    End Sub
+
+    Public Sub Init(ByVal uic As cUIContext)
+
+        'Me.m_data = New cBiomassLimits(Me.m_mse)
+        'Me.m_data.Load()
+
+        'Me.m_grid.Init(m_mse.BiomassLimits.lstBiomassLimits)
+        'Me.m_BiomassLimits = New cBiomassLimits(m_mse)
+        'Me.m_BiomassLimits.Load()
+        'Me.UpdateGrid(m_BiomassLimits.lstBiomassLimits, My.Resources.HEADER_SURVIVABILITIES)
+
+        Me.UIContext = uic
+        Me.m_grid.UIContext = uic
+        Me.m_grid.Init(Me.m_mse.BiomassLimits)
+        Me.m_data = New cBiomassLimits(Me.m_mse)
+        Me.m_data.Load()
+        Me.UpdateGrid(m_data, My.Resources.HEADER_BIOMASS_LIMITS)
+
+        'Me.m_grid.Init(Me.m_mse, )
+        'Me.m_quotashares = New cQuotaShares(Me.m_mse, uic.Core)
+        'Me.m_quotashares.Load()
+        'Me.UpdateGrid(Me.m_quotashares.GetLstGrpShares, My.Resources.HEADER_QUOTASHARE)
+
+    End Sub
+
+    Protected Overrides Sub OnLoad(e As System.EventArgs)
+
+        MyBase.OnLoad(e)
+
+        Me.QuickEditHandler.ShowImportExport = False
+        Me.QuickEditHandler.Attach(Me.m_grid, Me.UIContext, Me.m_ts)
+
+        AddHandler Me.m_grid.onEdited, AddressOf OnGridEdited
+
+        Me.m_bIsDirty = False
+        Me.UpdateControls()
+
+    End Sub
+
+    'Protected Overrides Sub OnFormClosing(e As System.Windows.Forms.FormClosingEventArgs)
+    '    Me.QuickEditHandler.Detach()
+    '    MyBase.OnFormClosed(e)
+
+    '    If (Me.m_bIsDirty = True) Then
+    '        ' JS 02Oct13: globalized this method
+    '        ' JS 02Oct13: replaced MsgBox with cFeedbackMessage
+    '        Dim fmsg As New cFeedbackMessage(My.Resources.PROMPT_UNSAVED_CHANGES,
+    '                             eCoreComponentType.External, eMessageType.Any, eMessageImportance.Question, eMessageReplyStyle.YES_NO)
+    '        fmsg.Reply = eMessageReply.YES
+    '        Me.Core.Messages.SendMessage(fmsg)
+    '        e.Cancel = (fmsg.Reply <> eMessageReply.YES)
+    '    End If
+
+    '    MyBase.OnFormClosing(e)
+
+    'End Sub
+
+    Protected Overrides Sub OnFormClosing(e As System.Windows.Forms.FormClosingEventArgs)
+
+        If (Me.m_bIsDirty = True) Then
+            Dim fmsg As New cFeedbackMessage(My.Resources.PROMPT_UNSAVED_CHANGES,
+                                 eCoreComponentType.External, eMessageType.Any, eMessageImportance.Question, eMessageReplyStyle.YES_NO)
+            fmsg.Reply = eMessageReply.YES
+            Me.Core.Messages.SendMessage(fmsg)
+            e.Cancel = (fmsg.Reply <> eMessageReply.YES)
+        End If
+
+        MyBase.OnFormClosing(e)
+
+    End Sub
+
+    Protected Overrides Sub OnFormClosed(e As System.Windows.Forms.FormClosedEventArgs)
+
+        Me.QuickEditHandler.Detach()
+        RemoveHandler Me.m_grid.onEdited, AddressOf OnGridEdited
+        Me.m_grid.UIContext = Nothing
+
+        MyBase.OnFormClosed(e)
+
+    End Sub
+
+    Protected Overrides Sub UpdateControls()
+
+        MyBase.UpdateControls()
+        Me.m_btnSave.Enabled = Me.m_bIsDirty
+
+    End Sub
+
+    Private Sub UpdateGrid(data As cBiomassLimits, strName As String)
+
+        Me.m_grid.Data = data
+        Me.m_grid.DataName = String.Format(SharedResources.GENERIC_LABEL_DOUBLE, My.Resources.CAPTION, strName)
+
+    End Sub
+
+    'Private Sub OutputMessageNotSum1(ByVal Groups_Not_Summing_to_1 As List(Of Integer))
+
+    '    Dim strGroupsNotSum1 As String
+
+    '    strGroupsNotSum1 = m_msePluginPoint.MSE.Core.EcoPathGroupInputs(Groups_Not_Summing_to_1(0)).Name
+
+    '    If Groups_Not_Summing_to_1.Count > 1 Then
+    '        For i = 2 To Groups_Not_Summing_to_1.Count
+    '            If i = Groups_Not_Summing_to_1.Count Then
+    '                strGroupsNotSum1 &= " and " & m_msePluginPoint.MSE.Core.EcoPathGroupInputs(Groups_Not_Summing_to_1(i - 1)).Name
+    '            Else
+    '                strGroupsNotSum1 &= ", " & m_msePluginPoint.MSE.Core.EcoPathGroupInputs(Groups_Not_Summing_to_1(i - 1)).Name
+    '            End If
+    '        Next
+    '    End If
+
+    '    'flag an error
+    '    If Groups_Not_Summing_to_1.Count = 1 Then
+    '        MessageBox.Show("The quotashares for group " & strGroupsNotSum1 & " do not sum to 1")
+    '    Else
+    '        MessageBox.Show("The quotashares for groups " & strGroupsNotSum1 & " do not sum to 1")
+    '    End If
+
+    'End Sub
+
+    Private Sub m_btnSave_Click(sender As System.Object, e As System.EventArgs) Handles m_btnSave.Click
+
+        Dim lstrSubMessages As New List(Of String)
+        Dim strFolder As String = cMSEUtils.MSEFolder(m_mse.DataPath, cMSEUtils.eMSEPaths.BiomassLimits)
+        'Dim Groups_Not_Summing_to_1 As New List(Of Integer)
+
+        'If Not QuotaSharesSumTo1(Groups_Not_Summing_to_1) Then
+
+        '    OutputMessageNotSum1(Groups_Not_Summing_to_1)
+        '    Exit Sub
+
+        'End If
+
+        'Saves all the parameters to csv when user clicks to save
+        'If m_quotashares.Save() Then lstrSubMessages.Add(String.Format(My.Resources.STATUS_SAVED_DETAIL, "QuotaShares.csv"))
+        If m_data.Save() Then lstrSubMessages.Add(String.Format(My.Resources.STATUS_SAVED_DETAIL, "BiomassLimits.csv"))
+
+        Me.m_bIsDirty = False
+
+        m_mse.InformUser(String.Format(My.Resources.STATUS_SAVED_BIOMASSLIMITS, My.Resources.CAPTION, strFolder),
+                                 eMessageImportance.Information, strFolder, lstrSubMessages.ToArray())
+
+        Me.DialogResult = Windows.Forms.DialogResult.OK
+        Me.Close()
+    End Sub
+
+    'Private Function QuotaSharesSumTo1(ByRef GroupsNotSum1 As List(Of Integer)) As Boolean
+
+    '    Const Precision As Single = 0.0001
+    '    Dim QShares As List(Of cQuotaShares.QuotaShare) = m_quotashares.GetLstGrpShares
+    '    Dim SumShare(m_mse.Core.nGroups) As Single
+    '    Dim PassTest As Boolean = True
+
+    '    For Each iQuotaShare In QShares
+    '        SumShare(iQuotaShare.mGroupNo) += iQuotaShare.mShare
+    '    Next
+
+    '    For iGrp = 1 To m_mse.Core.nGroups
+    '        If SumShare(iGrp) <> 0 Then
+    '            If SumShare(iGrp) < 1 - Precision Or SumShare(iGrp) > 1 + Precision Then
+    '                GroupsNotSum1.Add(iGrp)
+    '                PassTest = False
+    '            End If
+    '        End If
+    '    Next
+
+    '    Return PassTest
+
+    'End Function
+
+    Private Sub OnGridEdited()
+
+        Me.m_bIsDirty = True
+        Me.Invoke(New MethodInvoker(AddressOf UpdateControls))
+    End Sub
+
+    Private Sub OnCancel(ByVal sender As System.Object, ByVal e As System.EventArgs) _
+    Handles m_btnCancel.Click
+
+        Me.DialogResult = Windows.Forms.DialogResult.Cancel
+        Me.Close()
+
+    End Sub
+
+    'Private Sub btnSum2One_Click(sender As Object, e As EventArgs) Handles btnSum2One.Click
+
+    '    Dim iStart As Integer = 0
+    '    Dim SumQuotaShare As Single = 0
+
+
+    '    SumQuotaShare = m_quotashares.ReadRowDist(0).mShare
+
+    '    For i = 2 To m_quotashares.CountDist
+    '        If m_quotashares.ReadRowDist(i - 1).mGroupNo <> m_quotashares.ReadRowDist(i - 2).mGroupNo Then
+    '            For iChange = iStart To i - 2
+    '                m_quotashares.ReadRowDist(iChange).mShare /= SumQuotaShare
+    '            Next
+    '            SumQuotaShare = 0
+    '            iStart = i - 1
+    '        End If
+    '        If m_quotashares.ReadRowDist(i - 1).mShare > 0 Then SumQuotaShare += m_quotashares.ReadRowDist(i - 1).mShare
+    '    Next
+
+    '    For iChange = iStart To m_quotashares.CountDist - 1
+    '        m_quotashares.ReadRowDist(iChange).mShare /= SumQuotaShare
+    '    Next
+
+    '    Me.m_grid.Init(Me.m_mse, m_quotashares)
+
+    '    Me.UpdateGrid(Me.m_quotashares.GetLstGrpShares, My.Resources.HEADER_SURVIVABILITIES)
+
+    'End Sub
+
+End Class
