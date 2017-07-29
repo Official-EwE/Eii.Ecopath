5a5952850dfec4d2c15da3ea95ce602ecc647bc7
JeroenS <jeroen.steenbeek@gmail.com>
2017-07-29 12:19:30 +0000

    6.6, please
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@14810 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterface/My Project/AssemblyInfo.vb
+++ Sources/ScientificInterface/My Project/AssemblyInfo.vb
@@ -35,10 +35,10 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyCopyright("Copyright © 1991")> 
 <Assembly: AssemblyDescription("Ecopath with Ecosim (EwE) is a free ecological/ecosystem modeling software suite. EwE has three main components: Ecopath - a static, mass-balanced snapshot of the system; Ecosim - a time dynamic simulation module for policy exploration; and Ecospace - a spatial and temporal dynamic module primarily designed for exploring impact and placement of protected areas.")> 
 <Assembly: CLSCompliant(True)> 
-<Assembly: ComVisible(False)> 
+<Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("113d96bb-5c02-464c-a936-0813ce272e03")> 
+<Assembly: Guid("113d96bb-5c02-464c-a936-0813ce272e03")>
 
 
 '
@@ -51,5 +51,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.5.14808.0")> 
-<Assembly: AssemblyFileVersion("6.5.14808.0")> 
+<Assembly: AssemblyVersion("6.6.14808.0")>
+<Assembly: AssemblyFileVersion("6.6.14808.0")>
