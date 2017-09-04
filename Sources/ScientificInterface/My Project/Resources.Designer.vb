ea471ec6ea3b2e7835dd82459736d43290daeb1f
JeroenS <jeroen.steenbeek@gmail.com>
2017-09-04 10:52:56 +0000

    Fixed some unit issues Updated remark panel - info section with units
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@14833 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterface/My Project/Resources.Designer.vb
+++ Sources/ScientificInterface/My Project/Resources.Designer.vb
@@ -22,7 +22,7 @@ Namespace My.Resources
     '''<summary>
     '''  A strongly-typed resource class, for looking up localized strings, etc.
     '''</summary>
-    <Global.System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0"),  _
+    <Global.System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0"),  _
      Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
      Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
      Global.Microsoft.VisualBasic.HideModuleNameAttribute()>  _
@@ -2112,7 +2112,7 @@ Namespace My.Resources
         End Property
         
         '''<summary>
-        '''  Looks up a localized string similar to Additional description: {0}.
+        '''  Looks up a localized string similar to Description: {0}.
         '''</summary>
         Public ReadOnly Property INFOPANEL_DESCRIPTION() As String
             Get
@@ -2121,7 +2121,7 @@ Namespace My.Resources
         End Property
         
         '''<summary>
-        '''  Looks up a localized string similar to Allowed values: {0}.
+        '''  Looks up a localized string similar to Domain: {0}.
         '''</summary>
         Public ReadOnly Property INFOPANEL_DOMAIN() As String
             Get
