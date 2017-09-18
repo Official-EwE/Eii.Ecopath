90b2b175661706447b5baf111d768f4f4ecf23a6
JeroenS <jeroen.steenbeek@gmail.com>
2017-09-18 08:03:15 +0000

    Good grief
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@14851 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECore/EcoSim/Model/cMediationDataStructures.vb
+++ Sources/EwECore/EcoSim/Model/cMediationDataStructures.vb
@@ -1,6 +1,4 @@
-﻿Imports EwEUtils.Core
-
-fMDP ' ===============================================================================
+﻿' ===============================================================================
 ' This file is part of Ecopath with Ecosim (EwE)
 '
 ' EwE is free software: you can redistribute it and/or modify it under the terms
