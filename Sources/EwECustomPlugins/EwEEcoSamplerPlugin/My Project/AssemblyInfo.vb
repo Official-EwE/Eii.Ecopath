3c3ba2390391a44dbafbe7eed1c243ae7eca1b93
automatedbuild <automatedbuild@ecopath.org>
2017-11-04 09:04:42 +0000

    Build 6.6.14965.0
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@14968 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECustomPlugins/EwEEcoSamplerPlugin/My Project/AssemblyInfo.vb
+++ Sources/EwECustomPlugins/EwEEcoSamplerPlugin/My Project/AssemblyInfo.vb
@@ -1,4 +1,4 @@
-﻿' ===============================================================================
+' ===============================================================================
 ' This file is part of Ecopath with Ecosim (EwE)
 '
 ' EwE is free software: you can redistribute it and/or modify it under the terms
@@ -51,5 +51,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("6.6.14965.0")> 
+<Assembly: AssemblyFileVersion("6.6.14965.0")> 
--- Sources/EwEUtils/My Project/AssemblyInfo.vb
+++ Sources/EwEUtils/My Project/AssemblyInfo.vb
@@ -53,5 +53,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.6.14962.0")>
-<Assembly: AssemblyFileVersion("6.6.14962.0")>
+<Assembly: AssemblyVersion("6.6.14965.0")>
+<Assembly: AssemblyFileVersion("6.6.14965.0")>
