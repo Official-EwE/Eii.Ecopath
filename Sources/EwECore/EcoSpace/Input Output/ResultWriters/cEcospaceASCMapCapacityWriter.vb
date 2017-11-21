27893e6944b25d1b9f1c8358721a94a18af4258c
JeroenS <jeroen.steenbeek@gmail.com>
2017-11-21 21:25:20 +0000

    Added computed capacity Ecospace output writer
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@15036 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECore/EcoSpace/Input Output/ResultWriters/cEcospaceASCMapCapacityWriter.vb
+++ Sources/EwECore/EcoSpace/Input Output/ResultWriters/cEcospaceASCMapCapacityWriter.vb
@@ -27,16 +27,16 @@ Imports EwEUtils.Core
 
 ''' ---------------------------------------------------------------------------
 ''' <summary>
-''' Implementation of <see cref="IEcospaceResultsWriter"/> to write Ecospace biomass
-''' distributions maps to ESRI ASCII files. 
+''' Implementation of <see cref="IEcospaceResultsWriter"/> to write Ecospace 
+''' computed habitat capacity maps to ESRI ASCII files. 
 ''' </summary>
 ''' ---------------------------------------------------------------------------
-Public Class cEcospaceASCMapBiomassWriter
+Public Class cEcospaceASCMapCapacityWriter
     Inherits cEcospaceASCBaseResultsWriter
 
     Public Sub New()
         MyBase.New()
-        Me.vars = New eVarNameFlags() {eVarNameFlags.EcospaceMapBiomass}
+        Me.vars = New eVarNameFlags() {eVarNameFlags.LayerHabitatCapacity}
     End Sub
 
     Public Overrides Sub Init(theCore As Object)
@@ -46,7 +46,7 @@ Public Class cEcospaceASCMapBiomassWriter
 
     Public Overrides ReadOnly Property DisplayName As String
         Get
-            Return My.Resources.CoreDefaults.ECOSPACE_WRITER_ASC_BIOMASS
+            Return My.Resources.CoreDefaults.ECOSAPCE_WRITER_ASC_COMPUTEDCAPACITY
         End Get
     End Property
 
