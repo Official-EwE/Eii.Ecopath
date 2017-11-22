054e1cfb90fc4e0af76206a3c2d77092ef4db196
JeroenS <jeroen.steenbeek@gmail.com>
2017-11-22 22:19:02 +0000

    Discontinued unused advection parameters
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@15041 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECore/EcoSpace/Model/cEcoSpaceDataStructures.vb
+++ Sources/EwECore/EcoSpace/Model/cEcoSpaceDataStructures.vb
@@ -156,10 +156,6 @@ Public Class cEcospaceDataStructures
     Public HabAreaProportion() As Single
 
     Public AdvectSpeed As Single
-    Public Coriolis As Single = 0.5!
-    Public SorWv As Single = 0.5!
-    Public XVelocity As Single = 1.0!
-    Public YVelocity As Single = 1.0!
     Public jord(1000) As Integer
 
     Public MoveScale As Single
