733b1adb2d94e668cc694197104d3d5b237053e8
automatedbuild <automatedbuild@ecopath.org>
2018-06-02 09:02:40 +0000

    Build 6.6.15353.0
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@15354 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECustomPlugins/EwEFlowDiagramPlugin/My Project/AssemblyInfo.vb
+++ Sources/EwECustomPlugins/EwEFlowDiagramPlugin/My Project/AssemblyInfo.vb
@@ -35,5 +35,5 @@ Imports System.Runtime.InteropServices
 
 <Assembly: Guid("108750cd-9540-4c02-bbea-2851611b3f45")>
 
-<Assembly: AssemblyVersion("6.6.15344.0")>
-<Assembly: AssemblyFileVersion("6.6.15344.0")>
+<Assembly: AssemblyVersion("6.6.15353.0")>
+<Assembly: AssemblyFileVersion("6.6.15353.0")>
