39179684f0f8e97a500df1c244103326d853753d
JeroenS <jeroen.steenbeek@gmail.com>
2018-06-12 13:45:27 +0000

    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@15395 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECore/EcoSpace/Input Output/EcospaceBasemap/Base layers/cEcospaceLayerSingle.vb
+++ Sources/EwECore/EcoSpace/Input Output/EcospaceBasemap/Base layers/cEcospaceLayerSingle.vb
@@ -35,13 +35,13 @@ Public Class cEcospaceLayerSingle
 #Region " Private variables "
 
     ''' <summary>Layer max value.</summary>
-    Private m_sMaxValue As Single = 0.0!
+    Protected m_sMaxValue As Single = 0.0!
     ''' <summary>Layer min value.</summary>
-    Private m_sMinValue As Single = 0.0!
+    Protected m_sMinValue As Single = 0.0!
     ''' <summary>Layer mean value.</summary>
-    Private m_sMeanValue As Single = 0.0!
+    Protected m_sMeanValue As Single = 0.0!
     ''' <summary>Layer num of cells with a value.</summary>
-    Private m_iNumValueCells As Integer = 0
+    Protected m_iNumValueCells As Integer = 0
 
 #End Region ' Private variables
 
