f4a56c1599809cfbe95df192d1fe4222a5f25a29
JeroenS <jeroen.steenbeek@gmail.com>
2018-06-20 09:36:48 +0000

    Disabled splash screen while sorting out WinXP icon size issues
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@15417 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterface/My Project/Application.Designer.vb
+++ Sources/ScientificInterface/My Project/Application.Designer.vb
@@ -37,7 +37,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateSplashScreen()
-            Me.SplashScreen = Global.ScientificInterface.frmSplash
+            'Me.SplashScreen = Global.ScientificInterface.frmSplash
         End Sub
     End Class
 End Namespace
