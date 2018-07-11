b9826ef32f78bc186956c314bd8bb0108e0bfc22
JeroenS <jeroen.steenbeek@gmail.com>
2018-07-11 13:45:58 +0000

    Fixed issue #1583
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@15468 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterface/Ecopath/Tools/FlowDiagram/cEcopathFlowDiagramData.vb
+++ Sources/ScientificInterface/Ecopath/Tools/FlowDiagram/cEcopathFlowDiagramData.vb
@@ -49,6 +49,7 @@ Namespace Ecopath.Controls.FlowDiagram
         Private m_datatype As eFDNodeValueType = eFDNodeValueType.Biomass
 
         Private m_TTLX_all() As Single
+        Private m_totcatch() As Single
 
 #End Region ' Internals
 
@@ -183,6 +184,9 @@ Namespace Ecopath.Controls.FlowDiagram
         Public ReadOnly Property LinkValue(ByVal iPred As Integer, ByVal iPrey As Integer) As Single _
                Implements IFlowDiagramData.LinkValue
             Get
+                ' To prevent problems
+                If Me.m_bInvalid Then Return 0
+
                 Dim c As cCore = Me.UIContext.Core
                 If (iPred <= c.nGroups) And (iPrey <= c.nGroups) Then
                     Dim group As cEcoPathGroupInput = c.EcoPathGroupInputs(iPred)
@@ -191,7 +195,9 @@ Namespace Ecopath.Controls.FlowDiagram
                 iPred -= c.nGroups
                 If (iPrey <= c.nGroups) Then
                     Dim fleet As cEcopathFleetInput = c.EcopathFleetInputs(iPred)
-                    Return fleet.Landings(iPrey) + fleet.Discards(iPrey)
+                    Dim dTotCatch As Single = m_totcatch(iPred)
+                    If dTotCatch = 0 Then dTotCatch = 1
+                    Return (fleet.Landings(iPrey) + fleet.Discards(iPrey)) / dTotCatch
                 End If
                 Return 0
             End Get
@@ -294,6 +300,14 @@ Namespace Ecopath.Controls.FlowDiagram
             Dim val_all(c.nGroups + c.nFleets) As Single
             Dim PP_all(c.nGroups + c.nFleets) As Single
             ReDim Me.m_TTLX_all(c.nGroups + c.nFleets)
+            ReDim Me.m_totcatch(c.nFleets)
+
+            For i As Integer = 1 To c.nFleets
+                Dim fleet As cEcopathFleetInput = c.EcopathFleetInputs(i)
+                For j As Integer = 1 To c.nGroups
+                    Me.m_totcatch(i) += (fleet.Landings(j) + fleet.Discards(j))
+                Next
+            Next
 
             Me.m_sValueMax = 0
             Me.m_sValueMin = Single.MaxValue
