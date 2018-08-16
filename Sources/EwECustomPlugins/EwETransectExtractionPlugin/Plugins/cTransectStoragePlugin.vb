2345f7710562787a6264b9a4ab26a68b627ca11d
JeroenS <jeroen.steenbeek@gmail.com>
2018-08-16 10:31:28 +0000

    Made more robust
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@15522 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECustomPlugins/EwETransectExtractionPlugin/Plugins/cTransectStoragePlugin.vb
+++ Sources/EwECustomPlugins/EwETransectExtractionPlugin/Plugins/cTransectStoragePlugin.vb
@@ -57,11 +57,15 @@ Public Class cTransectStoragePlugin
         Me.m_data = cTransectDatastructures.Instance(Me.m_core)
     End Sub
 
+    Private m_iScenarioDBID As Integer = -1
+
     Public Sub LoadEcospaceScenario(dataSource As Object) Implements IEcospacePlugin.LoadEcospaceScenario
 
         Dim ds As IEcospaceDatasource = DirectCast(dataSource, IEcospaceDatasource)
         Dim scenario As cEcospaceScenario = Me.m_core.EcospaceScenarios(Me.m_core.ActiveEcospaceScenarioIndex)
-        Dim strDBFileNme As String = Me.TransectFileName(ds, scenario.DBID)
+
+        Me.m_iScenarioDBID = scenario.DBID
+        Dim strDBFileNme As String = Me.TransectFileName(ds, Me.m_iScenarioDBID)
 
         If Me.m_data.FromXML(strDBFileNme) Then
             Me.m_data.IsChanged = False
@@ -72,19 +76,19 @@ Public Class cTransectStoragePlugin
     Public Sub SaveEcospaceScenario(dataSource As Object) Implements IEcospacePlugin.SaveEcospaceScenario
 
         Dim ds As IEcospaceDatasource = DirectCast(dataSource, IEcospaceDatasource)
-        Dim scenario As cEcospaceScenario = Me.m_core.EcospaceScenarios(Me.m_core.ActiveEcospaceScenarioIndex)
-        Dim strDBFileNme As String = Me.TransectFileName(ds, scenario.DBID)
+        Dim strDBFileNme As String = Me.TransectFileName(ds, Me.m_iScenarioDBID)
 
-        If (Me.m_data.IsChanged()) Then
+        If (Me.m_data.IsChanged() And Me.m_iScenarioDBID > 0) Then
             If Me.m_data.ToXML(strDBFileNme) Then
-                Me.m_data.IsChanged = False
             End If
         End If
+        Me.m_data.IsChanged = False
 
     End Sub
 
     Public Sub CloseEcospaceScenario() Implements IEcospacePlugin.CloseEcospaceScenario
         Me.m_data.Clear()
+        Me.m_iScenarioDBID = -1
     End Sub
 
     Public Sub EcospaceScenarioAdded(dataSource As Object, scenarioID As Integer) _
