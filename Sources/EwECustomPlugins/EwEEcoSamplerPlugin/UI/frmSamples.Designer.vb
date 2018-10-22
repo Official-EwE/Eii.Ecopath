d850bc6c67366c0c652a94770fce431d741b1801
JeroenS <jeroen.steenbeek@gmail.com>
2018-10-22 19:48:11 +0000

    Why does this particular project keeps bringing back self-referencing?!
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@15663 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECustomPlugins/EwEEcoSamplerPlugin/UI/frmSamples.Designer.vb
+++ Sources/EwECustomPlugins/EwEEcoSamplerPlugin/UI/frmSamples.Designer.vb
@@ -48,7 +48,7 @@ Partial Class frmSamples
         Me.m_btnLoad = New System.Windows.Forms.Button()
         Me.m_lblAction = New System.Windows.Forms.Label()
         Me.m_tlpMain = New System.Windows.Forms.TableLayoutPanel()
-        Me.m_grid = New EwEEcosamplerPlugin.gridSamples()
+        Me.m_grid = New gridSamples()
         Me.m_tsMain = New ScientificInterfaceShared.Controls.cEwEToolstrip()
         Me.m_tsbnRecord = New System.Windows.Forms.ToolStripButton()
         Me.m_tsddImport = New System.Windows.Forms.ToolStripDropDownButton()
