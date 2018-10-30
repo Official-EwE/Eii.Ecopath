6edeb59713bb0a9ed78bbb7a76648a6df87c1c32
JeroenS <jeroen.steenbeek@gmail.com>
2018-10-30 21:02:47 +0000

    Added layer def import/export plug-in
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@15686 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/EwECustomPlugins/EwEImportExportLayersPlugin/My Project/AssemblyInfo.vb
@@ -0,0 +1,55 @@
+﻿' ===============================================================================
+' This file is part of the Safenet toolkit, contributed to Ecopath with Ecosim
+' as part of Safenet project.
+'
+' EwE is free software: you can redistribute it and/or modify it under the terms
+' of the GNU General Public License version 2 as published by the Free Software 
+' Foundation.
+'
+' EwE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+' without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+' PURPOSE. See the GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License along with EwE.
+' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
+'
+' Safenet Copyright 2017-, EwE copyright 1991- 
+'    Ecopath International Initiative, Barcelona, Spain
+' ===============================================================================
+'
+
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("EwEImportExportLayersPlugin")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Ecopath International Initiative")>
+<Assembly: AssemblyProduct("EwEImportExportLayersPlugin")>
+<Assembly: AssemblyCopyright("Copyright © 2017-")>
+<Assembly: AssemblyTrademark("SAFENET")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("adbc7f99-23c1-4a85-a659-803de8112bde")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
