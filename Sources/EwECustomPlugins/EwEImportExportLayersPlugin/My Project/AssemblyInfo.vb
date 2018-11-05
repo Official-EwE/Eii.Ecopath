cccc0f2ccc4bfd67b26667d6c84d0ae9537d89f7
automatedbuild <automatedbuild@ecopath.org>
2018-11-05 00:47:38 +0000

    Build 6.6.15704.0
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@15705 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECustomPlugins/EwEImportExportLayersPlugin/My Project/AssemblyInfo.vb
+++ Sources/EwECustomPlugins/EwEImportExportLayersPlugin/My Project/AssemblyInfo.vb
@@ -1,4 +1,4 @@
-﻿' ===============================================================================
+' ===============================================================================
 ' This file is part of the Safenet toolkit, contributed to Ecopath with Ecosim
 ' as part of Safenet project.
 '
@@ -51,5 +51,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.0.0")>
-<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: AssemblyVersion("6.6.15704.0")>
+<Assembly: AssemblyFileVersion("6.6.15704.0")>
--- Sources/EwECustomPlugins/EwEStepwiseFittingPlugin/My Project/AssemblyInfo.vb
+++ Sources/EwECustomPlugins/EwEStepwiseFittingPlugin/My Project/AssemblyInfo.vb
@@ -56,5 +56,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.6.15684.0")> 
-<Assembly: AssemblyFileVersion("6.6.15684.0")> 
+<Assembly: AssemblyVersion("6.6.15704.0")> 
+<Assembly: AssemblyFileVersion("6.6.15704.0")> 
