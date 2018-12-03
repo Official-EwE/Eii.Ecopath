18ae7069a98fa990d1fe42912b3f098f7fe1284a
JeroenS <jeroen.steenbeek@gmail.com>
2018-12-03 14:09:33 +0000

    Plop
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@15831 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources Internal/Plugins/EwESpatialAssetsPlugin/My Project/AssemblyInfo.vb
+++ Sources Internal/Plugins/EwESpatialAssetsPlugin/My Project/AssemblyInfo.vb
@@ -37,7 +37,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("a7940b36-a664-426d-ab70-60dc482e33cb")> 
+<Assembly: Guid("a7940b36-a664-426d-ab70-60dc482e33cb")>
 
 
 '
@@ -50,5 +50,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.3.6841.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("6.6.15813.0")>
+<Assembly: AssemblyFileVersion("6.6.15813.0")>
