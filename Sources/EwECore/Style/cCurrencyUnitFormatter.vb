57580cf1a2c0a001808f65975beafcf8e79259ba
JeroenS <jeroen.steenbeek@gmail.com>
2019-03-17 12:43:19 +0000

    Fixed MSBUild
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@16119 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECore/Style/cCurrencyUnitFormatter.vb
+++ Sources/EwECore/Style/cCurrencyUnitFormatter.vb
@@ -52,9 +52,8 @@ Namespace Style
             Return GetType(eUnitCurrencyType)
         End Function
 
-        Public Function ToString(ByVal value As Object, _
-                                      Optional ByVal descriptor As eDescriptorTypes = eDescriptorTypes.Name) As String _
-                                  Implements ITypeFormatter.ToString
+        Public Overloads Function ToString(ByVal value As Object, Optional ByVal descriptor As eDescriptorTypes = eDescriptorTypes.Name) As String _
+            Implements ITypeFormatter.ToString
 
             Dim unit As eUnitCurrencyType = DirectCast(value, eUnitCurrencyType)
 
