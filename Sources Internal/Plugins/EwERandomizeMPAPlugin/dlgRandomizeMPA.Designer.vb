178fd385580d0838ee64d07b0a87f11f4bc41748
JeroenS <jeroen.steenbeek@gmail.com>
2019-10-30 06:44:24 +0000

    Initial version
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@16418 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources Internal/Plugins/EwERandomizeMPAPlugin/dlgRandomizeMPA.Designer.vb
@@ -0,0 +1,110 @@
+﻿' ===============================================================================
+' This file is part of Ecopath with Ecosim (EwE)
+'
+' EwE is free software: you can redistribute it and/or modify it under the terms
+' of the GNU General Public License version 2 as published by the Free Software 
+' Foundation.
+'
+' EwE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+' without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+' PURPOSE. See the GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License along with EwE.
+' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
+'
+' Copyright 1991- 
+'    Ecopath International Initiative, Barcelona, Spain
+' ===============================================================================
+'
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class dlgRandomizeMPA
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(dlgRandomizeMPA))
+        Me.m_lblMPA = New System.Windows.Forms.Label()
+        Me.m_lblClose = New System.Windows.Forms.Label()
+        Me.m_cmbMPA = New System.Windows.Forms.ComboBox()
+        Me.m_nudPercentage = New System.Windows.Forms.NumericUpDown()
+        Me.m_btnCloseCells = New System.Windows.Forms.Button()
+        CType(Me.m_nudPercentage, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'm_lblMPA
+        '
+        resources.ApplyResources(Me.m_lblMPA, "m_lblMPA")
+        Me.m_lblMPA.Name = "m_lblMPA"
+        '
+        'm_lblClose
+        '
+        resources.ApplyResources(Me.m_lblClose, "m_lblClose")
+        Me.m_lblClose.Name = "m_lblClose"
+        '
+        'm_cmbMPA
+        '
+        resources.ApplyResources(Me.m_cmbMPA, "m_cmbMPA")
+        Me.m_cmbMPA.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.m_cmbMPA.FormattingEnabled = True
+        Me.m_cmbMPA.Name = "m_cmbMPA"
+        '
+        'm_nudPercentage
+        '
+        Me.m_nudPercentage.Increment = New Decimal(New Integer() {5, 0, 0, 0})
+        resources.ApplyResources(Me.m_nudPercentage, "m_nudPercentage")
+        Me.m_nudPercentage.Minimum = New Decimal(New Integer() {5, 0, 0, 0})
+        Me.m_nudPercentage.Name = "m_nudPercentage"
+        Me.m_nudPercentage.Value = New Decimal(New Integer() {5, 0, 0, 0})
+        '
+        'm_btnCloseCells
+        '
+        resources.ApplyResources(Me.m_btnCloseCells, "m_btnCloseCells")
+        Me.m_btnCloseCells.Name = "m_btnCloseCells"
+        Me.m_btnCloseCells.UseVisualStyleBackColor = True
+        '
+        'dlgRandomizeMPA
+        '
+        resources.ApplyResources(Me, "$this")
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.m_btnCloseCells)
+        Me.Controls.Add(Me.m_nudPercentage)
+        Me.Controls.Add(Me.m_cmbMPA)
+        Me.Controls.Add(Me.m_lblClose)
+        Me.Controls.Add(Me.m_lblMPA)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "dlgRandomizeMPA"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        CType(Me.m_nudPercentage, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Private WithEvents m_lblMPA As Windows.Forms.Label
+    Private WithEvents m_cmbMPA As Windows.Forms.ComboBox
+    Private WithEvents m_nudPercentage As Windows.Forms.NumericUpDown
+    Private WithEvents m_btnCloseCells As Windows.Forms.Button
+    Private WithEvents m_lblClose As Windows.Forms.Label
+End Class
---
+++ Sources Internal/Plugins/EwERandomizeMPAPlugin/dlgRandomizeMPA.vb
@@ -0,0 +1,98 @@
+﻿' ===============================================================================
+' This file is part of Ecopath with Ecosim (EwE)
+'
+' EwE is free software: you can redistribute it and/or modify it under the terms
+' of the GNU General Public License version 2 as published by the Free Software 
+' Foundation.
+'
+' EwE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+' without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+' PURPOSE. See the GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License along with EwE.
+' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
+'
+' Copyright 1991- 
+'    Ecopath International Initiative, Barcelona, Spain
+' ===============================================================================
+'
+#Region " Imports "
+
+Option Strict On
+Imports System.Windows.Forms
+Imports EwECore
+Imports ScientificInterfaceShared.Controls
+Imports ScientificInterfaceShared.Style
+
+#End Region ' Imports
+
+Public Class dlgRandomizeMPA
+
+    Public Sub New()
+        Me.InitializeComponent()
+    End Sub
+
+    Public Property UIContext As cUIContext
+
+    Protected Overrides Sub OnLoad(e As EventArgs)
+        MyBase.OnLoad(e)
+
+        If (Me.UIContext Is Nothing) Then Return
+
+        Dim core As cCore = Me.UIContext.Core
+
+        For i As Integer = 1 To Me.UIContext.Core.nMPAs
+            Me.m_cmbMPA.Items.Add(core.EcospaceMPAs(i))
+        Next
+        Me.m_cmbMPA.SelectedIndex = 0
+
+        Me.CenterToScreen()
+
+    End Sub
+
+    Private Sub m_cmbMPA_Format(sender As Object, e As ListControlConvertEventArgs) Handles m_cmbMPA.Format
+        Dim fmt As New cCoreInterfaceFormatter()
+        e.Value = fmt.ToString(e.ListItem, EwEUtils.Utilities.eDescriptorTypes.Name)
+    End Sub
+
+    Private Sub OnCloseCells(sender As Object, e As EventArgs) Handles m_btnCloseCells.Click
+
+        Dim core As cCore = Me.UIContext.Core
+        Dim mpa As cEcospaceMPA = DirectCast(Me.m_cmbMPA.SelectedItem, cEcospaceMPA)
+        Dim bm As cEcospaceBasemap = core.EcospaceBasemap
+        Dim mapDepth As cEcospaceLayerDepth = bm.LayerDepth
+        Dim mapMPA As cEcospaceLayerMPA = bm.LayerMPA(mpa.Index)
+        Dim nR As Integer = bm.InRow
+        Dim nC As Integer = bm.InCol
+        Dim rnd As New Random()
+
+        core.SetBatchLock(cCore.eBatchLockType.Update)
+        Dim lCells As New List(Of Integer)
+        For iRow As Integer = 1 To nR
+            For iCol As Integer = 1 To nC
+                If mapDepth.IsWaterCell(iRow, iCol) Then
+                    Dim x As Integer = (iRow - 1) * nC + iCol
+                    If (lCells.Count = 0) Then
+                        lCells.Add(x)
+                    Else
+                        lCells.Insert(CInt((rnd.NextDouble * 13 * lCells.Count) Mod lCells.Count), x)
+                    End If
+                    mapMPA.Cell(iRow, iCol) = 0
+                End If
+            Next iCol
+        Next iRow
+
+        For x As Integer = 1 To CInt(Me.m_nudPercentage.Value * lCells.Count / 100)
+            Dim iRow As Integer = 1 + ((lCells(0) - 1) \ nC)
+            Dim iCol As Integer = 1 + ((lCells(0) - 1) Mod nR)
+            mapMPA.Cell(iRow, iCol) = 1
+            lCells.RemoveAt(0)
+        Next
+
+        mapMPA.Invalidate()
+        core.onChanged(mapMPA)
+        core.ReleaseBatchLock(cCore.eBatchChangeLevelFlags.Ecospace)
+
+    End Sub
+
+End Class
