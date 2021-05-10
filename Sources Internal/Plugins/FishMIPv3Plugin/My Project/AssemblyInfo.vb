194b698a56fe59b48ff6146a6d18e22f40cce9e2
JeroenS <jeroen.steenbeek@gmail.com>
2021-05-10 10:10:57 +0000

    Build 17355
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@17355 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources Internal/Plugins/FishMIPv3Plugin/My Project/AssemblyInfo.vb
+++ Sources Internal/Plugins/FishMIPv3Plugin/My Project/AssemblyInfo.vb
@@ -42,5 +42,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("6.7.0.17348")>
-<Assembly: AssemblyFileVersion("6.7.0.17348")>
+<Assembly: AssemblyVersion("6.7.0.17355")>
+<Assembly: AssemblyFileVersion("6.7.0.17355")>
