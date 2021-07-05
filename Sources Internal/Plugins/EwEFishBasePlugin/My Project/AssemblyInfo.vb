86b9841b5530713ad1245a2c1260dff8e8ab6f6d
JeroenS <jeroen.steenbeek@gmail.com>
2021-07-05 09:36:20 +0000

    v17410
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@17410 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources Internal/Plugins/EwEFishBasePlugin/My Project/AssemblyInfo.vb
+++ Sources Internal/Plugins/EwEFishBasePlugin/My Project/AssemblyInfo.vb
@@ -45,7 +45,7 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.7.0.17363")> 
-<Assembly: AssemblyFileVersion("6.7.0.17363")> 
+<Assembly: AssemblyVersion("6.7.0.17410")> 
+<Assembly: AssemblyFileVersion("6.7.0.17410")> 
 
 <Assembly: GuidAttribute("0a14e3a4-be0a-44fa-ac03-7fb4cd70a769")> 
--- Sources/EwECustomPlugins/EwETransectExtractionPlugin/My Project/AssemblyInfo.vb
+++ Sources/EwECustomPlugins/EwETransectExtractionPlugin/My Project/AssemblyInfo.vb
@@ -50,5 +50,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("6.7.0.17363")>
-<Assembly: AssemblyFileVersion("6.7.0.17363")>
+<Assembly: AssemblyVersion("6.7.0.17410")>
+<Assembly: AssemblyFileVersion("6.7.0.17410")>
