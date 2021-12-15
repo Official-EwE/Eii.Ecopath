70225e2a425d6cad69fe4b906ea016b165b0d178
JeroenS <jeroen.steenbeek@gmail.com>
2021-12-15 17:08:09 +0000

    EwE 6.7.0.17650
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@17653 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources Internal/Plugins/EwEKeyRunComparison/My Project/AssemblyInfo.vb
+++ Sources Internal/Plugins/EwEKeyRunComparison/My Project/AssemblyInfo.vb
@@ -50,5 +50,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.7.0.17649")> 
-<Assembly: AssemblyFileVersion("6.7.0.17649")> 
+<Assembly: AssemblyVersion("6.7.0.17651")> 
+<Assembly: AssemblyFileVersion("6.7.0.17651")> 
