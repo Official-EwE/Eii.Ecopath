c0bbc7cc980cfa30c5b3ea96fb6259a31240ec83
JeroenS <jeroen.steenbeek@gmail.com>
2023-01-29 23:04:57 +0000

    Ecosampler saving fix
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@18176 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources Internal/External tools/StewiseFittingStandalone/My Project/AssemblyInfo.vb
+++ Sources Internal/External tools/StewiseFittingStandalone/My Project/AssemblyInfo.vb
@@ -56,5 +56,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.7.0.18166")> 
-<Assembly: AssemblyFileVersion("6.7.0.18166")> 
+<Assembly: AssemblyVersion("6.7.0.18176")> 
+<Assembly: AssemblyFileVersion("6.7.0.18176")> 
