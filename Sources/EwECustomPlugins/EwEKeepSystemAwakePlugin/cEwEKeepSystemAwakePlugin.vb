fe57d191a4de7141042af139779e8fb350b251f1
JeroenS <jeroen.steenbeek@gmail.com>
2023-02-04 22:56:47 +0000

    v1.0
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@18215 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/EwECustomPlugins/EwEKeepSystemAwakePlugin/cEwEKeepSystemAwakePlugin.vb
@@ -0,0 +1,116 @@
+﻿' ===============================================================================
+' This file is part of Ecopath with Ecosim (EwE)
+'
+' EwE is free software: you can redistribute it and/or modify it under the terms
+' of the GNU General Public License version 2 as published by the Free Software 
+' Foundation.
+'
+' EwE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+' without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+' PURPOSE. See the GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License along with EwE.
+' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
+'
+' Copyright 1991- 
+'    Ecopath International Initiative, Barcelona, Spain
+' ===============================================================================
+'
+#Region " Imports "
+
+Option Strict On
+Imports EwECore
+Imports EwEPlugin
+Imports EwEUtils.Core
+Imports ScientificInterfaceShared.Controls
+
+#End Region ' Imports
+
+Public Class cEwEKeepSystemAwakePlugin
+    Implements IUIContextPlugin
+    Implements IEwEOptionsPlugin
+
+#Region " Private parts "
+
+    Private Const PLUGIN_NAME As String = "EwEKeepOSAwakeWhileRunningPlugin"
+
+    Private m_core As cCore = Nothing
+    Private m_uic As cUIContext = Nothing
+    Private m_mon As cCoreStateMonitorMonitor = Nothing
+
+#End Region ' Private parts
+
+#Region " Generic plup-in bits "
+
+    Public ReadOnly Property Name As String Implements IPlugin.Name
+        Get
+            Return PLUGIN_NAME
+        End Get
+    End Property
+
+    Public ReadOnly Property DisplayName As String Implements IPlugin.DisplayName
+        Get
+            Return My.Resources.Caption
+        End Get
+    End Property
+
+    Public ReadOnly Property Description As String Implements IPlugin.Description
+        Get
+            Return "Windows-only plug-in to prevent the OS from sleeping while EwE is busy"
+        End Get
+    End Property
+
+    Public ReadOnly Property Author As String Implements IPlugin.Author
+        Get
+            Return "Jeroen Steenbeek"
+        End Get
+    End Property
+
+    Public ReadOnly Property Contact As String Implements IPlugin.Contact
+        Get
+            Return "ewedevteam@gmail.com"
+        End Get
+    End Property
+
+#End Region ' Generic plup-in bits
+
+#Region " Core and UI integration "
+
+    Public Sub Initialize(core As Object) Implements IPlugin.Initialize
+        Me.m_core = DirectCast(core, cCore)
+        Me.m_mon = New cCoreStateMonitorMonitor(Me.m_core.StateMonitor)
+    End Sub
+
+    Public Sub UIContext(uic As Object) Implements IUIContextPlugin.UIContext
+        Me.m_uic = DirectCast(uic, cUIContext)
+    End Sub
+
+#End Region ' Core and UI integration
+
+#Region " Config plug-in bits "
+
+    Public ReadOnly Property Label As String Implements IEwEOptionsPlugin.Label
+        Get
+            Return My.Resources.Caption
+        End Get
+    End Property
+
+    Public Function IsConfigured() As Boolean Implements IConfigurable.IsConfigured
+        Return True
+    End Function
+
+    Public Function GetConfigUI() As Windows.Forms.Control Implements IConfigurable.GetConfigUI
+        Return New ucConfig(Me)
+    End Function
+
+#End Region ' Config plug-in bits
+
+#Region " UI callback "
+
+    Friend Sub Prod()
+        Me.m_mon.Prod()
+    End Sub
+
+#End Region ' UI callback
+
+End Class
---
+++ Sources/EwECustomPlugins/EwEKeepSystemAwakePlugin/cNativeMethods.vb
@@ -0,0 +1,49 @@
+﻿' ===============================================================================
+' This file is part of Ecopath with Ecosim (EwE)
+'
+' EwE is free software: you can redistribute it and/or modify it under the terms
+' of the GNU General Public License version 2 as published by the Free Software 
+' Foundation.
+'
+' EwE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+' without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+' PURPOSE. See the GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License along with EwE.
+' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
+'
+' Copyright 1991- 
+'    Ecopath International Initiative, Barcelona, Spain
+' ===============================================================================
+'
+
+
+Imports System.Runtime.InteropServices
+
+''' <summary>
+''' https://stackoverflow.com/questions/57850624/prevent-a-computer-from-entering-sleep-standby-hibernate-while-program-is-runnin
+''' </summary>
+Friend Class cNativeMethods
+
+    Public Shared Sub PreventSleep(bMonitor As Boolean)
+        Dim flags As eExecutionState = eExecutionState.ES_CONTINUOUS Or eExecutionState.ES_SYSTEM_REQUIRED
+        If bMonitor Then flags += eExecutionState.ES_DISPLAY_REQUIRED
+        SetThreadExecutionState(flags)
+    End Sub
+
+    Public Shared Sub AllowSleep()
+        SetThreadExecutionState(eExecutionState.ES_CONTINUOUS)
+    End Sub
+
+    <DllImport("Kernel32.DLL", CharSet:=CharSet.Auto, SetLastError:=True)>
+    Private Shared Function SetThreadExecutionState(ByVal state As eExecutionState) As eExecutionState
+    End Function
+
+    <FlagsAttribute()>
+    Public Enum eExecutionState As UInteger
+        ES_SYSTEM_REQUIRED = &H1
+        ES_DISPLAY_REQUIRED = &H2
+        ES_CONTINUOUS = &H80000000UI
+    End Enum
+
+End Class
