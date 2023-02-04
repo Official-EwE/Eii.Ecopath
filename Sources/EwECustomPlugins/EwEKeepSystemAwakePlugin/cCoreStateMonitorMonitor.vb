1be0b1d953ffd9442b3ea23a7a86ad2119917d72
JeroenS <jeroen.steenbeek@gmail.com>
2023-02-04 22:56:47 +0000

    v1.0
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@18215 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/EwECustomPlugins/EwEKeepSystemAwakePlugin/cCoreStateMonitorMonitor.vb
@@ -0,0 +1,93 @@
+﻿' ===============================================================================
+' This file is part of Ecopath with Ecosim (EwE)
+'
+' EwE is free software: you can redistribute it and/or modify it under the terms
+' of the GNU General Public License version 2 as published by the Free Software 
+' Foundation.
+'
+' EwE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+' without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+' PURPOSE. See the GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License along with EwE.
+' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
+'
+' Copyright 1991- 
+'    Ecopath International Initiative, Barcelona, Spain
+' ===============================================================================
+'
+#Region " Imports "
+
+Option Strict On
+Imports System.Timers
+Imports EwECore
+
+#End Region ' Imports
+
+''' <summary>
+''' The best class name ever.
+''' </summary>
+Public Class cCoreStateMonitorMonitor
+
+    Private WithEvents m_sm As cCoreStateMonitor = Nothing
+    Private WithEvents m_timer As Timer = Nothing
+
+    Private m_bKeepingAlive As Boolean = False
+
+    Public Sub New(sm As cCoreStateMonitor)
+        Me.m_timer = New Timer(10000)
+        Me.m_sm = sm
+    End Sub
+
+    Public Sub Prod()
+
+        SyncLock Me.m_timer
+
+            ' Re-evaluate if keep alive situation needs to change
+            Dim bIsRunning As Boolean = Me.m_sm.IsBusy And My.Settings.KeepMonitorOn
+
+            ' No changes? Ok, abort
+            If (bIsRunning = Me.m_bKeepingAlive) Then Return
+
+            If (Not Me.m_bKeepingAlive) Then
+                Me.m_timer.Start()
+                Me.m_bKeepingAlive = True
+            Else
+                Me.m_timer.Stop()
+                Me.m_bKeepingAlive = False
+            End If
+
+            ' As a precaution
+            Me.UpdateThreadState()
+
+        End SyncLock
+
+    End Sub
+
+#Region " Event handlers "
+
+    Private Sub OnCoreStateEvent(statemonitor As cCoreStateMonitor) Handles m_sm.CoreExecutionStateEvent
+        Me.Prod()
+    End Sub
+
+    Private Sub OnTimerElapsed(sender As Object, e As ElapsedEventArgs) Handles m_timer.Elapsed
+        Me.UpdateThreadState()
+        Me.Prod()
+    End Sub
+
+#End Region ' Event handlers
+
+#Region " The magic "
+
+    Private Sub UpdateThreadState()
+
+        If Me.m_bKeepingAlive Then
+            cNativeMethods.PreventSleep(My.Settings.KeepMonitorOn)
+        Else
+            cNativeMethods.AllowSleep()
+        End If
+    End Sub
+
+#End Region ' The magic
+
+End Class
---
+++ Sources/EwECustomPlugins/EwEKeepSystemAwakePlugin/ucConfig.vb
@@ -0,0 +1,81 @@
+﻿' ===============================================================================
+' This file is part of Ecopath with Ecosim (EwE)
+'
+' EwE is free software: you can redistribute it and/or modify it under the terms
+' of the GNU General Public License version 2 as published by the Free Software 
+' Foundation.
+'
+' EwE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+' without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+' PURPOSE. See the GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License along with EwE.
+' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
+'
+' Copyright 1991- 
+'    Ecopath International Initiative, Barcelona, Spain
+' ===============================================================================
+'
+Imports ScientificInterfaceShared.Controls
+
+Public Class ucConfig
+    Implements IOptionsPage
+
+#Region " Construction / destruction "
+
+    Private m_plugin As cEwEKeepSystemAwakePlugin = Nothing
+
+    Public Sub New(pi As cEwEKeepSystemAwakePlugin)
+        Me.InitializeComponent()
+        Me.m_hdr.Text = My.Resources.Caption
+        Me.m_plugin = pi
+        Me.UpdateContent()
+    End Sub
+
+#End Region ' Construction / destruction
+
+#Region " IOptionsPage implementation "
+
+    Public Property UIContext As cUIContext Implements IUIElement.UIContext
+
+    Public Event OnChanged As IOptionsPage.OnChangedEventHandler Implements IOptionsPage.OnChanged
+
+    Public Sub SetDefaults() Implements IOptionsPage.SetDefaults
+        My.Settings.KeepOSAwake = False
+        My.Settings.KeepMonitorOn = False
+        Me.Persist()
+        Me.UpdateContent()
+    End Sub
+
+    Public Function CanApply() As Boolean Implements IOptionsPage.CanApply
+        Return True
+    End Function
+
+    Public Function Apply() As IOptionsPage.eApplyResultType Implements IOptionsPage.Apply
+        My.Settings.KeepOSAwake = Me.m_cbKeepOSAwake.Checked
+        My.Settings.KeepMonitorOn = Me.m_cbKeepMonitorOn.Checked
+        Me.Persist()
+        Return True
+    End Function
+
+    Public Function CanSetDefaults() As Boolean Implements IOptionsPage.CanSetDefaults
+        Return True
+    End Function
+
+#End Region ' IOptionsPage implementation
+
+#Region " Internals "
+
+    Private Sub UpdateContent()
+        Me.m_cbKeepOSAwake.Checked = My.Settings.KeepOSAwake
+        Me.m_cbKeepMonitorOn.Checked = My.Settings.KeepMonitorOn
+    End Sub
+
+    Private Sub Persist()
+        My.Settings.Save()
+        Me.m_plugin.Prod()
+    End Sub
+
+#End Region ' Internals
+
+End Class
