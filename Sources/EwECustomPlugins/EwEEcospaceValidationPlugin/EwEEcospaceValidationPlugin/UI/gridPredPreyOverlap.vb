b685a1c26d437d95d30491ab686997e02e91538c
JeroenS <jeroen.steenbeek@gmail.com>
2023-03-06 21:49:35 +0000

    Added code comments for the real men Solved long-standing issue with quick editors
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@18263 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECustomPlugins/EwEEcospaceValidationPlugin/EwEEcospaceValidationPlugin/UI/gridPredPreyOverlap.vb
+++ Sources/EwECustomPlugins/EwEEcospaceValidationPlugin/EwEEcospaceValidationPlugin/UI/gridPredPreyOverlap.vb
@@ -34,8 +34,11 @@ Public Class gridPredPreyOverlap
     Private m_viz As cAvgHighlightVisualizer = Nothing
 
     Public Sub New()
+        ' Prevent flashing while updating
         Me.DoubleBuffered = True
     End Sub
+
+#Region " Grid overrides "
 
     Public Overrides ReadOnly Property SuppressQuickEdits As Boolean
         Get
@@ -43,32 +46,45 @@ Public Class gridPredPreyOverlap
         End Get
     End Property
 
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Dimension the grid and create header cells.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
     Protected Overrides Sub InitStyle()
 
         MyBase.InitStyle()
 
-        Me.m_viz = New cAvgHighlightVisualizer(Me.UIContext.StyleGuide)
+        ' Define visualizer that does the mean cell background colouring
+        If (Me.m_viz Is Nothing) Then
+            Me.m_viz = New cAvgHighlightVisualizer(Me.UIContext.StyleGuide)
+        End If
 
-        Dim source As cCoreGroupBase = Nothing
-
+        ' Resize grid
         Me.Redim(Me.Core.nGroups + 4, Me.Core.nLivingGroups + 2)
         Dim rowCnt As Integer = Me.RowsCount
+        Dim source As cCoreGroupBase = Nothing
 
+        ' Create row and column header cells
         Me(0, 0) = New cEwEColumnHeaderCell("")
         Me(0, 1) = New cEwEColumnHeaderCell(SharedResources.HEADER_PREYPREDATOR)
 
         For i As Integer = 1 To Me.Core.nGroups
             source = Me.Core.EcopathGroupInputs(i)
 
+            ' Group index row header
             Me(i, 0) = New cEwERowHeaderCell(CStr(i))
+            ' Group name row header
             Me(i, 1) = New cPropertyRowHeaderCell(Me.PropertyManager, source, eVarNameFlags.Name)
 
+            ' Group index column header
             If (i <= Me.Core.nLivingGroups) Then
                 Me(0, i + 1) = New cPropertyColumnHeaderCell(Me.PropertyManager, source, eVarNameFlags.Index)
             End If
 
         Next
 
+        ' Headers for bottom rows: min, mean and max
         Me(rowCnt - 3, 0) = New cEwERowHeaderCell()
         Me(rowCnt - 3, 1) = New cEwERowHeaderCell("Min")
 
@@ -82,75 +98,126 @@ Public Class gridPredPreyOverlap
 
     End Sub
 
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Populate the contents of the grid
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
     Protected Overrides Sub FillData()
 
-        Dim styleNull As eStyleFlags = eStyleFlags.OK Or eStyleFlags.NotEditable Or eStyleFlags.Null
-        Dim styleDiet As eStyleFlags = eStyleFlags.OK Or eStyleFlags.NotEditable
+        ' Build the content cells for the grid. This only creates the cells, but does not
+        ' enter real values. These come later in response to user time step selections.
 
+        ' Define visual styles to be used in the grid
+        ' - Inactive cells
+        Dim styleNull As eStyleFlags = eStyleFlags.OK Or eStyleFlags.NotEditable Or eStyleFlags.Null
+        ' - Diet cells
+        Dim styleDiet As eStyleFlags = eStyleFlags.OK Or eStyleFlags.NotEditable
+        ' - Computed cells
+        Dim styleComputed As eStyleFlags = eStyleFlags.OK Or eStyleFlags.NotEditable Or eStyleFlags.Sum
+
+        ' - Special case: diagonal cells are done differently
         Dim visDiagonal As New SourceGrid2.VisualModels.Common
         visDiagonal.BackColor = Color.LightGray
         visDiagonal.TextAlignment = ContentAlignment.MiddleCenter
 
-        For j As Integer = 1 To Me.Core.nLivingGroups
-            Dim pred As cEcoPathGroupInput = Me.Core.EcopathGroupInputs(j)
-            For i As Integer = 1 To Me.Core.nGroups
-                Dim style As eStyleFlags = If(pred.DietComp(i) > 0, styleDiet, styleNull)
+        ' For all predatos
+        For iPred As Integer = 1 To Me.Core.nLivingGroups
+            ' Get the underlying Ecopath group
+            Dim pred As cEcoPathGroupInput = Me.Core.EcopathGroupInputs(iPred)
+            ' For all prey
+            For iPrey As Integer = 1 To Me.Core.nGroups
+                ' Determine cell style
+                Dim style As eStyleFlags = If(pred.DietComp(iPrey) > 0, styleDiet, styleNull)
+                ' Define new cell
                 Dim cell As New cEwECell(0, GetType(Single), style)
+                ' - that suopresses zeroes (0 =blank cell)
                 cell.SuppressZero(0.0!) = True
-                If i = j Then cell.VisualModel = visDiagonal
-                Me(i, j + 1) = cell
-            Next i
+                ' - set diagonal 
+                If iPrey = iPred Then cell.VisualModel = visDiagonal
+                ' Store cell in the grid
+                Me(iPrey, iPred + 1) = cell
+            Next iPrey
+
+            ' Define cells for summary rows too (min, mean, max)
             For i As Integer = 1 To 3
-                Dim cell As New cEwECell(0, GetType(Single), styleDiet)
+                ' Create cell
+                Dim cell As New cEwECell(0, GetType(Single), styleComputed)
+                ' - mean cells use the funky colour scheme that VC used in spreadsheet
                 If i = 2 Then cell.VisualModel = Me.m_viz
-                Me(Me.Core.nGroups + i, j + 1) = cell
+                ' Store cell in the grid
+                Me(Me.Core.nGroups + i, iPred + 1) = cell
             Next i
-        Next j
+        Next iPred
 
     End Sub
 
+#End Region ' Grid overrides
+
+#Region " Public access "
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Callback from the parent form to update the grid with real data values.
+    ''' </summary>
+    ''' <param name="data">The calculated stats to show. Beware, data may be 
+    ''' nothing if not computed yet.</param>
+    ''' -----------------------------------------------------------------------
     Public Sub UpdateData(data As Double(,))
 
-        Dim avgMin As Single = If(data Is Nothing, 0, Single.MaxValue)
-        Dim avgMax As Single = 0
+        ' The min and max bounds for the averages colour scale
+        Dim minAvg As Single = If(data Is Nothing, 0, Single.MaxValue)
+        Dim maxAvg As Single = 0
 
-        For j As Integer = 1 To Me.Core.nLivingGroups
+        For iPred As Integer = 1 To Me.Core.nLivingGroups
             Dim min As Single = If(data Is Nothing, 0, Single.MaxValue)
             Dim tot As Single = 0
             Dim max As Single = 0
             Dim n As Integer = 0
-            Dim pred As cEcoPathGroupInput = Me.Core.EcopathGroupInputs(j)
+            Dim pred As cEcoPathGroupInput = Me.Core.EcopathGroupInputs(iPred)
 
+            ' Only worry about consumers
             If (pred.IsConsumer) Then
-                For i As Integer = 1 To Me.Core.nGroups
-                    If (pred.DietComp(i) > 0) Then
+                ' For all potential prey
+                For iPrey As Integer = 1 To Me.Core.nGroups
+                    ' Mjam?
+                    If (pred.DietComp(iPrey) > 0) Then
                         Dim val As Single = 0
+                        ' If there is data, update min, max and tot for this predator
                         If (data IsNot Nothing) Then
                             ' pred x prey
-                            val = CSng(data(j, i))
-                            n += 1
+                            val = CSng(data(iPred, iPrey))
                             min = Math.Min(min, val)
                             max = Math.Max(max, val)
                             tot += val
-                            Me(i, j + 1).Value = val
+                            Me(iPrey, iPred + 1).Value = val
+                            ' Count data point
+                            n += 1
                         End If
                     End If
                 Next
 
+                ' Calculate mean. Make not to div by 0
                 Dim mean As Single = tot / Math.Max(n, 1)
-                avgMin = Math.Min(avgMin, mean)
-                avgMax = Math.Max(avgMax, mean)
 
-                Me(Me.Core.nGroups + 1, j + 1).Value = If(n = 0, 0, min)
-                Me(Me.Core.nGroups + 2, j + 1).Value = If(n = 0, 0, mean)
-                Me(Me.Core.nGroups + 3, j + 1).Value = If(n = 0, 0, max)
+                ' Populate summary cells; keep at 0 if there were no data points
+                Me(Me.Core.nGroups + 1, iPred + 1).Value = If(n = 0, 0, min)
+                Me(Me.Core.nGroups + 2, iPred + 1).Value = If(n = 0, 0, mean)
+                Me(Me.Core.nGroups + 3, iPred + 1).Value = If(n = 0, 0, max)
+
+                ' Update min/max across all predators
+                minAvg = Math.Min(minAvg, mean)
+                maxAvg = Math.Max(maxAvg, mean)
+
             End If
         Next
 
-        Me.m_viz.Min = avgMin
-        Me.m_viz.Max = avgMax
+        ' Update min/max colour scale in the magic visualizer
+        Me.m_viz.Min = minAvg
+        Me.m_viz.Max = maxAvg
 
     End Sub
 
+#End Region ' Public access
 
 End Class
