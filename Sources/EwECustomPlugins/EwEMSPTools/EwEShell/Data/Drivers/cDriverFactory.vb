66c3de0b0259d4f829f07bc66a9aad17daca41f2
JeroenS <jeroen.steenbeek@gmail.com>
2023-03-28 20:36:29 +0000

    Added MSP tools
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@18308 a725721c-0ed6-864e-a6a8-02c2a1988c9d

---
+++ Sources/EwECustomPlugins/EwEMSPTools/EwEShell/Data/Drivers/cDriverFactory.vb
@@ -0,0 +1,113 @@
+﻿' ===============================================================================
+' This file is part of Ecopath with Ecosim (EwE)
+'
+' EwE is free software: you can redistribute it and/or modify it under the terms
+' of the GNU General Public License version 2 as published by the Free Software 
+' Foundation.
+'
+' EwE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+' without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+' PURPOSE. See the GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License along with EwE.
+' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
+'
+' Copyright 2016- 
+'    Ecopath International Initiative, Barcelona, Spain
+' ===============================================================================
+'
+#Region " Imports "
+
+Option Strict On
+Imports EwECore
+Imports EwEUtils.Core
+
+#End Region ' Imports
+
+''' -----------------------------------------------------------------------
+''' <summary>
+''' Factory to access available Ecospace drivers.
+''' </summary>
+''' -----------------------------------------------------------------------
+Friend Class cDriverFactory
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Obtain all drivers available in a given Ecospace scenario.
+    ''' </summary>
+    ''' <param name="core"></param>
+    ''' <returns></returns>
+    ''' -----------------------------------------------------------------------
+    Public Shared Function GetDrivers(core As cCore, game As cGame, Optional datatype As cPressure.eDataTypes = cPressure.eDataTypes.NotSet) As cDriver()
+        Dim lPressures As New List(Of cDriver)
+        For Each vn As eVarNameFlags In cDriverFactory.SupportedVariables
+            lPressures.AddRange(cDriverFactory.GetDrivers(core, game, vn, datatype))
+        Next
+        Return lPressures.ToArray()
+    End Function
+
+#Region " Internals "
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Obtain all drivers for a <see cref="SupportedVariables()">supported variable</see>.
+    ''' </summary>
+    ''' <param name="core"></param>
+    ''' <returns></returns>
+    ''' -----------------------------------------------------------------------
+    Private Shared Function GetDrivers(core As cCore, game As cGame, vn As eVarNameFlags, datatype As cPressure.eDataTypes) As cDriver()
+
+        Dim l As New List(Of cDriver)
+        Dim d As cDriver = Nothing
+
+        Debug.Assert(Array.IndexOf(SupportedVariables, vn) > -1)
+
+        Select Case vn
+
+            Case eVarNameFlags.LayerDriver
+                For i As Integer = 1 To core.nEnvironmentalDriverLayers
+                    d = New cEnvironmentalDriver(core, game, core.EcospaceBasemap.LayerDriver(i))
+                    l.Add(d)
+                Next
+
+            Case eVarNameFlags.LayerMPA
+                For i As Integer = 1 To core.nMPAs
+                    d = New cMPADriver(core, game, core.EcospaceMPAs(i))
+                    l.Add(d)
+                Next
+
+            Case eVarNameFlags.LayerHabitat
+                For i As Integer = 1 To core.nHabitats - 1
+                    d = New cHabitatDriver(core, game, core.EcospaceHabitats(i))
+                    l.Add(d)
+                Next
+
+            Case eVarNameFlags.EcosimFleetEffort
+                For i As Integer = 1 To core.nFleets
+                    d = New cEffortDriver(core, game, core.EcopathFleetInputs(i))
+                    l.Add(d)
+                Next
+
+            Case Else
+                Debug.Assert(False, "Internal error: variable not supported")
+
+        End Select
+
+        l.RemoveAll(Function(tmp) tmp.DataType <> datatype And datatype <> cPressure.eDataTypes.NotSet)
+        Return l.ToArray()
+
+    End Function
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Returns the list of variables that can be used to drive Ecospace through
+    ''' MSP pressures.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Private Shared Function SupportedVariables() As eVarNameFlags()
+        Return New eVarNameFlags() {eVarNameFlags.LayerDriver, eVarNameFlags.LayerMPA, eVarNameFlags.LayerHabitat, eVarNameFlags.EcosimFleetEffort}
+    End Function
+
+#End Region ' Internals
+
+End Class
---
+++ Sources/EwECustomPlugins/EwEMSPTools/EwEShell/Data/cShellData.vb
@@ -0,0 +1,161 @@
+﻿' ===============================================================================
+' This file is part of Ecopath with Ecosim (EwE)
+'
+' EwE is free software: you can redistribute it and/or modify it under the terms
+' of the GNU General Public License version 2 as published by the Free Software 
+' Foundation.
+'
+' EwE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+' without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+' PURPOSE. See the GNU General Public License for more details.
+'
+' You should have received a copy of the GNU General Public License along with EwE.
+' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
+'
+' Copyright 2016- 
+'    Ecopath International Initiative, Barcelona, Spain
+' ===============================================================================
+'
+#Region " Imports "
+
+Option Strict On
+Imports System.Xml
+Imports EwECore
+Imports EwEUtils.Utilities
+
+#End Region ' Imports
+
+''' ---------------------------------------------------------------------------
+''' <summary>
+''' Data container for a list of games.
+''' </summary>
+''' ---------------------------------------------------------------------------
+Public Class cShellData
+
+#Region " Private vars "
+
+    ''' <summary>The core to operate onto.</summary>
+    Private m_core As cCore = Nothing
+    ''' <summary>All available games.</summary>
+    Private m_games As New List(Of cGame)
+
+#End Region ' Private vars
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Constructor.
+    ''' </summary>
+    ''' <param name="core">The core to operate onto.</param>
+    ''' -----------------------------------------------------------------------
+    Public Sub New(core As cCore)
+        Me.m_core = core
+    End Sub
+
+#Region " Game access "
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Get all available games.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Public ReadOnly Property Games As cGame()
+        Get
+            Return Me.m_games.ToArray()
+        End Get
+    End Property
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Obtain a <see cref="cGame"/> by name.
+    ''' </summary>
+    ''' <param name="strGame">The name of the game. It is.</param>
+    ''' <returns>The game with a given name, or Nothing if the game could not be found.</returns>
+    ''' <remarks>To obtain the first game pass in an empty name</remarks>
+    ''' -----------------------------------------------------------------------
+    Public Function Game(strGame As String) As cGame
+        If (Games.Count = 0) Then Return Nothing
+        If (Not String.IsNullOrWhiteSpace(strGame)) Then
+            For Each g As cGame In Me.m_games
+                If (String.Compare(g.Name, strGame, True) = 0) Then Return g
+            Next
+        End If
+        Return Games(0)
+    End Function
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Add a <see cref="cGame">game</see>.
+    ''' </summary>
+    ''' <param name="g">The g.</param>
+    ''' -----------------------------------------------------------------------
+    Public Sub Add(g As cGame)
+        Me.m_games.Add(g)
+    End Sub
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Remove a <see cref="cGame">game</see>.
+    ''' </summary>
+    ''' <param name="g">The g.</param>
+    ''' -----------------------------------------------------------------------
+    Public Sub Remove(g As cGame)
+        Me.m_games.Remove(g)
+    End Sub
+
+#End Region ' Game access
+
+#Region " Serialization "
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Serialize the games list from XML.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Friend Function FromXML(str As String) As Boolean
+
+        Me.m_games.Clear()
+
+        If (Not String.IsNullOrWhiteSpace(str)) Then
+            Try
+                Dim doc As New XmlDocument()
+                doc.LoadXml(str)
+                For Each xnGame As XmlNode In doc.SelectNodes("//game")
+                    Dim game As New cGame(Me.m_core)
+                    game.FromXML(xnGame)
+                    Me.m_games.Add(game)
+                Next
+                Return True
+            Catch ex As Exception
+                ' Whoah!
+                Console.WriteLine("cShellData.FromXML: " & ex.Message)
+            End Try
+        End If
+        Return False
+
+    End Function
+
+    ''' -----------------------------------------------------------------------
+    ''' <summary>
+    ''' Serialize the games list to XML.
+    ''' </summary>
+    ''' -----------------------------------------------------------------------
+    Friend Function ToXML() As String
+
+        Dim xnRoot As XmlNode = Nothing
+        Dim doc As XmlDocument = cXMLUtils.NewDoc("settings", xnRoot)
+        Dim xnNode As XmlNode = Nothing
+        Dim xnGames As XmlNode = Nothing
+
+        xnGames = doc.CreateElement("games")
+        xnRoot.AppendChild(xnGames)
+
+        For Each g As cGame In Me.Games
+            xnGames.AppendChild(g.ToXML(doc))
+        Next
+        Return doc.OuterXml
+
+    End Function
+
+#End Region ' Serialization
+
+End Class
