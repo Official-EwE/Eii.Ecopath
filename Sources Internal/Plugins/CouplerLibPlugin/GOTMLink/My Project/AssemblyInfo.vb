aee5afbcd034fc599bc9e2380c414733242c5243
JeroenS <jeroen.steenbeek@gmail.com>
2023-10-03 21:40:15 +0000

    6.7.0.18520
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@18520 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources Internal/Plugins/CouplerLibPlugin/GOTMLink/My Project/AssemblyInfo.vb
+++ Sources Internal/Plugins/CouplerLibPlugin/GOTMLink/My Project/AssemblyInfo.vb
@@ -49,5 +49,5 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("6.7.0.18512")> 
-<Assembly: AssemblyFileVersion("6.7.0.18512")> 
+<Assembly: AssemblyVersion("6.7.0.18520")> 
+<Assembly: AssemblyFileVersion("6.7.0.18520")> 
