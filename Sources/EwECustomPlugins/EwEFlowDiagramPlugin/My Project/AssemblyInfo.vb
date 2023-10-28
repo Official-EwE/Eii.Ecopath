443c722c837a103602c9df36ee8b62a2526dd0d9
JeroenS <jeroen.steenbeek@gmail.com>
2023-10-28 13:37:12 +0000

    v6.7.0.18558
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@18558 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwECustomPlugins/EwEFlowDiagramPlugin/My Project/AssemblyInfo.vb
+++ Sources/EwECustomPlugins/EwEFlowDiagramPlugin/My Project/AssemblyInfo.vb
@@ -35,5 +35,5 @@ Imports System.Runtime.InteropServices
 
 <Assembly: Guid("108750cd-9540-4c02-bbea-2851611b3f45")>
 
-<Assembly: AssemblyVersion("6.7.0.18536")>
-<Assembly: AssemblyFileVersion("6.7.0.18536")>
+<Assembly: AssemblyVersion("6.7.0.18558")>
+<Assembly: AssemblyFileVersion("6.7.0.18558")>
