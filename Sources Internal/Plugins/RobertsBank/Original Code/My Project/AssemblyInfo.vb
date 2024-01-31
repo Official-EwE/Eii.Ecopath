ee3ad3229e4ae9b278c42cdaad66c7b021effc61
JeroenS <jeroen.steenbeek@gmail.com>
2024-01-31 14:54:55 +0000

    6.7.18662 - again?!
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@18672 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources Internal/Plugins/RobertsBank/Original Code/My Project/AssemblyInfo.vb
+++ Sources Internal/Plugins/RobertsBank/Original Code/My Project/AssemblyInfo.vb
@@ -48,5 +48,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.7.0.18652")> 
-<Assembly: AssemblyFileVersion("6.7.0.18652")> 
+<Assembly: AssemblyVersion("6.7.0.18662")> 
+<Assembly: AssemblyFileVersion("6.7.0.18662")> 
