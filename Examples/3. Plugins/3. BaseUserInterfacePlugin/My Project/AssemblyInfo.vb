7c123adf50d886d3ad9ea339b40e3bace7f1f301
JeroenS <jeroen.steenbeek@gmail.com>
2024-05-30 14:04:30 +0000

    6.7.18875 (EwE 40)
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@18875 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Examples/3. Plugins/3. BaseUserInterfacePlugin/My Project/AssemblyInfo.vb
+++ Examples/3. Plugins/3. BaseUserInterfacePlugin/My Project/AssemblyInfo.vb
@@ -50,5 +50,5 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("6.7.0.18865")> 
-<Assembly: AssemblyFileVersion("6.7.0.18865")> 
+<Assembly: AssemblyVersion("6.7.0.18875")> 
+<Assembly: AssemblyFileVersion("6.7.0.18875")> 
