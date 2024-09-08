d48635256d4dc21a016b2ac854e534e838aa98e1
JeroenS <jeroen.steenbeek@gmail.com>
2024-09-08 08:57:19 +0000

    Updated code comments
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@18912 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/EwEPlugin/Plugin templates/GUI/IGUIPlugin.vb
+++ Sources/EwEPlugin/Plugin templates/GUI/IGUIPlugin.vb
@@ -35,7 +35,8 @@ Public Interface IGUIPlugin
 
     ''' -----------------------------------------------------------------------
     ''' <summary>
-    ''' Get a WinForms image to show in the control for this plug-in.
+    ''' Get an image to show in the control for this plug-in. In EwE 6.*, the 
+    ''' returned image is expected to be a WinForms Image instance.
     ''' </summary>
     ''' -----------------------------------------------------------------------
     ReadOnly Property ControlImage() As Object
@@ -54,8 +55,9 @@ Public Interface IGUIPlugin
     ''' </summary>
     ''' <param name="sender">The control that was clicked or activated.</param>
     ''' <param name="e">Event parameters pertaining the control.</param>
-    ''' <param name="frmPlugin">A reference to the form that the plug-in creates
-    ''' or activates in response to this event.</param>
+    ''' <param name="frmPlugin">A reference to the control that the plug-in creates
+    ''' or activates in response to this event. In EwE 6.*, this is expected to be
+    ''' a WinForms Form instance.</param>
     ''' -----------------------------------------------------------------------
     Sub OnControlClick(sender As Object, e As System.EventArgs, ByRef frmPlugin As Object)
 
