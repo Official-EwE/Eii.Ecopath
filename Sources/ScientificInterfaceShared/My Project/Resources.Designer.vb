9fc91897e681b951e8b344192c3d93ad5551d097
JeroenS <jeroen.steenbeek@gmail.com>
2025-02-24 11:43:02 +0000

    Localizing new shape behaviour
    git-svn-id: https://sources.ecopath.org/svn/Ecopath/trunk/Ecopath6@19227 a725721c-0ed6-864e-a6a8-02c2a1988c9d

--- Sources/ScientificInterfaceShared/My Project/Resources.Designer.vb
+++ Sources/ScientificInterfaceShared/My Project/Resources.Designer.vb
@@ -5065,6 +5065,15 @@ Namespace My.Resources
         End Property
         
         '''<summary>
+        '''  Looks up a localized string similar to Function type.
+        '''</summary>
+        Public ReadOnly Property HEADER_FUNCTIONTYPE() As String
+            Get
+                Return ResourceManager.GetString("HEADER_FUNCTIONTYPE", resourceCulture)
+            End Get
+        End Property
+        
+        '''<summary>
         '''  Looks up a localized string similar to g.
         '''</summary>
         Public ReadOnly Property HEADER_G() As String
