597098b489502c144663c6ba33569c35d44ddcf7
Jeroen Steenbeek <jeroen.steenbeek@gmail.com>
2026-01-16 00:29:23 +0100

    GPL-3

--- Sources/EwECustomPlugins/EwEMSPChallengeIntegration/EwEMSPPlugin/Test sets/cTestset.vb
+++ Sources/EwECustomPlugins/EwEMSPChallengeIntegration/EwEMSPPlugin/Test sets/cTestset.vb
@@ -3,7 +3,7 @@
 ' This file is part of Ecopath with Ecosim (EwE)
 '
 ' EwE is free software: you can redistribute it and/or modify it under the terms
-' of the GNU General Public License version 2 as published by the Free Software 
+' of the GNU General Public License version 3 as published by the Free Software 
 ' Foundation.
 '
 ' EwE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
@@ -11,7 +11,7 @@
 ' PURPOSE. See the GNU General Public License for more details.
 '
 ' You should have received a copy of the GNU General Public License along with EwE.
-' If not, see <http://www.gnu.org/licenses/gpl-2.0.html>. 
+' If not, see https://www.gnu.org/licenses/gpl-3.0.html>. 
 '
 ' Copyright 2016- 
 '    Ecopath International Initiative, Barcelona, Spain
--- Sources/ScientificInterfaceShared/Style/Type formatters/cTypeFormatterFactory.vb
+++ Sources/ScientificInterfaceShared/Style/Type formatters/cTypeFormatterFactory.vb
@@ -2,7 +2,7 @@
 ' This file is part of Ecopath with Ecosim (EwE)
 '
 ' EwE is free software: you can redistribute it and/or modify it under the terms
-' of the GNU General Public License version 2 as published by the Free Software 
+' of the GNU General Public License version 3 as published by the Free Software 
 ' Foundation.
 '
 ' EwE is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
