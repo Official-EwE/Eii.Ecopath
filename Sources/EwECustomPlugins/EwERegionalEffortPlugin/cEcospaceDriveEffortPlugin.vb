0fd8fc7414df4215635a44eac8d3fade8da3710b
Jeroen Steenbeek <jeroen.steenbeek@gmail.com>
2026-02-24 16:43:35 +0100

    Headers!

--- Sources/EwECustomPlugins/EwERegionalEffortPlugin/cEcospaceDriveEffortPlugin.vb
+++ Sources/EwECustomPlugins/EwERegionalEffortPlugin/cEcospaceDriveEffortPlugin.vb
@@ -1,13 +1,7 @@
-﻿' ===============================================================================
-' This file is part of the EcoOcean toolkit.
-'
-' To use EcoOceanUtils please contact the EcoOcean core team at
-' ecopathinternational@gmail.com
-'
-' Copyright 2017- 
-'    Ecopath International Initiative, Barcelona, Spain
-' ===============================================================================
-'
+﻿' SPDX-License-Identifier: EUPL-1.2
+' This file is part of Ecopath with Ecosim (EwE).
+' Copyright © 1991– Ecopath International Initiative (EII)
+
 Option Strict On
 Imports System.IO
 Imports EwECore
