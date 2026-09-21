45c57d407fb2c00a79235e596c6c14ab99843412
Rikkert01 <rik.kreeftenberg@spinsoft.nl>
2026-09-21 12:59:48 +0200

    remove hardcoded versions

--- Sources/EwECore/My Project/AssemblyInfo.vb
+++ Sources/EwECore/My Project/AssemblyInfo.vb
@@ -31,6 +31,3 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
-
-<Assembly: AssemblyVersion("6.7.0.19540")>
-<Assembly: AssemblyFileVersion("6.7.0.19540")>
