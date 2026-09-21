222e78012e3977aecc6fbd4e61fbd9b926b6504a
Rikkert01 <rik.kreeftenberg@spinsoft.nl>
2026-09-21 12:07:48 +0200

    removed version references

--- Sources/EwECore/My Project/AssemblyInfo.vb
+++ Sources/EwECore/My Project/AssemblyInfo.vb
@@ -32,5 +32,3 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("6.7.0.19540")>
-<Assembly: AssemblyFileVersion("6.7.0.19540")>
